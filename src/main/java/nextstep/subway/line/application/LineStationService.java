f5f307953f399df9a3d367595b9249380c433b35
devson <sonyc5720@gmail.com>
2020-07-05 14:57:59 +0900

    refactor(line): LineStationController에 있는 StationRepository를 LineStationService로 옮김

--- src/main/java/nextstep/subway/line/application/LineStationService.java
+++ src/main/java/nextstep/subway/line/application/LineStationService.java
@@ -4,25 +4,50 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.domain.LineStation;
 import nextstep.subway.line.dto.LineStationRequest;
+import nextstep.subway.line.dto.LineStationResponse;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Objects;
 
 @Service
 @Transactional
 public class LineStationService {
     private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
 
-    public LineStationService(final LineRepository lineRepository) {
+    public LineStationService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
     }
 
-    public LineStation addStation(Long lineId, LineStationRequest lineStationRequest) {
+    public LineStationResponse addStation(Long lineId, LineStationRequest lineStationRequest) {
+        checkAreStationsExist(lineStationRequest);
+
         Line line = this.lineRepository.findById(lineId)
                 .orElseThrow(EntityNotFoundException::new);
-        final LineStation station = lineStationRequest.toLineStation();
-        line.addStation(station);
-        return station;
+        LineStation lineStation = lineStationRequest.toLineStation();
+        line.addStation(lineStation);
+
+        final Station station = this.stationRepository.findById(lineStation.getStationId())
+                .orElseThrow(EntityNotFoundException::new);
+        return LineStationResponse.of(lineStation, station);
+    }
+
+    private void checkAreStationsExist(LineStationRequest lineStationRequest) {
+        if (!this.stationRepository.existsById(lineStationRequest.getStationId())) {
+            throw new EntityNotFoundException();
+        }
+
+        if (Objects.isNull(lineStationRequest.getPreStationId())) {
+            return;
+        }
+
+        if (!this.stationRepository.existsById(lineStationRequest.getPreStationId())) {
+            throw new EntityNotFoundException();
+        }
     }
 }
--- src/main/java/nextstep/subway/line/ui/LineStationController.java
+++ src/main/java/nextstep/subway/line/ui/LineStationController.java
@@ -1,45 +1,26 @@
 package nextstep.subway.line.ui;
 
 import nextstep.subway.line.application.LineStationService;
-import nextstep.subway.line.domain.LineStation;
 import nextstep.subway.line.dto.LineStationRequest;
 import nextstep.subway.line.dto.LineStationResponse;
-import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.domain.StationRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityNotFoundException;
 import java.net.URI;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/lines/{id}/stations")
 public class LineStationController {
-    private final StationRepository stationRepository;
     private final LineStationService lineStationService;
 
-    public LineStationController(final StationRepository stationRepository, final LineStationService lineStationService) {
-        this.stationRepository = stationRepository;
+    public LineStationController(LineStationService lineStationService) {
         this.lineStationService = lineStationService;
     }
 
     @PostMapping
     public ResponseEntity<LineStationResponse> addStation(@PathVariable Long id,
                                                           @RequestBody LineStationRequest lineStationRequest) {
-        if (!this.stationRepository.existsById(lineStationRequest.getStationId())) {
-            throw new EntityNotFoundException();
-        }
-
-        if (Objects.nonNull(lineStationRequest.getPreStationId())) {
-            if (!this.stationRepository.existsById(lineStationRequest.getPreStationId())) {
-                throw new EntityNotFoundException();
-            }
-        }
-
-        LineStation lineStation = this.lineStationService.addStation(id, lineStationRequest);
-        final Station station = this.stationRepository.findById(lineStation.getStationId()).orElseThrow(EntityNotFoundException::new);
-
-        return ResponseEntity.created(URI.create("/lines/" + id + "/stations")).body(LineStationResponse.of(lineStation, station));
+        LineStationResponse response = this.lineStationService.addStation(id, lineStationRequest);
+        return ResponseEntity.created(URI.create("/lines/" + id + "/stations")).body(response);
     }
 }
