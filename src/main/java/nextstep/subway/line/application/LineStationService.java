42842572ea5c286e5ec1b0be5ce2536c41890da0
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-06 12:03:59 +0900

    feat: adding LineStation to Line business logic - LineStationController to have a path access lineStation creation - LineStationService to work for registering LineStation after extracting information from Dto - LineResponseDto to have lombok getter annotation processing

---
+++ src/main/java/nextstep/subway/line/application/LineStationService.java
@@ -0,0 +1,42 @@
+package nextstep.subway.line.application;
+
+import org.springframework.stereotype.Service;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.LineStation;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
+import nextstep.subway.station.dto.LineStationCreateRequestDto;
+
+@Service
+public class LineStationService {
+	private final LineRepository lineRepository;
+	private final StationRepository stationRepository;
+
+	public LineStationService(LineRepository lineRepository,
+		StationRepository stationRepository) {
+		this.lineRepository = lineRepository;
+		this.stationRepository = stationRepository;
+	}
+
+	private Line findLineById(Long lineId) {
+		return lineRepository.findById(lineId).orElseThrow(() -> new IllegalArgumentException("no line found."));
+	}
+
+	private Station findStationById(Long stationId) {
+		return stationRepository.findById(stationId).orElseThrow(() -> new IllegalArgumentException("no station found."));
+	}
+
+	public void registerLineStation(LineStationCreateRequestDto dto) {
+		Line line = findLineById(dto.getLineId());
+		Station station = findStationById(dto.getStationId());
+		LineStation lineStation = LineStation.builder()
+			.line(line)
+			.station(station)
+			.distance(dto.getDistance())
+			.duration(dto.getDuration())
+			.build();
+		line.addLineStation(lineStation);
+	}
+}
--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,12 +1,14 @@
 package nextstep.subway.line.dto;
 
 import com.google.common.collect.Lists;
+import lombok.Getter;
 import nextstep.subway.line.domain.Line;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
+@Getter
 public class LineResponse {
     private Long id;
     private String name;
@@ -39,41 +41,5 @@ public class LineResponse {
 
     public static LineResponse of(Line line) {
         return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getStartTime(), line.getEndTime(), line.getIntervalTime(), Lists.newArrayList(), line.getCreatedDate(), line.getModifiedDate());
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
-
-    public int getIntervalTime() {
-        return intervalTime;
-    }
-
-    public List<LineStationResponse> getStations() {
-        return stations;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public LocalDateTime getModifiedDate() {
-        return modifiedDate;
     }
 }
