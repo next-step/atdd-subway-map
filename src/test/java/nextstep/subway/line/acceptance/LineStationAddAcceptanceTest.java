3ebb1eeebcbc75236eaf82fef958d6b1bfa64809
koamania <koamania2@gmail.com>
2020-07-07 02:15:38 +0900

    test(line-station): 중복 역 등록 및 잘못된 역 등록하는 인수테스트 추가

--- src/test/java/nextstep/subway/line/acceptance/LineStationAddAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineStationAddAcceptanceTest.java
@@ -22,18 +22,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("지하철 노선에 역 등록 관련 기능")
 public class LineStationAddAcceptanceTest extends AcceptanceTest {
 
-    private ExtractableResponse<Response> createdLineResponse;
-    private ExtractableResponse<Response> createdStationResponse1;
-    private ExtractableResponse<Response> createdStationResponse2;
-    private ExtractableResponse<Response> createdStationResponse3;
+    private ExtractableResponse<Response> 지하철2호선;
+    private ExtractableResponse<Response> 강남역;
+    private ExtractableResponse<Response> 역삼역;
+    private ExtractableResponse<Response> 선릉역;
 
     @BeforeEach
     public void background() {
         // given
-        this.createdLineResponse = 지하철_노선_등록되어_있음("2호선", "GREEN");
-        this.createdStationResponse1 = 지하철역_등록되어_있음("강남역");
-        this.createdStationResponse2 = 지하철역_등록되어_있음("역삼역");
-        this.createdStationResponse3 = 지하철역_등록되어_있음("선릉역");
+        this.지하철2호선 = 지하철_노선_등록되어_있음("2호선", "GREEN");
+        this.강남역 = 지하철역_등록되어_있음("강남역");
+        this.역삼역 = 지하철역_등록되어_있음("역삼역");
+        this.선릉역 = 지하철역_등록되어_있음("선릉역");
     }
 
     @DisplayName("지하철 노선에 역을 등록한다.")
@@ -41,12 +41,38 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
     void addLineStation() {
         // when
         // 지하철_노선에_지하철역_등록_요청
-        Long lineId = createdLineResponse.as(LineResponse.class).getId();
-        Long stationId = createdStationResponse1.as(StationResponse.class).getId();
+        Long lineId = 지하철2호선.as(LineResponse.class).getId();
+        Long stationId = 강남역.as(StationResponse.class).getId();
         ExtractableResponse<Response> response = 지하철_노선에_지하철역을_등록한다(lineId, stationId, null);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @DisplayName("이미 등록되어 있던 역을 등록한다.")
+    @Test
+    void addDuplicateLineStation() {
+        // when
+        Long lineId = 지하철2호선.as(LineResponse.class).getId();
+        StationResponse stationResponse = 강남역.as(StationResponse.class);
+        지하철_노선에_지하철역을_등록한다(lineId, stationResponse.getId(), null);
+        ExtractableResponse<Response> duplicateLineStationResponse = 지하철_노선에_지하철역을_등록한다(lineId, stationResponse.getId(), null);
+
+        // then
+        assertThat(duplicateLineStationResponse.statusCode()).isEqualTo(HttpStatus.CONFLICT.value());
+    }
+
+
+    @DisplayName("존재하지 않는 역을 노선에 등록한다.")
+    @Test
+    void addInvalidLineStation() {
+        // when
+        Long lineId = 지하철2호선.as(LineResponse.class).getId();
+        long invalidStationId = 11111L;
+        ExtractableResponse<Response> invalidStationResponse = 지하철_노선에_지하철역을_등록한다(lineId, invalidStationId, null);
+
+        // then
+        assertThat(invalidStationResponse.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
     @DisplayName("지하철 노선 상세정보 조회 시 역 정보가 포함된다.")
@@ -54,8 +80,8 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
     void getLineWithStations() {
         // given
         // 지하철_노선에_지하철역_등록_요청
-        Long lineId = createdLineResponse.as(LineResponse.class).getId();
-        Long stationId = createdStationResponse1.as(StationResponse.class).getId();
+        Long lineId = 지하철2호선.as(LineResponse.class).getId();
+        Long stationId = 강남역.as(StationResponse.class).getId();
         지하철_노선에_지하철역을_등록한다(lineId, stationId, null);
 
         // when
@@ -74,16 +100,16 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
     void addLineStationInOrder() {
         // when
         // 지하철_노선에_지하철역_등록_요청
-        Long lineId = createdLineResponse.as(LineResponse.class).getId();
-        Long stationId1 = createdStationResponse1.as(StationResponse.class).getId();
+        Long lineId = 지하철2호선.as(LineResponse.class).getId();
+        Long stationId1 = 강남역.as(StationResponse.class).getId();
         ExtractableResponse<Response> lineStationResponse = 지하철_노선에_지하철역을_등록한다(lineId, stationId1, null);
 
         // 지하철_노선에_지하철역_등록_요청
-        Long stationId2 = createdStationResponse2.as(StationResponse.class).getId();
+        Long stationId2 = 역삼역.as(StationResponse.class).getId();
         지하철_노선에_지하철역을_등록한다(lineId, stationId2, stationId1);
 
         // 지하철_노선에_지하철역_등록_요청
-        Long stationId3 = createdStationResponse3.as(StationResponse.class).getId();
+        Long stationId3 = 선릉역.as(StationResponse.class).getId();
         지하철_노선에_지하철역을_등록한다(lineId, stationId3, stationId2);
 
         // then
@@ -107,16 +133,16 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
     void addLineStationInAnyOrder() {
         // when
         // 지하철_노선에_지하철역_등록_요청
-        Long lineId = createdLineResponse.as(LineResponse.class).getId();
-        Long stationId1 = createdStationResponse1.as(StationResponse.class).getId();
+        Long lineId = 지하철2호선.as(LineResponse.class).getId();
+        Long stationId1 = 강남역.as(StationResponse.class).getId();
         ExtractableResponse<Response> lineStationResponse = 지하철_노선에_지하철역을_등록한다(lineId, stationId1, null);
 
         // 지하철_노선에_지하철역_등록_요청
-        Long stationId2 = createdStationResponse2.as(StationResponse.class).getId();
+        Long stationId2 = 역삼역.as(StationResponse.class).getId();
         지하철_노선에_지하철역을_등록한다(lineId, stationId2, stationId1);
 
         // 지하철_노선에_지하철역_등록_요청
-        Long stationId3 = createdStationResponse3.as(StationResponse.class).getId();
+        Long stationId3 = 선릉역.as(StationResponse.class).getId();
         지하철_노선에_지하철역을_등록한다(lineId, stationId3, stationId1);
 
         // then
