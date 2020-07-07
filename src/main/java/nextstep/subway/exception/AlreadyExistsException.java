1e43df2dfb2f066d386687467d7612a30b58df1d
junwoochoi <cjw4690@gmail.com>
2020-07-07 09:24:11 +0900

    feat : add @ResponseStatus on CustomExceptions

--- src/main/java/nextstep/subway/exception/AlreadyExistsException.java
+++ src/main/java/nextstep/subway/exception/AlreadyExistsException.java
@@ -1,6 +1,10 @@
 package nextstep.subway.exception;
 
-public class AlreadyExistsException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class AlreadyExistsException extends SubwayException {
     public AlreadyExistsException(String message) {
         super(message);
     }
