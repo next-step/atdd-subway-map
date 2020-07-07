a5fc76555636d3a80bab0c49aa9a3a51c1ff9e39
etff <wish2rich@gmail.com>
2020-07-07 22:24:32 +0900

    refactor(lineStationService): remove optional in appendStation

--- src/main/java/nextstep/subway/line/application/LineStationService.java
+++ src/main/java/nextstep/subway/line/application/LineStationService.java
@@ -5,19 +5,16 @@ import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.domain.LineStation;
 import nextstep.subway.line.dto.LineStationRequest;
 import nextstep.subway.line.dto.LineStationResponse;
-import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.application.StationNotFoundException;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 @Transactional
 public class LineStationService {
-    private static final String LINE_NOT_FOUND = "지하철 노선을 찾을 수 없습니다.";
     private final LineRepository lineRepository;
     private final StationRepository stationRepository;
 
@@ -27,18 +24,13 @@ public class LineStationService {
     }
 
     public LineStationResponse appendStation(Long lineId, LineStationRequest lineStationRequest) {
-        Optional<Line> findLine = lineRepository.findById(lineId);
+        Line findLine = lineRepository.findById(lineId).orElseThrow(LineNotFoundException::new);
 
-        if (findLine.isPresent()) {
-            LineStation lineStation = lineStationRequest.toLineStation();
-            findLine.get().addLineStation(lineStation);
+        LineStation lineStation = lineStationRequest.toLineStation();
+        findLine.addLineStation(lineStation);
 
-            Station station = stationRepository.findById(lineStation.getStationId())
-                    .orElseThrow(StationNotFoundException::new);
-
-            return LineStationResponse.of(lineStation, StationResponse.of(station));
-
-        }
-        throw new LineNotFoundException();
+        Station station = stationRepository.findById(lineStation.getStationId())
+                .orElseThrow(StationNotFoundException::new);
+        return LineStationResponse.of(lineStation, StationResponse.of(station));
     }
 }
--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -4,9 +4,7 @@ import nextstep.subway.config.BaseEntity;
 
 import javax.persistence.*;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Entity
 public class Line extends BaseEntity {
