e5f10c54ae71a58305fb5685b04eefd2c42b5967
Zeeyon <zeeyon.c@gmail.com>
2020-07-08 00:37:37 +0900

    fix: 노선에 역 등록 서비스 트랜잭션 설정

--- src/main/java/nextstep/subway/line/application/LineStationService.java
+++ src/main/java/nextstep/subway/line/application/LineStationService.java
@@ -4,6 +4,7 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineStationRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class LineStationService {
@@ -14,9 +15,10 @@ public class LineStationService {
         this.lineRepository = lineRepository;
     }
 
+    @Transactional
     public void addStation(Long lineId, LineStationRequest lineStationRequest) {
         Line line = lineRepository.findById(lineId)
-                .orElseThrow(() -> new IllegalArgumentException("id에 해당하는 라인이 없습니다"));
+                .orElseThrow(() -> new IllegalArgumentException("id에 해당하는 라인이 없습니다:" + lineId));
 
         line.addStation(lineStationRequest.toLineStation());
     }
