7395e1d256a0843ce16b859282cb2a920a2681fd
etff <wish2rich@gmail.com>
2020-07-07 22:41:45 +0900

    test(lineStationAdd): add setup method

--- src/test/java/nextstep/subway/line/acceptance/LineStationAddAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineStationAddAcceptanceTest.java
@@ -1,21 +1,18 @@
 package nextstep.subway.line.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.station.dto.StationResponse;
 import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 import java.time.LocalTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import static nextstep.subway.line.acceptance.step.LineAcceptanceStep.지하철_노선_등록되어_있음;
@@ -25,6 +22,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선에 역 등록 관련 기능")
 public class LineStationAddAcceptanceTest extends AcceptanceTest {
+
+    private Long lineId;
+    private Long stationId1;
+    private Long stationId2;
+    private Long stationId3;
+
+    @BeforeEach
+    public void setUp() {
+        ExtractableResponse<Response> createdLineResponse = 지하철_노선_등록되어_있음(
+                "2호선",
+                "GREEN",
+                LocalTime.of(5, 30),
+                LocalTime.of(23, 30),
+                5);
+
+        ExtractableResponse<Response> createdStationResponse1 = 지하철역_등록되어_있음("강남역");
+        ExtractableResponse<Response> createdStationResponse2 = 지하철역_등록되어_있음("역삼역");
+        ExtractableResponse<Response> createdStationResponse3 = 지하철역_등록되어_있음("선릉역");
+
+        lineId = createdLineResponse.as(LineResponse.class).getId();
+        stationId1 = createdStationResponse1.as(StationResponse.class).getId();
+        stationId2 = createdStationResponse2.as(StationResponse.class).getId();
+        stationId3 = createdStationResponse3.as(StationResponse.class).getId();
+    }
+
     @DisplayName("지하철 노선에 역을 등록한다.")
     @Test
     void addLineStation() {
@@ -42,7 +64,6 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
         // then
         지하철_노선에_지하철역_등록됨(response);
     }
-
 
 
     @DisplayName("지하철 노선 상세정보 조회 시 역 정보가 포함된다.")
