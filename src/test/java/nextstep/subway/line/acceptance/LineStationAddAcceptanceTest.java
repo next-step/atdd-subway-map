358f3afa9c5c6dcbf269b18081f791cbf877f5a5
etff <wish2rich@gmail.com>
2020-07-08 18:52:52 +0900

    test(lineStation): remove given statement getLineWithStations

--- src/test/java/nextstep/subway/line/acceptance/LineStationAddAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineStationAddAcceptanceTest.java
@@ -1,5 +1,6 @@
 package nextstep.subway.line.acceptance;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static nextstep.subway.line.acceptance.step.LineAcceptanceStep.지하철_노선_등록되어_있음;
+import static nextstep.subway.line.acceptance.step.LineAcceptanceStep.지하철_노선_생성됨;
 import static nextstep.subway.line.acceptance.step.LineStationAddAcceptanceStep.*;
 import static nextstep.subway.station.acceptance.step.StationAcceptanceStep.지하철역_등록되어_있음;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,8 +30,11 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
     private Long stationId2;
     private Long stationId3;
 
+
     @BeforeEach
     public void setUp() {
+        super.setUp();
+
         ExtractableResponse<Response> createdLineResponse = 지하철_노선_등록되어_있음(
                 "2호선",
                 "GREEN",
@@ -70,13 +75,7 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
     @Test
     void getLineWithStations() {
         // given
-        ExtractableResponse<Response> createdLineResponse = 지하철_노선_등록되어_있음("2호선", "GREEN",
-                LocalTime.of(5, 30), LocalTime.of(23, 30), 5);
-        ExtractableResponse<Response> createdStationResponse = 지하철역_등록되어_있음("강남역");
-
-        Long lineId = createdLineResponse.as(LineResponse.class).getId();
-        Long stationId = createdStationResponse.as(StationResponse.class).getId();
-        지하철_노선에_지하철역_등록_요청(lineId, null, stationId, 4, 2);
+        지하철_노선에_지하철역_등록_요청(lineId, null, stationId1, 4, 2);
 
         // when
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(lineId);
@@ -89,27 +88,15 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선에 여러개의 역을 순서대로 등록한다.")
     @Test
     void addLineStationInOrder() {
-        // given
-        ExtractableResponse<Response> createdLineResponse = 지하철_노선_등록되어_있음("2호선", "GREEN",
-                LocalTime.of(5, 30), LocalTime.of(23, 30), 5);
-        ExtractableResponse<Response> createdStationResponse1 = 지하철역_등록되어_있음("강남역");
-        ExtractableResponse<Response> createdStationResponse2 = 지하철역_등록되어_있음("역삼역");
-        ExtractableResponse<Response> createdStationResponse3 = 지하철역_등록되어_있음("선릉역");
 
         // when
-        Long lineId = createdLineResponse.as(LineResponse.class).getId();
-        Long stationId1 = createdStationResponse1.as(StationResponse.class).getId();
         ExtractableResponse<Response> lineStationResponse = 지하철_노선에_지하철역_등록_요청(lineId, null, stationId1, 4, 2);
-
-        Long stationId2 = createdStationResponse2.as(StationResponse.class).getId();
         지하철_노선에_지하철역_등록_요청(lineId, stationId1, stationId2, 4, 2);
-
-        Long stationId3 = createdStationResponse3.as(StationResponse.class).getId();
         지하철_노선에_지하철역_등록_요청(lineId, stationId2, stationId3, 4, 2);
 
 
         // then
-        assertThat(lineStationResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        지하철_노선_생성됨(lineStationResponse);
 
         // when
         // 지하철_노선_조회_요청
