4205d48a5d00b70984771940db15c27891f9dcce
wanho <wanho92@gmail.com>
2020-07-15 00:17:32 +0900

    refactor: 테스트 코드 메서드만 남기기

--- src/test/java/nextstep/subway/line/acceptance/LineStationAddAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineStationAddAcceptanceTest.java
@@ -49,55 +49,111 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
     @Test
     void addLineStation() {
         // when
-        // 지하철_노선에_지하철역_등록_요청
-        Map<String, String> params = createLineStationRequestParams(null, stationId1, "4", "2");
-        ExtractableResponse<Response> response = createLineStation(params, lineId);
+        ExtractableResponse<Response> response = 지하철_노선에_지하철역_등록_요청(null, stationId1, "4", "2");
 
         // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        지하철_노선에_역_등록됨(response);
     }
 
     @DisplayName("지하철 노선 상세정보 조회 시 역 정보가 포함된다.")
     @Test
     void getLineWithStations() {
-        // 지하철_노선에_지하철역_등록_요청
-        Map<String, String> params = createLineStationRequestParams(null, stationId1, "4", "2");
-        createLineStation(params, lineId);
+        // given
+        지하철_노선에_지하철역_등록_요청(null, stationId1, "4", "2");
 
         // when
-        // 지하철_노선_조회_요청
-        ExtractableResponse<Response> response = readLinesStation(lineId);
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(lineId);
 
         // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        LineResponse lineResponse = response.as(LineResponse.class);
-        assertThat(lineResponse).isNotNull();
-        assertThat(lineResponse.getStations()).hasSize(1);
+        지하철_노선_상세정보_조회_시_역_정보_포함됨(response);
     }
 
     @DisplayName("지하철 노선에 여러개의 역을 순서대로 등록한다.")
     @Test
     void addLineStationInOrder() {
         // when
-        // 지하철_노선에_지하철역_등록_요청
-        Map<String, String> params = createLineStationRequestParams(null, stationId1, "4", "2");
-        ExtractableResponse<Response> lineStationResponse = createLineStation(params, lineId);
+        ExtractableResponse<Response> lineStationResponse =  지하철_노선에_지하철역_등록_요청(null, stationId1, "4", "2");
 
-        // 지하철_노선에_지하철역_등록_요청
-        params = createLineStationRequestParams(stationId1, stationId2, "4", "2");
-        createLineStation(params, lineId);
-
-        // 지하철_노선에_지하철역_등록_요청
-        params = createLineStationRequestParams(stationId2, stationId3, "4", "2");
-        createLineStation(params, lineId);
+        지하철_노선에_지하철역_등록_요청(stationId1, stationId2, "4", "2");
+        지하철_노선에_지하철역_등록_요청(stationId2, stationId3, "4", "2");
 
         // then
-        assertThat(lineStationResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        지하철_노선에_역_등록됨(lineStationResponse);
 
         // when
-        // 지하철_노선_조회_요청
-        ExtractableResponse<Response> response = readLinesStation(lineId);
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(lineId);
 
+        // then
+        지하철_노선에_여러개의_역이_순서대로_등록됨(response);
+    }
+
+    @DisplayName("지하철 노선에 여러개의 역을 순서없이 등록한다.")
+    @Test
+    void addLineStationInAnyOrder() {
+        // when
+        ExtractableResponse<Response> lineStationResponse =  지하철_노선에_지하철역_등록_요청(null, stationId1, "4", "2");
+
+        지하철_노선에_지하철역_등록_요청(stationId1, stationId2, "4", "2");
+        지하철_노선에_지하철역_등록_요청(stationId1, stationId3, "4", "2");
+
+        // then
+        지하철_노선에_역_등록됨(lineStationResponse);
+
+        // when
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(lineId);
+
+        // then
+        지하철_노선에_여러개의_역이_순서없이_등록됨(response);
+    }
+
+    @DisplayName("이미 등록되어 있던 역을 등록한다.")
+    @Test
+    void addDuplicateStationInLine() {
+        // given
+        지하철_노선에_지하철역_등록_요청(null, stationId1, "4", "2");
+
+        // when
+        // 지하철_노선_이미_등록되어있는_지하철역_등록_요청
+        ExtractableResponse<Response> lineStationResponse =  지하철_노선에_지하철역_등록_요청(null, stationId1, "4", "2");
+
+        // then
+        지하철_노선에_이미_존재_역_등록_실패됨(lineStationResponse);
+    }
+
+    @DisplayName("존재하지 않는 역을 등록한다.")
+    @Test
+    void addNonExistStationInLine() {
+        // when
+        // 지하철_노선_존재하지_않는_지하철역_등록_요청
+        ExtractableResponse<Response> lineStationResponse = 지하철_노선에_지하철역_등록_요청(null, 4L, "4", "2");
+
+        // then
+        // 지하철 노선에 지하철역 등록 실패됨
+        존재하지_않는_역_등록_실패(lineStationResponse);
+    }
+
+    private ExtractableResponse<Response> 지하철_노선에_지하철역_등록_요청(Long preStationId, Long stationId1, String distance, String duration) {
+        Map<String, String> params = createLineStationRequestParams(preStationId, stationId1, distance, duration);
+
+        return createLineStation(params, lineId);
+    }
+
+    private void 지하철_노선에_역_등록됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private ExtractableResponse<Response> 지하철_노선_조회_요청(Long lineId) {
+        return readLinesStation(lineId);
+    }
+
+    private void 지하철_노선_상세정보_조회_시_역_정보_포함됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        LineResponse lineResponse = response.as(LineResponse.class);
+        assertThat(lineResponse).isNotNull();
+        assertThat(lineResponse.getStations()).hasSize(1);
+    }
+
+    private void 지하철_노선에_여러개의_역이_순서대로_등록됨(ExtractableResponse<Response> response) {
         LineResponse lineResponse = response.as(LineResponse.class);
         assertThat(lineResponse).isNotNull();
         assertThat(lineResponse.getStations()).hasSize(3);
@@ -107,29 +163,7 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
         assertThat(stationIds).containsExactlyElementsOf(Lists.newArrayList(1L, 2L, 3L));
     }
 
-    @DisplayName("지하철 노선에 여러개의 역을 순서없이 등록한다.")
-    @Test
-    void addLineStationInAnyOrder() {
-        // when
-        // 지하철_노선에_지하철역_등록_요청
-        Map<String, String> params = createLineStationRequestParams(null, stationId1, "4", "2");
-        ExtractableResponse<Response> lineStationResponse = createLineStation(params, lineId);
-
-        // 지하철_노선에_지하철역_등록_요청
-        params = createLineStationRequestParams(stationId1, stationId2, "4", "2");
-        createLineStation(params, lineId);
-
-        // 지하철_노선에_지하철역_등록_요청
-        params = createLineStationRequestParams(stationId1, stationId3, "4", "2");
-        createLineStation(params, lineId);
-
-        // then
-        assertThat(lineStationResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-
-        // when
-        // 지하철_노선_조회_요청
-        ExtractableResponse<Response> response = readLinesStation(lineId);
-
+    private void 지하철_노선에_여러개의_역이_순서없이_등록됨(ExtractableResponse<Response> response) {
         LineResponse lineResponse = response.as(LineResponse.class);
         assertThat(lineResponse).isNotNull();
         assertThat(lineResponse.getStations()).hasSize(3);
@@ -139,35 +173,11 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
         assertThat(stationIds).containsExactlyElementsOf(Lists.newArrayList(1L, 3L, 2L));
     }
 
-    @DisplayName("이미 등록되어 있던 역을 등록한다.")
-    @Test
-    void addDuplicateStationInLine() {
-        // given
-        // 지하철_노선에_지하철역_등록_요청
-        Map<String, String> params = createLineStationRequestParams(null, stationId1, "4", "2");
-        createLineStation(params, lineId);
-
-        // when
-        // 지하철_노선_이미_등록되어있는_지하철역_등록_요청
-        params = createLineStationRequestParams(null, stationId1, "4", "2");
-        ExtractableResponse<Response> lineStationResponse = createLineStation(params, lineId);
-
-        // then
-        // 지하철 노선에 지하철역 등록 실패됨
+    private void 지하철_노선에_이미_존재_역_등록_실패됨(ExtractableResponse<Response> lineStationResponse) {
         assertThat(lineStationResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         assertThat(lineStationResponse.body().asString()).contains("지하철 역이 해당 노선에 이미 등록되어 있습니다");
     }
-
-    @DisplayName("존재하지 않는 역을 등록한다.")
-    @Test
-    void addNonExistStationInLine() {
-        // when
-        // 지하철_노선_존재하지_않는_지하철역_등록_요청
-        Map<String, String> params = createLineStationRequestParams(null, 4L, "4", "2");
-        ExtractableResponse<Response> lineStationResponse = createLineStation(params, lineId);
-
-        // then
-        // 지하철 노선에 지하철역 등록 실패됨
+    private void 존재하지_않는_역_등록_실패(ExtractableResponse<Response> lineStationResponse) {
         assertThat(lineStationResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         assertThat(lineStationResponse.body().asString()).contains("존재하지 않는 역은 등록이 불가능 합니다.");
     }
