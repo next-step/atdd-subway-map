0da6c174c0591c3b93221a59d27efa0e5e44501a
임한결 <dlagksruf19@naver.com>
2021-03-05 01:04:24 +0900

    test : 지하철 노선 생성 인수 테스트 구현 (실습2)

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,19 +1,40 @@
 package nextstep.subway.line;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
-    @DisplayName("지하철 노선을 생성한다.")
+
     @Test
+    @DisplayName("지하철 노선을 생성한다.")
     void createLine() {
+        // given
+        // 지하철_노선_생성_요청
+        LineRequest lineRequest = new LineRequest("2", "green");
+
         // when
         // 지하철_노선_생성_요청
-
+        ExtractableResponse<Response> createLineResponse = RestAssured
+                .given().log().all()
+                .body(lineRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .post("/lines")
+                .then().log().all()
+                .extract();
         // then
         // 지하철_노선_생성됨
+        assertThat(createLineResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(createLineResponse.header("Location")).isNotBlank();
     }
 
     @DisplayName("지하철 노선 목록을 조회한다.")
