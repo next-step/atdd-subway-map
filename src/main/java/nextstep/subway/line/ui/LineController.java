19542bb268c44709126eaf7e11b9a6b596274a6e
HyunSung-Na <oceana57@naver.com>
2021-03-05 10:45:44 +0900

    LineController CRUD 로직 add

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -1,18 +1,20 @@
 package nextstep.subway.line.ui;
 
+import nextstep.subway.error.NameExistsException;
 import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
-@RequestMapping("/lines")
+@RequestMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
 public class LineController {
     private final LineService lineService;
 
@@ -21,8 +23,35 @@ public class LineController {
     }
 
     @PostMapping
-    public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
+    public ResponseEntity<?> createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @GetMapping("/{lineId}")
+    public ResponseEntity<LineResponse> viewLine(@PathVariable Long lineId) {
+        return ResponseEntity.ok().body(lineService.getLine(lineId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LineResponse>> showLines() {
+        return ResponseEntity.ok().body(lineService.findAllLine());
+    }
+
+    @PutMapping("/{lineId}")
+    public ResponseEntity<?> updateLine(@PathVariable Long lineId, LineRequest request) {
+        lineService.updateLineById(request, lineId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{lineId}")
+    public ResponseEntity<?> deleteLine(@PathVariable Long lineId) {
+        lineService.deleteLineById(lineId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(NameExistsException.class)
+    public ResponseEntity handleIllegalArgsException(NameExistsException e) {
+        return ResponseEntity.badRequest().build();
     }
 }
