4b9e36a4c7668b0ef222b249410468db3f4ea982
perelandrax <luvyum4e@gmail.com>
2021-03-05 15:45:12 +0900

    test: 지하철_노선_생성 인수테스트 작성

---
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -0,0 +1,100 @@
+package nextstep.subway.line;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("지하철 노선 관련 기능")
+public class LineAcceptanceTest extends AcceptanceTest {
+
+    @DisplayName("지하철 노선을 생성한다.")
+    @Test
+    void createLine() {
+        // when
+        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = 지하철_노선_생성요청("1호선", "노랑색");
+
+        // then
+        // 지하철_노선_생성됨
+        지하철_노선_생성됨(response);
+    }
+
+    @DisplayName("지하철 노선 목록을 조회한다.")
+    @Test
+    void getLines() {
+        // given
+        // 지하철_노선_등록되어_있음
+        // 지하철_노선_등록되어_있음
+
+        // when
+        // 지하철_노선_목록_조회_요청
+
+        // then
+        // 지하철_노선_목록_응답됨
+        // 지하철_노선_목록_포함됨
+    }
+
+    @DisplayName("지하철 노선을 조회한다.")
+    @Test
+    void getLine() {
+        // given
+        // 지하철_노선_등록되어_있음
+
+        // when
+        // 지하철_노선_조회_요청
+
+        // then
+        // 지하철_노선_응답됨
+    }
+
+    @DisplayName("지하철 노선을 수정한다.")
+    @Test
+    void updateLine() {
+        // given
+        // 지하철_노선_등록되어_있음
+
+        // when
+        // 지하철_노선_수정_요청
+
+        // then
+        // 지하철_노선_수정됨
+    }
+
+    @DisplayName("지하철 노선을 제거한다.")
+    @Test
+    void deleteLine() {
+        // given
+        // 지하철_노선_등록되어_있음
+
+        // when
+        // 지하철_노선_제거_요청
+
+        // then
+        // 지하철_노선_삭제됨
+    }
+
+    private ExtractableResponse<Response> 지하철_노선_생성요청(String name, String color) {
+        LineRequest request = new LineRequest(name, color);
+
+        return RestAssured
+                .given().log().all().body(request).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all().extract();
+    }
+
+    private void 지하철_노선_생성됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
+    }
+}
