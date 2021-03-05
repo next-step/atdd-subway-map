86f8f745f30c6891d030211f7f9d8eda6afa6fba
sangypark <sangypark@ebay.com>
2021-03-05 11:58:09 +0900

    feature : 지하철 노선 목록 조회 기능 구현

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -1,9 +1,10 @@
 package nextstep.subway.line.ui;
-
+import java.util.List;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +25,11 @@ public class LineController {
     public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @GetMapping
+    public ResponseEntity getLines() {
+       List<LineResponse> lineResponseList =  lineService.getLines();
+       return ResponseEntity.ok().body(lineResponseList);
     }
 }
