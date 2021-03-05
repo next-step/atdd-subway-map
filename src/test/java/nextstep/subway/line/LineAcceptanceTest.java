54d83ecf23ad071fcd4bb64b1c7f5da4c0433763
catsbi <killfine1988@gmail.com>
2021-03-06 01:09:49 +0900

    [refactor] 코드정리 및 노선등록 메소드 모듈화

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -7,47 +7,58 @@ import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.domain.Line;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.ListAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
+    private Map<String, String> 신분당선;
+    private Map<String, String> 이호선;
+
+    @BeforeEach
+    void setup() {
+        신분당선 = new HashMap<String, String>() {{
+            put("name", "신분당선");
+            put("color", "bg-red-600");
+        }};
+        이호선 = new HashMap<String, String>() {{
+            put("name", "2호선");
+            put("color", "bg-green-600");
+        }};
+    }
+
+
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
         // when
-        ExtractableResponse<Response> response = 지하철_노선_등록되어_있음("신분당선", "bg-red-600");
+        ExtractableResponse<Response> response = 지하철_노선_등록(신분당선);
 
         // then
-        // 지하철_노선_생성됨
-        지하철_노선_목록_응답됨(response, HttpStatus.CREATED);
+        응답_결과_확인(response, HttpStatus.CREATED);
     }
 
     @DisplayName("지하철 노선 목록을 조회한다.")
     @Test
     void getLines() {
         // given
-        ExtractableResponse<Response> createResponse1 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600");
-        ExtractableResponse<Response> createResponse2 = 지하철_노선_등록되어_있음("2호선", "bg-green-600");
+        ExtractableResponse<Response> createResponse1 = 지하철_노선_등록(신분당선);
+        ExtractableResponse<Response> createResponse2 = 지하철_노선_등록(이호선);
 
         // when
-        // 지하철_노선_목록_조회_요청
         ExtractableResponse<Response> response = 지하철_노선_목록_조회_요청();
 
         // then
-        // 지하철_노선_목록_응답됨
-        지하철_노선_목록_응답됨(response, HttpStatus.OK);
+        응답_결과_확인(response, HttpStatus.OK);
 
-        //지하철_노선_목록_포함됨
-        List<Long> createdLineIds = 지하철_노선_아이디_추출(Arrays.asList(createResponse1, createResponse2));
+        List<Long> createdLineIds = 지하철_노선_아이디_추출(createResponse1, createResponse2);
         List<Long> resultLineIds = 지하철_노선_객체_리스트_반환(response);
 
         지하철_노선_목록_포함됨(createdLineIds, resultLineIds);
@@ -56,7 +67,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
     private ExtractableResponse<Response> 지하철_노선_목록_조회_요청() {
         return RestAssured
                 .given().log().all()
-                .when().get("/lines")
+                .when().get(서비스_호출_경로_생성(null))
                 .then().log().all().extract();
 
     }
@@ -71,41 +82,32 @@ public class LineAcceptanceTest extends AcceptanceTest {
         return Assertions.assertThat(resultLineIds).containsAll(createdLineIds);
     }
 
-    private void 지하철_노선_목록_응답됨(ExtractableResponse<Response> response, HttpStatus status) {
-        Assertions.assertThat(response.statusCode()).isEqualTo(status.value());
-    }
-
     @DisplayName("지하철 노선을 조회한다.")
     @Test
     void getLine() {
         // given
-        // 지하철_노선_등록되어_있음
-        ExtractableResponse<Response> createdResponse1 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600");
+        ExtractableResponse<Response> createdResponse1 = 지하철_노선_등록(신분당선);
 
         Long createdId = 지하철_노선_아이디_추출(createdResponse1);
 
         // when
-        // 지하철_노선_조회_요청
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(createdId);
 
         // then
-        // 지하철_노선_응답됨
-        지하철_노선_응답됨(response, HttpStatus.OK);
+        응답_결과_확인(response, HttpStatus.OK);
     }
 
-    private void 지하철_노선_응답됨(ExtractableResponse<Response> response, HttpStatus status) {
+    private void 응답_결과_확인(ExtractableResponse<Response> response, HttpStatus status) {
         Assertions.assertThat(response.statusCode()).isEqualTo(status.value());
     }
+
 
     @DisplayName("지하철 노선을 수정한다.")
     @Test
     void updateLine() {
         // given
-        // 지하철_노선_등록되어_있음
-        ExtractableResponse<Response> createdResponse1 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600");
+        ExtractableResponse<Response> createdResponse1 = 지하철_노선_등록(신분당선);
 
-        // when
-        // 지하철_노선_수정_요청
         // when
         HashMap<String, String> params = new HashMap<>();
         params.put("name", "구분당선");
@@ -116,13 +118,12 @@ public class LineAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response2 = 지하철_노선_조회_요청(createdId);
 
         // then
-        // 지하철_노선_수정됨
         지하철_노선_수정됨(response1);
         지하철_노선_수정_확인(response2, params);
     }
 
     private ExtractableResponse<Response> 지하철_노선_수정_요청(Long createdId, HashMap<String, String> params) {
-        String path = String.format("/lines/%d", createdId);
+        String path = 서비스_호출_경로_생성(createdId);
 
         return RestAssured
                 .given().log().all()
@@ -144,7 +145,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
     }
 
     private ExtractableResponse<Response> 지하철_노선_조회_요청(Long id) {
-        String path = String.format("/lines/%d", id);
+        String path = 서비스_호출_경로_생성(id);
 
         return RestAssured
                 .given().log().all()
@@ -156,21 +157,18 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void deleteLine() {
         // given
-        // 지하철_노선_등록되어_있음
-        ExtractableResponse<Response> createdResponse1 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600");
+        ExtractableResponse<Response> createdResponse1 = 지하철_노선_등록(신분당선);
         Long createdId = 지하철_노선_아이디_추출(createdResponse1);
 
         // when
-        // 지하철_노선_제거_요청
         ExtractableResponse<Response> response = 지하철_노선_제거_요청(createdId);
 
         // then
-        // 지하철_노선_삭제됨
         지하철_노선_삭제됨(response);
     }
 
     private ExtractableResponse<Response> 지하철_노선_제거_요청(Long createdId) {
-        String path = String.format("/lines/%d", createdId);
+        String path = 서비스_호출_경로_생성(createdId);
         return RestAssured
                 .given().log().all()
                 .when().delete(path)
@@ -181,7 +179,11 @@ public class LineAcceptanceTest extends AcceptanceTest {
         Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
-    private ExtractableResponse<Response> 지하철_노선_등록되어_있음(String name, String color) {
+    private ExtractableResponse<Response> 지하철_노선_등록(Map<String, String> params) {
+        return 지하철_노선_등록(params.get("name"), params.get("color"));
+    }
+
+    private ExtractableResponse<Response> 지하철_노선_등록(String name, String color) {
         Map<String, String> params = new HashMap<>();
         params.put("name", name);
         params.put("color", color);
@@ -190,18 +192,27 @@ public class LineAcceptanceTest extends AcceptanceTest {
                 .given().log().all()
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/lines")
+                .when().post(서비스_호출_경로_생성(null))
                 .then().log().all().extract();
     }
 
-    private List<Long> 지하철_노선_아이디_추출(List<ExtractableResponse<Response>> list) {
-        return list.stream()
+    private List<Long> 지하철_노선_아이디_추출(ExtractableResponse<Response>... list) {
+        return Stream.of(list)
                 .map(this::지하철_노선_아이디_추출)
                 .collect(Collectors.toList());
     }
 
     private Long 지하철_노선_아이디_추출(ExtractableResponse<Response> response) {
         return Long.parseLong(response.header("Location").split("/")[2]);
+    }
+
+    private String 서비스_호출_경로_생성(Long createdId) {
+        String path = "/lines";
+        if (Objects.nonNull(createdId)) {
+            return path + "/" + createdId;
+        }
+
+        return path;
     }
 
 }
