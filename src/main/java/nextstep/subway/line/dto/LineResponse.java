198aa4f68e62a7f3322ef9d429b34abc23cf574a
dbeod2 <70149382+dbeod2@users.noreply.github.com>
2021-03-06 11:17:15 +0900

    [1단계 - 지하철 노선 관리] (#62)
    * feat : 지하철 노선 api 요구사항 구현 완료 (생성,목록 조회, 조회, 수정, 삭제)
    
    * test : 지하철 노선 api 테스트 코드 작성

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -3,46 +3,60 @@ package nextstep.subway.line.dto;
 import nextstep.subway.line.domain.Line;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class LineResponse {
-    private Long id;
-    private String name;
-    private String color;
-    private LocalDateTime createdDate;
-    private LocalDateTime modifiedDate;
+  private Long id;
+  private String name;
+  private String color;
+  private LocalDateTime createdDate;
+  private LocalDateTime modifiedDate;
 
-    public LineResponse() {
-    }
+  public LineResponse() {
+  }
 
-    public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-        this.createdDate = createdDate;
-        this.modifiedDate = modifiedDate;
-    }
+  public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    this.id = id;
+    this.name = name;
+    this.color = color;
+    this.createdDate = createdDate;
+    this.modifiedDate = modifiedDate;
+  }
 
-    public static LineResponse of(Line line) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate());
-    }
+  public static LineResponse of(Line line) {
+    return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate());
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getColor() {
-        return color;
-    }
+  public String getColor() {
+    return color;
+  }
 
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
+  public LocalDateTime getCreatedDate() {
+    return createdDate;
+  }
 
-    public LocalDateTime getModifiedDate() {
-        return modifiedDate;
-    }
+  public LocalDateTime getModifiedDate() {
+    return modifiedDate;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof LineResponse)) return false;
+    LineResponse that = (LineResponse) o;
+    return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(color, that.color) && Objects.equals(createdDate, that.createdDate) && Objects.equals(modifiedDate, that.modifiedDate);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, color, createdDate, modifiedDate);
+  }
 }
