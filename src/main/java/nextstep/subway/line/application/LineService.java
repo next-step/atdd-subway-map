c77959a3a05c8f4f1ae53c8440fc072be0fcad97
dbeod2 <darung.0211@gmail.com>
2021-03-06 09:35:27 +0900

    feat : 지하철 노선 api 요구사항 구현 완료 (생성,목록 조회, 조회, 수정, 삭제)

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -7,17 +7,56 @@ import nextstep.subway.line.dto.LineResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+  private LineRepository lineRepository;
 
-    public LineService(LineRepository lineRepository) {
-        this.lineRepository = lineRepository;
+  public LineService(LineRepository lineRepository) {
+    this.lineRepository = lineRepository;
+  }
+
+  public LineResponse saveLine(LineRequest request) {
+    Line persistLine = lineRepository.save(request.toLine());
+    return LineResponse.of(persistLine);
+  }
+
+  public List<LineResponse> getAll() {
+    List<Line> lines = lineRepository.findAll();
+    if (lines.isEmpty()) {
+      return Collections.emptyList();
     }
 
-    public LineResponse saveLine(LineRequest request) {
-        Line persistLine = lineRepository.save(request.toLine());
-        return LineResponse.of(persistLine);
-    }
+    return lines.stream()
+            .map(LineResponse::of)
+            .collect(Collectors.toList());
+  }
+
+  private Line getLine(long id) {
+    return Optional.of(lineRepository.findById(id))
+            .orElseThrow(NullPointerException::new)
+            .get();
+  }
+
+  public LineResponse get(long id) {
+    return LineResponse.of(getLine(id));
+  }
+
+  public LineResponse saveLine(Long id, LineRequest lineRequest) {
+    Line line = getLine(id);
+    line.update(lineRequest.toLine());
+
+    lineRepository.save(line);
+    return LineResponse.of(line);
+  }
+
+  public void delete(long id) {
+    Line line = getLine(id);
+    lineRepository.delete(line);
+  }
 }
--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -4,25 +4,45 @@ import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
 @RestController
 @RequestMapping("/lines")
 public class LineController {
-    private final LineService lineService;
+  private final LineService lineService;
 
-    public LineController(final LineService lineService) {
-        this.lineService = lineService;
-    }
+  public LineController(final LineService lineService) {
+    this.lineService = lineService;
+  }
 
-    @PostMapping
-    public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
-        LineResponse line = lineService.saveLine(lineRequest);
-        return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
-    }
+  @PostMapping
+  public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
+    LineResponse line = lineService.saveLine(lineRequest);
+    return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+  }
+
+  @GetMapping
+  public ResponseEntity getAllLine() {
+    return ResponseEntity.ok().body(lineService.getAll());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity getLine(@PathVariable long id) {
+    LineResponse line = lineService.get(id);
+    return ResponseEntity.ok().body(line);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity update(@PathVariable long id, @RequestBody LineRequest lineRequest) {
+    LineResponse line = lineService.saveLine(id, lineRequest);
+    return ResponseEntity.ok().body(line);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity remove(@PathVariable long id) {
+    lineService.delete(id);
+    return ResponseEntity.noContent().build();
+  }
 }
