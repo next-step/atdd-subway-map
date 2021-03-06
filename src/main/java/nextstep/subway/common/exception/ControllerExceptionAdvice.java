4d4130b2391d6f28f204cc3869fa6ca5a0804db7
임한결 <dlagksruf19@naver.com>
2021-03-06 21:21:30 +0900

    refactor : ControllerExceptionAdvice @ResponseStatus 제거

--- src/main/java/nextstep/subway/common/exception/ControllerExceptionAdvice.java
+++ src/main/java/nextstep/subway/common/exception/ControllerExceptionAdvice.java
@@ -1,28 +1,23 @@
 package nextstep.subway.common.exception;
 
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerExceptionAdvice {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ExistResourceException.class)
     public ResponseEntity<Void> existResourceExceptionHandler(RuntimeException e) {
         return ResponseEntity.badRequest().build();
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NonExistResourceException.class)
     public ResponseEntity<Void> nonExistResourceExceptionHandler(RuntimeException e) {
         return ResponseEntity.notFound().build();
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler()
     public ResponseEntity<Void> handleIllegalArgsException(DataIntegrityViolationException e) {
         return ResponseEntity.badRequest().build();
