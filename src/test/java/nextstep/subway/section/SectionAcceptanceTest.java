0d86beb4958f31626c792cdf0321b56e87871d6a
SC <pch838811@gmail.com>
2021-03-07 10:57:19 +0900

    test: 지하철 구간 인수 테스트 조건 생성
    - SectionAcceptanceTest : 지하철 구간에 관련된 기능을 인수 테스트 한다
      1. 지하철 구간 등록
      2. 지하철 구간 제거
      3. 지하철 구간으로 역 목록 조회

---
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -0,0 +1,26 @@
+package nextstep.subway.section;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("지하철 구간에 관련된 기능")
+public class SectionAcceptanceTest {
+
+	@DisplayName("지하철 구간을 등록한다.")
+	@Test
+	void createSection() {
+
+	}
+
+	@DisplayName("지하철 구간을 제거한다.")
+	@Test
+	void deleteSection() {
+
+	}
+
+	@DisplayName("지하철 구간을 통해 역 목록을 조회한다.")
+	@Test
+	void findStationsBySection() {
+
+	}
+}
