d2c69a8f032c3f256267e63daba57753261b09a8
bbbnam <bbbnam@naver.com>
2021-03-07 20:58:20 +0900

    refactor : 지하철노선 등록상태, 요청 테스트 메소드 분리

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -22,7 +22,11 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
-        ExtractableResponse<Response> response = 지하철_노선_등록되어_있음("2호선", "초록색");
+        //given
+        LineRequest lineRequest = 지하철_노선_등록되어_있음("2호선", "green");
+
+        //when
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청(lineRequest);
 
         // then
         지하철_노선_생성됨(response);
@@ -32,9 +36,11 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void getLines() {
         // given
-        ExtractableResponse<Response> createdResponse1 = 지하철_노선_등록되어_있음("2호선", "초록색");
+        LineRequest lineRequest1 = 지하철_노선_등록되어_있음("2호선", "green");
+        ExtractableResponse<Response> createdResponse1 = 지하철_노선_생성_요청(lineRequest1);
 
-        ExtractableResponse<Response> createdResponse2 = 지하철_노선_등록되어_있음("1호선", "군청색");
+        LineRequest lineRequest2 = 지하철_노선_등록되어_있음("1호선", "blue");
+        ExtractableResponse<Response> createdResponse2 = 지하철_노선_생성_요청(lineRequest2);
 
         // when
         ExtractableResponse<Response> response = 지하철_노선_목록_조회_요청();
@@ -48,7 +54,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void getLine() {
         // given
-        ExtractableResponse<Response> createdResponse = 지하철_노선_등록되어_있음("2호선", "초록색");
+        LineRequest lineRequest = 지하철_노선_등록되어_있음("2호선", "green");
+        ExtractableResponse<Response> createdResponse = 지하철_노선_생성_요청(lineRequest);
 
         // when
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(createdResponse);
@@ -60,7 +67,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void updateLine() {
         // given
-        ExtractableResponse<Response> createdResponse = 지하철_노선_등록되어_있음("2호선", "green");
+        LineRequest lineRequest = 지하철_노선_등록되어_있음("2호선", "green");
+        ExtractableResponse<Response> createdResponse = 지하철_노선_생성_요청(lineRequest);
 
         // when
         ExtractableResponse<Response> response = 지하철_노선_수정_요청("2호선", "red", createdResponse);
@@ -73,7 +81,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void deleteLine() {
         // given
-        ExtractableResponse<Response> createdResponse = 지하철_노선_등록되어_있음("2호선", "green");
+        LineRequest lineRequest = 지하철_노선_등록되어_있음("2호선", "green");
+        ExtractableResponse<Response> createdResponse = 지하철_노선_생성_요청(lineRequest);
 
         // when
         ExtractableResponse<Response> response = 지하철_노선_제거_요청(createdResponse);
@@ -82,8 +91,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         지하철_노선_삭제됨(response);
     }
 
-    private ExtractableResponse<Response> 지하철_노선_등록되어_있음(String lineName, String lineColor) {
-        LineRequest lineRequest = new LineRequest(lineName, lineColor);
+    private ExtractableResponse<Response> 지하철_노선_생성_요청(LineRequest lineRequest) {
         return RestAssured.given().log().all()
                 .body(lineRequest)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -91,6 +99,10 @@ public class LineAcceptanceTest extends AcceptanceTest {
                 .post("/lines")
                 .then().log().all()
                 .extract();
+    }
+
+    private LineRequest 지하철_노선_등록되어_있음(String lineName, String lineColor) {
+        return new LineRequest(lineName, lineColor);
     }
 
     private void 지하철_노선_생성됨(ExtractableResponse<Response> response) {
@@ -147,7 +159,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
     private ExtractableResponse<Response> 지하철_노선_수정_요청(String lineName, String lineColor,
                                                        ExtractableResponse<Response> createdResponse) {
-        LineRequest lineRequest = new LineRequest(lineName, lineColor);
+        LineRequest lineRequest = 지하철_노선_등록되어_있음(lineName, lineColor);
         String uri = createdResponse.header("Location");
         return RestAssured.given().log().all()
                 .body(lineRequest)
