750029b13e177414d338db7186d57582332603ce
SC <pch838811@gmail.com>
2021-03-07 22:19:37 +0900

    refactor: 패키지 변경, 인수조건 수정
    - SectionAcceptanceTest : 패키지 위치를 line 아래로 변경, 인수조건을 조금 더 명시적으로 변경
      - Section 은 Line 에 강하게 연결된 엔티티여서 line 패키지 아래에 속하는 게 맞다고 생각하여 변경

--- src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
@@ -1,4 +1,4 @@
-package nextstep.subway.section;
+package nextstep.subway.line;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -6,19 +6,19 @@ import org.junit.jupiter.api.Test;
 @DisplayName("지하철 구간에 관련된 기능")
 public class SectionAcceptanceTest {
 
-	@DisplayName("지하철 구간을 등록한다.")
+	@DisplayName("지하철 노선에 구간을 등록한다.")
 	@Test
 	void createSection() {
 
 	}
 
-	@DisplayName("지하철 구간을 제거한다.")
+	@DisplayName("지하철 노선에 구간을 제거한다.")
 	@Test
 	void deleteSection() {
 
 	}
 
-	@DisplayName("지하철 구간을 통해 역 목록을 조회한다.")
+	@DisplayName("지하철 노선에 등록된 구간을 통해 역 목록을 조회한다.")
 	@Test
 	void findStationsBySection() {
 
