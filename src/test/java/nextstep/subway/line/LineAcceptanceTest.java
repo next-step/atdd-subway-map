a3c9f5c0dc8e3caa4ba3129c96a95cf9f4bf24cd
bbbnam <bbbnam@naver.com>
2021-03-07 21:18:30 +0900

    refactor : 공백수정

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -75,6 +75,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // then
         지하철_노선_조회됨(response, 1);
     }
+
     @DisplayName("지하철 노선을 수정한다.")
     @Test
     void updateLine() {
