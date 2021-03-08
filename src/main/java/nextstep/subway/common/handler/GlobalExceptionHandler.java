38f57c86cdadbe2c9946220c21be60024f24e997
SC <pch838811@gmail.com>
2021-03-09 06:45:09 +0900

    refactor: 예외처리 핸들러 리팩터링
    - GlobalExceptionHandler : BusinessException 따로 핸들링하지 않는 예외들을 처리하는 메서드 추가

--- src/main/java/nextstep/subway/common/handler/GlobalExceptionHandler.java
+++ src/main/java/nextstep/subway/common/handler/GlobalExceptionHandler.java
@@ -27,4 +27,9 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 	}
+
+	@ExceptionHandler(BusinessException.class)
+	public ResponseEntity<String> handleBusinessException(BusinessException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+	}
 }
