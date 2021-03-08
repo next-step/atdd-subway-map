0dc6b70954560bf59a4a251600426b200fcd0a7e
고정완 (Can) <jw.gho@ecubelabs.com>
2021-03-08 18:24:31 +0900

    [feat] 지하철 노선 관련 기능 구현

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -7,6 +7,9 @@ import nextstep.subway.line.dto.LineResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
 public class LineService {
@@ -19,5 +22,29 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> getLines() {
+        return lineRepository.findAll()
+                .stream()
+                .map(LineResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public LineResponse getLine(Long id) {
+        Line line = lineRepository.findById(id).get();
+        return LineResponse.of(line);
+    }
+
+    public LineResponse updateLine(Long id, LineRequest lineRequest) {
+        Line line = lineRepository.getOne(id);
+        line.update(lineRequest.toLine());
+        return LineResponse.of(line);
+    }
+
+    public void deleteLine(Long id) {
+        lineRepository.deleteById(id);
     }
 }
