b5cd56fe53603484f6111e4fc031e8cd2bf96405
임한결 <dlagksruf19@naver.com>
2021-03-08 20:08:03 +0900

    feat : LineRequest 종점역, 거리 멤버변수 추가

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -6,6 +6,9 @@ public class LineRequest {
 
     private String name;
     private String color;
+    private Long upStationId;
+    private Long downStationId;
+    private int distance;
 
     public LineRequest() {
     }
@@ -15,12 +18,32 @@ public class LineRequest {
         this.color = color;
     }
 
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
     public String getName() {
         return name;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 
     public Line toLine() {
