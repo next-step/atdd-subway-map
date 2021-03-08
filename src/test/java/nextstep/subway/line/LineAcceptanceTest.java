b28e9de88c6f63860108c35fab950df0336e5c56
BlueCitron <sds901234@naver.com>
2021-03-08 22:27:23 +0900

    feat: 노선 요청, Http Assertion -> 별도 클래스로 분리

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,28 +1,22 @@
 package nextstep.subway.line;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.utils.DatabaseCleanup;
 import org.apache.groovy.util.Maps;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static nextstep.subway.line.LineSteps.*;
+import static nextstep.subway.utils.HttpAssertions.*;
+
 @DisplayName("지하철 노선 관련 기능")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LineAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("지하철 노선을 생성한다.")
@@ -77,7 +71,6 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         // then
         응답_HTTP_OK(response);
-
     }
 
     @DisplayName("지하철 노선을 제거한다.")
@@ -91,59 +84,6 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         // then
         응답_HTTP_NO_CONTENT(response);
-    }
-
-    private ExtractableResponse<Response> 지하철_노선_등록되어_있음(String name, String color) {
-        Map<String, String> params = Maps.of("name", name, "color", color);
-        return RestAssured
-                .given().log().all()
-                .body(params).contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/lines")
-                .then().log().all().extract();
-    }
-
-    private ExtractableResponse<Response> 지하철_노선_목록_조회_요청() {
-        return RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/lines")
-                .then().log().all().extract();
-    }
-
-    private ExtractableResponse<Response> 지하철_노선_조회_요청() {
-        return RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/lines/1")
-                .then().log().all().extract();
-    }
-
-    private ExtractableResponse<Response> 지하철_노선_수정_요청(Map<String, String> params) {
-        return RestAssured
-                .given().log().all()
-                .body(params).contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().put("/lines/1")
-                .then().log().all().extract();
-    }
-
-    private ExtractableResponse<Response> 지하철_노선_제거_요청() {
-        return RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().delete("/lines/1")
-                .then().log().all().extract();
-    }
-
-    private void 응답_HTTP_CREATED(ExtractableResponse<Response> response) {
-        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-    private void 응답_HTTP_OK(ExtractableResponse<Response> response) {
-        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
-
-    private void 응답_HTTP_NO_CONTENT(ExtractableResponse<Response> response) {
-        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
     private void 지하철_노선_목록_포함됨(ExtractableResponse<Response> response) {
---
+++ src/test/java/nextstep/subway/utils/HttpAssertions.java
@@ -0,0 +1,22 @@
+package nextstep.subway.utils;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.springframework.http.HttpStatus;
+
+public class HttpAssertions {
+
+    public static void 응답_HTTP_CREATED(ExtractableResponse<Response> response) {
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    public static void 응답_HTTP_OK(ExtractableResponse<Response> response) {
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    public static void 응답_HTTP_NO_CONTENT(ExtractableResponse<Response> response) {
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+    
+}
