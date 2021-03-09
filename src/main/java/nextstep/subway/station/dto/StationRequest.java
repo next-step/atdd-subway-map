8a47d5c75cfe957e63a638392d1c21bc548a1d2a
bbbnam <bbbnam@naver.com>
2021-03-09 23:44:52 +0900

    test : step 클래스 패키지 이동

--- src/main/java/nextstep/subway/station/dto/StationRequest.java
+++ src/main/java/nextstep/subway/station/dto/StationRequest.java
@@ -3,7 +3,13 @@ package nextstep.subway.station.dto;
 import nextstep.subway.station.domain.Station;
 
 public class StationRequest {
+    private Long id;
+
     private String name;
+
+    public Long getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
--- src/test/java/nextstep/subway/line/LineSteps.java
+++ src/test/java/nextstep/subway/line/LineSteps.java
@@ -1,4 +1,4 @@
-package nextstep.subway.utils;
+package nextstep.subway.line;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
