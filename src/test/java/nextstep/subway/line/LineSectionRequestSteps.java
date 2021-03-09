fab3d397d748fd3166c2b1c9d357f80c0a1f05e1
임한결 <dlagksruf19@naver.com>
2021-03-09 16:06:53 +0900

    test : 지하철 노선에 구간 등록 기능 Test 정리

--- src/test/java/nextstep/subway/line/LineSectionRequestSteps.java
+++ src/test/java/nextstep/subway/line/LineSectionRequestSteps.java
@@ -3,20 +3,27 @@ package nextstep.subway.line;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.dto.SectionRequest;
-import nextstep.subway.station.dto.StationResponse;
 import org.springframework.http.MediaType;
 
 public class LineSectionRequestSteps {
 
-    public static ExtractableResponse<Response> 지하철_노선에_지하철_역_등록_요청(long lineId, StationResponse upStation, StationResponse downStation, int distance) {
-        SectionRequest sectionRequest = new SectionRequest(upStation.getId(), downStation.getId(), distance);
+    public static LineRequest 노선_요청(String name, String color, long upStationId, long downStationId, int distance) {
+        return new LineRequest(name, color, upStationId, downStationId, distance);
+    }
+
+    public static ExtractableResponse<Response> 지하철_노선에_새로운_구간_등록_요청(LineResponse lineResponse, long upStationId, long downStationId, int distance) {
+        SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
+
         return RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(sectionRequest)
                 .when()
-                .post("/lines/{id}/sections", lineId) // TODO : URI 외부에서 주입
+                .pathParam("id", lineResponse.getId())
+                .post("/lines/{id}/sections")
                 .then().log().all()
                 .extract();
     }
--- src/test/java/nextstep/subway/line/LineSectionVerificationSteps.java
+++ src/test/java/nextstep/subway/line/LineSectionVerificationSteps.java
@@ -2,23 +2,14 @@ package nextstep.subway.line;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.station.dto.StationResponse;
 import org.springframework.http.HttpStatus;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineSectionVerificationSteps {
 
-    public static void 지하철_노선에_지하철_역_등록됨(ExtractableResponse<Response> response) {
+    public static void 지하철_노선에_새로운_구간_등록됨(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
-
-    public static void 지하철_노선에_지하철_역_포함_됨(ExtractableResponse<Response> response, List<StationResponse> stationResponses) {
-        LineResponse lineResponse = response.as(LineResponse.class);
-        assertThat(lineResponse.getStations()).containsExactlyElementsOf(stationResponses);
     }
 
 }
