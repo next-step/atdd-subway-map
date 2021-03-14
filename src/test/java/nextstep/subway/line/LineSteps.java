27093ccf514aae37e7cd02fa64eff7951a2a3e98
kelvin <dev.kelvinlee@gmail.com>
2021-03-15 05:28:02 +0900

    test: 지하철 노선 이름 중복 금지 기능 테스트 작성

--- src/test/java/nextstep/subway/line/LineSteps.java
+++ src/test/java/nextstep/subway/line/LineSteps.java
@@ -18,36 +18,36 @@ public class LineSteps {
 		LineRequest request = new LineRequest(name, color);
 
 		return RestAssured
-			.given().log().all()
-			.body(request)
-			.contentType(MediaType.APPLICATION_JSON_VALUE)
-			.when()
-			.post("/lines")
-			.then().log().all()
-			.extract();
+				.given().log().all()
+				.body(request)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.when()
+				.post("/lines")
+				.then().log().all()
+				.extract();
 	}
 
 	protected static ExtractableResponse<Response> 지하철_노선_수정_요청(String name, String color, Long id) {
 		LineRequest request = new LineRequest(name, color);
 
 		return RestAssured
-			.given().log().all()
-			.body(request)
-			.contentType(MediaType.APPLICATION_JSON_VALUE)
-			.when()
-			.put("/lines/{id}", id)
-			.then().log().all()
-			.extract();
+				.given().log().all()
+				.body(request)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.when()
+				.put("/lines/{id}", id)
+				.then().log().all()
+				.extract();
 	}
 
 	protected static ExtractableResponse<Response> 지하철_노선_제거_요청(Long id) {
 
 		return RestAssured
-			.given().log().all()
-			.when()
-			.delete("/lines/{id}", id)
-			.then().log().all()
-			.extract();
+				.given().log().all()
+				.when()
+				.delete("/lines/{id}", id)
+				.then().log().all()
+				.extract();
 	}
 
 	protected static Long 지하철_노선_등록되어_있음(String name, String color) {
@@ -58,30 +58,30 @@ public class LineSteps {
 	protected static ExtractableResponse<Response> 지하철_노선_목록_조회_요청() {
 
 		return RestAssured
-			.given().log().all()
-			.when()
-			.get("/lines")
-			.then().log().all()
-			.extract();
+				.given().log().all()
+				.when()
+				.get("/lines")
+				.then().log().all()
+				.extract();
 
 	}
 
 	protected static List<Long> 지하철_노선_목록_포함됨(ExtractableResponse<Response> response) {
 
 		return response.jsonPath().getList(".", LineResponse.class)
-			.stream()
-			.map(LineResponse::getId)
-			.collect(Collectors.toList());
+				.stream()
+				.map(LineResponse::getId)
+				.collect(Collectors.toList());
 	}
 
 	protected static ExtractableResponse<Response> 지하철_노선_조회_요청(long id) {
 
 		return RestAssured
-			.given().log().all()
-			.when()
-			.get("/lines/{id}", id)
-			.then().log().all()
-			.extract();
+				.given().log().all()
+				.when()
+				.get("/lines/{id}", id)
+				.then().log().all()
+				.extract();
 
 	}
 
@@ -114,5 +114,9 @@ public class LineSteps {
 
 	protected static void 지하철_노선_삭제됨(ExtractableResponse<Response> response) {
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+	}
+
+	protected static void 지하철_노선_생성_실패됨(ExtractableResponse<Response> response) {
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
 	}
 }
