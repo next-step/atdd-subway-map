86e058de5e08082e6f31993b62d1f4b25dba68b0
darkant99 <darkant99@naver.com>
2022-01-21 00:11:04 +0900

    [FEAT] 지하철 노선 목록 조회

--- src/main/java/nextstep/subway/applicaion/LineService.java
+++ src/main/java/nextstep/subway/applicaion/LineService.java
@@ -1,29 +1,37 @@
 package nextstep.subway.applicaion;
 
-import nextstep.subway.applicaion.dto.LineRequest;
-import nextstep.subway.applicaion.dto.LineResponse;
-import nextstep.subway.domain.Line;
-import nextstep.subway.domain.LineRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import nextstep.subway.applicaion.dto.LineRequest;
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.applicaion.responseconverter.LineResponseConverter;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
 
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+    private final LineRepository lineRepository;
+    private final LineResponseConverter lineResponseConverter;
 
-    public LineService(LineRepository lineRepository) {
+    public LineService(LineRepository lineRepository, LineResponseConverter lineResponseConverter) {
         this.lineRepository = lineRepository;
+        this.lineResponseConverter = lineResponseConverter;
     }
 
     public LineResponse saveLine(LineRequest request) {
         Line line = lineRepository.save(new Line(request.getName(), request.getColor()));
-        return new LineResponse(
-                line.getId(),
-                line.getName(),
-                line.getColor(),
-                line.getCreatedDate(),
-                line.getModifiedDate()
-        );
+        return lineResponseConverter.toResponse(line);
+    }
+
+    public List<LineResponse> findAllLines() {
+        return lineRepository.findAll()
+                             .stream()
+                             .map(lineResponseConverter::toResponse)
+                             .collect(Collectors.toList());
     }
 }
--- src/main/java/nextstep/subway/ui/LineController.java
+++ src/main/java/nextstep/subway/ui/LineController.java
@@ -1,15 +1,18 @@
 package nextstep.subway.ui;
 
-import nextstep.subway.applicaion.LineService;
-import nextstep.subway.applicaion.dto.LineRequest;
-import nextstep.subway.applicaion.dto.LineResponse;
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
+import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.LineRequest;
+import nextstep.subway.applicaion.dto.LineResponse;
 
 @RestController
 @RequestMapping("/lines")
@@ -24,5 +27,11 @@ public class LineController {
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LineResponse>> getLines() {
+        List<LineResponse> responses = lineService.findAllLines();
+        return ResponseEntity.ok().body(responses);
     }
 }
