55a352923e2eb57656bb0361546178763c561dd0
00hongjun <00hongjun@gmail.com>
2022-01-22 01:23:29 +0900

    <test> 지하철 노선 업데이트 테스트 코드 추가

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -100,6 +100,48 @@ class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선 수정")
     @Test
     void updateLine() {
+        // given
+        Map<String, String> shinbundangLine = createShinbundangLine();
+        callCreateLines(shinbundangLine);
+
+        Map<String, String> param = new HashMap<>();
+        param.put("id", "1");
+        param.put("name", "구분당선");
+        param.put("color", "blue");
+
+        // when
+        callUpdateLines(param);
+
+        // then
+        ExtractableResponse<Response> response = callGetLines();
+        List<String> lineNames = response.jsonPath()
+            .getList(".", ShowLineResponse.class)
+            .stream()
+            .map(ShowLineResponse::getLineName)
+            .collect(toList());
+
+        assertThat(lineNames).contains("구분당선");
+        assertThat(lineNames).doesNotContain(SHINBUNDANG_NAME);
+    }
+
+    /**
+     * When 없는 지하철 노선의 정보 수정을 요청 하면
+     * Then 400 응답
+     */
+    @DisplayName("지하철 노선 수정 요청 시 노선을 못 찾으면 400응답 처리")
+    @Test
+    void updateLine_fail() {
+        // given
+        Map<String, String> param = new HashMap<>();
+        param.put("id", "1");
+        param.put("name", "구분당선");
+        param.put("color", "blue");
+
+        // when
+        ExtractableResponse<Response> response = callUpdateLines(param);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     /**
@@ -133,6 +175,20 @@ class LineAcceptanceTest extends AcceptanceTest {
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .when()
             .get("lines")
+            .then()
+            .log()
+            .all()
+            .extract();
+    }
+
+    private ExtractableResponse<Response> callUpdateLines(Map<String, String> lineParams) {
+        return RestAssured.given()
+            .log()
+            .all()
+            .body(lineParams)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .put("lines/" + lineParams.get("id"))
             .then()
             .log()
             .all()
