1fa8776f717f02633cf8bc0cb609bfe2ca28e2f2
mj950425 <minjoon1995@naver.com>
2022-01-21 16:35:28 +0900

    add : Line 전체, 개별 조회 기능 controller add

--- src/main/java/nextstep/subway/ui/LineController.java
+++ src/main/java/nextstep/subway/ui/LineController.java
@@ -3,13 +3,12 @@ package nextstep.subway.ui;
 import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.dto.LineRequest;
 import nextstep.subway.applicaion.dto.LineResponse;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lines")
@@ -24,5 +23,17 @@ public class LineController {
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LineResponse>> getLines() {
+        List<LineResponse> lines = lineService.getLines();
+        return ResponseEntity.ok().body(lines);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LineResponse> getLine(@RequestParam Long id) {
+        LineResponse line = lineService.getLine(id);
+        return ResponseEntity.ok().body(line);
     }
 }
