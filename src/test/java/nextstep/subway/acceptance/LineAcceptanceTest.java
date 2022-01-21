687e7c7b4a82dc8c0bfe25348d8d1617c83eafc6
abcdsds <choiyurim2@naver.com>
2022-01-21 22:47:05 +0900

    test: 지하철 노선 생성

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,7 +1,18 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관리 기능")
 class LineAcceptanceTest extends AcceptanceTest {
@@ -12,6 +23,36 @@ class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선 생성")
     @Test
     void createLine() {
+        // given
+        String bgRed600 = "bg-red-600";
+        String 신분당선 = "신분당선";
+        Map<String, String> params = new HashMap<>();
+        params.put("color", bgRed600);
+        params.put("name", 신분당선);
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        Integer id = response.jsonPath().get("id");
+        String name = response.jsonPath().get("name");
+        String color = response.jsonPath().get("color");
+        String createdDate = response.jsonPath().get("createdDate");
+        String modifiedDate = response.jsonPath().get("modifiedDate");
+
+        assertThat(id).isNotNull();
+        assertThat(name).isEqualTo(신분당선);
+        assertThat(color).isEqualTo(bgRed600);
+        assertThat(createdDate).isNotNull();
+        assertThat(modifiedDate).isNotNull();
+
     }
 
     /**
