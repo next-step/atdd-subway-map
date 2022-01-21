e7bd424bf5afaee74ff831a21de7a8f546a31a65
hiro <kiryuinh32@gmail.com>
2022-01-21 11:48:34 +0900

    feat: Stations 클래스 구현

---
+++ src/main/java/nextstep/subway/domain/Stations.java
@@ -0,0 +1,20 @@
+package nextstep.subway.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
+public class Stations {
+    @OneToMany(cascade = CascadeType.PERSIST)
+    private List<Station> stations = new ArrayList<>();
+
+    protected Stations() {
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+}
