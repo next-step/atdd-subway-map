bde56679989923156e9aa06225084c28551beb8b
povia <47127607+povia@users.noreply.github.com>
2022-01-21 23:23:09 +0900

    step1 feat: 라인 생성 테스트 추가

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,14 +1,46 @@
 package nextstep.subway.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @DisplayName("지하철 노선 관리 기능")
 class LineAcceptanceTest extends AcceptanceTest {
     /** When 지하철 노선 생성을 요청 하면 Then 지하철 노선 생성이 성공한다. */
     @DisplayName("지하철 노선 생성")
     @Test
-    void createLine() {}
+    void createLine() {
+        // given
+        Map<String, String> lineRequest = new HashMap<>();
+        lineRequest.put("name", "신분당선");
+        lineRequest.put("color", "red");
+
+        // when
+        ExtractableResponse<Response> response =
+                RestAssured.given()
+                        .log()
+                        .all()
+                        .body(lineRequest)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when()
+                        .post("/lines")
+                        .then()
+                        .log()
+                        .all()
+                        .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
+    }
 
     /**
      * Given 지하철 노선 생성을 요청 하고 Given 새로운 지하철 노선 생성을 요청 하고 When 지하철 노선 목록 조회를 요청 하면 Then 두 노선이 포함된 지하철
