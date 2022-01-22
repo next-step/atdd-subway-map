a5a145fba5d68b979a88afcb95629060ce64f770
mandykr <hongseokii92@gmail.com>
2022-01-22 19:45:05 +0900

    feat: 지하철역 이름 중복체크 기능 구현

--- src/main/java/nextstep/subway/applicaion/StationService.java
+++ src/main/java/nextstep/subway/applicaion/StationService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,6 +21,8 @@ public class StationService {
     }
 
     public StationResponse saveStation(StationRequest stationRequest) {
+        checkDuplicated(stationRequest.getName());
+
         Station station = stationRepository.save(new Station(stationRequest.getName()));
         return createStationResponse(station);
     }
@@ -45,4 +48,12 @@ public class StationService {
                 station.getModifiedDate()
         );
     }
+
+    private void checkDuplicated(String name) {
+        Optional<Station> findStation = stationRepository.findByName(name);
+        if (findStation.isPresent()) {
+            throw new RuntimeException("해당 지하철역이 이미 존재합니다.");
+        }
+    }
+
 }
