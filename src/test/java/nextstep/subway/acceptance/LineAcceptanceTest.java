1184da33d8686caee2dc17e9f44e018e2693bba0
YSK <zerowest1001@gmail.com>
2022-01-22 19:10:11 +0900

    feat: 지하철 노선 관리 기능

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,7 +1,14 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관리 기능")
 class LineAcceptanceTest extends AcceptanceTest {
@@ -12,6 +19,23 @@ class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선 생성")
     @Test
     void createLine() {
+        // given
+        var params = new HashMap<String, String>();
+        params.put("color", "bg-red-600");
+        params.put("name", "신분당선");
+
+        // when
+        var response = RestAssured.given().log().all()
+                .body(params)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
     }
 
     /**
@@ -23,6 +47,42 @@ class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선 목록 조회")
     @Test
     void getLines() {
+        // given
+        var params1 = new HashMap<String, String>();
+        params1.put("color", "bg-red-600");
+        params1.put("name", "신분당선");
+        var createResponse1 = RestAssured.given().log().all()
+                .body(params1)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        var params2 = new HashMap<String, String>();
+        params2.put("color", "bg-green-600");
+        params2.put("name", "2호선");
+
+        var createResponse2 = RestAssured.given().log().all()
+                .body(params2)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        // when
+        var response = RestAssured.given().log().all()
+                .accept(ContentType.JSON)
+                .when()
+                .get("/lines")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        var lineNames = response.jsonPath().getList("name");
+        assertThat(lineNames).contains(params1.get("name"), params2.get("name"));
     }
 
     /**
@@ -33,6 +93,31 @@ class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선 조회")
     @Test
     void getLine() {
+        // given
+        var params = new HashMap<String, String>();
+        params.put("color", "bg-red-600");
+        params.put("name", "신분당선");
+
+        var createResponse = RestAssured.given().log().all()
+                .body(params)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        // when
+        var uri = createResponse.header("Location");
+        var response = RestAssured.given().log().all()
+                .accept(ContentType.JSON)
+                .when()
+                .get(uri)
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
     }
 
     /**
@@ -43,6 +128,37 @@ class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선 수정")
     @Test
     void updateLine() {
+        // given
+        var params = new HashMap<String, String>();
+        params.put("color", "bg-red-600");
+        params.put("name", "신분당선");
+
+        var createResponse = RestAssured.given().log().all()
+                .body(params)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        // when
+        var modifyParams = new HashMap<String, String>();
+        modifyParams.put("color", "bg-blue-600");
+        modifyParams.put("name", "구분당선");
+
+        var uri = createResponse.header("Location");
+        var response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(modifyParams)
+                .when()
+                .put(uri)
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        var lineName = response.jsonPath().getString("name");
+        assertThat(lineName).isEqualTo(modifyParams.get("name"));
     }
 
     /**
@@ -53,5 +169,28 @@ class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선 삭제")
     @Test
     void deleteLine() {
+        // given
+        var params = new HashMap<String, String>();
+        params.put("color", "bg-red-600");
+        params.put("name", "신분당선");
+
+        var createResponse = RestAssured.given().log().all()
+                .body(params)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        // when
+        var uri = createResponse.header("Location");
+        var response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .delete(uri)
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 }
