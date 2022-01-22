b4779f24035248d43c4a391115354b2292b48097
lehdqlsl <lehdqlsl@naver.com>
2022-01-22 20:15:49 +0900

    fix: createLineResponse 메소드 서비스에서 제거

--- src/main/java/nextstep/subway/applicaion/LineService.java
+++ src/main/java/nextstep/subway/applicaion/LineService.java
@@ -21,38 +21,28 @@ public class LineService {
 
     public LineResponse saveLine(LineRequest request) {
         Line line = lineRepository.save(new Line(request.getName(), request.getColor()));
-        return createLineResponse(line);
+        return LineResponse.of(line);
     }
 
     public List<LineResponse> findAllLines() {
         return lineRepository.findAll().stream()
-                .map(this::createLineResponse)
+                .map(LineResponse::of)
                 .collect(Collectors.toList());
     }
 
     public LineResponse findLine(Long id) {
         Line line = lineRepository.findById(id).orElseThrow(() -> new RuntimeException());
-        return createLineResponse(line);
+        return LineResponse.of(line);
     }
 
     public LineResponse modifyLine(Long id, LineRequest lineRequest) {
         Line line = lineRequest.toEntity(id);
         Line modifiedLine = lineRepository.save(line);
-        return createLineResponse(modifiedLine);
+        return LineResponse.of(modifiedLine);
     }
 
     public void deleteLineById(Long id) {
         lineRepository.deleteById(id);
-    }
-
-    private LineResponse createLineResponse(Line line) {
-        return new LineResponse(
-                line.getId(),
-                line.getName(),
-                line.getColor(),
-                line.getCreatedDate(),
-                line.getModifiedDate()
-        );
     }
 
 }
