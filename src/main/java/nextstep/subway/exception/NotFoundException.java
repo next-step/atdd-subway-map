3a6cd3ace946405aaaf2ba11fae785dfe6a94a32
povia <47127607+povia@users.noreply.github.com>
2022-01-23 00:33:04 +0900

    step2 feat: exception 추가

--- src/main/java/nextstep/subway/exception/NotFoundException.java
+++ src/main/java/nextstep/subway/exception/NotFoundException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
-        super("Not Found");
+        super(Messages.NOT_FOUND.message());
     }
 }
