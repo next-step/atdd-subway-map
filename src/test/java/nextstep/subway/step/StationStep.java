9af1bb668403052eeab7fd54849b351547f73032
minyul <ggomjae@gmail.com>
2022-01-22 19:56:12 +0900

    refactor : Station 중복 코드를 StationStep 으로 옮기다.

---
+++ src/test/java/nextstep/subway/step/StationStep.java
@@ -0,0 +1,48 @@
+package nextstep.subway.step;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StationStep {
+
+	public static ExtractableResponse<Response> saveStation(final String name) {
+		Map<String, String> request = request(name);
+
+		return RestAssured
+						.given().body(request).contentType(MediaType.APPLICATION_JSON_VALUE).log().all()
+						.when().post("/stations")
+						.then().log().all()
+						.extract();
+	}
+
+	public static ExtractableResponse<Response> showStation() {
+
+		return RestAssured.given().log().all()
+						.when()
+						.get("/stations")
+						.then().log().all()
+						.extract();
+	}
+
+	public static ExtractableResponse<Response> deleteStation(final String uri) {
+
+		return RestAssured.given().log().all()
+						.when()
+						.delete(uri)
+						.then().log().all()
+						.extract();
+	}
+
+	// 요청을 생성하는 메소드
+	public static Map<String, String> request(final String name) {
+		Map<String, String> request = new HashMap<>();
+		request.put("name", name);
+
+		return request;
+	}
+}
