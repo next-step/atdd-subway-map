eb2d573fd7f5b92b85431246f84118a933cd6e12
monkeyDugi <lbd4946@gmail.com>
2022-01-22 19:33:51 +0900

    feat : 리뷰 요청 테스트

--- src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
@@ -7,6 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+/*
+* 테스트를 시 데이터 베이스 초기화를 위해 존재하는 클래스
+* 직접 *Repository -> deleteAll()과 같이 해도 되지만, 이건 리포지토리가 변경 되면 같이 변경되기 때문에
+* 사용하지 않는다. 물론 결국 DatabaseCleanup이 JPA에 의존하긴 하지만 특정 리포지토리에 의존하지는 않기 때문에
+* 이 과정에서는 이렇게 한다.
+* */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
     @LocalServerPort
