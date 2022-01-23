b23e83bef8526126d6aec2f4e60a83f8cd567a5a
hiro <kiryuinh32@gmail.com>
2022-01-23 15:07:36 +0900

    refactor: LineAcceptanceTest 중복 제거
    문자열을 상수로 추출해 중복 제거

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -17,17 +17,22 @@ import org.springframework.http.HttpStatus;
 @DisplayName("지하철 노선 관리 기능")
 class LineAcceptanceTest extends AcceptanceTest {
 
+    private static final String DEFAULT_PATH = "/lines";
+    private static final String DEFAULT_NAME_KEY = "name";
+    private static final String DEFAULT_NAME_VALUE = "신분당선";
+    private static final String DEFAULT_COLOR_KEY = "color";
+    private static final String DEFAULT_COLOR_VALUE = "bg-red-600";
+
     /**
      * When 지하철 노선 생성을 요청 하면 Then 지하철 노선 생성이 성공한다.
      */
     @DisplayName("지하철 노선 생성")
     @Test
     void createLine() {
-        RequestParams params = new RequestParams("name", "신분당선");
-        params.addParams("color", "bg-red-600");
+        RequestParams params = createDefaultParams();
 
         // when
-        ExtractableResponse<Response> response = RequestMethod.post("/lines", params);
+        ExtractableResponse<Response> response = RequestMethod.post(DEFAULT_PATH, params);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
@@ -41,22 +46,22 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void getLines() {
         //given
-        RequestParams params1 = new RequestParams("name", "신분당선");
-        params1.addParams("color", "bg-red-600");
+        RequestParams params1 = createDefaultParams();
 
         RequestParams params2 = new RequestParams("name", "2호선");
         params2.addParams("color", "bg-green-600");
 
-        RequestMethod.post("/lines", params1);
-        RequestMethod.post("/lines", params2);
+        RequestMethod.post(DEFAULT_PATH, params1);
+        RequestMethod.post(DEFAULT_PATH, params2);
 
         //when
-        ExtractableResponse<Response> response = RequestMethod.get("/lines");
+        ExtractableResponse<Response> response = RequestMethod.get(DEFAULT_PATH);
 
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.jsonPath().getList("name")).contains("신분당선", "2호선");
-        assertThat(response.jsonPath().getList("color")).contains("bg-green-600", "bg-red-600");
+        assertThat(response.jsonPath().getList(DEFAULT_NAME_KEY)).contains("신분당선", "2호선");
+        assertThat(response.jsonPath().getList(DEFAULT_COLOR_KEY)).contains("bg-green-600",
+            "bg-red-600");
     }
 
     /**
@@ -66,10 +71,9 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void getLine() {
         //given
-        RequestParams params = new RequestParams("name", "신분당선");
-        params.addParams("color", "bg-red-600");
+        RequestParams params = createDefaultParams();
 
-        ExtractableResponse<Response> createResponse = RequestMethod.post("/lines", params);
+        ExtractableResponse<Response> createResponse = RequestMethod.post(DEFAULT_PATH, params);
 
         // when
         ExtractableResponse<Response> response = RequestMethod.get(
@@ -87,11 +91,10 @@ class LineAcceptanceTest extends AcceptanceTest {
     @MethodSource
     void updateLine(RequestParams params) {
         //given
-        ExtractableResponse<Response> createResponse = RequestMethod.post("/lines", params);
+        ExtractableResponse<Response> createResponse = RequestMethod.post(DEFAULT_PATH, params);
 
         // when
-        RequestParams changeParam = new RequestParams("name", "구분당선");
-        changeParam.addParams("color", "bg-blue-600");
+        RequestParams changeParam = createDefaultParams();
 
         ExtractableResponse<Response> response = RequestMethod.put(
             "/lines/" + createResponse.jsonPath().getString("id"), changeParam);
@@ -101,8 +104,7 @@ class LineAcceptanceTest extends AcceptanceTest {
     }
 
     static Stream<Arguments> updateLine() {
-        RequestParams params = new RequestParams("name", "신분당선");
-        params.addParams("color", "bg-red-600");
+        RequestParams params = createDefaultParams();
         return Stream.of(
             Arguments.of(
                 params
@@ -117,10 +119,9 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void deleteLine() {
         //given
-        RequestParams params = new RequestParams("name", "신분당선");
-        params.addParams("color", "bg-red-600");
+        RequestParams params = createDefaultParams();
 
-        ExtractableResponse<Response> createResponse = RequestMethod.post("/lines", params);
+        ExtractableResponse<Response> createResponse = RequestMethod.post(DEFAULT_PATH, params);
 
         // when
         ExtractableResponse<Response> response = RequestMethod.delete(
@@ -131,22 +132,26 @@ class LineAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     *     Given 지하철 노선 생성을 요청 하고
-     *     When 같은 이름으로 지하철 노선 생성을 요청 하면
-     *     Then 지하철 노선 생성이 실패한다.
+     * Given 지하철 노선 생성을 요청 하고 When 같은 이름으로 지하철 노선 생성을 요청 하면 Then 지하철 노선 생성이 실패한다.
      */
     @Test
     @DisplayName("중복이름으로 지하철 노선 생성 실패")
     void duplicationLineNameExceptionTest() {
         // given
-        RequestParams params = new RequestParams("name", "신분당선");
-        params.addParams("color", "bg-red-600");
+        RequestParams params = createDefaultParams();
 
-        RequestMethod.post("/lines", params);
+        RequestMethod.post(DEFAULT_PATH, params);
         // when
-        ExtractableResponse<Response> response = RequestMethod.post("/lines", params);
+        ExtractableResponse<Response> response = RequestMethod.post(DEFAULT_PATH, params);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
+    }
+
+    private static RequestParams createDefaultParams() {
+        RequestParams params = new RequestParams(DEFAULT_NAME_KEY, DEFAULT_NAME_VALUE);
+        params.addParams(DEFAULT_COLOR_KEY, DEFAULT_COLOR_VALUE);
+
+        return params;
     }
 }
