b0f9cd903a680d1bd63ba5b171184e4bcffc6624
skid901 <tlrudejr1993@gmail.com>
2022-01-23 21:58:55 +0900

    test: verify endpoint to create line

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,17 +1,47 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("지하철 노선 관리 기능")
 class LineAcceptanceTest extends AcceptanceTest {
     /**
      * When 지하철 노선 생성을 요청 하면
-     * Then 지하철 노선 생성이 성공한다.
+     * Then 지하철 노선 생성이 성공 한다.
      */
     @DisplayName("지하철 노선 생성")
     @Test
     void createLine() {
+        // given
+        final Map<String, String> requestParams = new HashMap<>();
+        requestParams.put("color", "bg-red-600");
+        requestParams.put("name", "신분당선");
+
+        // when
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(requestParams)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(response.header("Location")).isNotBlank()
+        );
     }
 
     /**
