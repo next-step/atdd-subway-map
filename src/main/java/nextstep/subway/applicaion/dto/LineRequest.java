0d593e00b5aaea6db2868c709595e9f17c56f2bc
MrPPak <matht1115@naver.com>
2022-01-23 22:28:30 +0900

    feat: 지하철 노선, 지하철역 중복이름 생성방지 코드 구현 및 테스트 완료

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -1,14 +1,19 @@
 package nextstep.subway.applicaion.dto;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LineRequest {
     private String name;
     private String color;
 
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
+    @Builder
+    private LineRequest(String name, String color) {
+        this.name = name;
+        this.color = color;
     }
 }
--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -3,7 +3,6 @@ package nextstep.subway.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import nextstep.subway.applicaion.dto.LineResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -13,7 +12,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static nextstep.subway.acceptance.steps.LineSteps.*;
+import static nextstep.subway.acceptance.steps.LineSteps.지하철노선_단건조회;
+import static nextstep.subway.acceptance.steps.LineSteps.지하철노선_생성요청;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -48,6 +48,25 @@ class LineAcceptanceTest extends AcceptanceTest {
 
     /**
      * Given 지하철 노선 생성을 요청 하고
+     * When 같은 이름으로 지하철 노선 생성을 요청 하면
+     * Then 지하철 노선 생성이 실패한다.
+     */
+    @DisplayName("중복이름으로 지하철 노선 생성")
+    @Test
+    void duplicateLineName() {
+        // given
+        지하철노선_생성요청("2호선", "bg-green");
+
+        // when
+        ExtractableResponse<Response> response = 지하철노선_생성요청("2호선", "bg-green-200");
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        assertThat(jsonPath("$.exceptionMessage", is("중복된 이름입니다.")));
+    }
+
+    /**
+     * Given 지하철 노선 생성을 요청 하고
      * Given 새로운 지하철 노선 생성을 요청 하고
      * When 지하철 노선 목록 조회를 요청 하면
      * Then 두 노선이 포함된 지하철 노선 목록을 응답받는다
@@ -67,16 +86,7 @@ class LineAcceptanceTest extends AcceptanceTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        List<String> lineNames = response.jsonPath().getList("name");
-        List<Integer> lineIds = response.jsonPath().getList("id");
-
-        /*
-           목록을 조회할 때 PK까지만 검증을 하면 되는건지 (75번 라인만 검증해도 되는건지)
-           아니면 이름도 중요한 거 같으니 이름까지 검증을 하면 되는건지 리뷰어님의 의견이 궁금합니다.
-           저의 경우는 지하철 라인의 경우 이름도 중요한 거 같아 이름까지 검증해야 한다고 생각합니다.
-         */
-        assertThat(lineIds).contains(1, 2);
-        assertThat(lineNames).contains("2호선", "3호선");
+        assertThat(response.jsonPath().getList("id").size()).isEqualTo(2);
     }
 
     /**
@@ -112,15 +122,8 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void updateLine() {
         // given
-        /*
-            지하철노선_생성요청의 경우 리턴타입이 ExtractableResponse<Response>인데 이경우
-            Long lineId = createLineResponse.jsonPath().getLong("id"); 이렇게 코딩하여 불편하였습니다.
-
-            그래서 지하철노선_생성요청2의 경우 리턴타입을 LineResponse로 하여
-            Long lineId = createLineResponse.getId(); 이렇게 간단하게 바꾸게 하였는데 의견이 궁금합니다.
-         */
-        LineResponse createLineResponse = 지하철노선_생성요청2("2호선", "bg-green");
-        Long lineId = createLineResponse.getId();
+        ExtractableResponse<Response> createLineResponse = 지하철노선_생성요청("2호선", "bg-green");
+        Long lineId = createLineResponse.jsonPath().getLong("id");
 
         Map<String, String> params = new HashMap<>();
         params.put("name", "신1호선");
