b5a75ce71728954444bd42450c2184778ddf273d
dk-ko <dev.dkko@gmail.com>
2022-01-23 20:15:59 +0900

    feat: 지하철 노선 목록 조회 기능, 지하철 노선생성 테스트

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -4,6 +4,12 @@ public class LineRequest {
     private String name;
     private String color;
 
+
+    public LineRequest(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
     public String getName() {
         return name;
     }
--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,10 +1,23 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.applicaion.dto.LineRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static nextstep.subway.utils.ResponseUtils.*;
 
 @DisplayName("지하철 노선 관리 기능")
 class LineAcceptanceTest extends AcceptanceTest {
+
     /**
      * When 지하철 노선 생성을 요청 하면
      * Then 지하철 노선 생성이 성공한다.
@@ -12,6 +25,20 @@ class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선 생성")
     @Test
     void createLine() {
+        Map<String, Object> param = new HashMap<>();
+        param.put("name", "1호선");
+        param.put("color", "blue darken-4");
+
+        final ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .body(param)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when()
+                        .post("/lines")
+                        .then().log().all()
+                        .extract();
+
+        httpStatus가_CREATED면서_Location이_존재함(response);
     }
 
     /**
@@ -23,6 +50,34 @@ class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선 목록 조회")
     @Test
     void getLines() {
+        LineRequest lineRequest1 = new LineRequest("1호선", "blue darken-4");
+        LineRequest lineRequest2 = new LineRequest("7호선", "green darken-3");
+
+        RestAssured.given().log().all()
+                .body(lineRequest1)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        RestAssured.given().log().all()
+                .body(lineRequest2)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .get("/lines")
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("name")).contains("1호선");
+        assertThat(response.jsonPath().getList("name")).contains("7호선");
     }
 
     /**
