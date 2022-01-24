1313f4aa5055c8023c80d6f0dd1d957743f1411d
kimuno89 <kimunoya@gmail.com>
2022-01-25 00:58:25 +0900

    test: 상행역 검증 테스트 코드 추가

---
+++ src/test/java/nextstep/subway/domain/LineTest.java
@@ -0,0 +1,28 @@
+package nextstep.subway.domain;
+
+import nextstep.subway.applicaion.Section;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LineTest {
+    @DisplayName("새로운 구간의 상행역은 현재 등록되어있는 하행 종점역이어야 한다.")
+    @Test
+    void validateUpStation() {
+        // given
+        Station station1 = new Station("강남역");
+        Station station2 = new Station("역삼역");
+        Station station3 = new Station("선릉역");
+        Section section = new Section(station1, station2, 10);
+        Line line = new Line("2호선", "bg-green-600");
+
+        line.addSection(section);
+
+        // when
+        Section newSection = new Section(station2, station3, 5);
+
+        // then
+        assertThat(line.validateUpStation(newSection.getUpStation())).isEqualTo(true);
+    }
+}
