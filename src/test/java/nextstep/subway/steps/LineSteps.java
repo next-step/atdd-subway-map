5c3b50551d87c21d76842b0a3c6f91a88abaac94
JoJo <buyongjo@gmail.com>
2022-01-25 08:36:37 +0900

    test: 지하철 노선 관리 요청 메서드 분리

---
+++ src/test/java/nextstep/subway/steps/LineSteps.java
@@ -0,0 +1,63 @@
+package nextstep.subway.steps;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LineSteps {
+    public static ExtractableResponse<Response> executeLineListGetRequest() {
+        return RestAssured.given()
+                .when()
+                .get("/lines")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> executeLineGetRequest(Long id) {
+        return RestAssured.given()
+                .when()
+                .get("/lines/{id}", id)
+                .then()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> executeLineCreateRequest(String name, String color) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+
+        return RestAssured.given()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> executeLineUpdateRequest(Long id, String name, String color) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+
+        return RestAssured.given()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .put("/lines/{id}", id)
+                .then()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> executeLineDeleteRequest(Long id) {
+        return RestAssured.given()
+                .when()
+                .delete("/lines/{id}", id)
+                .then()
+                .extract();
+    }
+}
