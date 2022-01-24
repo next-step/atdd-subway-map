e5bcd3ae34ce532bf1bd02fd87a5b8d23d46c077
skid901 <tlrudejr1993@gmail.com>
2022-01-25 00:03:54 +0900

    refactor: extract class for line CRUD request

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,17 +1,13 @@
 package nextstep.subway.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import static nextstep.subway.acceptance.LineSteps.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -47,7 +43,6 @@ class LineAcceptanceTest extends AcceptanceTest {
         // given
         final String name1 = "신분당선";
         final String name2 = "2호선";
-
         지하철_노선_생성_요청(name1, "bg-red-600");
         지하철_노선_생성_요청(name2, "bg-green-600");
 
@@ -130,53 +125,5 @@ class LineAcceptanceTest extends AcceptanceTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-    }
-
-    private ExtractableResponse<Response> 지하철_노선_생성_요청(final String name, final String color) {
-        final Map<String, String> requestParams = new HashMap<>();
-        requestParams.put("name", name);
-        requestParams.put("color", color);
-
-        return RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(requestParams)
-                .when()
-                .post("/lines")
-                .then().log().all()
-                .extract();
-    }
-
-    private ExtractableResponse<Response> 지하철_노선_목록_조회_요청() {
-        return 지하철_노선_조회_요청("/lines");
-    }
-
-    private ExtractableResponse<Response> 지하철_노선_조회_요청(final String path) {
-        return RestAssured.given().log().all()
-                .when()
-                .get(path)
-                .then().log().all()
-                .extract();
-    }
-
-    private ExtractableResponse<Response> 지하철_노선_수정_요청(final String path, final String name, final String color) {
-        final Map<String, String> requestParams = new HashMap<>();
-        requestParams.put("name", name);
-        requestParams.put("color", color);
-
-        return RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(requestParams)
-                .when()
-                .put(path)
-                .then().log().all()
-                .extract();
-    }
-
-    private ExtractableResponse<Response> 지하철_노선_삭제_요청(final String path) {
-        return RestAssured.given().log().all()
-                .when()
-                .delete(path)
-                .then().log().all()
-                .extract();
     }
 }
