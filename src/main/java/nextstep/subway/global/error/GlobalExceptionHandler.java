c2f2ea34a017b983973b518ce7bfd23829fe8aec
hiro <kiryuinh32@gmail.com>
2022-01-24 13:25:31 +0900

    rename: class name 오타 수정

--- src/main/java/nextstep/subway/global/error/GlobalExceptionHandler.java
+++ src/main/java/nextstep/subway/global/error/GlobalExceptionHandler.java
@@ -4,30 +4,29 @@ import nextstep.subway.exception.LineNameDuplicationException;
 import nextstep.subway.exception.NotFoundLineException;
 import nextstep.subway.exception.StationNameDuplicationException;
 import nextstep.subway.global.error.exception.ErrorCode;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlocalExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(StationNameDuplicationException.class)
     public ResponseEntity<ErrorResponse> handleStationNameDuplicationException() {
         ErrorResponse response = ErrorResponse.of(ErrorCode.DUPLICATION_STATION_NAME);
-        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(response, response.getStatus());
     }
 
     @ExceptionHandler(LineNameDuplicationException.class)
     public ResponseEntity<ErrorResponse> handleLinenNameDuplicationException() {
         ErrorResponse response = ErrorResponse.of(ErrorCode.DUPLICATION_LINE_NAME);
-        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(response, response.getStatus());
     }
 
     @ExceptionHandler(NotFoundLineException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundLineException() {
         ErrorResponse response = ErrorResponse.of(ErrorCode.NOT_FOUND_LINE);
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(response, response.getStatus());
     }
 
 }
