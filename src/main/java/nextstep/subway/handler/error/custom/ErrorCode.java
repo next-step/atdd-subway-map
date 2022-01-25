9a12158a3eba995580f989857a377537aa750972
mac_daehwan2yo <daehwan7989@naver.com>
2022-01-25 15:08:30 +0900

    feat : 역 중복 이름 검증, success

--- src/main/java/nextstep/subway/handler/error/custom/ErrorCode.java
+++ src/main/java/nextstep/subway/handler/error/custom/ErrorCode.java
@@ -1,7 +1,9 @@
 package nextstep.subway.handler.error.custom;
 
 public enum ErrorCode {
-    LINE_NOT_FOUND_BY_ID(404, "[ERROR] 해당하는 id의 노선이 존재하지 않습니다.");
+    LINE_NOT_FOUND_BY_ID(404, "[ERROR] 해당하는 id의 노선이 존재하지 않습니다."),
+    FOUND_DUPLICATED_NAME(409, "[ERROR] 중복된 이름이 존재합니다. "),
+    ;
 
     private final int status;
     private final String detail;
