330ba065d19844aa13db57c90119a4af1776ca0e
hiro <kiryuinh32@gmail.com>
2022-01-25 22:07:07 +0900

    feat: LineRequest 필드 추가 & validation 추가

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -1,9 +1,32 @@
 package nextstep.subway.applicaion.dto;
 
+import javax.validation.constraints.NotEmpty;
+
 public class LineRequest {
 
     private String name;
     private String color;
+
+    @NotEmpty
+    private Long upStationId;
+
+    @NotEmpty
+    private Long downStationId;
+
+    @NotEmpty
+    private int distance;
+
+    protected LineRequest() {
+    }
+
+    public LineRequest(String name, String color, Long upStationId, Long downStationId,
+        int distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
 
     public String getName() {
         return name;
@@ -12,4 +35,17 @@ public class LineRequest {
     public String getColor() {
         return color;
     }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
 }
