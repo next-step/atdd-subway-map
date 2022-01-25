090e93dd2ef105b8bd6b91c64a12d336d715ee0f
lehdqlsl <lehdqlsl@naver.com>
2022-01-25 19:39:26 +0900

    add: StationSteps 클래스 생성

---
+++ src/test/java/nextstep/subway/acceptance/StationSteps.java
@@ -0,0 +1,29 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StationSteps {
+    public static final String DEFAULT_NAME = "강남역";
+
+    public static ExtractableResponse<Response> 지하철역_생성() {
+        return 지하철역_생성(DEFAULT_NAME);
+    }
+
+    public static ExtractableResponse<Response> 지하철역_생성(String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+
+        return RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all().extract();
+    }
+}
