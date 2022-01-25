6c66e1c1e91a893b1a92317556f0050e7e7fef06
lehdqlsl <lehdqlsl@naver.com>
2022-01-25 22:29:42 +0900

    feature: Exception 처리

--- src/main/java/nextstep/subway/applicaion/StationService.java
+++ src/main/java/nextstep/subway/applicaion/StationService.java
@@ -4,6 +4,7 @@ import nextstep.subway.applicaion.dto.StationRequest;
 import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import nextstep.subway.exception.DuplicationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +21,12 @@ public class StationService {
     }
 
     public StationResponse saveStation(StationRequest stationRequest) {
-        Station station = stationRepository.save(new Station(stationRequest.getName()));
-        return createStationResponse(station);
+        try {
+            Station station = stationRepository.save(new Station(stationRequest.getName()));
+            return createStationResponse(station);
+        } catch (Exception exception) {
+            throw new DuplicationException(stationRequest.getName(), exception);
+        }
     }
 
     @Transactional(readOnly = true)
