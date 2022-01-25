cab6ab97b07473cd8d0335597d175eeb9533dd9c
sang5c <skah321@gmail.com>
2022-01-25 21:35:11 +0900

    refactor: /lines 경로 공통 @RequestMapping 사용

--- src/main/java/nextstep/subway/ui/LineController.java
+++ src/main/java/nextstep/subway/ui/LineController.java
@@ -10,6 +10,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/lines")
 public class LineController {
     private final LineService lineService;
 
@@ -17,31 +18,31 @@ public class LineController {
         this.lineService = lineService;
     }
 
-    @GetMapping("/lines")
+    @GetMapping
     public ResponseEntity<List<LineResponse>> getLines() {
         List<LineResponse> lines = lineService.findAll();
         return ResponseEntity.ok().body(lines);
     }
 
-    @GetMapping("/lines/{lineId}")
+    @GetMapping("/{lineId}")
     public ResponseEntity<LineResponse> getLine(@PathVariable Long lineId) {
         LineResponse line = lineService.findLine(lineId);
         return ResponseEntity.ok().body(line);
     }
 
-    @PostMapping("/lines")
+    @PostMapping
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
     }
 
-    @PutMapping("/lines/{lineId}")
+    @PutMapping("/{lineId}")
     public ResponseEntity<Void> updateLine(@PathVariable Long lineId, @RequestBody LineRequest lineRequest) {
         lineService.updateLine(lineId, lineRequest);
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/lines/{lineId}")
+    @DeleteMapping("/{lineId}")
     public ResponseEntity<Void> deleteLine(@PathVariable Long lineId) {
         lineService.deleteLine(lineId);
         return ResponseEntity.noContent().build();
