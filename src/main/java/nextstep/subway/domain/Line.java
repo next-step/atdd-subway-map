09bbb993478ddf779b455e10e0dc42647c0d4e71
mintheon <mintheon@mintheon.com>
2022-01-26 23:56:31 +0900

    feat: 지하철 노선 생성시 필요한 인자 추가 (상행선, 하행선, 거리)

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,16 +1,16 @@
 package nextstep.subway.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Line extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String name;
+
     private String color;
 
     public Line() {
