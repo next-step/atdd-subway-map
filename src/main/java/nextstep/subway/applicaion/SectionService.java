e13b574caf3783f7b890e410c506739918392a9a
minyul <ggomjae@gmail.com>
2022-01-26 21:11:13 +0900

    refactor : 읽기 쉽게 메세지에서 사용하는 메소드드를 바로 밑에 배치한다

--- src/main/java/nextstep/subway/applicaion/SectionService.java
+++ src/main/java/nextstep/subway/applicaion/SectionService.java
@@ -40,16 +40,10 @@ public class SectionService {
 		return SectionResponse.of(section.getId(), upStation.getId(), downStation.getId(), section.getDistance());
 	}
 
-	public void deleteSection(final Long lineId, final Long stationId) {
-		Line line = lineRepository.findById(lineId).orElseThrow(RuntimeException::new);
-		Station downStation = stationRepository.findById(stationId).orElseThrow(RuntimeException::new);
-		Section section = sectionRepository.findByLineAndDownStation(line, downStation).orElseThrow(RuntimeException::new);
-		sectionRepository.delete(section);
-	}
-
 	private void verifyStationsRelation(final Station upStation) {
 		Section sectionByUpStation = sectionRepository.findSectionByUpStation(upStation);
 		if (!ObjectUtils.isEmpty(sectionByUpStation)) {
+
 			throw new DuplicationException("새로운 구간의 상행역은 이미 등록되어있습니다.");
 		}
 	}
@@ -58,10 +52,19 @@ public class SectionService {
 		if (NUMBER_ZERO != sectionRepository.count()) {
 			Section sectionByDownStation = sectionRepository.findSectionByDownStation(upStation);
 			if (ObjectUtils.isEmpty(sectionByDownStation)) {
+
 				throw new RuntimeException("새로운 상행역은 무조건 하행역으로 등록되어있어야합니다.");
 			}
 		}
 	}
+
+	public void deleteSection(final Long lineId, final Long stationId) {
+		Line line = lineRepository.findById(lineId).orElseThrow(RuntimeException::new);
+		Station downStation = stationRepository.findById(stationId).orElseThrow(RuntimeException::new);
+		Section section = sectionRepository.findByLineAndDownStation(line, downStation).orElseThrow(RuntimeException::new);
+		sectionRepository.delete(section);
+	}
+
 
 	private void deleteSectionMustBeDownStation(final Long stationId) {
 
