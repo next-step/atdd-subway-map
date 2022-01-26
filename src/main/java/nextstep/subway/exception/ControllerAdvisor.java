f5acfe05e0b437d352b27344eb395a9eeeca535a
lehdqlsl <lehdqlsl@naver.com>
2022-01-26 22:42:44 +0900

    fix: Exception 처리 변경

--- src/main/java/nextstep/subway/exception/ControllerAdvisor.java
+++ src/main/java/nextstep/subway/exception/ControllerAdvisor.java
@@ -1,6 +1,5 @@
 package nextstep.subway.exception;
 
-import nextstep.subway.applicaion.dto.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,9 +8,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ControllerAdvisor {
 
-    @ExceptionHandler(value = DuplicationException.class)
-    public ResponseEntity<ExceptionResponse> exception(DuplicationException exception) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), exception.getCause().getMessage());
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.CONFLICT);
+    @ExceptionHandler(value = LogicException.class)
+    public ResponseEntity<String> exception(LogicException exception) {
+        return new ResponseEntity<>(exception.getLogicError().getErrMsg(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/nextstep/subway/exception/DuplicationException.java
+++
@@ -1,9 +0,0 @@
-package nextstep.subway.exception;
-
-public class DuplicationException extends RuntimeException {
-
-    public DuplicationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
