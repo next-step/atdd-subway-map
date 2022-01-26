2bb39c4700773c28e322b37b5219c159c2d8aaa5
soo <yunsoo0510@daum.net>
2022-01-27 00:30:52 +0900

    [Test] 구간 제거 기능: 인수테스트 작성(비정상적인 시나리오2)
    - 구간 1개일 때 구간 제거 요청

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -277,11 +277,31 @@ class LineAcceptanceTest extends AcceptanceTest {
      */
     @DisplayName("구간 제거 실패 - 하행 종점역 이외의 역을 제거")
     @Test
-    void removeSectionFail() {
+    void removeSectionFailNotLastStation() {
         // given
         ExtractableResponse<Response> createResponse = 지하철_노선_등록되어_있음(신분당선);
         String uri = createResponse.header("Location");
         구간_등록되어_있음(uri, 양재_판교_구간);
+
+        // when
+        ExtractableResponse<Response> response = 구간_제거_요청(uri, 양재역id);
+
+        // then
+        구간_제거_실패됨(response);
+    }
+
+    /**
+     * Scenario: 구간 제거(실패 - 구간 1개일 때 제거)
+     * Given 지하철 노선 생성(상행:강남역, 하행:양재역) 요청 하고
+     * When 구간(양재역) 제거 요청하면
+     * Then 구간 제거가 실패한다.
+     */
+    @DisplayName("구간 제거 실패 - 하행 종점역 이외의 역을 제거")
+    @Test
+    void removeSectionFailUniqueSection() {
+        // given
+        ExtractableResponse<Response> createResponse = 지하철_노선_등록되어_있음(신분당선);
+        String uri = createResponse.header("Location");
 
         // when
         ExtractableResponse<Response> response = 구간_제거_요청(uri, 양재역id);
