7948cc36d9a9f03d25062ff798d2e0c9a4164adb
itdar <thesockerr@gmail.com>
2022-01-26 13:15:53 +0900

    test: 노선에서 역 삭제 후 조회해서 확인하도록 테스트 수정

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -92,16 +92,21 @@ public class LineSectionAcceptanceTest extends AcceptanceTest{
         응답결과가_BAD_REQUEST(구간등록결과);
     }
 
-    @DisplayName("지하철 노선에서 특정 역을 삭제 성공한다.")
+    @DisplayName("지하철 노선에서 특정 역을 삭제 성공한 후 조회해서 확인한다.")
     @Test
     void 지하철_노선에서_역을_삭제_성공한다() {
         // given
-        지하철_노선에_구간을_등록한다(일호선, 일호선역1, 일호선역2, 10);
-        지하철_노선에_구간을_등록한다(일호선, 일호선역2, 일호선역3, 10);
+        지하철_노선에_구간을_등록한다(일호선, 일호선역1, 일호선역3, 10);
+        지하철_노선에_구간을_등록한다(일호선, 일호선역3, 일호선역4, 10);
 
         // when // then
-        ExtractableResponse<Response> 구간삭제결과 = 지하철_노선에서_역을_삭제한다(일호선.getId(), 일호선역3.getId());
+        ExtractableResponse<Response> 구간삭제결과 = 지하철_노선에서_역을_삭제한다(일호선.getId(), 일호선역4.getId());
         응답결과가_OK(구간삭제결과);
+
+        // when
+        ExtractableResponse<Response> 노선역조회결과 = 지하철_노선의_역들을_조회한다(일호선);
+        // then
+        원하는_역들이_들어있다(노선역조회결과, Arrays.asList(일호선역1, 일호선역3));
     }
 
     @DisplayName("지하철 노선에서 역이 최하행역이 아니라서 삭제 불가하다.")
