74d052038448ceac9bd54e4a6d37f1bca9cf1120
soosue <yunsoo0510@daum.net>
2022-01-26 16:12:25 +0900

    [Feat] 구간 등록 기능: 기능 구현(비정상적인 시나리오2)
    - 새로운 구간의 하행역은 해당 노선에 등록되어있는 역일 수 없음.

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -56,14 +56,23 @@ public class Line extends BaseEntity {
     }
 
     private void validAddSection(Section section) {
-        checkUpstationCondition(section);
+        checkUpStationCondition(section.getUpStation());
+        checkDownStationCondition(section.getDownStation());
     }
 
-    private void checkUpstationCondition(Section section) {
-        if (section.getUpStation().equals(lastDownStation())) {
+    private void checkUpStationCondition(Station upStation) {
+        if (upStation.equals(lastDownStation())) {
             return;
         }
         throw new IllegalArgumentException("새로운 구간의 상행역은 해당 노선에 등록되어있는 하행 종점역이어야 합니다.");
+    }
+
+    private void checkDownStationCondition(Station downStation) {
+        if (sections.stream()
+                .anyMatch(section -> section.getDownStation().equals(downStation))
+                || sections.get(0).getUpStation().equals(downStation)) {
+            throw new IllegalArgumentException("새로운 구간의 하행역은 해당 노선에 등록되어있는 역일 수 없습니다.");
+        }
     }
 
     private Station lastDownStation() {
