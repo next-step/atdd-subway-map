0ba3b65c88f75271f9c11455b2a76b05c7a53bf5
00hongjun <00hongjun@gmail.com>
2022-01-26 19:19:50 +0900

    <style> 상수 대문자로 수정

--- src/main/java/nextstep/subway/exception/section/DeleteLastDownStationException.java
+++ src/main/java/nextstep/subway/exception/section/DeleteLastDownStationException.java
@@ -2,10 +2,10 @@ package nextstep.subway.exception.section;
 
 public class DeleteLastDownStationException extends RuntimeException {
 
-    private static final String message = "마지막역(하행 종점역)만 삭제 가능합니다. - %s";
+    private static final String MESSAGE = "마지막역(하행 종점역)만 삭제 가능합니다. - %s";
 
     public DeleteLastDownStationException(String stationName) {
-        super(String.format(message, stationName));
+        super(String.format(MESSAGE, stationName));
 
     }
 
