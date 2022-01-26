e77c3bcc77eea81761e6ecbf8bef01cfd4c98e33
00hongjun <00hongjun@gmail.com>
2022-01-26 20:21:20 +0900

    <refactor> StationService의 command, query 분리

--- src/main/java/nextstep/subway/ui/StationController.java
+++ src/main/java/nextstep/subway/ui/StationController.java
@@ -1,47 +1,48 @@
 package nextstep.subway.ui;
 
-import java.net.URI;
-import java.util.List;
-import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.command.StationCommandService;
 import nextstep.subway.applicaion.dto.StationRequest;
 import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.subway.applicaion.query.StationQueryService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 public class StationController {
 
-    private StationService stationService;
+    private final StationCommandService stationCommandService;
+    private final StationQueryService stationQueryService;
 
-    public StationController(StationService stationService) {
-        this.stationService = stationService;
+    public StationController(StationCommandService stationCommandService,
+                             StationQueryService stationQueryService) {
+        this.stationCommandService = stationCommandService;
+        this.stationQueryService = stationQueryService;
     }
 
     @PostMapping("/stations")
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
-        StationResponse station = stationService.saveStation(stationRequest);
+        StationResponse station = stationCommandService.saveStation(stationRequest);
         return ResponseEntity.created(URI.create("/stations/" + station.getId())).body(station);
     }
 
     @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StationResponse>> showStations() {
-        return ResponseEntity.ok().body(stationService.findAllStations());
+        return ResponseEntity.ok().body(stationQueryService.findAllStations());
     }
 
     @GetMapping(value = "/stations/{id}")
     public ResponseEntity<StationResponse> showStation(@PathVariable Long id) {
-        return ResponseEntity.ok().body(stationService.showStationById(id));
+        return ResponseEntity.ok().body(stationQueryService.showStationById(id));
     }
 
     @DeleteMapping("/stations/{id}")
     public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
-        stationService.deleteStationById(id);
+        stationCommandService.deleteStationById(id);
         return ResponseEntity.noContent().build();
     }
+
 }
