04c85618d35754105a6d717065c51c4aa2cf3be2
mandykr <hongseokii92@gmail.com>
2022-01-27 13:29:53 +0900

    refactor: code 필드 삭제

--- src/main/java/nextstep/subway/error/exception/ErrorCode.java
+++ src/main/java/nextstep/subway/error/exception/ErrorCode.java
@@ -1,24 +1,18 @@
 package nextstep.subway.error.exception;
 
 public enum ErrorCode {
-    INVALID_INPUT_VALUE(400, "C001", " Invalid Input Value"),
-    ENTITY_NOT_FOUND(400, "C003", " Entity Not Found"),
-    INTERNAL_SERVER_ERROR(500, "C004", "Server Error"),
-    ENTITY_DUPLICATION(400, "C007", " entity is duplicate")
+    INVALID_INPUT_VALUE(400, " Invalid Input Value"),
+    ENTITY_NOT_FOUND(400, " Entity Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Server Error"),
+    ENTITY_DUPLICATION(400, "entity is duplicate")
     ;
 
-    private final String code;
     private final String message;
     private final int status;
 
-    ErrorCode(int status, String code, String message) {
+    ErrorCode(int status, String message) {
         this.status = status;
-        this.code = code;
         this.message = message;
-    }
-
-    public String getCode() {
-        return code;
     }
 
     public String getMessage() {
