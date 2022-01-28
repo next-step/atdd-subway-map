5e3ae783fc5cd87bc4d63981e48c21d86295e822
kimuno89 <kimunoya@gmail.com>
2022-01-28 23:33:46 +0900

    refactor: 미사용 코드 제거

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -17,20 +17,8 @@ public class LineRequest {
         this.color = color;
     }
 
-    private LineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
-        this.name = name;
-        this.color = color;
-        this.upStationId = upStationId;
-        this.downStationId = downStationId;
-        this.distance = distance;
-    }
-
     public static LineRequest of(String name, String color) {
         return new LineRequest(name, color);
-    }
-
-    public static LineRequest of(String name, String color, Long upStationId, Long downStationId, int distance) {
-        return new LineRequest(name, color, upStationId, downStationId, distance);
     }
 
     public String getName() {
@@ -39,18 +27,6 @@ public class LineRequest {
 
     public String getColor() {
         return color;
-    }
-
-    public Long getUpStationId() {
-        return upStationId;
-    }
-
-    public Long getDownStationId() {
-        return downStationId;
-    }
-
-    public int getDistance() {
-        return distance;
     }
 
     public Line toEntity() {
