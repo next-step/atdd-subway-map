6f2cefd722061d2970ea5d5dd3f0c8c11922dcd9
sang5c <skah321@gmail.com>
2022-01-28 16:23:52 +0900

    test: 노선 생성 인수 테스트에 파라미터 추가

--- src/test/java/nextstep/subway/fixture/TLine.java
+++ src/test/java/nextstep/subway/fixture/TLine.java
@@ -2,15 +2,21 @@ package nextstep.subway.fixture;
 
 public enum TLine {
 
-    신분당선("신분당선", "bg-red-600"),
-    구분당선("구분당선", "bg-blue-600"),
-    _2호선("2호선", "bg-green-600");
+    신분당선("신분당선", "bg-red-600", 1L, 2L, 10),
+    구분당선("구분당선", "bg-blue-600", 2L, 3L, 10),
+    _2호선("2호선", "bg-green-600", 4L, 5L, 10);
 
     public final String 이름;
     public final String 색;
+    public final Long 상행역;
+    public final Long 하행역;
+    public final int 거리;
 
-    TLine(String 이름, String 색) {
+    TLine(String 이름, String 색, Long 상행역, Long 하행역, int 거리) {
         this.이름 = 이름;
         this.색 = 색;
+        this.상행역 = 상행역;
+        this.하행역 = 하행역;
+        this.거리 = 거리;
     }
 }
