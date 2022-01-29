49df77605858d2744261fab8466fc06046e188cb
skid901 <tlrudejr1993@gmail.com>
2022-01-29 21:10:13 +0900

    feat: add the section entity

---
+++ src/main/java/nextstep/subway/domain/entity/Section.java
@@ -0,0 +1,51 @@
+package nextstep.subway.domain.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Section {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "line_id")
+    private Line line;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "up_station_id")
+    private Station upStation;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "down_station_id")
+    private Station downStation;
+
+    private int distance;
+
+    public Section() {
+    }
+
+    public Section(final Line line, final Station upStation, final Station downStation, final int distance) {
+        this.line = line;
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
+    public Line getLine() {
+        return line;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
