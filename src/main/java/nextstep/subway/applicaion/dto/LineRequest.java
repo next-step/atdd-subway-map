2d2c3b27b6cbc4136a4bb521b572e4c53c2b0970
dk-ko <dev.dkko@gmail.com>
2022-01-29 15:40:55 +0900

    feat: 지하철 노선 생성 인자 추가 및 빌더 패턴으로 수정

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -3,7 +3,21 @@ package nextstep.subway.applicaion.dto;
 public class LineRequest {
     private String name;
     private String color;
+    private Long upStationId;
+    private Long downStationId;
+    private Integer distance;
 
+    public LineRequest() {
+
+    }
+
+    private LineRequest(String name, String color, Long upStationId, Long downStationId, Integer distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
 
     public String getName() {
         return name;
@@ -11,5 +25,58 @@ public class LineRequest {
 
     public String getColor() {
         return color;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public static class Builder {
+        private String name;
+        private String color;
+        private Long upStationId;
+        private Long downStationId;
+        private Integer distance;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder color(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Builder upStationId(Long upStationId) {
+            this.upStationId = upStationId;
+            return this;
+        }
+
+        public Builder downStationId(Long downStationId) {
+            this.downStationId = downStationId;
+            return this;
+        }
+
+        public Builder distance(Integer distance) {
+            this.distance = distance;
+            return this;
+        }
+
+        public LineRequest build() {
+            if (name == null || color == null || upStationId == null || downStationId == null || distance == null) {
+                throw new IllegalArgumentException("Cannot create LineRequest");
+            }
+
+            return new LineRequest(name, color, upStationId, downStationId, distance);
+        }
     }
 }
