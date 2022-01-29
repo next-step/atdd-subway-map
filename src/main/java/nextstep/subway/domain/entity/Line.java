5fff3d3e01c1061d9e4d7f14610306cf667c16bb
skid901 <tlrudejr1993@gmail.com>
2022-01-29 21:12:14 +0900

    refactor: add method to add the section and get stations

--- src/main/java/nextstep/subway/domain/entity/Line.java
+++ src/main/java/nextstep/subway/domain/entity/Line.java
@@ -2,11 +2,10 @@ package nextstep.subway.domain.entity;
 
 import nextstep.subway.domain.service.Validator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.Objects;
+import javax.persistence.*;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Entity
 public class Line extends BaseEntity {
@@ -17,14 +16,24 @@ public class Line extends BaseEntity {
     private String name;
     private String color;
 
+    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    private List<Section> sections = new ArrayList<>();
+
     public Line() {
     }
 
-    public Line(final String name, final String color, final Validator<Line> lineValidator) {
+    public Line(final String name,
+                final String color,
+                final Station upStation,
+                final Station downStation,
+                final int distance,
+                final Validator<Line> lineValidator) {
         this.name = name;
         this.color = color;
 
         lineValidator.validate(this);
+
+        addSection(new Section(this, upStation, downStation, distance));
     }
 
     public Long getId() {
@@ -39,11 +48,32 @@ public class Line extends BaseEntity {
         return color;
     }
 
+    public List<Station> getStations() {
+        final Map<Station, Station> stationMap = sections.stream().collect(Collectors.toMap(Section::getUpStation, Section::getDownStation));
+        final Set<Station> upStations = stationMap.keySet();
+        final Set<Station> downStations = new HashSet<>(stationMap.values());
+
+        final List<Station> stations = new ArrayList<>();
+        Station nextStation = upStations.stream()
+                .filter(Predicate.not(downStations::contains))
+                .findAny().orElseThrow(IllegalStateException::new);
+        while (upStations.contains(nextStation)) {
+            stations.add(nextStation);
+            nextStation = stationMap.get(nextStation);
+        }
+        stations.add(nextStation);
+        return stations;
+    }
+
     public void change(final String name, final String color, final Validator<Line> lineValidator) {
         this.name = name;
         this.color = color;
 
         lineValidator.validate(this);
+    }
+
+    public void addSection(final Section section) {
+        this.sections.add(section);
     }
 
     @Override
