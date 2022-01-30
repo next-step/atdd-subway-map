a943d74b949b35dace443eebc32e6d0df109f755
mintheon <mintheon@mintheon.com>
2022-01-30 19:29:28 +0900

    feat: Exception 핸들러 추가
    - Illegar Argument, Entity Not Found 예외를 BadRequest로 보내도록 핸들링 로직 추가

--- src/main/java/nextstep/subway/applicaion/exception/GlobalExceptionHandler.java
+++ src/main/java/nextstep/subway/applicaion/exception/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.persistence.EntityNotFoundException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -13,5 +15,10 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> duplicateException(DuplicateException e) {
 
         return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.valueOf(e.getCode().value()));
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class, EntityNotFoundException.class})
+    public ResponseEntity<ErrorResponse> illegalException(Exception e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
