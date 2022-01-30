048c11cd7acc0a6d0249a2a06e6c57aefd33ba2a
kimuno89 <kimunoya@gmail.com>
2022-01-30 19:24:41 +0900

    feat: ControllerExceptionHandler 추가

--- src/main/java/nextstep/subway/advice/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/advice/ControllerExceptionHandler.java
@@ -5,9 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({DeleteSectionException.class, DuplicateStationException.class, DuplicateLineException.class, SameStationException.class, SectionNotValidException.class})
     public ResponseEntity<String> internalSeverErrorExceptionHandler(RuntimeException exception) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
