ef5e498bb5849a856afb5a7defd3fbba54cf87e6
mintheon <mintheon@mintheon.com>
2022-01-30 19:28:44 +0900

    test: 구간 삭제 시 마지막 구간 하행 종점이 아닌 역으로 삭제 요청 했을 경우 테스트 작성

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -71,4 +71,30 @@ public class SectionAcceptanceTest extends AcceptanceTest {
 
         assertThat(deleteReponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
+
+    /**
+     * Given 지하철 노선과 구간이 있는 상태에서
+     * When 생성한 지하철 마지막 구간이 아닌 역을 삭제 요청하면
+     * Then 생성한 지하철 구간 삭제가 실패한다.
+     */
+    @DisplayName("마지막 구간 하행 종점이 아닌 역으로 구간 삭제 요청")
+    @Test
+    void deleteNotLastSection() {
+        final int distance = 100;
+
+        // given
+        final Long 광교역_번호 = StationSteps.지하철_역_생성_요청(광교역).jsonPath().getLong(번호);
+        final Long 양재역_번호 = StationSteps.지하철_역_생성_요청(양재역).jsonPath().getLong(번호);
+        final Long 판교역_번호 = StationSteps.지하철_역_생성_요청(판교역).jsonPath().getLong(번호);
+
+        // when
+        final Long lineId = LineSteps.지하철_노선_생성_요청("신분당선", "bg-red-600", 광교역_번호, 양재역_번호, distance)
+                .jsonPath().getLong(번호);
+
+        SectionSteps.지하철_구간_생성_요청(lineId, 양재역_번호, 판교역_번호, distance);
+
+        ExtractableResponse<Response> deleteReponse = SectionSteps.지하철_구간_삭제_요청(lineId, 광교역_번호);
+
+        assertThat(deleteReponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 }
