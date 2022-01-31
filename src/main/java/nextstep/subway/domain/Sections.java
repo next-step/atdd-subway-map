bef78d94b0fb8981228ace9539058f2303d7f97f
mj950425 <minjoon1995@naver.com>
2022-01-31 17:29:50 +0900

    fix : section delete 시에 line 에서도 같이 사라지게 cascade remove 추가

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -1,11 +1,13 @@
 package nextstep.subway.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+
 public class Sections {
-    @OneToMany(mappedBy = "line")
-    private List<Section> sections = new ArrayList<>();
+    @OneToMany(mappedBy = "line", cascade = CascadeType.PERSIST)
+    private final List<Section> sections = new ArrayList<>();
 
     public void addSection(Section section) {
         sections.add(section);
