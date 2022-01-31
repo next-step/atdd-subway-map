e33d2a7aceb03a1aab877791bfeec8c0de9fea80
원스톤 <onestone@socar.kr>
2022-01-31 22:32:12 +0900

    refactor: Column 단위 unique를 Table 단위로 변경 - 코드리뷰 반영 [https://github.com/next-step/atdd-subway-map/pull/283#discussion_r795282119]

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -10,14 +10,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames= { "name" }
+                )
+        })
 public class Line extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(name = "name")
     private String name;
     private String color;
 
