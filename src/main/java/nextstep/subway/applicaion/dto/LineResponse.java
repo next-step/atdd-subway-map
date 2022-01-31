80cacdf9dbf3120eeec3b018e17d946423a7e002
mj950425 <minjoon1995@naver.com>
2022-01-31 14:46:08 +0900

    fix : Line response 에서 section return 하도록 수정

--- src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -1,30 +1,39 @@
 package nextstep.subway.applicaion.dto;
 
 import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Sections;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LineResponse {
     private Long id;
     private String name;
     private String color;
+    private List<SectionResponse> sectionResponses;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
-    private LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    private LineResponse(Long id, String name, String color, List<SectionResponse> sectionResponses,
+                         LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.sectionResponses = sectionResponses;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
     }
 
-    public static LineResponse of(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate) {
-        return new LineResponse(id, name, color, createdDate, modifiedDate);
-    }
     public static LineResponse of(Line line) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate());
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), sectionsResponseMapper(line.getSections()),
+                line.getCreatedDate(), line.getModifiedDate());
     }
+
+    private static List<SectionResponse> sectionsResponseMapper(Sections sections) {
+        return sections.getSections().stream().map(section -> new SectionResponse(section)).collect(Collectors.toList());
+    }
+
     public Long getId() {
         return id;
     }
