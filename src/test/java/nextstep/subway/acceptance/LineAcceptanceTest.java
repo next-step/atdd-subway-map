490c8e2778d3b32e16f931062f2ea05b78e4e785
dk-ko <dev.dkko@gmail.com>
2022-02-01 17:12:14 +0900

    refactor: Line AT

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,20 +1,16 @@
 package nextstep.subway.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.utils.LineUtils;
 import nextstep.subway.utils.StationUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static nextstep.subway.utils.LineUtils.*;
-import static nextstep.subway.utils.StationUtils.*;
 
 @DisplayName("지하철 노선 관리 기능")
 class LineAcceptanceTest extends AcceptanceTest {
@@ -27,11 +23,11 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void createLine() {
         // given
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선상행역"));
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선하행역"));
+        final Long upStationId = 지하철역_생성요청_및_생성한_지하철역_가져오기("1호선상행역");
+        final Long downStationId = 지하철역_생성요청_및_생성한_지하철역_가져오기("1호선하행역");
 
         // when
-        final Map<String, Object> ONE_LINE = 지하철_노선_데이터_생성("1호선", "blue darken-4", 1L, 2L, 1);
+        final Map<String, Object> ONE_LINE = 지하철_노선_데이터_생성("1호선", "blue darken-4", upStationId, downStationId, 1);
         final ExtractableResponse<Response> response = 지하철_노선_생성요청(ONE_LINE);
 
         // then
@@ -48,25 +44,16 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void getLines() {
         // given
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선상행역"));
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선하행역"));
-
-        final Map<String, Object> LINE = 지하철_노선_데이터_생성("1호선", "blue darken-4", 1L, 2L, 1);
-        final ExtractableResponse<Response> response = 지하철_노선_생성요청(LINE);
-
-
-        final Map<String, Object> NEW_LINE = 지하철_노선_데이터_생성("7호선", "blue darken-4", 3L, 4L, 1);
-
-        지하철_역_생성_요청(지하철_역_데이터_생성("7호선상행역"));
-        지하철_역_생성_요청(지하철_역_데이터_생성("7호선하행역"));
-
-        지하철_노선_생성요청(Arrays.asList(LINE, NEW_LINE));
+        final String lineName = "1호선";
+        상하행역을_포함한_지하철_노선_생성요청(lineName);
+        final String newLineName = "7호선";
+        상하행역을_포함한_지하철_노선_생성요청(newLineName);
 
         // when
         final ExtractableResponse<Response> responseList = 지하철_모든_노선_목록요청();
 
         // then
-        생성요청한_지하철_노선들이_포함된_응답을_받음(responseList, Arrays.asList("1호선", "7호선"));
+        생성요청한_지하철_노선들이_포함된_응답을_받음(responseList, Arrays.asList(lineName, newLineName));
     }
 
     /**
@@ -78,19 +65,15 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void getLine() {
         // given
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선상행역"));
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선하행역"));
-
-        final String ONE_LINE_NAME = "1호선";
-        final Map<String, Object> ONE_LINE = 지하철_노선_데이터_생성(ONE_LINE_NAME, "blue darken-4", 1L, 2L, 1);
-        final ExtractableResponse<Response> createResponse = 지하철_노선_생성요청(ONE_LINE);
+        final String oneLineName = "1호선";
+        final ExtractableResponse<Response> createResponse = 상하행역을_포함한_지하철_노선_생성요청(oneLineName);
 
         // when
         final String uri = createResponse.header("Location");
         final ExtractableResponse<Response> getResponse = 지하철_노선_목록요청(uri);
 
         // then
-        생성요청한_지하철_노선이_포함된_응답을_받음(getResponse, ONE_LINE_NAME);
+        생성요청한_지하철_노선이_포함된_응답을_받음(getResponse, oneLineName);
     }
 
     /**
@@ -102,27 +85,18 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void updateLine() {
         // given
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선상행역"));
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선하행역"));
-
-        final String ONE_LINE_NAME = "1호선";
-        final Map<String, Object> ONE_LINE = 지하철_노선_데이터_생성(ONE_LINE_NAME, "blue darken-4", 1L, 2L, 1);
-        final ExtractableResponse<Response> createResponse = 지하철_노선_생성요청(ONE_LINE);
+        final String line = "1호선";
+        final ExtractableResponse<Response> createResponse = 상하행역을_포함한_지하철_노선_생성요청(line);
 
         // when
         final String uri = createResponse.header("Location");
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선상행역"));
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선하행역"));
+        final String editedLineName = "7호선";
+        final Map<String, Object> editedLineParam = 상하행역을_포함한_지하철_노선_데이터_생성(editedLineName);
 
-        final String SEVEN_LINE_NAME = "7호선";
-        final Map<String, Object> SEVEN_LINE = 지하철_노선_데이터_생성(SEVEN_LINE_NAME, "blue darken-4", 3L, 4L, 1);
-        지하철_노선_생성요청(SEVEN_LINE);
-        final Map<String, Object> editedLine = SEVEN_LINE;
-
-        final ExtractableResponse<Response> editResponse = 지하철_노선_수정요청(editedLine, uri);
+        final ExtractableResponse<Response> editResponse = 지하철_노선_수정요청(editedLineParam, uri);
 
         // then
-        지하철노선_수정요청이_성공함(editResponse, SEVEN_LINE_NAME);
+        지하철노선_수정요청이_성공함(editResponse, editedLineName);
     }
 
     /**
@@ -134,15 +108,11 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void deleteLine() {
         // given
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선상행역"));
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선하행역"));
-
-        final String ONE_LINE_NAME = "1호선";
-        final Map<String, Object> ONE_LINE = 지하철_노선_데이터_생성(ONE_LINE_NAME, "blue darken-4", 1L, 2L, 1);
-        final ExtractableResponse<Response> response = 지하철_노선_생성요청(ONE_LINE);
+        final String line = "1호선";
+        final ExtractableResponse<Response> createResponse = 상하행역을_포함한_지하철_노선_생성요청(line);
 
         // when
-        final String uri = response.header("Location");
+        final String uri = createResponse.header("Location");
         final ExtractableResponse<Response> deleteResponse = 지하철_노선_삭제요청(uri);
 
         // then
@@ -153,17 +123,40 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void duplicateCheck() {
         // given
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선상행역"));
-        지하철_역_생성_요청(지하철_역_데이터_생성("1호선하행역"));
-
-        final String ONE_LINE_NAME = "1호선";
-        final Map<String, Object> ONE_LINE = 지하철_노선_데이터_생성(ONE_LINE_NAME, "blue darken-4", 1L, 2L, 1);
-        final ExtractableResponse<Response> response = 지하철_노선_생성요청(ONE_LINE);
+        final String lineName = "1호선";
+        final Map<String, Object> lineParam = 상하행역을_포함한_지하철_노선_데이터_생성(lineName);
+        LineUtils.지하철_노선_생성요청(lineParam);
 
         // when
-        final ExtractableResponse<Response> duplicateResponse = 지하철_노선_생성요청(ONE_LINE);
+        final String newLineName = "7호선";
+        final Map<String, Object> duplicateParam = 상하행역을_포함한_지하철_노선_데이터_생성(newLineName);
+        duplicateParam.put("name", lineName);
+        final ExtractableResponse<Response> duplicateResponse = 지하철_노선_생성요청(duplicateParam);
 
         // then
         중복이름으로_지하철_노선_생성_실패함(duplicateResponse);
+    }
+
+
+    private Long 지하철역_생성요청_및_생성한_지하철역_가져오기(String stationName) {
+        return Long.valueOf(
+                StationUtils.지하철_역_생성_요청(
+                                StationUtils.지하철_역_데이터_생성(stationName)
+                        )
+                        .jsonPath()
+                        .get("id")
+                        .toString());
+    }
+
+    private Map<String, Object> 상하행역을_포함한_지하철_노선_데이터_생성(String lineName) {
+        final Long upStationId = this.지하철역_생성요청_및_생성한_지하철역_가져오기(lineName + "상행역");
+        final Long downStationId = this.지하철역_생성요청_및_생성한_지하철역_가져오기(lineName + "하행역");
+        final Map<String, Object> LINE = LineUtils.지하철_노선_데이터_생성(lineName, "blue darken-4", upStationId, downStationId, 1);
+        return LINE;
+    }
+
+    private ExtractableResponse<Response> 상하행역을_포함한_지하철_노선_생성요청(String lineName) {
+        final Map<String, Object> LINE = 상하행역을_포함한_지하철_노선_데이터_생성(lineName);
+        return LineUtils.지하철_노선_생성요청(LINE);
     }
 }
