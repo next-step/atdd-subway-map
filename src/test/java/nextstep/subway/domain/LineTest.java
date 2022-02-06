407dbdb6dc52535c660c7b357adb5bc454f168f3
dk-ko <dev.dkko@gmail.com>
2022-02-06 14:44:52 +0900

    test: 3단계 피드백 반영 | line 테스트 추가

---
+++ src/test/java/nextstep/subway/domain/LineTest.java
@@ -0,0 +1,25 @@
+package nextstep.subway.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("지하철 노선 단위 테스트")
+class LineTest {
+    @DisplayName("지하철 노선 section 추가시 Line 매핑")
+    @Test
+    void createLine() {
+        // given
+        final Line line = new Line();
+        final Station 상행역 = new Station("상행역");
+        final Station 하행역 = new Station("하행역");
+        final int distance = 1;
+
+        // when
+        line.addSection(상행역, 하행역, distance);
+
+        // then
+        assertThat(line.getSections().get(0).getLine()).isEqualTo(line);
+    }
+}
