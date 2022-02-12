f856715c1811eaf3e623e23284f2406a52755c7a
JoyDaheeCha <daheecha0612@gmail.com>
2022-02-12 22:09:56 +0900

    refactor: ExceptionHandler catch type 변경

--- src/main/java/nextstep/subway/application/advice/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/application/advice/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package nextstep.subway.application.advice;
 
+import nextstep.subway.exception.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequestException(BadRequestException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
