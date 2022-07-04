22b08364657296e9712ecd67006b4c137ddd4997
choiseon <com1368@naver.com>
2022-07-05 00:44:54 +0900

    test : 인수 테스트 메소드 시그니쳐 작성

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -66,6 +66,10 @@ public class StationAcceptanceTest {
      * Then 2개의 지하철역을 응답 받는다
      */
     // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @Test
+    void 지하철역_목록_조회() {
+
+    }
 
     /**
      * Given 지하철역을 생성하고
@@ -73,5 +77,8 @@ public class StationAcceptanceTest {
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
     // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @Test
+    void 지하철역_제거() {
 
+    }
 }
