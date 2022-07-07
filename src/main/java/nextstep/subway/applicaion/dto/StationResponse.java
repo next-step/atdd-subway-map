6abd28df659c3d8b7543be2e671d5603da535ff1
flamme <flamme1004@gmail.com>
2022-07-08 00:10:59 +0900

    feat: StationResponse 의 기본 생성자를 만들어라

--- src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
@@ -4,6 +4,9 @@ public class StationResponse {
     private Long id;
     private String name;
 
+    private StationResponse() {
+    }
+
     public StationResponse(Long id, String name) {
         this.id = id;
         this.name = name;
