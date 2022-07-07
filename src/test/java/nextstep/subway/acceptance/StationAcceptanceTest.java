d227cdd17e50c8e5b6e7a8d9465bd2aa1b1dfdc6
Sejin Jeon <jinaidy93@gmail.com>
2022-07-07 19:22:51 +0900

    [step1] StationAcceptanceTest 작성

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -37,26 +38,24 @@ public class StationAcceptanceTest {
     @Test
     void createStation() {
         // when
-        Map<String, String> params = new HashMap<>();
+        final Map<String, String> params = new HashMap<>();
         params.put("name", "강남역");
 
         ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
+                RestAssured.given()
                         .body(params)
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+                        .then().extract();
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames =
-                RestAssured.given().log().all()
+        final List<String> stationNames =
+                RestAssured.given()
                         .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
+                        .then().extract().jsonPath().getList("name", String.class);
         assertThat(stationNames).containsAnyOf("강남역");
     }
 
@@ -65,13 +64,76 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다.")
+    @Test
+    void getStations() {
+        // given
+        final List<String> stationNamesToInsert = List.of("선릉역", "역삼역");
+        stationNamesToInsert.forEach((stationName) -> RestAssured.given()
+                .body(Map.of("name", stationName))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().extract());
+
+        // when
+        final ExtractableResponse<Response> response =
+                RestAssured.given()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when().get("/stations")
+                        .then().extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        // then
+        final List<String> respondedNames = response.body().jsonPath().getList("name", String.class);
+        respondedNames.containsAll(stationNamesToInsert);
+        assertThat(respondedNames).hasSize(stationNamesToInsert.size());
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 삭제한다.")
+    @Test
+    void deleteStation() {
+        // given
+        final String stationNameToBeDeleted = "선릉역";
+        final String stationNameToBeRemained = "역삼역";
+        final List<String> stationNamesToInsert = List.of(stationNameToBeDeleted, stationNameToBeRemained);
+        stationNamesToInsert.forEach((stationName) -> RestAssured.given()
+                .body(Map.of("name", stationName))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().extract());
 
+        // given
+        // FIXME: jsonPath로 우아하게 가져올 방법 없나...
+        final List<LinkedHashMap<String, String>> respondedBody = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/stations")
+                .then().extract()
+//                .jsonPath().get("$[0].name");
+                .jsonPath().get();
+        final LinkedHashMap<String, String> stationToBeDeleted = respondedBody.get(0);
+
+        // when
+        final ExtractableResponse<Response> deleteResponse = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete("/stations/{id}", stationToBeDeleted.get("id"))
+                .then().extract();
+
+        // then
+        assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        // then
+        final List<String> remainedStations = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/stations")
+                .then().extract()
+                .jsonPath().getList("name");
+        assertThat(remainedStations).doesNotContain(stationToBeDeleted.get("name"));
+    }
 }
