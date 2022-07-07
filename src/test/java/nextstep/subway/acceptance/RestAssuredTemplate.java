e483b1dfa7b5b02c2877f27fb9ae5ff07edfa76c
Flamme1004K <50702723+Flamme1004K@users.noreply.github.com>
2022-07-07 23:48:06 +0900

    🚀 1단계 - 지하철역 인수 테스트 작성 (#356)
    * feat: StationRequest 클래스의 생성자를 만들어라
    
    * test: 지하철역 목록 조회 인수 테스트를 작성하라
    
    * test: 지하철역 삭제 인수 테스트를 작성하라
    
    * feat: 공통 RestAssuredTemplate 를 만들어라
    
    * refactor: getList에서 genericType을 지정하라
    
    * chore: RestAssuredTemplate 클래스를 static import 시켜라
    
    * docs: 기능 요구사항을 정리하라

---
+++ src/test/java/nextstep/subway/acceptance/RestAssuredTemplate.java
@@ -0,0 +1,34 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+public class RestAssuredTemplate {
+
+    public static ExtractableResponse<Response> getRequest(String url) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get(url)
+                .then().log().all()
+                .extract();
+    }
+
+    public static <T> ExtractableResponse<Response> postRequestWithRequestBody(String url, T body) {
+        return RestAssured.given().log().all()
+                .body(body)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post(url)
+                .then().log().all()
+                .extract();
+    }
+
+    public static <T> ExtractableResponse<Response> deleteRequestWithParameter(String url, T parameter) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete(url, parameter)
+                .then().log().all()
+                .extract();
+    }
+}
--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -3,6 +3,8 @@ package nextstep.subway.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.applicaion.dto.StationRequest;
+import nextstep.subway.applicaion.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,11 +17,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static nextstep.subway.acceptance.RestAssuredTemplate.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StationAcceptanceTest {
+
     @LocalServerPort
     int port;
 
@@ -65,10 +69,20 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
     @DisplayName("지하철역을 조회한다.")
     @Test
     void getStations() {
+        // given
+        역을_만들다("잠실역");
+        역을_만들다("한성백제역");
+
+        // when
+        ExtractableResponse<Response> response = 지하철역_목록을_조회한다();
+
+        // then
+        List<String> stationNames = response.jsonPath().getList("name");
+        assertThat(stationNames).containsExactly("잠실역", "한성백제역");
+
     }
 
     /**
@@ -76,9 +90,39 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
     @DisplayName("지하철역을 제거한다.")
     @Test
     void deleteStation() {
+        // given
+        StationResponse 잠실역 = 역을_만들다("잠실역").as(StationResponse.class);
+
+        // when
+        역을_삭제한다(잠실역.getId());
+
+        // then
+        ExtractableResponse<Response> response = 지하철역_목록을_조회한다();
+        List<String> stationNames = response.jsonPath().getList("name", String.class);
+        assertThat(stationNames).isEmpty();
     }
+
+
+    private ExtractableResponse<Response> 역을_만들다(String name) {
+        StationRequest stationRequest = new StationRequest(name);
+        ExtractableResponse<Response> response = postRequestWithRequestBody("/stations", stationRequest);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        return response;
+    }
+
+    private ExtractableResponse<Response> 지하철역_목록을_조회한다() {
+        ExtractableResponse<Response> response = getRequest("/stations");
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        return response;
+    }
+
+    private void 역을_삭제한다(Long id) {
+        ExtractableResponse<Response> response = deleteRequestWithParameter("/stations/{id}", id);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+
 }
