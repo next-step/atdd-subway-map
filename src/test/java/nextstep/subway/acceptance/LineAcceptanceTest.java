83bf58f60582117090a9b6cf931c940853361d6f
wlroh <wlroh.paul@gmail.com>
2022-07-09 00:36:45 +0900

    test: 지하철 노선 생성 인수테스트 추가

---
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -0,0 +1,76 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("지하철 노선 관련 기능")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LineAcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.port = port;
+    }
+
+    /**
+     * Given    지하철 노선을 생성하면
+     * When     지하철 노선 목록을 조회 시 생성한 노선을 찾을 수 있다.
+     */
+    @Test
+    @DisplayName("지하철 노선 생성한다.")
+    void createStationLine() {
+        // given
+        노선_생성_요청("신분당선", "bg-red-600", 1L, 2L, 10);
+
+        // when
+        ExtractableResponse<Response> response = 노선_목록_조회_요청();
+        List<String> 노선_이름_목록 = response.jsonPath().getList("name", String.class);
+
+        // then
+        assertThat(노선_이름_목록).contains("신분당선");
+    }
+
+    private ExtractableResponse<Response> 노선_생성_요청(String name, String color, long upStationId, long downStationId, int distance) {
+        final Map<String, Object> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+        params.put("upStationId", upStationId);
+        params.put("downStationId", downStationId);
+        params.put("distance", distance);
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        return response;
+    }
+
+    private ExtractableResponse<Response> 노선_목록_조회_요청() {
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when().get("/lines")
+                .then().log().all()
+                .extract();
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        return response;
+    }
+}
