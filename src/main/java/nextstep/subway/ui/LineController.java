ca282a8855a5fb5f63f8bd513d8e1f7a23855506
flamme <flamme1004@gmail.com>
2022-07-08 13:20:02 +0900

    feat: Line Controller 에 지하철 노선 저장을 구현하라

---
+++ src/main/java/nextstep/subway/ui/LineController.java
@@ -0,0 +1,27 @@
+package nextstep.subway.ui;
+
+import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.LineRequest;
+import nextstep.subway.applicaion.dto.LineResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.net.URI;
+
+@Controller
+public class LineController {
+
+    private final LineService lineService;
+
+    public LineController(LineService lineService) {
+        this.lineService = lineService;
+    }
+
+    @PostMapping(value = "/lines")
+    public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
+        LineResponse line = lineService.saveLine(lineRequest);
+        return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+}
