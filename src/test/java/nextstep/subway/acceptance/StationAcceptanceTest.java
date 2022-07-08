40bc9a5ea9e241777c48fb907e3b34538c034a6c
giyeon95 <giyeon15@gmail.com>
2022-07-08 21:30:21 +0900

    🚀 1단계 - 지하철역 인수 테스트 작성 (#359)
    * tests: 지하철역 목록 조회 인수 테스트 메서드 생성
    
    * refactor: 중복 코드 제거 리펙토링
    
    * tests: 지하철역 제거 인수 테스트 메서드 생성
    
    * refactor: 추상화 레벨 통일 & 공통 키 상수화

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StationAcceptanceTest {
+    private static final String NAME = "name";
     @LocalServerPort
     int port;
 
@@ -37,26 +37,15 @@ public class StationAcceptanceTest {
     @Test
     void createStation() {
         // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "강남역");
-
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+        ExtractableResponse<Response> response = createStation(Map.of(NAME, "강남역"));
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
+        List<String> stationNames = getAllStations()
+                .jsonPath()
+                .getList(NAME, String.class);
         assertThat(stationNames).containsAnyOf("강남역");
     }
 
@@ -65,10 +54,20 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
     @DisplayName("지하철역을 조회한다.")
     @Test
     void getStations() {
+        // given
+        createStation(Map.of(NAME, "방배역"));
+        createStation(Map.of(NAME, "신논현역"));
+
+        // when
+        List<Map<String, Object>> response = getAllStations()
+                .jsonPath()
+                .getList(".");
+
+        // then
+        assertThat(response).hasSize(2);
     }
 
     /**
@@ -76,10 +75,47 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
     @DisplayName("지하철역을 제거한다.")
     @Test
     void deleteStation() {
+        // given
+        long id = createStation(Map.of(NAME, "사당역"))
+                .jsonPath()
+                .getLong("id");
+
+        // when
+        deleteStation(id);
+
+        // then
+        List<Long> allStationIds = getAllStations()
+                .jsonPath()
+                .getList("id", Long.class);
+
+
+        assertThat(allStationIds).isNotIn(id);
+    }
+
+
+    private ExtractableResponse<Response> createStation(Map<String, String> params) {
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> getAllStations() {
+        return RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private void deleteStation(long id) {
+        RestAssured.given().log().all()
+                .when().delete("/stations/{id}", id)
+                .then().log().all();
     }
 
 }
