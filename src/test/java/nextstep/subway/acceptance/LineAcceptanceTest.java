fde2153fe1e181fe990dfd5ca508d630b976ffcf
DahamLeee <dlek1011@gmail.com>
2022-07-09 18:23:19 +0900

    test: Add CreateLineAcceptanceTest

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,11 +1,23 @@
 package nextstep.subway.acceptance;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.applicaion.dto.LineRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("지하철 노선 관리 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,7 +38,34 @@ public class LineAcceptanceTest {
     @DisplayName("지하철노선 생성")
     @Test
     void createLine() {
+        // Given
+        String 첫번째역 = "지하철역";
+        String 두번째역 = "새로운지하철역";
 
+        ExtractableResponse<Response> 첫번째역_생성_응답 = createStationRequest(첫번째역);
+        ExtractableResponse<Response> 두번째역_생성_응답 = createStationRequest(두번째역);
+
+        assertAll(
+                () -> assertThat(첫번째역_생성_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(두번째역_생성_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value())
+        );
+
+        // When
+        String lineName = "신분당선";
+        String lineColor = "bg-red-600";
+        Long upStationId = 1L;
+        Long downStationId = 2L;
+        int distance = 10;
+
+        ExtractableResponse<Response> 첫번째노선_생성_응답 = createLineRequest(lineName, lineColor, upStationId, downStationId, distance);
+
+        // Then
+        List<String> stationNames = 첫번째노선_생성_응답.jsonPath().getList("stations.name", String.class);
+
+        assertAll(
+                () -> assertThat(첫번째노선_생성_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(stationNames).containsAnyOf("지하철역", "새로운지하철역")
+        );
     }
 
     /**
@@ -71,6 +110,31 @@ public class LineAcceptanceTest {
     @Test
     void deleteLine() {
 
+    }
+
+    private ExtractableResponse<Response> createStationRequest(String stationName) {
+        Map<String, String> station = new HashMap<>();
+        station.put("name", stationName);
+
+        return RestAssured
+                .given().log().all()
+                .body(station)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> createLineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
+        LineRequest line = new LineRequest(name, color, upStationId, downStationId, distance);
+
+        return RestAssured
+                .given().log().all()
+                .body(line)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
     }
 
 }
