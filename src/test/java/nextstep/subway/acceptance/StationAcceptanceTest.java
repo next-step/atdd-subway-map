1a653ae28bb8c67350224e3008a94b6264ca1a21
lsj8367 <lsj8367@naver.com>
2022-07-09 23:36:18 +0900

    :fire: TODO 주석 제거

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -56,7 +56,6 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
     @Test
     @DisplayName("지하철역을 조회한다.")
     void getStations() {
@@ -82,7 +81,6 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
     @Test
     @DisplayName("지하철역을 제거한다.")
     void deleteStation() {
