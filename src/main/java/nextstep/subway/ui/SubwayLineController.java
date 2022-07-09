75493ef3e6eacdb9b1c822fee3b2a22cdce47c2d
cafitac <cafitac99@gmail.com>
2022-07-09 20:02:17 +0900

    feat: 지하철 노선 목록 조회에 대한 API 를 작성합니다

--- src/main/java/nextstep/subway/ui/SubwayLineController.java
+++ src/main/java/nextstep/subway/ui/SubwayLineController.java
@@ -4,11 +4,13 @@ import nextstep.subway.applicaion.SubwayLineService;
 import nextstep.subway.applicaion.dto.subwayLine.CreateSubwayLineRequest;
 import nextstep.subway.applicaion.dto.subwayLine.SubwayLineResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class SubwayLineController {
@@ -23,5 +25,11 @@ public class SubwayLineController {
     public ResponseEntity<SubwayLineResponse> createStation(@RequestBody CreateSubwayLineRequest subwayLineRequest) {
         final SubwayLineResponse subwayLineResponse = subwayLineService.saveSubwayLine(subwayLineRequest);
         return ResponseEntity.created(URI.create("/stations/" + subwayLineResponse.getId())).body(subwayLineResponse);
+    }
+
+    @GetMapping("/subway-lines")
+    public ResponseEntity<List<SubwayLineResponse>> getStations() {
+        final List<SubwayLineResponse> subwayLinesResponse = subwayLineService.findAllSubwayLines();
+        return ResponseEntity.ok().body(subwayLinesResponse);
     }
 }
