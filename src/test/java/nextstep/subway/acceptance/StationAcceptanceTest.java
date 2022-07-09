18b33f94b21797f834fdaf528a65acdaf8f240a6
lsj8367 <lsj8367@naver.com>
2022-07-09 10:55:06 +0900

    :white_check_mark: 지하철역 삭제 테스트 작성

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -110,6 +110,29 @@ public class StationAcceptanceTest {
     @Test
     @DisplayName("지하철역을 제거한다.")
     void deleteStation() {
+        //given
+        final Map<String, String> body = Map.of("name", "잠실역");
 
+        RestAssured.given().log().all()
+            .body(body)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/stations")
+            .then().log().all();
+
+        //when
+        final ExtractableResponse<Response> deleteExtract = RestAssured.given().log().all()
+            .when().delete("/stations/1")
+            .then().log().all()
+            .extract();
+
+        assertThat(deleteExtract.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        //then
+        final List<String> name = RestAssured.given().log().all()
+            .when().get("/stations")
+            .then().log().all()
+            .extract().jsonPath().getList("name", String.class);
+
+        assertThat(name).isEmpty();
     }
 }
