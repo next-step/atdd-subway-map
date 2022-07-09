bf327ea00d9f9e2831d8c270976110b773b399a4
cafitac <cafitac99@gmail.com>
2022-07-09 19:26:24 +0900

    feat: StationResponse 에 대한 생성자를 추가합니다

--- src/main/java/nextstep/subway/applicaion/dto/station/StationResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/station/StationResponse.java
@@ -1,5 +1,7 @@
 package nextstep.subway.applicaion.dto.station;
 
+import nextstep.subway.domain.station.Station;
+
 public class StationResponse {
     private Long id;
     private String name;
@@ -7,6 +9,11 @@ public class StationResponse {
     public StationResponse(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public StationResponse(Station station) {
+        id = station.getId();
+        name = station.getName();
     }
 
     public Long getId() {
