db2feda3b92aebe16ec25af5f08b9e086452a07f
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-10 02:39:35 +0900

    feat : 예외 처리 핸들러 구현

---
+++ src/main/java/nextstep/subway/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package nextstep.subway.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Void> handle() {
+        return ResponseEntity.badRequest().build();
+    }
+}
