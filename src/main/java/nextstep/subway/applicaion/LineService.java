d52a7c606a7f09df8923a92d5136534ef2910e4f
jiwoo <zbqmgldjfh@gmail.com>
2022-07-09 13:58:49 +0900

    feat(LineController.createLine): 라인을 생성하는 Controller 구현 완료

---
+++ src/main/java/nextstep/subway/applicaion/LineService.java
@@ -0,0 +1,29 @@
+package nextstep.subway.applicaion;
+
+import nextstep.subway.applicaion.dto.LineRequest;
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LineService {
+
+    private LineRepository lineRepository;
+    private StationRepository stationRepository;
+
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
+        this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
+    }
+
+    public LineResponse createLine(LineRequest request) {
+        Line savedLine = lineRepository.save(request.toDomain());
+        List<Station> stations = stationRepository.findAllById(List.of(savedLine.getUpStationId(), savedLine.getDownStationId()));
+        return LineResponse.of(savedLine, stations);
+    }
+}
---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,70 @@
+package nextstep.subway.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Line {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String color;
+
+    private Long upStationId;
+    private Long downStationId;
+    private Integer distance;
+
+    public Line() {
+    }
+
+    public Line(String name, String color, Long upStationId, Long downStationId, Integer distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(id, line.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
