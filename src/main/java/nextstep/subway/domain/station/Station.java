c2b02be4ba63c5db8845b1479ece00c0dc867bca
cafitac <cafitac99@gmail.com>
2022-07-09 15:16:17 +0900

    feat: Station entity 에 SubwayLine 을 FK 로 연결합니다

---
+++ src/main/java/nextstep/subway/domain/station/Station.java
@@ -0,0 +1,39 @@
+package nextstep.subway.domain.station;
+
+import nextstep.subway.domain.subwayLine.SubwayLine;
+
+import javax.persistence.*;
+
+import static javax.persistence.CascadeType.PERSIST;
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+public class Station {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "station_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(fetch = LAZY, cascade = PERSIST)
+    @JoinColumn(name = "subway_line_id")
+    private SubwayLine subwayLine;
+
+    public Station() {
+    }
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
