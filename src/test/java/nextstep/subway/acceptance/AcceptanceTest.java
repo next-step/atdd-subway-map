56308d2c806ef72ba3292659ce43156c368a7f68
jiwoo <zbqmgldjfh@gmail.com>
2022-07-09 17:58:36 +0900

    feat(DatabaseCleanup): 테스트에서 데이터 베이스에 의존적인 환경을 격리를 위한 DatabaseCleanup 구현

--- src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
@@ -1,7 +1,9 @@
 package nextstep.subway.acceptance;
 
 import io.restassured.RestAssured;
+import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
@@ -11,8 +13,12 @@ public class AcceptanceTest {
     @LocalServerPort
     int port;
 
+    @Autowired
+    DatabaseCleanup databaseCleanup;
+
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
+        databaseCleanup.execute();
     }
 }
