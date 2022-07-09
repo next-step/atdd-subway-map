b105bac064aff3c6568691f221b89075b32434b2
MRSTK90 <dkgell13@gmail.com>
2022-07-10 02:12:27 +0900

    new : 지하철 노선도 요청/응답 dto 생성

---
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -0,0 +1,39 @@
+package nextstep.subway.applicaion.dto;
+
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
+
+import java.util.List;
+
+public class LineResponse {
+    private Long id;
+    private String name;
+    private String color;
+    private List<Station> stations;
+
+    public LineResponse(Line line) {
+        this.id = line.getId();
+        this.name = line.getName();
+        this.color = line.getColor();
+
+    }
+    public void setStation(Station station){
+        stations.add(station);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+}
