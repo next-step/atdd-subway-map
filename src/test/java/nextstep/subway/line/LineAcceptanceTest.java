4ee02a5b5227584fd6812d64c4aacdb6419cfb62
MangKyu <whalsrb1226@gmail.com>
2022-07-09 13:55:11 +0900

    test: 노선 조회를 위한 인수테스트 추가

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -8,6 +8,7 @@ import nextstep.subway.acceptance.AcceptanceTest;
 import nextstep.subway.station.StationAcceptanceTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -45,24 +46,59 @@ class LineAcceptanceTest {
     @Test
     void createLine() {
         final String lineName = "신분당선";
+        final ExtractableResponse<Response> createLineResponse = createLineRequest(lineName);
+
+        assertThat(createLineResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        final List<Long> idList = createLineResponse.jsonPath().getList("stations.id", Long.class);
+        assertThat(idList).contains(upStationId, downStationId);
+    }
+
+    private ExtractableResponse<Response> createLineRequest(final String lineName) {
+        return RestAssured
+                .given().log().all()
+                .body(createLineParams(lineName))
+                .contentType(ContentType.JSON)
+                .when().post("/lines")
+                .then().log().all().extract();
+    }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+
+    @DisplayName("지하철 노선을 조회한다.")
+    @Test
+    void getLines() {
+        final String lineName1 = "신분당선";
+        createLineRequest(lineName1);
+
+        final String lineName2 = "2호선";
+        createLineRequest(lineName2);
+
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when().get("/lines")
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        final List<String> names = response.jsonPath().getList("name", String.class);
+        assertThat(names).hasSize(2);
+        assertThat(names).contains(lineName1);
+        assertThat(names).contains(lineName2);
+    }
+
+    private Map<String, Object> createLineParams(final String lineName) {
         final Map<String, Object> params = new HashMap<>();
         params.put("name", lineName);
         params.put("color", "bg-red-600");
         params.put("upStationId", upStationId);
         params.put("downStationId", downStationId);
         params.put("distance", 10);
-
-        final ExtractableResponse<Response> createLineResponse = RestAssured
-                .given().log().all()
-                .body(params)
-                .contentType(ContentType.JSON)
-                .when().post("/lines")
-                .then().log().all().extract();
-
-        assertThat(createLineResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-
-        final List<Long> idList = createLineResponse.jsonPath().getList("stations.id", Long.class);
-        assertThat(idList).contains(upStationId, downStationId);
+        return params;
     }
 
 }
