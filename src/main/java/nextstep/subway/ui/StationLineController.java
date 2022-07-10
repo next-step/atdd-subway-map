1e50f91f02cac59336cc07d92654d4cb0b62b8e3
김현수 <hsu.kim@sk.com>
2022-07-10 21:02:06 +0900

    feat: 노선 생성 controller 구현

--- src/main/java/nextstep/subway/ui/StationLineController.java
+++ src/main/java/nextstep/subway/ui/StationLineController.java
@@ -1,4 +1,25 @@
 package nextstep.subway.ui;
 
+import nextstep.subway.applicaion.StationLineService;
+import nextstep.subway.applicaion.dto.stationLine.CreateStationLineRequest;
+import nextstep.subway.applicaion.dto.stationLine.CreateStationLineResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StationLineController {
+    private final StationLineService stationLineService;
+
+    public StationLineController(final StationLineService stationLineService) {
+        this.stationLineService = stationLineService;
+    }
+
+    @PostMapping("/lines")
+    public ResponseEntity<CreateStationLineResponse> createStationLine(@RequestBody CreateStationLineRequest request){
+        CreateStationLineResponse response = stationLineService.saveStationLine(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
