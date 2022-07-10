64ddb29b55c27da37a84c3bf401031b56b800f4e
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-10 10:45:25 +0900

    refact : 일급 컬렉션 생성

--- src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -9,15 +9,17 @@ public class LineResponse {
     private final Long id;
     private final String name;
     private final String color;
-    private final List<StationResponse> stations;
+    private final StationResponses stations;
 
     public LineResponse(Long id, String name, String color, List<Station> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
-        this.stations = stations.stream()
-            .map(station -> new StationResponse(station.getId(), station.getName()))
-            .collect(Collectors.toList());
+        this.stations = new StationResponses(
+            stations.stream()
+                .map(station -> new StationResponse(station.getId(), station.getName()))
+                .collect(Collectors.toList())
+        );
     }
 
     public Long getId() {
@@ -32,7 +34,7 @@ public class LineResponse {
         return color;
     }
 
-    public List<StationResponse> getStations() {
+    public StationResponses getStations() {
         return stations;
     }
 }
