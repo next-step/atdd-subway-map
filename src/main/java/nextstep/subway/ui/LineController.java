2a8ca9e3cdc496cb63673d535f78c796944e59a7
beadoer1 <beadoer1@gmail.com>
2022-07-10 22:44:32 +0900

    feat: 지하철 노선 기능 요청 메서드 생성

--- src/main/java/nextstep/subway/ui/LineController.java
+++ src/main/java/nextstep/subway/ui/LineController.java
@@ -1,5 +1,43 @@
 package nextstep.subway.ui;
 
-public class LineController {
+import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.LineRequest;
+import nextstep.subway.applicaion.dto.LineResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+public class LineController {
+	private LineService lineService;
+
+	public LineController(LineService lineService) {
+		this.lineService = lineService;
+	}
+
+	@PostMapping("/lines")
+	public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
+		return null;
+	}
+
+	@GetMapping("/lines")
+	public ResponseEntity<List<LineResponse>> showLines() {
+		return null;
+	}
+
+	@GetMapping("/lines/{lineId}")
+	public ResponseEntity<LineResponse> showLine(@PathVariable Long lineId) {
+		return null;
+	}
+
+	@PutMapping("/lines/{lineId}")
+	public ResponseEntity<Void> updateLine(@PathVariable Long lineId, @RequestBody LineRequest lineRequest) {
+		return null;
+	}
+
+	@DeleteMapping("/lines/{lineId}")
+	public ResponseEntity<Void> deleteLine(@PathVariable Long lineId) {
+		return null;
+	}
 }
