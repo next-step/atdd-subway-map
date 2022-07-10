c2a9a2bf0d2bfb25ff89025d5608565268d2f529
MoonHKLee <mf311110@gmail.com>
2022-07-10 23:30:36 +0900

    fix : 노선명 unique column 변경. - 개념상 노선명이 겹치면 안된다.

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,5 +1,6 @@
 package nextstep.subway.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,7 @@ public class Line {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String name;
     private String color;
 
