599824caba0c907d3ae7d9ee789343302f775808
ashley <ashley@dunamu.com>
2022-07-11 02:18:29 +0900

    [modify] line 테스트 beforeAll 추가해서 자원공유하도록 수정

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -4,10 +4,10 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.applicaion.line.dto.LineRequest;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
@@ -17,13 +17,27 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("노선 관련 기능")
+@DirtiesContext
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LineAcceptanceTest extends AcceptanceTest {
 	@LocalServerPort
 	int port;
 
+	Long stationId1;
+	Long stationId2;
+	Long stationId3;
+
+
 	@BeforeEach
 	public void beforeEach() {
 		RestAssured.port = port;
+	}
+
+	@BeforeAll
+	public void beforeAll() {
+		stationId1 = createStation("양재역").jsonPath().getLong("id");
+		stationId2 = createStation("양재시민의숲역").jsonPath().getLong("id");
+		stationId3 = createStation("청계산입구역").jsonPath().getLong("id");
 	}
 
 	/**
@@ -34,12 +48,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
 	@Transactional
 	@Test
 	void createLine() {
-		//given
-		Long upStationId = createStation("양재역").jsonPath().getLong("id");
-		Long downStationId = createStation("양재시민의숲역").jsonPath().getLong("id");
-
 		//when
-		LineRequest lineRequest = new LineRequest("신분당선", "bg-red-600", upStationId, downStationId, 10);
+		LineRequest lineRequest = new LineRequest("신분당선", "bg-red-600", stationId1, stationId2, 10);
 		ExtractableResponse<Response> response = post("/lines", lineRequest);
 
 		//then
@@ -96,6 +106,10 @@ public class LineAcceptanceTest extends AcceptanceTest {
 		Map<String, String> params = new HashMap<>();
 		params.put("name", name);
 
-		return post("/stations", params);
+		return RestAssured.given().port(port).log().all()
+			.body(params)
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.when().post("/stations")
+			.then().log().all().extract();
 	}
 }
