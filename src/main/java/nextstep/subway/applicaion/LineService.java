263624816c97ebc37869e44b770cc91a41370cad
MoonHKLee <mf311110@gmail.com>
2022-07-10 20:23:32 +0900

    fix : 노선 response service 수정. - 현재 구현에 당장 필요하지 않은 기능 삭제.

--- src/main/java/nextstep/subway/applicaion/LineService.java
+++ src/main/java/nextstep/subway/applicaion/LineService.java
@@ -1,14 +1,10 @@
 package nextstep.subway.applicaion;
 
-import java.util.ArrayList;
 import java.util.List;
 import nextstep.subway.applicaion.dto.LineRequest;
 import nextstep.subway.applicaion.dto.LineResponse;
-import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.Line;
-import nextstep.subway.domain.LineEndpoint;
 import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,25 +12,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class LineService {
     private final LineRepository lineRepository;
-    private final StationRepository stationRepository;
 
-    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
+    public LineService(LineRepository lineRepository) {
         this.lineRepository = lineRepository;
-        this.stationRepository = stationRepository;
     }
 
     @Transactional
     public LineResponse saveLine(LineRequest lineRequest) {
         Line createdLine = lineRepository.save(lineRequest.toEntity());
-        List<StationResponse> lineEndpoints = getLineEndPoints(lineRequest);
-        return new LineResponse(createdLine.getId(),createdLine.getName(),createdLine.getColor(),lineEndpoints);
+        return new LineResponse(createdLine.getId(),createdLine.getName(),createdLine.getColor());
     }
 
-    private List<StationResponse> getLineEndPoints(LineRequest lineRequest) {
-        Station upStation = stationRepository.findById(lineRequest.getUpStationId())
-            .orElseThrow(()-> new RuntimeException("해당 역이 없습니다."));
-        Station downStation = stationRepository.findById(lineRequest.getDownStationId())
-            .orElseThrow(()-> new RuntimeException("해당 역이 없습니다."));
-        return new LineEndpoint(upStation, downStation).toStationResponse();
+    public List<LineResponse> getLines() {
+        List<Line> lines = lineRepository.findAll();
+        return null;
     }
 }
--- src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -1,18 +1,14 @@
 package nextstep.subway.applicaion.dto;
 
-import java.util.List;
-
 public class LineResponse {
     private final Long id;
     private final String name;
     private final String color;
-    private final List<StationResponse> stations;
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
+    public LineResponse(Long id, String name, String color) {
         this.id = id;
         this.name = name;
         this.color = color;
-        this.stations = stations;
     }
 
     public Long getId() {
@@ -27,7 +23,4 @@ public class LineResponse {
         return color;
     }
 
-    public List<StationResponse> getStations() {
-        return stations;
-    }
 }
