3ff220ba05ce3a396a8a28c62055ceda82369499
cafitac <cafitac99@gmail.com>
2022-07-10 15:38:30 +0900

    feat: StationToSubwayLine 을 찾을 수 있는 query 를 추가합니다

--- src/main/java/nextstep/subway/domain/m2m/StationToSubwayLineRepository.java
+++ src/main/java/nextstep/subway/domain/m2m/StationToSubwayLineRepository.java
@@ -1,6 +1,12 @@
 package nextstep.subway.domain.m2m;
 
+import nextstep.subway.domain.station.Station;
+import nextstep.subway.domain.subwayLine.SubwayLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StationToSubwayLineRepository extends JpaRepository<StationToSubwayLine, Long> {
+
+    Optional<StationToSubwayLine> findByStationAndSubwayLine(Station station, SubwayLine subwayLine);
 }
