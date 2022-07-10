271f113dacbce5314af9ab6105f50ba540686d92
waterdragon <tndyd5390@naver.com>
2022-07-10 16:22:07 +0900

    feat: 지하철 노선 엔티티 생성

---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,48 @@
+package nextstep.subway.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Line {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String color;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "up_station_id")
+	private Station upStation;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "down_station_id")
+	private Station downStation;
+
+	private int distance;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public Station getUpStation() {
+		return upStation;
+	}
+
+	public Station getDownStation() {
+		return downStation;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+}
--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -1,29 +1,48 @@
 package nextstep.subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Station {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
 
-    public Station() {
-    }
+	@OneToMany(mappedBy = "upStation", fetch = FetchType.LAZY)
+	private List<Line> upStationList = new ArrayList<>();
 
-    public Station(String name) {
-        this.name = name;
-    }
+	@OneToMany(mappedBy = "downStation", fetch = FetchType.LAZY)
+	private List<Line> downStationList = new ArrayList<>();
 
-    public Long getId() {
-        return id;
-    }
+	public Station() {
+	}
 
-    public String getName() {
-        return name;
-    }
+	public Station(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public List<Line> getUpStationList() {
+		return upStationList;
+	}
+
+	public List<Line> getDownStationList() {
+		return downStationList;
+	}
 }
