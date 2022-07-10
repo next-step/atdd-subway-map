802621c0a8cd4cf7ef944050aff946c7d46ecc1e
waterdragon <tndyd5390@naver.com>
2022-07-10 16:54:39 +0900

    feat: 노선 응답용 객체 구현

---
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -0,0 +1,29 @@
+package nextstep.subway.applicaion.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nextstep.subway.domain.Line;
+
+public class LineResponse {
+	private Long id;
+	private String name;
+	private String color;
+	private List<StationResponse> stations = new ArrayList<>();
+
+	public LineResponse() {
+	}
+
+	public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
+		this.id = id;
+		this.name = name;
+		this.color = color;
+		this.stations = stations;
+	}
+
+	public static LineResponse from(Line line) {
+		return new LineResponse(line.getId(), line.getName(), line.getColor(), StationResponse.fromList(List.of(line.getUpStation(),
+			line.getDownStation())));
+	}
+
+}
--- src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
@@ -1,19 +1,30 @@
 package nextstep.subway.applicaion.dto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import nextstep.subway.domain.Station;
+
 public class StationResponse {
-    private Long id;
-    private String name;
+	private Long id;
+	private String name;
 
-    public StationResponse(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+	public StationResponse(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
+
+	public static List<StationResponse> fromList(List<Station> stationList) {
+		return stationList.stream()
+			.map(station -> new StationResponse(station.getId(), station.getName()))
+			.collect(Collectors.toList());
+	}
 }
