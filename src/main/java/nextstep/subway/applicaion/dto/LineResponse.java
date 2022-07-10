a66635f532ac1de8af94db02829ec607728758ee
MRSTK90 <dkgell13@gmail.com>
2022-07-11 00:25:50 +0900

    fix : 비선언 구문 수정

--- src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -3,6 +3,7 @@ package nextstep.subway.applicaion.dto;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Station;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LineResponse {
@@ -15,6 +16,7 @@ public class LineResponse {
         this.id = line.getId();
         this.name = line.getName();
         this.color = line.getColor();
+        stations = new ArrayList<>();
 
     }
     public void setStation(Station station){
