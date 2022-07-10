47b81e8706f8af0ef4402316a49f50d8cc684881
realrains <real.longrain@gmail.com>
2022-07-10 23:23:56 +0900

    feature: 지하철 노선 수정기능 추가
    - 지하철 노선 수정기능을 담당하는 LineModifier 정의
    - 노선 수정기능 PUT /lines/{lineId} 에 맵핑

--- src/main/java/nextstep/subway/ui/LineController.java
+++ src/main/java/nextstep/subway/ui/LineController.java
@@ -3,13 +3,16 @@ package nextstep.subway.ui;
 import java.net.URI;
 import java.util.List;
 import nextstep.subway.applicaion.dto.LineCreationRequest;
+import nextstep.subway.applicaion.dto.LineModificationRequest;
 import nextstep.subway.applicaion.dto.LineResponse;
 import nextstep.subway.applicaion.line.LineCreator;
+import nextstep.subway.applicaion.line.LineModifier;
 import nextstep.subway.applicaion.line.LineQueryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class LineController {
 
     private final LineCreator lineCreator;
+    private final LineModifier lineModifier;
     private final LineQueryService lineQueryService;
 
     public LineController(
             LineCreator lineCreator,
+            LineModifier lineModifier,
             LineQueryService lineQueryService
     ) {
         this.lineCreator = lineCreator;
+        this.lineModifier = lineModifier;
         this.lineQueryService = lineQueryService;
     }
 
@@ -42,5 +48,14 @@ public class LineController {
     public ResponseEntity<LineResponse> createLine(@RequestBody LineCreationRequest request) {
         var line = lineCreator.create(request);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @PutMapping("/lines/{lineId}")
+    public ResponseEntity<Void> modifyLine(
+            @PathVariable("lineId") Long lineId,
+            @RequestBody LineModificationRequest request
+    ) {
+        lineModifier.modify(lineId, request);
+        return ResponseEntity.ok().build();
     }
 }
