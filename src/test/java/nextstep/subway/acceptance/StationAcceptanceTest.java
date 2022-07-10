1435967c6b23f8cc499e6f58e3cd5f90fe8fa071
SangwookChoi <94peterchoi@gmail.com>
2022-07-10 17:19:16 +0900

    Revert "- 지하철역 목록 조회, 지하철역 제거 인수 테스트 메서드 작성"
    This reverts commit 458ff1832b119d3bc1811889af68daa62676e9fd.

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -5,7 +5,6 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -36,7 +35,6 @@ public class StationAcceptanceTest {
      */
     @DisplayName("지하철역을 생성한다.")
     @Test
-    @Order(1)
     void createStation() {
         // when
         Map<String, String> params = new HashMap<>();
@@ -70,34 +68,7 @@ public class StationAcceptanceTest {
     // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
     @DisplayName("지하철역을 조회한다.")
     @Test
-    @Order(2)
     void getStations() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "신도림역");
-
-        ExtractableResponse<Response> response1 = RestAssured.given().log().all()
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/stations")
-                .then().log().all().extract();
-
-        params.put("name", "구로디지털단지역");
-        ExtractableResponse<Response> response2 = RestAssured.given().log().all()
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/stations")
-                .then().log().all().extract();
-
-        assertThat(response1.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        assertThat(response2.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-
-        List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
-
-        assertThat(stationNames).contains("신도림역", "구로디지털단지역");
     }
 
     /**
@@ -108,38 +79,7 @@ public class StationAcceptanceTest {
     // TODO: 지하철역 제거 인수 테스트 메서드 생성
     @DisplayName("지하철역을 제거한다.")
     @Test
-    @Order(3)
     void deleteStation() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "부천역");
-
-        ExtractableResponse<Response> createResponse = RestAssured.given().log().all()
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/stations")
-                .then().log().all().extract();
-
-        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-
-        String bucheonId = "1";
-
-        ExtractableResponse<Response> deleteResponse = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().delete("/stations/" + bucheonId)
-                .then().log().all().extract();
-
-        assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-
-        List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
-
-        boolean isBucheonNotExisted = stationNames.stream().noneMatch((station) -> station.equals("부천역"));
-
-        assertThat(true).isEqualTo(isBucheonNotExisted);
-        assertThat(stationNames).doesNotContain("부천역");
     }
 
 }
