eb9128e600b4fc5756cb8f1dc53b58255f8fbc0f
cafitac <cafitac99@gmail.com>
2022-07-10 15:39:48 +0900

    feat: 지하철 노선 삭제에 대한 API 를 작성합니다

--- src/main/java/nextstep/subway/ui/SubwayLineController.java
+++ src/main/java/nextstep/subway/ui/SubwayLineController.java
@@ -4,6 +4,8 @@ import nextstep.subway.applicaion.SubwayLineService;
 import nextstep.subway.applicaion.dto.subwayLine.CreateSubwayLineRequest;
 import nextstep.subway.applicaion.dto.subwayLine.SubwayLineResponse;
 import nextstep.subway.applicaion.dto.subwayLine.UpdateSubwayLineRequest;
+import nextstep.subway.domain.subwayLine.SubwayLine;
+import nextstep.subway.domain.subwayLine.SubwayLineRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,10 +15,13 @@ import java.util.List;
 @RestController
 public class SubwayLineController {
 
+    private final SubwayLineRepository subwayLineRepository;
+
     private final SubwayLineService subwayLineService;
 
-    public SubwayLineController(SubwayLineService subwayLineService) {
+    public SubwayLineController(SubwayLineService subwayLineService, SubwayLineRepository subwayLineRepository) {
         this.subwayLineService = subwayLineService;
+        this.subwayLineRepository = subwayLineRepository;
     }
 
     @PostMapping("/subway-lines")
@@ -38,10 +43,17 @@ public class SubwayLineController {
     }
 
     @PutMapping("/subway-lines/{subwayLineId}")
-    public ResponseEntity<SubwayLineResponse> updateSubway(
+    public ResponseEntity<SubwayLineResponse> updateSubwayLine(
             @PathVariable Long subwayLineId,
             @RequestBody UpdateSubwayLineRequest updateSubwayLineRequest) {
         final SubwayLineResponse subwayLineResponse = subwayLineService.updateSubwayLine(subwayLineId, updateSubwayLineRequest);
         return ResponseEntity.ok().body(subwayLineResponse);
+    }
+
+    @DeleteMapping("/subway-lines/{subwayLineId}")
+    public ResponseEntity<Void> deleteSubwayLine(@PathVariable Long subwayLineId) {
+        subwayLineService.performDeleteSubwayLine(subwayLineId);
+        subwayLineService.deleteSubwayLine(subwayLineId);
+        return ResponseEntity.noContent().build();
     }
 }
