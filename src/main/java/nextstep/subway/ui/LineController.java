50bddb0ec9aea096634b95317d870cf98cec0429
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-10 09:32:40 +0900

    refact : log 추가

--- src/main/java/nextstep/subway/ui/LineController.java
+++ src/main/java/nextstep/subway/ui/LineController.java
@@ -22,28 +22,32 @@ public class LineController {
 
     @PostMapping("/lines")
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
-        log.info("request create line, line name is[{}]", lineRequest.getName());
+        log.debug("request create line, line name is[{}]", lineRequest.getName());
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
     }
 
     @GetMapping("/lines")
     public ResponseEntity<List<LineResponse>> showLines() {
+        log.debug("request find lines");
         return ResponseEntity.ok().body(lineService.findAllLines());
     }
 
     @GetMapping("/lines/{id}")
     public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
+        log.debug("request find line, id is {}", id);
         return ResponseEntity.ok().body(lineService.findOneLine(id));
     }
 
     @PostMapping("/lines/{id}")
     public ResponseEntity<LineResponse> editLine(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
+        log.debug("request edit line, id is {} and required change name {}", id, lineRequest.getName());
         return ResponseEntity.ok().body(lineService.editLine(id, lineRequest));
     }
 
     @DeleteMapping("/lines/{id}")
     public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
+        log.debug("request delete line, id is {}", id);
         lineService.deleteLine(id);
         return ResponseEntity.noContent().build();
     }
