f869c5db1026390244b4fcc1d45d05c5d7e66c2d
임명섭 <lms@elonsoft.co.kr>
2022-07-10 10:36:55 +0900

    refactor final 필드로 선언

--- src/main/java/nextstep/subway/applicaion/dto/SubwayLineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/SubwayLineResponse.java
@@ -5,10 +5,10 @@ import nextstep.subway.domain.SubwayLine;
 import java.util.List;
 
 public class SubwayLineResponse {
-    private Long id;
-    private String name;
-    private String color;
-    private List<StationResponse> stations;
+    private final Long id;
+    private final String name;
+    private final String color;
+    private final List<StationResponse> stations;
 
     public SubwayLineResponse(Long id, String name, String color, List<StationResponse> stations) {
         this.id = id;
