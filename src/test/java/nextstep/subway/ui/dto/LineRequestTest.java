08978e25c567f03ee4536ccd02f8c8510a50ce24
giyeon95 <giyeon15@gmail.com>
2022-07-11 22:17:35 +0900

    test: 단위 테스트 추가 (UpdateDto)

--- src/test/java/nextstep/subway/ui/dto/LineRequestTest.java
+++ src/test/java/nextstep/subway/ui/dto/LineRequestTest.java
@@ -1,6 +1,7 @@
 package nextstep.subway.ui.dto;
 
 import nextstep.subway.applicaion.dto.LineCreateDto;
+import nextstep.subway.applicaion.dto.LineUpdateDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +20,15 @@ class LineRequestTest {
         assertThat(lineRequest.getUpStationId()).isEqualTo(dto.getUpStationId());
         assertThat(lineRequest.getDownStationId()).isEqualTo(dto.getDownStationId());
         assertThat(lineRequest.getDistance()).isEqualTo(dto.getDistance());
+    }
+
+    @Test
+    @DisplayName("DTO 변환이 정상적으로 수행된다.")
+    void toUpdateDtoTest() {
+        LineRequest lineRequest = new LineRequest("4호선", "blue", 1L, 2L, 5);
+        LineUpdateDto dto = lineRequest.toUpdateDto();
+
+        assertThat(lineRequest.getName()).isEqualTo(dto.getName());
+        assertThat(lineRequest.getColor()).isEqualTo(dto.getColor());
     }
 }
