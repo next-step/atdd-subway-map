90fe302c0abfe313a8161de40694acc162893a2e
임명섭 <lms@elonsoft.co.kr>
2022-07-11 23:06:26 +0900

    test table truncate 테이블 연관관계 처리

--- src/test/java/nextstep/subway/acceptance/CleanUpSchema.java
+++ src/test/java/nextstep/subway/acceptance/CleanUpSchema.java
@@ -17,9 +17,11 @@ public class CleanUpSchema {
     @Transactional(readOnly = true)
     public void execute(String...tableNames) {
         entityManager.flush();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
         for (String tableName : tableNames) {
             entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
             entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN id RESTART WITH 1").executeUpdate();
         }
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
     }
 }
