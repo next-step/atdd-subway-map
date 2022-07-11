d13fefd0395f94674e785e768dd75d20864ce055
lsj8367 <lsj8367@naver.com>
2022-07-12 00:40:18 +0900

    :sparkles: 호선 저장 dto 구현

---
+++ src/main/java/nextstep/subway/applicaion/dto/LineSaveRequest.java
@@ -0,0 +1,31 @@
+package nextstep.subway.applicaion.dto;
+
+public class LineSaveRequest {
+
+    private String name;
+    private String color;
+    private Long upStationId;
+    private Long downStationId;
+    private Integer distance;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+}
