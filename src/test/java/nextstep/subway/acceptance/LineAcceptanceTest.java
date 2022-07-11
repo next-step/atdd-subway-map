cccf812aec9cfe1768fca2f05b53f309957270f6
hjeom <nara1030@naver.com>
2022-07-12 01:25:07 +0900

    test] 지하철노선 목록 조회 테스트 작성

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,15 +1,12 @@
 package nextstep.subway.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,6 +22,23 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void createLine() {
         // when
+        createStations();
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "2호선");
+        params.put("color", "bg-red-600");
+        params.put("upStationId", "1");
+        params.put("downStationId", "3");
+        params.put("distance", "10");
+        ExtractableResponse<Response> response = createLines(params);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        // then
+        assertThat(getList("name", "/lines").get(0)).isEqualTo("2호선");
+    }
+
+    private void createStations() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "강남역");
         create(params, "/stations");
@@ -32,20 +46,41 @@ public class LineAcceptanceTest extends AcceptanceTest {
         create(params, "/stations");
         params.put("name", "선릉역");
         create(params, "/stations");
+        params.put("name", "양재역");
+        create(params, "/stations");
+    }
 
-        params = new HashMap<>();
+    private ExtractableResponse<Response> createLines(Map params) {
+        ExtractableResponse<Response> response = create(params, "/lines");
+        return response;
+    }
+
+    /**
+     * Given 2개의 지하철역을 생성하고
+     * When 지하철역 목록을 조회하면
+     * Then 2개의 지하철역을 응답 받는다
+     */
+    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철노선을 조회한다.")
+    @Test
+    void getLines() {
+        // given
+        createStations();
+        Map<String, String> params = new HashMap<>();
         params.put("name", "2호선");
         params.put("color", "bg-red-600");
         params.put("upStationId", "1");
         params.put("downStationId", "3");
         params.put("distance", "10");
-
-        ExtractableResponse<Response> response = create(params, "/lines");
+        createLines(params);
+        params.put("name", "신분당선");
+        params.put("color", "bg-green-300");
+        params.put("upStationId", "1");
+        params.put("downStationId", "4");
+        params.put("distance", "3");
+        createLines(params);
 
         // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-
-        // then
-        assertThat(getList("name", "/lines").get(0)).isEqualTo("2호선");
+        assertThat(getList("name", "/lines")).containsExactly("2호선", "신분당선");
     }
 }
