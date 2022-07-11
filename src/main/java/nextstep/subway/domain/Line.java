58484d4365e5eedf482f6f1fcc6d1cb9e2b724d9
JangHyeonJun2 <epfvkdlxj@naver.com>
2022-07-12 02:54:43 +0900

    create Line, LineRepository

---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,45 @@
+package nextstep.subway.domain;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Line {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String color;
+
+    @NotNull
+    private Long upStationId;
+
+    @NotNull
+    private Long downStationId;
+
+    @NotNull
+    private Integer distance;
+
+    public Line() {
+    }
+
+    public Line(String name, String color, Long upStationId, Long downStationId, Integer distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+
+}
