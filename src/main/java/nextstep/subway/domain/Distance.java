53a23fc70f60f2a26f2f7ce4e9325fd6c9485429
lsj8367 <lsj8367@naver.com>
2022-07-12 00:40:02 +0900

    :sparkles: 지하철역 거리 값 객체 구현

---
+++ src/main/java/nextstep/subway/domain/Distance.java
@@ -0,0 +1,44 @@
+package nextstep.subway.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Distance {
+
+    @Column(name = "distance")
+    private int value;
+
+    protected Distance() {
+    }
+
+    public Distance(final int value) {
+        value(value);
+        this.value = value;
+    }
+
+    private void value(final int value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("지하철 노선의 거리 값이 0보다 커야합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Distance distance = (Distance) o;
+        return value == distance.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+}
