027e1a0c7bcbc6eb4ff80ec990e996a8011a28c0
lsj8367 <lsj8367@naver.com>
2022-07-12 00:42:30 +0900

    :sparkles: 호선 생성, 조회 api 구현

---
+++ src/main/java/nextstep/subway/ui/LineController.java
@@ -0,0 +1,36 @@
+package nextstep.subway.ui;
+
+import java.net.URI;
+import java.util.List;
+import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.applicaion.dto.LineSaveRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lines")
+public class LineController {
+
+    private final LineService lineService;
+
+    public LineController(final LineService lineService) {
+        this.lineService = lineService;
+    }
+
+    @PostMapping
+    public ResponseEntity<LineResponse> createLines(@RequestBody final LineSaveRequest request) {
+        final LineResponse lineResponse = lineService.createLines(request);
+        return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId())).body(lineResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LineResponse>> getLines() {
+        return ResponseEntity.ok(lineService.getLines());
+    }
+
+}
