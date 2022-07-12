2a023d1e9b47dec7e95254c4a8e8fdbcf6ed86a7
younlll <youn.lee96@gmail.com>
2022-07-12 22:26:15 +0900

    test: 공통적으로 사용되는 메서드 추출

--- src/test/java/nextstep/subway/acceptance/SubwayLineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SubwayLineAcceptanceTest.java
@@ -1,6 +1,7 @@
 package nextstep.subway.acceptance;
 
 import static io.restassured.RestAssured.UNDEFINED_PORT;
+import static nextstep.subway.acceptance.SubwayLineCommon.지하철_노선_등록_요청;
 import static nextstep.subway.acceptance.SubwayStationCommon.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -158,28 +159,6 @@ public class SubwayLineAcceptanceTest {
         ExtractableResponse<Response> response = 지하철_노선_목록_조회_요청();
         List<String> names = response.jsonPath().getList("name", String.class);
         assertThat(names).doesNotContain("9호선");
-    }
-
-    private ExtractableResponse<Response> 지하철_노선_등록_요청(String name, String color, Long upStationId,
-                                                       Long downStationId, int distance) {
-        return RestAssured.given().log().all()
-                .body(지하철_노선_등록_파라미터_생성(name, color, upStationId, downStationId, distance))
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/lines")
-                .then().log().all()
-                .extract();
-    }
-
-    private Map<String, String> 지하철_노선_등록_파라미터_생성(String name, String color, Long upStationId, Long downStationId,
-                                                  int distance) {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", name);
-        params.put("color", color);
-        params.put("upStationId", String.valueOf(upStationId));
-        params.put("downStationId", String.valueOf(downStationId));
-        params.put("distance", String.valueOf(distance));
-
-        return params;
     }
 
     private ExtractableResponse<Response> 지하철_노선_목록_조회_요청() {
---
+++ src/test/java/nextstep/subway/acceptance/SubwayLineCommon.java
@@ -0,0 +1,32 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.MediaType;
+
+public class SubwayLineCommon {
+    public static ExtractableResponse<Response> 지하철_노선_등록_요청(String name, String color, Long upStationId,
+                                                       Long downStationId, int distance) {
+        return RestAssured.given().log().all()
+                .body(지하철_노선_등록_파라미터_생성(name, color, upStationId, downStationId, distance))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+    }
+
+    private static Map<String, String> 지하철_노선_등록_파라미터_생성(String name, String color, Long upStationId, Long downStationId,
+                                                  int distance) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+        params.put("upStationId", String.valueOf(upStationId));
+        params.put("downStationId", String.valueOf(downStationId));
+        params.put("distance", String.valueOf(distance));
+
+        return params;
+    }
+}
