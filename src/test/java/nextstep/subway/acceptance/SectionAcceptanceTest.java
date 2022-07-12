bd2da8c2046cdc76416ed9c2db228423a038cd8c
김현수 <hsu.kim@sk.com>
2022-07-13 01:04:27 +0900

    test: 인수조건 재작성

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -23,8 +23,8 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     class 구간등록기능 {
         /**
          * Given A,B,C 라는 이름을 가진 3개의 역을 생성한다.
-         * Given 노선을 생성하고 A역은 상행역, B역은 하행역으로 등록한다.
-         * When 상행역은 B이며 하행역은 C인 새로운 구간을 노선에 등록하고
+         * Given 노선(A역은 상행역, B역은 하행역)을 생성한다.
+         * When 새로운 구간(B역은 상행역 C역은 하행역)을 노선에 등록하고
          * Then 등록된 노선의 상행역, 하행역을 확인한다.
          */
         @Test
@@ -50,8 +50,8 @@ public class SectionAcceptanceTest extends AcceptanceTest {
 
         /**
          * Given A,B 라는 이름을 가진 2개의 역을 생성한다.
-         * Given 노선을 생성하고 A역은 상행역, B역은 하행역으로 등록한다.
-         * When 상행역은 B이며 하행역은 A인 새로운 구간을 노선에 등록한다.
+         * Given 노선(A역은 상행역, B역은 하행역)을 생성한다.
+         * When 새로운 구간(B역은 상행역 A역은 하행역)을 노선에 등록한다.
          * Then 노선에 새로운 구간을 등록할 수 없다.
          */
         @Test
@@ -72,8 +72,8 @@ public class SectionAcceptanceTest extends AcceptanceTest {
 
         /**
          * Given A,B,C,D 라는 이름을 가진 4개의 역을 생성한다.
-         * Given 노선을 생성하고 A역은 상행역, B역은 하행역으로 등록한다.
-         * When 상행역은 C이며 하행역은 D인 새로운 구간을 노선에 등록한다.
+         * Given 노선(A역은 상행역, B역은 하행역)을 생성한다.
+         * When 새로운 구간(C역은 상행역 D역은 하행역)을 노선에 등록한다.
          * Then 노선에 새로운 구간을 등록할 수 없다.
          */
         @Test
