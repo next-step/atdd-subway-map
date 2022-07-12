299c94749ed3f5d085724fa4fef8d23239516515
choiseon <com1368@naver.com>
2022-07-13 00:07:30 +0900

    test : 지하철_노선_조회 테스트 구현

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
@@ -87,7 +86,7 @@ public class LineAcceptanceTest {
   }
 
   /**
-   * When 2개의 지하철 노선을 생성하고
+   * Given 2개의 지하철 노선을 생성하고
    * When 지하철 노선 목록을 조회하면
    * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
    */
@@ -161,5 +160,58 @@ public class LineAcceptanceTest {
 
     List<String> colors = response.jsonPath().getList("color", String.class);
     assertThat(colors).containsAnyOf(Color.BLUE.name(), Color.GREEN.name());
+  }
+
+  /**
+   * Given 지하철 노선을 생성하고
+   * When 생성한 지하철 노선을 조회하면
+   * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+   */
+  @Test
+  void 지하철_노선_조회() {
+    Map<String, Object> params = new HashMap<>();
+    params.put("name", "도농역");
+
+    StationResponse donongStation = RestAssured.given().log().all()
+        .body(params)
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when().post("/stations")
+        .then().log().all()
+        .extract().as(StationResponse.class);
+
+    params.put("name", "구리역");
+
+    StationResponse gooriStation = RestAssured.given().log().all()
+        .body(params)
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when().post("/stations")
+        .then().log().all()
+        .extract().as(StationResponse.class);
+
+    params.put("name", "1호선");
+    params.put("color", Color.BLUE);
+    params.put("upStationId", donongStation.getId());
+    params.put("downStationId", gooriStation.getId());
+    params.put("distance", 10);
+
+    ExtractableResponse<Response> donongGooriLine = RestAssured.given().log().all()
+        .body(params)
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when().post("/lines")
+        .then().log().all()
+        .extract();
+
+    assertThat(donongGooriLine.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+    ExtractableResponse<Response> response = RestAssured.given().log().all()
+        .when().get("/lines/" + donongGooriLine.jsonPath().getLong("id"))
+        .then().log().all()
+        .extract();
+
+    List<String> names = response.jsonPath().getList("name", String.class);
+    assertThat(names).containsAnyOf("1호선");
+
+    List<String> colors = response.jsonPath().getList("color", String.class);
+    assertThat(colors).containsAnyOf(Color.BLUE.name());
   }
 }
