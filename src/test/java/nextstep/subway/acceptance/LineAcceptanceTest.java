9a13f55605686178c7afe8a6ad7f83da25f03577
choiseon <com1368@naver.com>
2022-07-12 23:06:02 +0900

    test : 지하철_노선_목록_조회 및 테스트 격리 적용

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -15,9 +15,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 
+@Sql("/subway.sql")
 @DisplayName("지하철역 노선 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LineAcceptanceTest {
@@ -40,7 +43,7 @@ public class LineAcceptanceTest {
     Map<String, Object> params = new HashMap<>();
     params.put("name", "도농역");
 
-    StationResponse gangnamStation = RestAssured.given().log().all()
+    StationResponse donongStation = RestAssured.given().log().all()
         .body(params)
         .contentType(MediaType.APPLICATION_JSON_VALUE)
         .when().post("/stations")
@@ -58,7 +61,7 @@ public class LineAcceptanceTest {
 
     params.put("name", "1호선");
     params.put("color", Color.BLUE);
-    params.put("upStationId", gangnamStation.getId());
+    params.put("upStationId", donongStation.getId());
     params.put("downStationId", gooriStation.getId());
     params.put("distance", 10);
 
@@ -81,5 +84,82 @@ public class LineAcceptanceTest {
 
     List<String> colors = response1.jsonPath().getList("color", String.class);
     assertThat(colors).containsAnyOf(Color.BLUE.name());
+  }
+
+  /**
+   * When 2개의 지하철 노선을 생성하고
+   * When 지하철 노선 목록을 조회하면
+   * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+   */
+  @Test
+  void 지하철_노선_목록_조회() {
+    Map<String, Object> params = new HashMap<>();
+    params.put("name", "도농역");
+
+    StationResponse donongStation = RestAssured.given().log().all()
+        .body(params)
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when().post("/stations")
+        .then().log().all()
+        .extract().as(StationResponse.class);
+
+    params.put("name", "구리역");
+
+    StationResponse gooriStation = RestAssured.given().log().all()
+        .body(params)
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when().post("/stations")
+        .then().log().all()
+        .extract().as(StationResponse.class);
+
+    params.put("name", "덕소역");
+
+    StationResponse ducksoStation = RestAssured.given().log().all()
+        .body(params)
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when().post("/stations")
+        .then().log().all()
+        .extract().as(StationResponse.class);
+
+    params.put("name", "1호선");
+    params.put("color", Color.BLUE);
+    params.put("upStationId", donongStation.getId());
+    params.put("downStationId", gooriStation.getId());
+    params.put("distance", 10);
+
+    ExtractableResponse<Response> donongGooriLine = RestAssured.given().log().all()
+        .body(params)
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when().post("/lines")
+        .then().log().all()
+        .extract();
+
+    assertThat(donongGooriLine.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+    params.put("name", "2호선");
+    params.put("color", Color.GREEN);
+    params.put("upStationId", donongStation.getId());
+    params.put("downStationId", ducksoStation.getId());
+    params.put("distance", 10);
+
+    ExtractableResponse<Response> donongDucksoLine = RestAssured.given().log().all()
+        .body(params)
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when().post("/lines")
+        .then().log().all()
+        .extract();
+
+    assertThat(donongDucksoLine.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+    ExtractableResponse<Response> response = RestAssured.given().log().all()
+        .when().get("/lines")
+        .then().log().all()
+        .extract();
+
+    List<String> names = response.jsonPath().getList("name", String.class);
+    assertThat(names).containsAnyOf("1호선", "2호선");
+
+    List<String> colors = response.jsonPath().getList("color", String.class);
+    assertThat(colors).containsAnyOf(Color.BLUE.name(), Color.GREEN.name());
   }
 }
