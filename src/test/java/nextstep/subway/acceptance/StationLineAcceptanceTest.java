98a23591d52a6ce428dfc32f321848c5a75efe2a
uram <parkuram12@gmail.com>
2022-07-12 21:18:33 +0900

    test: 지하철 노선 수정 테스트 작성

--- src/test/java/nextstep/subway/acceptance/StationLineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationLineAcceptanceTest.java
@@ -4,6 +4,7 @@ package nextstep.subway.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.domain.Line;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -118,7 +120,7 @@ public class StationLineAcceptanceTest {
                                                            .extract();
         // when 지하철 노선 조회
         String name = RestAssured.given().log().all()
-                              .pathParam("id", createResponse.jsonPath().get("id"))
+                              .pathParam("id", createResponse.jsonPath().getLong("id"))
                               .when().log().all()
                               .get("/station/line/{id}")
                               .then().log().all()
@@ -126,6 +128,44 @@ public class StationLineAcceptanceTest {
 
         // then 생성한 지하철 노선 확인
         assertThat(name).isEqualTo("우이신설");
+    }
+
+    /**
+     * given 지하철 노선을 생성하고
+     * when 생성된 노선을 수정하면
+     * then 변경된 노선을 확인할 수 있다
+     */
+    @DisplayName("지하철 노선 정보 수정")
+    @Test
+    void updateLine() {
+        //given
+        Map<String, Object> 경춘선 = new HashMap<>();
+        경춘선.put("name", "경춘선");
+        경춘선.put("color", "green");
+        경춘선.put("upStationId", 20L);
+        경춘선.put("downStationId", 21L);
+        ExtractableResponse<Response> createResponse = RestAssured.given().log().all()
+                                                           .body(경춘선)
+                                                           .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                                           .when().log().all()
+                                                           .post("/station/line")
+                                                           .then().log().all()
+                                                           .extract();
+        //when
+        경춘선.put("name", "춘경선");
+        경춘선.put("color", "red");
+        Line line = RestAssured.given().log().all()
+                               .pathParam("id", createResponse.jsonPath().getLong("id"))
+                               .and()
+                               .body(경춘선)
+                               .contentType(MediaType.APPLICATION_JSON_VALUE)
+                               .when().log().all()
+                               .patch("/station/line/{id}")
+                               .then().log().all()
+                               .extract().as(Line.class);
+        //then
+        assertThat(line.getName()).isEqualTo("춘경선");
+        assertThat(line.getColor()).isEqualTo("red");
     }
 
 }
