3cc04e09055c131ed3d2dc265a20ceea6efe135e
uram <parkuram12@gmail.com>
2022-07-12 12:09:52 +0900

    refact: 1단계 미션 PR 피드백 반영
    - 메서드명과 의미가 겹치는 주석 삭제
    - create 메서드 인라이닝
    - 삭제 메서드 파라미터 타입 변경

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -36,7 +36,7 @@ public class StationAcceptanceTest {
     @Test
     void createStation() {
         // when
-        ExtractableResponse<Response> response = 지하철역을_생성한다(createRequestBody("name", "강남역"));
+        ExtractableResponse<Response> response = 지하철역을_생성한다("name", "강남역");
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
@@ -54,14 +54,14 @@ public class StationAcceptanceTest {
     @DisplayName("지하철역 목록을 조회한다.")
     @Test()
     void getStations() {
-        // given 2개의 지하철역을 생성하고
-        지하철역을_생성한다(createRequestBody("name", "영통역"));
-        지하철역을_생성한다(createRequestBody("name", "선릉역"));
+        // given
+        지하철역을_생성한다("name", "영통역");
+        지하철역을_생성한다("name", "선릉역");
 
-        // when 지하철역 목록을 조회하면
+        // when
         List<String> names = 지하철역을_조회한다("name");
 
-        // then 생성한 2개의 지하철역을 응답 받는다.
+        // then
         assertThat(names).hasSize(2);
     }
 
@@ -74,24 +74,22 @@ public class StationAcceptanceTest {
     @DisplayName("지하철역을 제거한다.")
     @Test()
     void deleteStation() {
-        // given 지하철역을 생성하고
-        ExtractableResponse<Response> response = 지하철역을_생성한다(createRequestBody("name", "사당역"));
+        // given
+        ExtractableResponse<Response> response = 지하철역을_생성한다("name", "사당역");
 
-        // when 지하철을 삭제하면
+        // when
         지하철역을_삭제한다(response.jsonPath().get("id"));
 
-        // then 그 지하철 목록 조회 시 생성한 역을 찾을 수 없다.
+        // then
         assertThat(지하철역을_조회한다("name")).isEmpty();
 
     }
 
-    private Map<String, String> createRequestBody(String id, String value) {
-        return Map.of(id, value);
-    }
 
-    private ExtractableResponse<Response> 지하철역을_생성한다(Map<String, String> params2) {
+    private ExtractableResponse<Response> 지하철역을_생성한다(String id, String name) {
+        Map<String, Object> body = Map.of(id, name);
         return RestAssured.given().log().all()
-                          .body(params2)
+                          .body(body)
                           .contentType(MediaType.APPLICATION_JSON_VALUE)
                           .when().log().all()
                           .post("/stations")
@@ -108,7 +106,7 @@ public class StationAcceptanceTest {
                           .jsonPath().getList(id, String.class);
     }
 
-    private void 지하철역을_삭제한다(Integer id) {
+    private void 지하철역을_삭제한다(Long id) {
         RestAssured.given().log().all()
                    .pathParam("id", id)
                    .when()
