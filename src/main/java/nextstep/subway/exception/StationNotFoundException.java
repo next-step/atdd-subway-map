865c28387df1a4a1ef811251d313682cc678d0a1
임명섭 <lms@elonsoft.co.kr>
2022-07-12 21:47:57 +0900

    style 사용하지 않는 예외 클래스 삭제

--- src/main/java/nextstep/subway/exception/StationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package nextstep.subway.exception;
-
-public class StationNotFoundException extends RuntimeException {
-    public StationNotFoundException(Long id) {
-        super("not found station id by " + id);
-    }
-}
