40f41b06d8e07d468ff5a069c7803553b7e8c5d2
94peterchoi <94peterchoi@gmail.com>
2022-07-13 20:46:38 +0900

    - 인수테스트 메서드 간 데이터 격리를 위해 truncate sql문 활용 - 지하철 노선 목록 조회 인수테스트 메서드 작성 완료 (리팩토링 필요) - 지하철 노선 수정 인수테스트 메서드 작성 시작

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +20,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철노선 관련 기능")
+@Sql({"classpath:subway_truncate.sql"})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LineAcceptanceTest {
     @LocalServerPort
@@ -66,6 +68,9 @@ public class LineAcceptanceTest {
         assertThat(lineName).isEqualTo("신분당선");
         assertThat(lineColor).isEqualTo("bg-red-600");
 
+        List<String> stationNames = getStationNames();
+        System.out.println(stationNames);
+
     }
 
     /**
@@ -76,18 +81,74 @@ public class LineAcceptanceTest {
     @DisplayName("지하철 노선 목록을 조회한다.")
     @Test
     void getLines() {
-        ExtractableResponse<Response> response1 = createLine(new Line("신분당선", "bg-red-600", 1L, 2L, 10));
-        ExtractableResponse<Response> response2 = createLine(new Line("7호선", "bg-brown-600", 1L, 3L, 10));
+
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "강남역");
+
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations");
+
+        params.put("name", "판교역");
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations");
+
+        params.put("name", "부평구청역");
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations");
+
+        params.put("name", "장암역");
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations");
+
+        ExtractableResponse<Response> response1 = createLine(new Line("신분당선", "bg-red-600", 3L, 4L, 10));
+        ExtractableResponse<Response> response2 = createLine(new Line("7호선", "bg-brown-600", 5L, 6L, 10));
 
         List<String> lineNames = RestAssured.given().log().all()
                 .when().get("/lines")
                 .then().log().all()
                 .extract().jsonPath().getList("name", String.class);
 
+        assertThat(lineNames).contains("신분당선", "7호선");
+
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 수정하면
+     * Then 해당 지하철 노선 정보는 수정된다
+     */
+    @DisplayName("지하철 노선을 수정한다.")
+    @Test
+    void updateLines() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "인천역");
+
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations");
+
+        params.put("name", "소요산역");
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations");
+
+
+
+
     }
 
 
-        // 지하철노선 생성
+    // 지하철노선 생성
     private ExtractableResponse<Response> createLine(Line line) {
         Map<String, Object> params = new HashMap<>();
         params.put("name", line.getName());
@@ -103,6 +164,15 @@ public class LineAcceptanceTest {
                 .then().log().all()
                 .extract();
     }
+
+    // 지하철역 이름 조회
+    private List<String> getStationNames() {
+        return RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+    }
+
 
 
 
