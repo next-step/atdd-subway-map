222a1f7515bc949a8b188c0800b17553fb1d70df
MoonHKLee <mf311110@gmail.com>
2022-07-14 03:25:42 +0900

    test : 지하철 노선 구간 제거 테스트 수정.

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -18,8 +18,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Sql(scripts = {
         "classpath:sql/truncate.sql",
         "classpath:sql/createStations.sql",
-        "classpath:sql/createLine.sql"})
-class SectionAcceptanceTest extends BaseAcceptanceTest{
+        "classpath:sql/createLine.sql",
+        "classpath:sql/createSection.sql"})
+public class SectionAcceptanceTest extends BaseAcceptanceTest{
 
     /**
      * When     새로운 구간을 지하철 노선의 하행 종점역에 등록하면
@@ -34,17 +35,21 @@ class SectionAcceptanceTest extends BaseAcceptanceTest{
         ExtractableResponse<Response> createdSection = createSection(requestBody);
 
         //then
-        ExtractableResponse<Response> sections = RestAssured
-                .given().log().all()
-                .when().get("/lines/{id}/sections",1)
-                .then().log().all()
-                .extract();
+        ExtractableResponse<Response> sections = getSections(1);
         int sectionId = createdSection.jsonPath().getInt("id");
         assertThat(sections.jsonPath().getList("id",Integer.class)).contains(sectionId);
 
         //then
         ExtractableResponse<Response> line = getLine(1);
         assertThat(line.jsonPath().getInt("stations[1].id")).isEqualTo(3);
+    }
+
+    private ExtractableResponse<Response> getSections(long lineId) {
+        return RestAssured
+                .given().log().all()
+                .when().get("/lines/{id}/sections",lineId)
+                .then().log().all()
+                .extract();
     }
 
     /**
@@ -56,11 +61,11 @@ class SectionAcceptanceTest extends BaseAcceptanceTest{
     void registerIllegalUpStationSectionTest() {
         //when
         Map<String, Object> requestBody = getRequestBody("1","3",10);
-        ExtractableResponse<Response> createdSection = createSection(requestBody);
+        ExtractableResponse<Response> response = createSection(requestBody);
 
         //then
-        assertThat(createdSection.jsonPath().getString("message"))
-            .isEqualTo(ExceptionMessages.getNoEndpointInputExceptionMessage(1,2));
+        assertThat(response.jsonPath().getString("message"))
+            .isEqualTo(ExceptionMessages.getNotEndpointInputExceptionMessage(1,2));
     }
 
     /**
@@ -78,7 +83,7 @@ class SectionAcceptanceTest extends BaseAcceptanceTest{
         int createdSectionId = createdSection.jsonPath().getInt("id");
 
         //when
-        deleteSection(1,2);
+        deleteSection(1,3);
 
         //then
         ExtractableResponse<Response> lines = getLines();
@@ -86,7 +91,7 @@ class SectionAcceptanceTest extends BaseAcceptanceTest{
 
         //then
         ExtractableResponse<Response> line = getLine(1);
-        assertThat(line.jsonPath().getInt("stations[1].id")).isEqualTo(3);
+        assertThat(line.jsonPath().getInt("stations[1].id")).isEqualTo(2);
 
     }
 
@@ -98,17 +103,26 @@ class SectionAcceptanceTest extends BaseAcceptanceTest{
     @Test
     @DisplayName("지하철 노선의 하행 종점역이 아닌 역의 구간을 제거한다.")
     void removeIllegalDownStationSectionTest() {
+        //given
+        Map<String, Object> requestBody = getRequestBody("2","3",10);
+        createSection(requestBody);
 
+        //when
+        ExtractableResponse<Response> response = deleteSection(1, 2);
+        assertThat(response.jsonPath().getString("message"))
+            .isEqualTo(ExceptionMessages.getNotEndpointInputExceptionMessage(2,3));
     }
 
     /**
-     * When     삭제할 지하철 노선의 구간이 1개이면
-     * Then     에러처리한다.
+     * Then     삭제할 지하철 노선의 구간이 1개이면 에러처리한다.
      */
     @Test
     @DisplayName("지하철 노선의 구간이 1개일 때 구간을 제거한다.")
     void remainAtLeastOneSectionTest() {
-
+        //then
+        ExtractableResponse<Response> response = deleteSection(1, 2);
+        assertThat(response.jsonPath().getString("message"))
+            .isEqualTo(ExceptionMessages.getNeedAtLeastOneSectionExceptionMessage());
     }
 
     private ExtractableResponse<Response> createSection(Map<String, Object> requestBody) {
@@ -145,8 +159,8 @@ class SectionAcceptanceTest extends BaseAcceptanceTest{
             .extract();
     }
 
-    private void deleteSection(long lineId, long stationId) {
-        RestAssured
+    private ExtractableResponse<Response> deleteSection(long lineId, long stationId) {
+        return RestAssured
             .given().log().all()
             .param("stationId", stationId)
             .when().delete("/lines/{id}/sections", lineId)
