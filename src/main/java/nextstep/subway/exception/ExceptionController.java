4504b58d1516ed7c092bb848c2b18c9249a71136
uram <parkuram12@gmail.com>
2022-07-13 15:46:51 +0900

    feat: ExceptionHandler 추가
    - Exception 일괄 예외처리용 ControllerAdvice 추가
    - MethodArgumentNotValidException Handler 구현
    - ErrorResponse 클래스 생성

---
+++ src/main/java/nextstep/subway/exception/ExceptionController.java
@@ -0,0 +1,24 @@
+package nextstep.subway.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Objects;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodValidException(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(makeErrorResponse(e.getBindingResult()), HttpStatus.BAD_REQUEST);
+    }
+
+    private ErrorResponse makeErrorResponse(BindingResult bindingResult) {
+        String detail = Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage();
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), detail);
+    }
+}
