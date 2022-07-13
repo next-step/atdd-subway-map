ae9bbaa0e07ac04dbba4c559972af79048b83340
MoonHKLee <mf311110@gmail.com>
2022-07-14 00:24:17 +0900

    feat : 예외처리 반환을 위한 controller advice 생성.

---
+++ src/main/java/nextstep/subway/applicaion/exception/ExceptionAdvice.java
@@ -0,0 +1,14 @@
+package nextstep.subway.applicaion.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<ExceptionResponse> exceptionHandler(IllegalArgumentException e) {
+        return ResponseEntity.internalServerError().body(new ExceptionResponse(e.getMessage()));
+    }
+}
--- src/main/java/nextstep/subway/applicaion/exception/ExceptionMessages.java
+++ src/main/java/nextstep/subway/applicaion/exception/ExceptionMessages.java
@@ -2,17 +2,18 @@ package nextstep.subway.applicaion.exception;
 
 public class ExceptionMessages {
 
-    private static final String NO_LINE_EXCEPTION_MESSAGE = "노선이 존재하지 않습니다.";
-    private static final String NO_STATION_EXCEPTION_MESSAGE = "지하철역이 존재하지 않습니다.";
-
     private ExceptionMessages() {
     }
 
     public static String getNoLineExceptionMessage(long lineId) {
-        return NO_LINE_EXCEPTION_MESSAGE+" lineId="+lineId;
+        return "노선이 존재하지 않습니다. lineId="+lineId;
     }
 
     public static String getNoStationExceptionMessage(long stationId) {
-        return NO_STATION_EXCEPTION_MESSAGE+" stationId="+stationId;
+        return "지하철역이 존재하지 않습니다. stationId="+stationId;
+    }
+
+    public static String getNoEndpointInputExceptionMessage(long upStationId,long downEndpointStationId) {
+        return "상행선의 입력값이 하행종점역이 아닙니다. upStationId = "+upStationId+", downEndpointStationId = "+downEndpointStationId;
     }
 }
