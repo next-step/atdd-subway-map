45ca3a8c0bad2132bcbe49b1b0fc56e4d2126cf7
MoonHKLee <mf311110@gmail.com>
2022-07-13 18:03:18 +0900

    fix : no-args-constructor 추가.

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -3,12 +3,15 @@ package nextstep.subway.applicaion.dto;
 import nextstep.subway.domain.Line;
 
 public class LineRequest {
-    private final String name;
-    private final String color;
-    private final Long upStationId;
-    private final Long downStationId;
-    private final Long distance;
+    private String name;
+    private String color;
+    private Long upStationId;
+    private Long downStationId;
+    private Long distance;
 
+    public LineRequest() {
+
+    }
     public LineRequest(String name, String color, Long upStationId, Long downStationId, Long distance) {
         this.name = name;
         this.color = color;
