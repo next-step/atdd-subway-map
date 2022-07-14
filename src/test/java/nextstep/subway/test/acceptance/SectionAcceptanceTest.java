993e65d4461b95b6223b2e1f412300010ebb2f00
김민수 <minsu@sk.com>
2022-07-14 16:59:27 +0900

    refactor : 인수 조건 도출

--- src/test/java/nextstep/subway/test/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/test/acceptance/SectionAcceptanceTest.java
@@ -6,6 +6,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 import static nextstep.subway.test.acceptance.LineTestMethod.*;
 import static nextstep.subway.test.acceptance.StationTestMethod.*;
@@ -44,10 +45,9 @@ public class SectionAcceptanceTest extends AcceptanceTest{
         구간_생성(초록선_종착ID, 새로운구간_종착ID, 초록선_라인ID);
     }
     /**
-     * 지하철 노선에 구간을 등록하는 기능을 구현
-     * 새로운 구간의 상행역은 해당 노선에 등록되어있는 하행 종점역이어야 한다.
-     * 새로운 구간의 하행역은 해당 노선에 등록되어있는 역일 수 없다.
-     * 새로운 구간 등록시 위 조건에 부합하지 않는 경우 에러 처리한다.
+     * given 초록선 노선을 생성한다.
+     * when 초록선 노선에 구간을 생성한다.
+     * then 등록한 노선의 구간에 지하철역이 모두 조회 가능하다.
      */
     @Test
     @DisplayName("지하철구간을 생성한다")
@@ -67,10 +67,10 @@ public class SectionAcceptanceTest extends AcceptanceTest{
     }
 
     /**
-     * 지하철 노선에 구간을 제거하는 기능 구현
-     * 지하철 노선에 등록된 역(하행 종점역)만 제거할 수 있다. 즉, 마지막 구간만 제거할 수 있다.
-     * 지하철 노선에 상행 종점역과 하행 종점역만 있는 경우(구간이 1개인 경우) 역을 삭제할 수 없다.
-     * 새로운 구간 제거시 위 조건에 부합하지 않는 경우 에러 처리한다.
+     * given 초록선 노선을 생성한다.
+     * given 초록선 노선에 구간을 생성한다.(2개)
+     * when  구간을 삭제하면
+     * then  구간 목록에서 해당 구간을 찾을 수 없다. (종착역)
      */
     @Test
     @DisplayName("구간을 제거한다")
@@ -101,37 +101,64 @@ public class SectionAcceptanceTest extends AcceptanceTest{
 
     }
 
+    /**
+     * given 초록선 노선을 생성한다.
+     * given 초록선 노선에 구간을 생성한다.(2개)
+     * when  해당 노선의 하행 종점역에 맞지 않는 새로운 구간의 상행역 생성을 오청하면
+     * then  구간을 생성 할 수 없다.
+     */
     @Test
     @DisplayName("구간생성을 실패한다.")
     void failCreateSection(){
         //given
         Long 구간생성테스트_종착역ID = 지하철역_생성("홍대입구역").jsonPath().getLong("id");
-        구간_생성(초록선_시작ID, 구간생성테스트_종착역ID, 초록선_라인ID);
+
+        //when
+        ExtractableResponse<Response> response = 구간_생성(초록선_시작ID, 구간생성테스트_종착역ID, 초록선_라인ID);
         //새로운 구간의 상행역은 해당 노선에 등록되어있는 하행 종점역이어야 한다
-        // Exception 발생
+        // then 실패한다.
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
+
+    /**
+     * given 초록선 노선을 생성한다.
+     * given 초록선 노선에 구간을 생성한다.(2개)
+     * when  새로운 구간의 하행역을 해당 노선에 등록되어 있는 역으로 생성한다.
+     * then  구간을 생성 할 수 없다.
+     */
     @Test
     @DisplayName("구간생성을 실패한다 두번째 시나리오.")
     void failCreateSection2(){
-        //given
+        // given
         Long 구간생성테스트_종착역ID = 지하철역_생성("홍대입구역").jsonPath().getLong("id");
-        구간_생성(새로운구간_종착ID, 초록선_시작ID, 초록선_라인ID);
+
+        // when
+        ExtractableResponse<Response> response = 구간_생성(새로운구간_종착ID, 초록선_시작ID, 초록선_라인ID);
         //새로운 구간의 하행역은 해당 노선에 등록되어있는 역일 수 없다.
-        // Exception 발생
+        // then 실패한다.
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
+    /**
+     * given 초록선 노선을 생성한다.
+     * given 초록선 노선에 구간을 생성한다.(2개)
+     * when  마지막 구간이 아닌 구간삭제 요청을 한다.
+     * then  구간을 제거할 수 없다.
+     */
     @Test
-    @DisplayName("구간을 제거한다_실페 케이스1")
+    @DisplayName("구간을 제거한다_실페 케이스")
     void failDeleteSection(){
-
+        //given
         Long 구간제거테스트_종작역ID = 지하철역_생성("홍대입구역").jsonPath().getLong("id");
-
         구간_생성(새로운구간_종착ID, 구간제거테스트_종작역ID, 초록선_라인ID);
 
-        구간_제거(초록선_라인ID, 새로운구간_종착ID);
+        //when
+        ExtractableResponse<Response> response = 구간_제거(초록선_라인ID, 새로운구간_종착ID);
 
         //지하철 노선에 등록된 역(하행 종점역)만 제거할 수 있다. 즉, 마지막 구간만 제거할 수 있다.
-        // Exception 발생
+        // then 실패한다.
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
     }
 
 }
