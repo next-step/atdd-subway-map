3190e44b61527a203dd8c6133efca6d5ded080db
MangKyu <whalsrb1226@gmail.com>
2022-07-14 19:37:43 +0900

    인수테스트 격리 자동화 구현

--- src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
@@ -1,18 +1,13 @@
 package nextstep.subway.acceptance;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Sql("classpath:clear.sql")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Retention(RetentionPolicy.RUNTIME)
 @TestExecutionListeners(value = {MyTestExecutionListener.class,}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
-@ActiveProfiles("randomport")
 public @interface AcceptanceTest {
 }
--- src/test/java/nextstep/subway/acceptance/MyTestExecutionListener.java
+++ src/test/java/nextstep/subway/acceptance/MyTestExecutionListener.java
@@ -4,16 +4,60 @@ import io.restassured.RestAssured;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.support.AbstractTestExecutionListener;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyTestExecutionListener extends AbstractTestExecutionListener {
 
     @Override
-    public void beforeTestMethod(final TestContext testContext) throws Exception {
+    public void beforeTestMethod(final TestContext testContext) {
         final Integer serverPort = testContext.getApplicationContext().getEnvironment().getProperty("local.server.port", Integer.class);
         if (serverPort == null) {
             throw new IllegalStateException("localServerPort cannot be null");
         }
 
         RestAssured.port = serverPort;
+    }
+
+    @Override
+    public void afterTestExecution(final TestContext testContext) throws Exception {
+        final Connection connection = getConnection(testContext);
+        truncateTables(connection, getTruncateQueries(connection));
+        connection.close();
+    }
+
+    private Connection getConnection(final TestContext testContext) throws SQLException {
+        final DataSource dataSource = testContext.getApplicationContext().getBean(DataSource.class);
+        return dataSource.getConnection();
+    }
+
+    private List<String> getTruncateQueries(final Connection connection) throws SQLException {
+        final PreparedStatement statement = connection.prepareStatement("SELECT Concat('TRUNCATE TABLE ', TABLE_NAME, ';') AS q FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'");
+        final ResultSet resultSet = statement.executeQuery();
+        final List<String> queryList = new ArrayList<>();
+        while (resultSet.next()) {
+            queryList.add(resultSet.getString("q"));
+        }
+        return queryList;
+    }
+
+    private void truncateTables(final Connection connection, final List<String> truncateQueries) {
+        execute(connection, "SET REFERENTIAL_INTEGRITY FALSE");
+        truncateQueries.forEach(v -> execute(connection, v));
+        execute(connection, "SET REFERENTIAL_INTEGRITY TRUE");
+    }
+
+    private void execute(final Connection connection, final String v) {
+        try {
+            connection.prepareStatement(v).execute();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
 }
