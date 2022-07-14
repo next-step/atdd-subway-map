e7a75a55c4556345c7f1468529f06a8fcf143e1a
MangKyu <whalsrb1226@gmail.com>
2022-07-14 20:33:21 +0900

    JdbcTemplate을 사용하도록 리팩토링

--- src/test/java/nextstep/subway/acceptance/MyTestExecutionListener.java
+++ src/test/java/nextstep/subway/acceptance/MyTestExecutionListener.java
@@ -1,15 +1,10 @@
 package nextstep.subway.acceptance;
 
 import io.restassured.RestAssured;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.support.AbstractTestExecutionListener;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class MyTestExecutionListener extends AbstractTestExecutionListener {
@@ -25,39 +20,28 @@ public class MyTestExecutionListener extends AbstractTestExecutionListener {
     }
 
     @Override
-    public void afterTestExecution(final TestContext testContext) throws Exception {
-        final Connection connection = getConnection(testContext);
-        truncateTables(connection, getTruncateQueries(connection));
-        connection.close();
+    public void afterTestExecution(final TestContext testContext) {
+        final JdbcTemplate jdbcTemplate = getJdbcTemplate(testContext);
+        final List<String> truncateQueries = getTruncateQueries(jdbcTemplate);
+        truncateTables(jdbcTemplate, truncateQueries);
     }
 
-    private Connection getConnection(final TestContext testContext) throws SQLException {
-        final DataSource dataSource = testContext.getApplicationContext().getBean(DataSource.class);
-        return dataSource.getConnection();
+    private List<String> getTruncateQueries(final JdbcTemplate jdbcTemplate) {
+        return jdbcTemplate.queryForList("SELECT Concat('TRUNCATE TABLE ', TABLE_NAME, ';') AS q FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'", String.class);
     }
 
-    private List<String> getTruncateQueries(final Connection connection) throws SQLException {
-        final PreparedStatement statement = connection.prepareStatement("SELECT Concat('TRUNCATE TABLE ', TABLE_NAME, ';') AS q FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'");
-        final ResultSet resultSet = statement.executeQuery();
-        final List<String> queryList = new ArrayList<>();
-        while (resultSet.next()) {
-            queryList.add(resultSet.getString("q"));
-        }
-        return queryList;
+    private JdbcTemplate getJdbcTemplate(final TestContext testContext) {
+        return testContext.getApplicationContext().getBean(JdbcTemplate.class);
     }
 
-    private void truncateTables(final Connection connection, final List<String> truncateQueries) {
-        execute(connection, "SET REFERENTIAL_INTEGRITY FALSE");
-        truncateQueries.forEach(v -> execute(connection, v));
-        execute(connection, "SET REFERENTIAL_INTEGRITY TRUE");
+    private void truncateTables(final JdbcTemplate jdbcTemplate, final List<String> truncateQueries) {
+        execute(jdbcTemplate, "SET REFERENTIAL_INTEGRITY FALSE");
+        truncateQueries.forEach(v -> execute(jdbcTemplate, v));
+        execute(jdbcTemplate, "SET REFERENTIAL_INTEGRITY TRUE");
     }
 
-    private void execute(final Connection connection, final String v) {
-        try {
-            connection.prepareStatement(v).execute();
-        } catch (SQLException e) {
-            throw new IllegalStateException(e);
-        }
+    private void execute(final JdbcTemplate jdbcTemplate, final String query) {
+        jdbcTemplate.execute(query);
     }
 
 }
