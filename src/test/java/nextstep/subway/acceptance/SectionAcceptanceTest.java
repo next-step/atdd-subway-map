e6ab7318f273977285cf91dbb7b4996c0da18f8e
flamme <flamme1004@gmail.com>
2022-07-14 22:54:27 +0900

    test: 예외 경우에 대한 인수테스트를 작성하라

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -81,6 +81,31 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
+     * Given 역을 생성한다.
+     * When 생성한 역을 이용하여 구간을 추가한다
+     * Then 추가한 구간의 상행역은 해당 노선에 등록되어있는 하행 종점역이 아니면 등록이 실패한다.
+     */
+    @Test
+    void 하행_종점역이_아닌_역에_구간을_추가하면_실패한다() {
+        // given
+        var 한성백제역 = 역을_만들다("한성백제역").as(StationResponse.class).getId();
+        var 문정역 = 역을_만들다("문정역").as(StationResponse.class).getId();
+
+        // when & then
+        구간_생성에_실패한다(_8호선, 한성백제역, 문정역);
+    }
+
+    /**
+     * When 구간을 생성할 때
+     * Then 새로운 구간의 하행역이 해당 노선에 등록되어있으면 등록이 실패한다.
+     */
+    @Test
+    void 새로운_구간의_하행역은_해당_노선에_등록되어있는_역이면_실패한다() {
+        // when & then
+        구간_생성에_실패한다(_8호선, 모란역, 암사역);
+    }
+
+    /**
      * Given 지하철역 구간을 생성하고
      * When 지하철역 구간을 삭제하면
      * Then 지하철 노선 조회 시 삭제 된 구간이 존재하지 않는다.
@@ -101,6 +126,30 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         );
     }
 
+    /**
+     * When 상행 종점역과 하행 좀정역에 대한 구간을 삭제하면
+     * Then 상행 종점역과 하행 종점역만 있는 경우이기 때문에 삭제가 실패된다.
+     */
+    @Test
+    void 상행_종점역과_하행_종점역만_있는_경우_에_지하철_구간을_삭제하면_실패한다() {
+        // when & then
+        지하철_구간_삭제에_실패한다(_8호선, 암사역);
+    }
+
+    /**
+     * Given 지하철 구간을 추가하고
+     * When & Then 지하철 중간 구간을 삭제 시 실패한다
+     */
+    @Test
+    void 중간_지점_구간을_삭제하면_실패한다() {
+        // given
+        var 한성백제역 = 역을_만들다("한성백제역").as(StationResponse.class).getId();
+        구간을_만들다(_8호선, 암사역, 한성백제역, 10L);
+
+        // when & then
+        지하철_구간_삭제에_실패한다(_8호선, 암사역);
+    }
+
     private ExtractableResponse<Response> 구간을_만들다(Long id, Long upStationId, Long downStationId, Long distance) {
         var sectionRequest = new SectionRequest(upStationId, downStationId, distance);
         var response = postRequestWithParameterAndRequestBody("/lines/{id}/sections", id, sectionRequest);
@@ -108,9 +157,22 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         return response;
     }
 
+    private void 구간_생성에_실패한다(Long lineId, Long upStationId, Long downStationId) {
+        var sectionRequest = new SectionRequest(upStationId, downStationId, 10L);
+        var response = postRequestWithParameterAndRequestBody("/lines/{id}/sections", lineId, sectionRequest);
+
+        // Then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
     private ExtractableResponse<Response> 구간을_삭제한다(Long lineId, Long stationId) {
         var response = deleteRequestWithParameter("/lines/{id}/sections?stationId=" + stationId, lineId);
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
         return response;
+    }
+
+    private void 지하철_구간_삭제에_실패한다(Long lineId, Long stationId) {
+        var response = deleteRequestWithParameter("/lines/{id}/sections?stationId=" + lineId, stationId);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
