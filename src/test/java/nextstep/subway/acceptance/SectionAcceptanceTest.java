6b265ea6ad883194de47c0c39fc490f7c83939ea
wlroh <wlroh.paul@gmail.com>
2022-07-15 00:49:35 +0900

    test: 구간 등록 인수테스트 수정 및 가독성 개선

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -18,83 +18,74 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("구간 관련 기능")
 public class SectionAcceptanceTest extends BaseAcceptanceTest {
 
+    Long 상행역_ID;
+    Long 하행역_ID;
+    Long _2호선_ID;
+
     @BeforeEach
     void dataSetUp() {
-        StationAcceptanceTest.지하철역_등록_요청("강남역");
-        StationAcceptanceTest.지하철역_등록_요청("역삼역");
-        StationAcceptanceTest.지하철역_등록_요청("판교역");
-
-        LineAcceptanceTest.노선_생성_요청("2호선", "bg-green-600", 1L, 2L, 10);
+        상행역_ID = 지하철역_생성("상행역");
+        하행역_ID = 지하철역_생성("하행역");
+        _2호선_ID = 노선_생성("2호선", "bg-green-600", 상행역_ID, 하행역_ID, 10);
     }
 
     /**
-     * `Given`  지하철 노선을 조회하고
-     * `When`   구간(`상행역` : 조회한 노선의 하행 종점역이 아닌 역, `하행역` : 노선에 등록되어 있지 않은 역)을 등록하면
-     * `Then`   400 에러 코드를 응답받는다.
+     * `Given`  지하철 역을 생성하고
+     * `When`   구간(`상행역` : 하행 종점역이 아닌 역, `하행역` : 새로 생성한 역)을 등록하면
+     * `Then`   400 에러 코드를 응답받고 등록한 구간을 찾을 수 없다.
      */
     @Test
     @DisplayName("구간 등록 예외 - 잘못된 상행역")
     void createSection_invalid() {
         // given
-        final long _2호선_노선_ID = 1L;
-        ExtractableResponse<Response> response = LineAcceptanceTest.노선_조회_요청(_2호선_노선_ID);
-        List<Long> _2호선_지하철역_ID_목록 = response.jsonPath().getList("stations.id", Long.class);
-        final int 상행역_INDEX = 0;
-        final long _2호선_상행종점역_ID = _2호선_지하철역_ID_목록.get(상행역_INDEX);
+        Long 신규역_ID = 지하철역_생성("신규역");
 
         // when
         ExtractableResponse<Response> 구간_생성_요청_결과 =
-                구간_등록_요청(_2호선_노선_ID, _2호선_상행종점역_ID, 3L, 5, HttpStatus.BAD_REQUEST);
+                구간_등록_요청(_2호선_ID, 상행역_ID, 신규역_ID, 5, HttpStatus.BAD_REQUEST);
 
         // then
-        assertThat(구간_생성_요청_결과.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        List<Long> _2호선_지하철_목록 = 노선_지하철_목록_조회(_2호선_ID);
+        assertThat(_2호선_지하철_목록).doesNotContain(신규역_ID);
     }
 
     /**
-     * `Given` 지하철 노선을 조회하고
-     * `When` 구간(`상행역` : 조회한 노선의 하행 종점역, `하행역` : 노선에 등록되어 있는 역)을 등록하면
-     * `Then` 400 에러 코드를 응답받는다.
+     * `Given` 지하철 역을 생성하고
+     * `When` 구간(`상행역` : 하행 종점역, `하행역` : 노선에 등록되어 있는 역)을 등록하면
+     * `Then` 400 에러 코드를 응답받고 등록한 구간을 찾을 수 없다.
      */
     @Test
     @DisplayName("구간 등록 예외 - 잘못된 하행역")
     void createSection_invalid_wrong_downStation() {
         // given
-        final long _2호선_노선_ID = 1L;
-        ExtractableResponse<Response> response = LineAcceptanceTest.노선_조회_요청(_2호선_노선_ID);
-        List<Long> _2호선_지하철역_ID_목록 = response.jsonPath().getList("stations.id", Long.class);
-        final int 하행역_INDEX = 1;
-        final long _2호선_하행종점역_ID = _2호선_지하철역_ID_목록.get(하행역_INDEX);
+        Long 신규역_ID = 지하철역_생성("신규역");
 
         // when
         ExtractableResponse<Response> 구간_생성_요청_결과 =
-                구간_등록_요청(_2호선_노선_ID, _2호선_하행종점역_ID, 1L, 5, HttpStatus.BAD_REQUEST);
+                구간_등록_요청(_2호선_ID, 하행역_ID, 상행역_ID, 5, HttpStatus.BAD_REQUEST);
 
         // then
-        assertThat(구간_생성_요청_결과.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        List<Long> _2호선_지하철_목록 = 노선_지하철_목록_조회(_2호선_ID);
+        assertThat(_2호선_지하철_목록).doesNotContain(신규역_ID);
     }
 
     /**
-     * `Given` 지하철 노선을 조회하고
-     * `When` 구간(`상행역` : 조회한 노선의 하행 종점역, `하행역` : 노선에 등록되어 있지 않은 역)을 등록하면
+     * `Given` 지하철 역을 생성하고
+     * `When` 구간(`상행역` : 하행 종점역, `하행역` : 새로 생성한 역)을 등록하면
      * `Then` 해당 지하철 노선을 조회 시 등록한 구간을 찾을 수 있다.
      */
     @Test
     @DisplayName("구간 등록")
     void createSection() {
         // given
-        final long _2호선_노선_ID = 1L;
-        ExtractableResponse<Response> response = LineAcceptanceTest.노선_조회_요청(_2호선_노선_ID);
-        List<Long> _2호선_지하철역_ID_목록 = response.jsonPath().getList("stations.id", Long.class);
-        final int 하행역_INDEX = 1;
-        final long _2호선_하행종점역_ID = _2호선_지하철역_ID_목록.get(하행역_INDEX);
+        Long 신규역_ID = 지하철역_생성("신규역");
 
         // when
-        구간_등록_요청(_2호선_노선_ID, _2호선_하행종점역_ID, 3L, 5, HttpStatus.CREATED);
+        구간_등록_요청(_2호선_ID, 하행역_ID, 신규역_ID, 5, HttpStatus.CREATED);
 
         // then
-        ExtractableResponse<Response> 노선_조회_응답 = LineAcceptanceTest.노선_조회_요청(_2호선_노선_ID);
-        List<String> _2호선_지하철역_이름_목록 = 노선_조회_응답.jsonPath().getList("stations.name", String.class);
-        assertThat(_2호선_지하철역_이름_목록).containsOnly("강남역", "역삼역", "판교역");
+        List<Long> _2호선_지하철_목록 = 노선_지하철_목록_조회(_2호선_ID);
+        assertThat(_2호선_지하철_목록).contains(신규역_ID);
     }
 
     private ExtractableResponse<Response> 구간_등록_요청(long id, long upStationId, long downStationId, int distance, HttpStatus httpStatus) {
@@ -111,5 +102,23 @@ public class SectionAcceptanceTest extends BaseAcceptanceTest {
                 .extract();
         assertThat(response.statusCode()).isEqualTo(httpStatus.value());
         return response;
+    }
+
+    private Long 지하철역_생성(String name) {
+        return StationAcceptanceTest.지하철역_등록_요청(name)
+                .jsonPath()
+                .getLong("id");
+    }
+
+    private Long 노선_생성(String name, String color, Long upStationId, Long downStationId, int distance) {
+        return LineAcceptanceTest.노선_생성_요청(name, color, upStationId, downStationId, distance)
+                .jsonPath()
+                .getLong("id");
+    }
+
+    private List<Long> 노선_지하철_목록_조회(Long id) {
+        return LineAcceptanceTest.노선_조회_요청(id)
+                .jsonPath()
+                .getList("stations.id", Long.class);
     }
 }
