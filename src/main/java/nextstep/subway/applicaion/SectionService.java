f81ad2866d3ca3bfeefb5a99880d99d787127de7
lsj8367 <lsj8367@naver.com>
2022-07-15 13:56:52 +0900

    :sparkles: 노선 구간 등록 api 구현

---
+++ src/main/java/nextstep/subway/applicaion/SectionService.java
@@ -0,0 +1,52 @@
+package nextstep.subway.applicaion;
+
+import java.util.stream.Collectors;
+import nextstep.subway.applicaion.dto.SectionCreateRequest;
+import nextstep.subway.applicaion.dto.SectionResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.subway.domain.Distance;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.SectionRepository;
+import nextstep.subway.domain.Station;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class SectionService {
+
+    private final SectionRepository sectionRepository;
+    private final LineService lineService;
+    private final StationService stationService;
+
+    public SectionService(final SectionRepository sectionRepository, final LineService lineService,
+        final StationService stationService) {
+        this.sectionRepository = sectionRepository;
+        this.lineService = lineService;
+        this.stationService = stationService;
+    }
+
+    @Transactional
+    public SectionResponse addSections(Long id, final SectionCreateRequest request) {
+        Line line = lineService.findById(id);
+
+        Station beforeDownStation = line.beforeDownStation();
+
+        beforeDownStation.isSameStationId(request.getUpStationId());
+
+        Station downStation = stationService.findById(request.getDownStationId());
+
+        Section section = new Section(beforeDownStation, downStation,
+            new Distance(request.getDistance()));
+
+        Section savedSection = sectionRepository.save(section);
+
+        line.addSection(savedSection);
+
+        return new SectionResponse(savedSection.getLine().getId(),
+            savedSection.getStations().toList().stream()
+                .map(station -> new StationResponse(station.getId(), station.getName()))
+                .collect(Collectors.toList()));
+    }
+}
---
+++ src/main/java/nextstep/subway/ui/SectionController.java
@@ -0,0 +1,29 @@
+package nextstep.subway.ui;
+
+import java.net.URI;
+import nextstep.subway.applicaion.SectionService;
+import nextstep.subway.applicaion.dto.SectionCreateRequest;
+import nextstep.subway.applicaion.dto.SectionResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SectionController {
+
+    private final SectionService sectionService;
+
+    public SectionController(SectionService sectionService) {
+        this.sectionService = sectionService;
+    }
+
+    @PostMapping("/lines/{id}/sections")
+    public ResponseEntity<Object> addSections(@PathVariable final Long id,
+        @RequestBody final SectionCreateRequest request) {
+        SectionResponse sectionResponse = sectionService.addSections(id, request);
+        return ResponseEntity.created(URI.create("/lines/" + sectionResponse.getLineId() + "/sections")).body(sectionResponse);
+    }
+
+}
