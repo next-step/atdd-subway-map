cebd6065006967801b3e838edcd88c5d23f93f5b
lsj8367 <lsj8367@naver.com>
2022-07-15 16:04:37 +0900

    :white_check_mark: 노선 구간 제거 테스트

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -35,10 +35,9 @@ public class SectionAcceptanceTest extends BaseAcceptance {
         //given
         long lineId = 지하철_노선_생성("강남역", "양재역", "신분당선", "red");
 
-        ExtractableResponse<Response> newDownStationResponse = 지하철_역_생성("대림역");
-        long newDownStationId = newDownStationResponse.jsonPath().getLong("id");
+        long newDownStationId = 지하철_역_생성_후_id값_반환("대림역");
 
-        ExtractableResponse<Response> extract = 새로운_구간_등록(lineId, newDownStationId);
+        ExtractableResponse<Response> extract = 새로운_구간_등록(lineId, newDownStationId, 2);
 
         long actualLineId = extract.jsonPath().getLong("lineId");
         String upStationName = extract.jsonPath().getString("stations[0].name");
@@ -51,20 +50,58 @@ public class SectionAcceptanceTest extends BaseAcceptance {
         );
     }
 
+    private long 지하철_역_생성_후_id값_반환(final String stationName) {
+        ExtractableResponse<Response> newDownStationResponse = 지하철_역_생성(stationName);
+        return newDownStationResponse.jsonPath().getLong("id");
+    }
+
+    @Test
+    @DisplayName("지하철 노선 구간 제거")
+    void deleteSection() {
+        long lineId = 지하철_노선_생성("강남역", "양재역", "신분당선", "red");
+
+        long newDownStationId = 지하철_역_생성_후_id값_반환("판교역");
+        long newDownStationId2 = 지하철_역_생성_후_id값_반환("광교역");
+
+        ExtractableResponse<Response> 첫_구간_등록_응답 = 새로운_구간_등록(lineId, newDownStationId,
+            2);
+
+        long 첫_새로운_구간등록_후_id = 첫_구간_등록_응답.jsonPath().getLong("stations[1].id");
+
+        ExtractableResponse<Response> 마지막_구간_등록_응답 = 새로운_구간_등록(lineId, newDownStationId2,
+            첫_새로운_구간등록_후_id);
+
+        long 하행_종점역_id = 마지막_구간_등록_응답.jsonPath().getLong("stations[1].id");
+
+        ExtractableResponse<Response> removeResponse = 지하철_구간_삭제(lineId, 하행_종점역_id);
+
+        assertThat(removeResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        findAllLines();
+    }
+
+    private ExtractableResponse<Response> 지하철_구간_삭제(long lineId,
+        long removeStationId) {
+        return RestAssured.given().log().all()
+            .param("stationId", removeStationId)
+            .when().delete("/lines/" + lineId + "/sections")
+            .then().log().all()
+            .extract();
+    }
+
     private ExtractableResponse<Response> 새로운_구간_등록(long lineId,
-        long newDownStationId) {
+        long newDownStationId, final long upStationId) {
         Map<String, Object> param = Map.of(
             "downStationId", newDownStationId,
-            "upStationId", 2,
+            "upStationId", upStationId,
             "distance", 10
         );
 
-        ExtractableResponse<Response> extract = RestAssured.given().log().all()
+        return RestAssured.given().log().all()
             .body(param).contentType(MediaType.APPLICATION_JSON_VALUE)
             .when().post("/lines/" + lineId + "/sections")
             .then().log().all()
             .extract();
-        return extract;
     }
 
 }
