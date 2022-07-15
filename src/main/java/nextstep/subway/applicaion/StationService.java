3ccb8835c2e43a76ea716da804e4414c17eb96ac
lsj8367 <lsj8367@naver.com>
2022-07-15 11:49:50 +0900

    :sparkles: 역 찾기 로직 구현

--- src/main/java/nextstep/subway/applicaion/StationService.java
+++ src/main/java/nextstep/subway/applicaion/StationService.java
@@ -4,6 +4,7 @@ import nextstep.subway.applicaion.dto.StationRequest;
 import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import nextstep.subway.exception.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,7 +14,7 @@ import java.util.stream.Collectors;
 @Service
 @Transactional(readOnly = true)
 public class StationService {
-    private StationRepository stationRepository;
+    private final StationRepository stationRepository;
 
     public StationService(StationRepository stationRepository) {
         this.stationRepository = stationRepository;
@@ -31,10 +32,9 @@ public class StationService {
                 .collect(Collectors.toList());
     }
 
-    public List<StationResponse> findBothEndStations(final List<Long> bothEndIdList) {
-        return stationRepository.findAllByIdIn(bothEndIdList)
-            .stream().map(this::createStationResponse)
-            .collect(Collectors.toList());
+    public Station findById(final Long id) {
+        return stationRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("해당하는 지하철역을 찾을 수 없습니다."));
     }
 
     @Transactional
