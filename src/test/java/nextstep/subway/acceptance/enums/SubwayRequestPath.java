2fb0043798d2d66743a6bb3f7bdd750c2a710502
uram <parkuram12@gmail.com>
2022-07-16 14:52:27 +0900

    refactor: SubwayRequestPath 사용되지 않는 함수 삭제

--- src/test/java/nextstep/subway/acceptance/enums/SubwayRequestPath.java
+++ src/test/java/nextstep/subway/acceptance/enums/SubwayRequestPath.java
@@ -3,16 +3,20 @@ package nextstep.subway.acceptance.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.File;
+
 @AllArgsConstructor
 @Getter
 public enum SubwayRequestPath {
     STATION("/stations", "/{id}"),
-    LINE("/lines", "/{id}");
+    LINE("/lines", "/{id}"),
+
+    SECTION("/sections", "/{id}");
 
     private final String value;
     private final String pathParam;
 
-    public String addPathParam() {
-        return value + pathParam;
+    public String sectionsRequestPath(Long id) {
+        return LINE.getValue() + File.separator + id + SECTION.getValue();
     }
 }
