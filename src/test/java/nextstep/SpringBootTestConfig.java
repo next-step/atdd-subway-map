f9c5bc9a126c94e6160e14ebc145886ba4481ccc
uram <parkuram12@gmail.com>
2022-07-16 16:07:52 +0900

    refactor:  SpringBootTestConfig 수정
    - setUp 메서드 접근 지시자 변경

--- src/test/java/nextstep/SpringBootTestConfig.java
+++ src/test/java/nextstep/SpringBootTestConfig.java
@@ -3,10 +3,6 @@ package nextstep;
 
 import io.restassured.RestAssured;
 import lombok.RequiredArgsConstructor;
-import nextstep.subway.acceptance.client.SubwayRestAssured;
-import nextstep.subway.domain.Line;
-import nextstep.subway.domain.Station;
-import nextstep.subway.domain.domain.Section;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -15,13 +11,11 @@ import org.springframework.boot.web.server.LocalServerPort;
 @SpringBootTest(webEnvironment = org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RequiredArgsConstructor
 public class SpringBootTestConfig {
-
-
     @LocalServerPort
     int port;
 
     @BeforeEach
-    public void setUp() {
+    protected void setUp() {
         RestAssured.port = port;
     }
 }
