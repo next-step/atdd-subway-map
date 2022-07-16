d594d5764efd72527462c340cde8791a0713282c
uram <parkuram12@gmail.com>
2022-07-16 16:07:00 +0900

    test: 구간 등록 실패 테스트 - 이미 등록된 하행역일 경우, 상행역이 하행 종점이 아닐 경우
    - 지하철_구간_실패_등록된_하행역() 시나리오 수정
    - 지하철_구간_실패_하행_종점() 테스트 추가

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -56,8 +56,8 @@ class SectionAcceptanceTest extends SpringBootTestConfig {
     }
 
     /*
-     * Given 4개의 지하철역과 지하철역 리스트를 생성한다.
-     * When 지하철역 리스트에 역을 추가한다.
+     * Given 4개의 지하철역과 구간목록을 생성한다.
+     * When 구간목록에 구되을 추가한다.
      * Then 동일한 지하철역을 추가했을 때 Exception 발생한다.
      */
     @Test
@@ -68,8 +68,8 @@ class SectionAcceptanceTest extends SpringBootTestConfig {
         Station 선릉역 = new Station(2L, "선릉역");
         Section 첫번째_구간 = new Section(강남역.getId(), 선릉역.getId(), 10);
 
-        Station 영통역 = new Station(3L, "영통역");
-        Section 두번째_구간 = new Section(영통역.getId(), 선릉역.getId(), 10);
+        Station 영통역 = new Station(2L, "영통역");
+        Section 두번째_구간 = new Section(영통역.getId(), 강남역.getId(), 10);
 
         //when
         Sections sections = new Sections(List.of(첫번째_구간));
@@ -81,11 +81,37 @@ class SectionAcceptanceTest extends SpringBootTestConfig {
     }
 
 
+    /*
+     * Given 4개의 지하철역과 구간, 구간목록을 생성한다.
+     * When 구간목록에 구간을 추가한다.
+     * Then 새로 추가할 구간의 상행역이 하행 종점역과 동일하지 않으면 Exception
+     */
+    @Test
+    @DisplayName("지하철 구간 실패 테스트 - 새로운 구간의 상행역이 하행 종점이 아닌 경우")
+    void 지하철_구간_실패_하행_종점() {
+        //given
+        Station 강남역 = new Station(1L, "강남역");
+        Station 선릉역 = new Station(2L, "선릉역");
+        Section 첫번째_구간 = new Section(강남역.getId(), 선릉역.getId(), 10);
+
+        Station 영통역 = new Station(3L, "영통역");
+        Station 판교역 = new Station(4L, "판교역");
+        Section 두번째_구간 = new Section(영통역.getId(), 판교역.getId(), 10);
+
+        //when
+        Sections sections = new Sections(List.of(첫번째_구간));
+
+        //then
+        assertThatThrownBy(() -> sections.add(두번째_구간))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구간의 상행역이 노선의 하행 종점역이 아닙니다.");
+    }
+
+
     /* Given 2개의 지하철역을 생성하고, 지하철 구간에 추가한다. 지하철 구간을 노선에 추가한다.
      * When 하행 종점의 지하철 구간을 삭제한다.
      * Then 하행 종점역이 비어있는지 확인한다.
      */
-
 
     private Line createLine() {
       return lineRestAssured.postRequest(SubwayRequestPath.LINE.getValue(), LineFactory.경춘선())
