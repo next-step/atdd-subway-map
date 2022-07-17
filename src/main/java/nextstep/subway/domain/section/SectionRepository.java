9a585b0e81374d2c176b42c2df42ab8ff7621668
chosundeveloper <iddaumid@naver.com>
2022-07-17 19:06:30 +0900

    feat : 구간 조회 쿼리 추가

--- src/main/java/nextstep/subway/domain/section/SectionRepository.java
+++ src/main/java/nextstep/subway/domain/section/SectionRepository.java
@@ -2,5 +2,10 @@ package nextstep.subway.domain.section;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface SectionRepository extends JpaRepository<Section, Long> {
+
+    Set<Section> findAllByLine_Id(Long lineId);
+
 }
