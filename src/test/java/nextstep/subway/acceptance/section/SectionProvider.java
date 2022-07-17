5ee635d9cfa6a3de67755e94856e46b7cc231435
asdfg5415 <asdfg5415@gmail.com>
2022-07-17 16:08:09 +0900

    test: SectionProvider

---
+++ src/test/java/nextstep/subway/acceptance/section/SectionProvider.java
@@ -0,0 +1,23 @@
+package nextstep.subway.acceptance.section;
+
+import static nextstep.subway.acceptance.section.SectionRestAssuredProvider.*;
+import static org.springframework.http.HttpStatus.*;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.acceptance.common.ProviderBase;
+
+public class SectionProvider extends ProviderBase {
+
+	public static ExtractableResponse<Response> 지하철_구간_추가_성공(Long lineId, Long upStationId, Long downStationId,
+		int distance) {
+		ExtractableResponse<Response> response = 지하철_구간_추가(lineId, upStationId, downStationId, distance);
+		응답코드_검증(response, CREATED);
+		return response;
+	}
+
+	public static void 지하철_노선_제거_성공(Long lineId, Long stationId) {
+		ExtractableResponse<Response> response = 지하철_구간_삭제(lineId, stationId);
+		응답코드_검증(response, NO_CONTENT);
+	}
+}
