8225d45d19f9774fe5958260881d8677b4e1cc79
chosundeveloper <iddaumid@naver.com>
2022-07-17 19:05:56 +0900

    feat : 도메인 구조 변경에 따른 response 멤버 변경

--- src/main/java/nextstep/subway/applicaion/dto/SectionResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/SectionResponse.java
@@ -3,12 +3,14 @@ package nextstep.subway.applicaion.dto;
 public class SectionResponse {
 
     private Long id;
+    private Long lineId;
     private Long upStationId;
     private Long downStationId;
     private Long distance;
 
-    public SectionResponse(Long id, Long upStationId, Long downStationId, Long distance) {
+    public SectionResponse(Long id, Long lineId, Long upStationId, Long downStationId, Long distance) {
         this.id = id;
+        this.lineId = lineId;
         this.upStationId = upStationId;
         this.downStationId = downStationId;
         this.distance = distance;
@@ -28,5 +30,9 @@ public class SectionResponse {
 
     public Long getDistance() {
         return distance;
+    }
+
+    public Long getLineId() {
+        return lineId;
     }
 }
