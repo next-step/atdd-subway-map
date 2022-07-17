30c5d2dcfc661accd96a4574a89c934d765ff765
asdfg5415 <asdfg5415@gmail.com>
2022-07-17 13:10:00 +0900

    refactor: 리뷰반영 - 인수테스트 setUp 제거

---
+++ src/test/java/nextstep/subway/acceptance/AcceptanceTestBase.java
@@ -0,0 +1,27 @@
+package nextstep.subway.acceptance;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import io.restassured.RestAssured;
+import nextstep.subway.acceptance.isolation.TestIsolationUtil;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class AcceptanceTestBase {
+	@LocalServerPort
+	int port;
+
+	@Autowired
+	TestIsolationUtil testIsolationUtil;
+
+	@BeforeEach
+	protected void setUp() {
+		RestAssured.port = port;
+		testIsolationUtil.clean();
+	}
+}
