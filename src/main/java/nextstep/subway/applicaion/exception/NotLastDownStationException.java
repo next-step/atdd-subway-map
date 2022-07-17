0b779e5ea7f7ec2e8e16aa5e7a196313f8f7ef8d
sangnyyy <dltkdals2202@gmail.com>
2022-07-18 07:37:57 +0900

    feat : NotLastDownStationException 추가

---
+++ src/main/java/nextstep/subway/applicaion/exception/NotLastDownStationException.java
@@ -0,0 +1,9 @@
+package nextstep.subway.applicaion.exception;
+
+public class NotLastDownStationException extends RuntimeException {
+
+    public NotLastDownStationException(String message) {
+        super(message);
+    }
+
+}
