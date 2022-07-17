dbd2fd200eff46fea16ec649f6f622cdad89f4bd
asdfg5415 <asdfg5415@gmail.com>
2022-07-17 15:49:32 +0900

    test: RestAssured 추가

---
+++ src/test/java/nextstep/subway/acceptance/section/SectionRestAssuredProvider.java
@@ -0,0 +1,37 @@
+package nextstep.subway.acceptance.section;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class SectionRestAssuredProvider {
+
+	public static ExtractableResponse<Response> 지하철_구간_추가(Long lineId, Long upStationId, Long downStationId,
+		int distance) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("upStationId", upStationId);
+		params.put("downStationId", downStationId);
+		params.put("distance", distance);
+
+		return RestAssured.given().log().all()
+			.body(params)
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.when().post("/lines/{lineId}/sections", lineId)
+			.then().log().all()
+			.extract();
+	}
+
+	public static ExtractableResponse<Response> 지하철_구간_삭제(Long lineId, Long stationId) {
+		return RestAssured.given().log().all()
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.queryParam("stationId", stationId)
+			.when().delete("/lines/{id}/sections", lineId)
+			.then().log().all()
+			.extract();
+	}
+}
