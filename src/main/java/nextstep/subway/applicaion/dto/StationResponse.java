5bff8558b8896706bb38fbd04043dc2db4d4c5af
jess <official1126@naver.com>
2022-07-17 21:44:25 +0900

    feat StationResponse equals&hashCode function

--- src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
@@ -1,8 +1,13 @@
 package nextstep.subway.applicaion.dto;
 
+import java.util.Objects;
+
 public class StationResponse {
     private Long id;
     private String name;
+
+    public StationResponse() {
+    }
 
     public StationResponse(Long id, String name) {
         this.id = id;
@@ -15,5 +20,18 @@ public class StationResponse {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StationResponse that = (StationResponse) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
