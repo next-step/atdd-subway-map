14d12b507b5975415853dcdf711b55b63937a0c0
sangnyyy <dltkdals2202@gmail.com>
2022-07-18 08:17:41 +0900

    feat : AlreadyExistStationException, StationNotRegisteredException 추가

---
+++ src/main/java/nextstep/subway/applicaion/exception/AlreadyExistStationException.java
@@ -0,0 +1,8 @@
+package nextstep.subway.applicaion.exception;
+
+public class AlreadyExistStationException extends RuntimeException {
+
+    public AlreadyExistStationException(String message) {
+        super(message);
+    }
+}
