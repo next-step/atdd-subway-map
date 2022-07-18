5bffeb631a668eecc40d3d938bd953cfa3bea88c
MRSTK90 <dkgell13@gmail.com>
2022-07-19 01:42:23 +0900

    fix : 삭제 관련 메소드 추가 및 일부 리턴 타입 수정

--- src/test/java/nextstep/subway/acceptance/RestUtil.java
+++ src/test/java/nextstep/subway/acceptance/RestUtil.java
@@ -6,13 +6,15 @@ import io.restassured.response.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RestUtil {
 
-    public Long createEntityData(Map<String, Object> params, String requestUrl) {
+    public Long createEntityData(String requestUrl, int expectHttpStatusCode, Map<String, Object> params) {
         ExtractableResponse<Response> response =
                 RestAssured.given().log().all()
                         .body(params)
@@ -20,10 +22,13 @@ public class RestUtil {
                         .when().post(requestUrl)
                         .then().log().all()
                         .extract();
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.statusCode()).isEqualTo(expectHttpStatusCode);
+        if(expectHttpStatusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()){
+            return -1L;
+        }
         return ((Number)response.jsonPath().get("id")).longValue();
     }
-    public Long registEntityData(Map<String, Object> params, String requestUrl){
+    public Long registEntityData(String requestUrl, Map<String, Object> params){
         ExtractableResponse<Response> response =
                 RestAssured.given().log().all()
                         .body(params)
@@ -42,6 +47,12 @@ public class RestUtil {
                 .then().log().all()
                 .extract();
     }
+    public <T> List<T> getResponseJsonListDataByKey(String requestUrl, String key, Class<T> klazz){
+
+        return getResponseData(requestUrl)
+                .jsonPath().getList(key,klazz)
+                .stream().collect(Collectors.toList());
+    }
 
     public ExtractableResponse<Response> getResponseDataById(String requestUrl, Long id){
         return RestAssured.given().log().all()
@@ -54,6 +65,15 @@ public class RestUtil {
     public ExtractableResponse<Response> deleteEntityDataById(String requestUrl, Long id){
         return RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete(requestUrl, id)
+                .then().log().all()
+                .extract();
+    }
+
+    public ExtractableResponse<Response> deleteEntityDataByIdWithQueryParam(String requestUrl, Long id, String key, Long value){
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .given().queryParam(key, value)
                 .when().delete(requestUrl, id)
                 .then().log().all()
                 .extract();
