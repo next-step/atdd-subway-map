08dd171d5786a3b842d0c5647a215e7be0e11d33
DahamLeee <dlek1011@gmail.com>
2022-07-18 17:27:26 +0900

    feat: [add] deleteSectionException

--- src/main/java/nextstep/subway/advice/LineControllerAdvice.java
+++ src/main/java/nextstep/subway/advice/LineControllerAdvice.java
@@ -1,6 +1,7 @@
 package nextstep.subway.advice;
 
 import nextstep.subway.exception.AddSectionException;
+import nextstep.subway.exception.DeleteSectionException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,6 +11,11 @@ public class LineControllerAdvice {
 
     @ExceptionHandler(AddSectionException.class)
     public ResponseEntity<ErrorResult> duplicateStationException(AddSectionException e) {
+        return ResponseEntity.badRequest().body(new ErrorResult(e.getMessage()));
+    }
+
+    @ExceptionHandler(DeleteSectionException.class)
+    public ResponseEntity<ErrorResult> deleteSectionException(DeleteSectionException e) {
         return ResponseEntity.badRequest().body(new ErrorResult(e.getMessage()));
     }
 }
