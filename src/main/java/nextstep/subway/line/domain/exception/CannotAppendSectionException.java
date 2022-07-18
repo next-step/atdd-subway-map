44152a4073a80f8f0f2f7bbfb4f7fb873ac35a9f
imcool2551 <imcool2551@gmail.com>
2022-07-18 13:42:27 +0900

    feat: Line 도메인 예외 클래스들

---
+++ src/main/java/nextstep/subway/line/domain/exception/CannotAppendSectionException.java
@@ -0,0 +1,8 @@
+package nextstep.subway.line.domain.exception;
+
+public class CannotAppendSectionException extends RuntimeException {
+
+    public CannotAppendSectionException(String message) {
+        super(message);
+    }
+}
