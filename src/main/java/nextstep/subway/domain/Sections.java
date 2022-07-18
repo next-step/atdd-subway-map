99dd65f3c0f15cc81671dd41dd3e1e5940cd3930
DahamLeee <dlek1011@gmail.com>
2022-07-18 16:41:05 +0900

    feat: exception when add section[not matched last station and new upstation]

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -1,5 +1,7 @@
 package nextstep.subway.domain;
 
+import nextstep.subway.exception.UnmatchedLastStationAndNewUpStationException;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.FetchType;
@@ -16,7 +18,26 @@ public class Sections {
     private List<Section> value = new ArrayList<>();
 
     public void addSection(Section section) {
+        if (value.isEmpty()) {
+            this.value.add(section);
+            return;
+        }
+
+        matchLastStationAndNewUpStation(section.getUpStation());
+
         this.value.add(section);
+    }
+
+    private void matchLastStationAndNewUpStation(Station upStation) {
+        Station lastStation = value.get(lastSection()).getDownStation();
+
+        if (!lastStation.equals(upStation)) {
+            throw new UnmatchedLastStationAndNewUpStationException("기존 노선의 종점역과 신규 노선의 상행역이 일치하지 않습니다.");
+        }
+    }
+
+    private int lastSection() {
+        return value.size() - 1;
     }
 
     public List<Station> allStations() {
