5859f535e3c7ae37b1c8917151908382c8d1e026
MRSTK90 <dkgell13@gmail.com>
2022-07-19 01:43:42 +0900

    fix : 헬퍼 클래스, 유틸 클래스 수정을 통한 리팩토링, 인수 테스트 내용 추가

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,203 +31,159 @@ public class SectionAcceptanceTest {
     public void setUp() throws Exception {
         RestAssured.port = port;
         databaseTruncator.afterPropertiesSet();
+        databaseTruncator.cleanTable();
     }
 
     private static Map<String, Object> line1;
-    private static Map<String, Object> section1;
-    private static Map<String, Object> section2;
-    private static Map<String, Object> section3;
-    private static Map<String, Object> station1;
-    private static Map<String, Object> station2;
-    private static Map<String, Object> station3;
-    private static Map<String, Object> station4;
+    private static List<Map<String, Object>> sections;
+    private static List<Map<String, Object>> stations;
 
     static RestUtil restUtil;
+    static SubwayTestHelper testHelper;
 
     @BeforeAll
     static void init() {
         restUtil = new RestUtil();
+        testHelper = new SubwayTestHelper();
         line1 = new HashMap<>();
         line1.put("name", "신분당선");
         line1.put("color", "bg-red-600");
-        line1.put("upStationId", 1);
-        line1.put("downStationId", 2);
-        line1.put("distance", 10);
-
-        section1 = new HashMap<>();
-        section1.put("upStationId", 2);
-        section1.put("downStationId", 3);
-        section1.put("distance", 10);
-
-        section2 = new HashMap<>();
-        section2.put("upStationId", 3);
-        section2.put("downStationId", 4);
-        section2.put("distance", 10);
-
-        section3 = new HashMap<>();
-        section3.put("upStationId", 2);
-        section3.put("downStationId", 1);
-        section3.put("distance", 10);
-
-        station1 = new HashMap<>();
-        station1.put("name", "강남역");
-
-        station2 = new HashMap<>();
-        station2.put("name", "양재역");
-
-        station3 = new HashMap<>();
-        station3.put("name", "교대역");
-
-        station4 = new HashMap<>();
-        station4.put("name", "신촌역");
-
+        line1.put("upStationId", 1L);
+        line1.put("downStationId", 2L);
+        line1.put("distance", 10L);
+        sections = testHelper.makeSectionList(List.of(2L, 3L, 10L), List.of(3L, 4L, 10L), List.of(2L, 1L, 10L));
+        stations = testHelper.makeStationList("강남역", "양재역", "교대역", "신촌역");
 
     }
 
-    @AfterEach
-    void tableClear() {
-        databaseTruncator.cleanTable();
-    }
-
-    /*
+    /**
+     * Given 지하철 역과 노선을 생성하고
      * When 지하철 노선에 지하철 구간을 등록하면
      * Then 지하철 노선 조회 시 등록한 지하철 구간을 확인할 수 있다
-     * */
+     */
     @DisplayName("지하철 구간을 등록한다")
     @Test
     void registerSection() {
-        restUtil.createEntityData(station1, "/stations");
-        restUtil.createEntityData(station2, "/stations");
-        restUtil.createEntityData(station3, "/stations");
-        restUtil.createEntityData(station4, "/stations");
-        Long id = restUtil.createEntityData(line1, "/lines");
+        //Given
+        testHelper.makeGivenCondition("/stations", HttpStatus.CREATED.value(), stations.get(0), stations.get(1), stations.get(2), stations.get(3));
+        Long id = restUtil.createEntityData("/lines", HttpStatus.CREATED.value(), line1);
 
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(section1)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/lines/" + id + "/sections")
-                        .then().log().all()
-                        .extract();
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        //When
+        testHelper.makeGivenCondition("/lines/" + id + "/sections", HttpStatus.OK.value(), sections.get(0), sections.get(1));
 
-        ExtractableResponse<Response> response2 =
-                RestAssured.given().log().all()
-                        .body(section2)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/lines/" + id + "/sections")
-                        .then().log().all()
-                        .extract();
-        assertThat(response2.statusCode()).isEqualTo(HttpStatus.OK.value());
-
-
+        //Then
         ExtractableResponse<Response> lineResponse = restUtil.getResponseDataById("/lines/{id}", id);
-
         assertThat(lineResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
 
 
     }
 
+    /**
+     * Given 지하철역과 노선을 생성하고
+     * When 생성한 지하철 노선의 하행과 등록할 구간의 상행이 맞지 않으면
+     * Then 구간을 등록할 수 없어 에러가 출력된다
+     */
     @DisplayName("등록된 노선의 하행과 새로 등록할 구간의 상행이 맞지 않으면 등록할 수 없다.")
     @Test
     void matchStation() {
-        restUtil.createEntityData(station1, "/stations");
-        restUtil.createEntityData(station2, "/stations");
-        restUtil.createEntityData(station3, "/stations");
-        restUtil.createEntityData(station4, "/stations");
-        Long id = restUtil.createEntityData(line1, "/lines");
+        //Given
+        testHelper.makeGivenCondition("/stations", HttpStatus.CREATED.value(), stations.get(0), stations.get(1), stations.get(2), stations.get(3));
+        Long id = restUtil.createEntityData("/lines", HttpStatus.CREATED.value(), line1);
 
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(section2)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/lines/" + id + "/sections")
-                        .then().log().all()
-                        .extract();
+        //When
+        Long result = restUtil.createEntityData("/lines/" + id + "/sections", HttpStatus.INTERNAL_SERVER_ERROR.value(), sections.get(1));
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        //Then
+        assertThat(result).isEqualTo(-1L);
     }
 
+    /**
+     * Given 지하철역과 노선을 생성하고
+     * When 등록하려는 구간의 하행이 이미 지하철 노선에 등록되어 있다면
+     * Then 구간을 등록할 수 없어 에러가 출력된다
+     */
     @DisplayName("등록할 구간의 하행은 이미 등록되어 있으면 안된다")
     @Test
-    void checkDuplicate(){
-        restUtil.createEntityData(station1, "/stations");
-        restUtil.createEntityData(station2, "/stations");
-        Long id = restUtil.createEntityData(line1, "/lines");
+    void checkDuplicateStation() {
+        //Given
+        testHelper.makeGivenCondition("/stations", HttpStatus.CREATED.value(), stations.get(0), stations.get(1), stations.get(2));
+        Long id = restUtil.createEntityData("/lines", HttpStatus.CREATED.value(), line1);
 
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(section3)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/lines/" + id + "/sections")
-                        .then().log().all()
-                        .extract();
+        //When
+        Long result = restUtil.createEntityData("/lines/" + id + "/sections", HttpStatus.INTERNAL_SERVER_ERROR.value(), sections.get(2));
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        //Then
+        assertThat(result).isEqualTo(-1L);
     }
 
-    /*
+    /**
      * Given 지하철 노선에 지하철 구간을 등록하고
      * when 등록한 지하설 구간을 삭제하면
      * Then 해당 지하철 구간은 삭제된다
-     * */
+     */
     @DisplayName("지하철 구간을 삭제한다")
     @Test
     void deleteSection() {
-        restUtil.createEntityData(station1, "/stations");
-        restUtil.createEntityData(station2, "/stations");
-        restUtil.createEntityData(station3, "/stations");
-        Long id = restUtil.createEntityData(line1, "/lines");
-        restUtil.registEntityData(section1, "/lines/" + id + "/sections");
+        //Given
+        testHelper.makeGivenCondition("/stations", HttpStatus.CREATED.value(), stations.get(0), stations.get(1), stations.get(2));
+        Long id = restUtil.createEntityData("/lines", HttpStatus.CREATED.value(), line1);
+        restUtil.registEntityData("/lines/" + id + "/sections", sections.get(0));
 
-        ExtractableResponse<Response> deleteResponse = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .given().queryParam("stationId", section1.get("upStationId"))
-                .when().delete("/lines/{id}/sections", id)
-                .then().log().all()
-                .extract();
+        //When
+        ExtractableResponse<Response> deleteResponse =
+                restUtil.deleteEntityDataByIdWithQueryParam("/lines/{id}/sections"
+                , id, "stationId", (Long) sections.get(0).get("upStationId"));
         assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
 
+        //Then
         ExtractableResponse<Response> lineResponse = restUtil.getResponseDataById("/lines/{id}", id);
         assertThat(lineResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
 
     }
 
+    /**
+     * Given 지하철역과 노선을 생성하고
+     * when 한개의 구간만 지닌 지하철 노선의 구간을 삭제하려고 하면
+     * Then 해당 지하철 구간은 삭제할 수 없어 에러를 출력한다
+     */
     @DisplayName("한개의 구간만 지닌 노선의 구간을 삭제할 수 없다.")
     @Test
-    void canNotDeleteOneSection() {
-        restUtil.createEntityData(station1, "/stations");
-        restUtil.createEntityData(station2, "/stations");
-        Long id = restUtil.createEntityData(line1, "/lines");
+    void canNotDeleteOnlyOneSection() {
+        //Given
+        testHelper.makeGivenCondition("/stations", HttpStatus.CREATED.value(), stations.get(0), stations.get(1));
+        Long id = restUtil.createEntityData("/lines", HttpStatus.CREATED.value(), line1);
 
-        ExtractableResponse<Response> deleteResponse = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .given().queryParam("stationId", line1.get("upStationId"))
-                .when().delete("/lines/{id}/sections", id)
-                .then().log().all()
-                .extract();
+        //When
+        ExtractableResponse<Response> deleteResponse =
+                restUtil.deleteEntityDataByIdWithQueryParam("/lines/{id}/sections"
+                        , id, "stationId", (Long) line1.get("upStationId"));
+
+        //Then
         assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
 
     }
 
+    /**
+     * Given 지하철역과 노선, 구간을 생성하고
+     * when  마지막 구간이 아닌 구간을 삭제하려고 하면
+     * Then 해당 지하철 구간은 삭제할 수 없어 에러를 출력한다
+     */
     @DisplayName("마지막 구간이 아닌 것은 제거 할 수 없다.")
     @Test
-    void deleteOnlyDownSation() {
-        restUtil.createEntityData(station1, "/stations");
-        restUtil.createEntityData(station2, "/stations");
-        restUtil.createEntityData(station3, "/stations");
-        restUtil.createEntityData(station4, "/stations");
-        Long id = restUtil.createEntityData(line1, "/lines");
-        restUtil.registEntityData(section1, "/lines/" + id + "/sections");
-        restUtil.registEntityData(section2, "/lines/" + id + "/sections");
+    void canNotDeleteNotLastSection() {
+        //Given
+        testHelper.makeGivenCondition("/stations", HttpStatus.CREATED.value(), stations.get(0), stations.get(1), stations.get(2), stations.get(3));
+        Long id = restUtil.createEntityData("/lines", HttpStatus.CREATED.value(), line1);
+        testHelper.makeGivenCondition("/lines/" + id + "/sections", HttpStatus.OK.value(), sections.get(0), sections.get(1));
 
-        ExtractableResponse<Response> deleteResponse = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .given().queryParam("stationId", section1.get("upStationId"))
-                .when().delete("/lines/{id}/sections", id)
-                .then().log().all()
-                .extract();
+        //When
+        ExtractableResponse<Response> deleteResponse =
+                restUtil.deleteEntityDataByIdWithQueryParam("/lines/{id}/sections"
+                        , id, "stationId", (Long) sections.get(0).get("upStationId"));
+
+
+        //Then
         assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
     }
