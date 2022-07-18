e1b905e507cfb5c28fd491c3411c655171762469
uram <parkuram12@gmail.com>
2022-07-18 20:05:36 +0900

    modify: SubwayException
    - 가변인자를 받는 makeValueFormat 메서드 추가

--- src/main/java/nextstep/subway/exception/SubwayException.java
+++ src/main/java/nextstep/subway/exception/SubwayException.java
@@ -3,6 +3,8 @@ package nextstep.subway.exception;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.Arrays;
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SubwayException extends RuntimeException {
     private static final String SEPARATOR = " :::::::: ";
@@ -11,12 +13,23 @@ public class SubwayException extends RuntimeException {
         super(message);
     }
 
+    public SubwayException(String message, Long ... ids) {
+        super(message + makeValueFormat(ids));
+    }
+
     public SubwayException(String message, Object org) {
         super(message + makeValueFormat(org));
     }
 
     private static String makeValueFormat(Object org) {
         return SEPARATOR + "[Value = " + org + "]";
+    }
+
+    private static String makeValueFormat(Long ... ids) {
+        return SEPARATOR +
+                "[Value = "
+                + Arrays.toString(ids)
+                + "]";
     }
 
 }
