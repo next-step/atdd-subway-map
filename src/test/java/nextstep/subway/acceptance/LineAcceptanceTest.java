15faccc3bf59ce17d39677a4d187fe607def4c05
iiaii <iiaii@gwoowahan.com>
2022-07-20 01:29:35 +0900

    feat: 실패하는 테스트 추가 및 공통 부분 추상화

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,28 +1,35 @@
 package nextstep.subway.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.acceptance.client.LineClient;
 import nextstep.subway.acceptance.client.StationClient;
+import nextstep.subway.acceptance.client.dto.LineCreationRequest;
+import nextstep.subway.acceptance.util.HttpStatusValidator;
 import nextstep.subway.acceptance.util.JsonResponseConverter;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.annotation.DirtiesContext;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
+import static nextstep.subway.acceptance.fixture.ColorFixtures.GREEN;
+import static nextstep.subway.acceptance.fixture.ColorFixtures.RED;
+import static nextstep.subway.acceptance.fixture.ColorFixtures.YELLOW;
+import static nextstep.subway.acceptance.fixture.DistanceFixtures.FIVE;
+import static nextstep.subway.acceptance.fixture.DistanceFixtures.TEN;
+import static nextstep.subway.acceptance.fixture.LineFixtures.분당선;
+import static nextstep.subway.acceptance.fixture.LineFixtures.신분당선;
+import static nextstep.subway.acceptance.fixture.LineFixtures.이호선;
+import static nextstep.subway.acceptance.fixture.StationFixtures.신분당선역_이름들;
+import static nextstep.subway.acceptance.fixture.StationFixtures.이호선역_이름들;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관련 기능")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class LineAcceptanceTest {
-    @LocalServerPort
-    int port;
+public class LineAcceptanceTest extends AcceptanceTest {
 
     @Autowired
     StationClient stationClient;
@@ -31,12 +38,10 @@ public class LineAcceptanceTest {
     LineClient lineClient;
 
     @Autowired
-    JsonResponseConverter responseConverter;
+    HttpStatusValidator statusValidator;
 
-    @BeforeEach
-    public void setUp() {
-        RestAssured.port = port;
-    }
+    @Autowired
+    JsonResponseConverter responseConverter;
 
     /**
      * When 지하철 노선을 생성하면
@@ -47,14 +52,62 @@ public class LineAcceptanceTest {
     @DirtiesContext
     void createLine() {
         // given
-        String 이호선 = "2호선";
-        List<Long> stationIds = responseConverter.convertToIds(stationClient.createStations(List.of("강남역", "역삼역")));
 
         // when
-        lineClient.createLine(이호선, "green", stationIds.get(0), stationIds.get(1), 10);
+        statusValidator.validateCreated(lineClient.createLine(이호선_생성_요청()));
 
         // then
-        assertThat(responseConverter.convertToNames(lineClient.fetchLines())).contains(이호선);
+        assertThat(responseConverter.convertToNames(lineClient.fetchLines()))
+                .contains(이호선.getValue());
+    }
+
+    /**
+     * When 동일한 지하철 노선을 중복해서 생성하면
+     * Then 기존에 저장된 노선을 반환한다
+     */
+    @DisplayName("동일한 지하철 노선을 중복 생성한다.")
+    @Test
+    @DirtiesContext
+    void createLineTwice() {
+        // given
+
+        // when
+        statusValidator.validateCreated(lineClient.createLine(이호선_생성_요청()));
+        ExtractableResponse<Response> duplicatedResponse =
+                statusValidator.validateCreated(lineClient.createLine(이호선_생성_요청()));
+
+        // then
+        assertThat(responseConverter.convert(duplicatedResponse, "stations", List.class))
+                .hasSize(이호선역_이름들.size());
+        assertThat(responseConverter.convertToName(duplicatedResponse)).isEqualTo(이호선.getValue());
+    }
+
+    /**
+     * When 존재하지 않는 역의 id로 노선을 생성하면
+     * Then 오류(EntityNotFoundException) 객체를 반환한다
+     */
+    @DisplayName("존재하지 않는 역의 id로 지하철 노선을 생성한다.")
+    @Test
+    @DirtiesContext
+    void createLineByInvalidStationIds() {
+        // given
+        long upStationId = 1L;
+        long downStationId = 2L;
+        LineCreationRequest lineRequest = new LineCreationRequest(
+                이호선.getValue(),
+                GREEN.getValue(),
+                upStationId,
+                downStationId,
+                TEN.getValue()
+        );
+
+        // when
+        ExtractableResponse<Response> response =
+                statusValidator.validateBadRequest(lineClient.createLine(lineRequest));
+
+        // then
+        assertThat(responseConverter.convertToError(response))
+                .contains(EntityNotFoundException.class.getName());
     }
 
     /**
@@ -69,21 +122,19 @@ public class LineAcceptanceTest {
         // given
         int stationSize = 2;
         int lineSize = 2;
-        String 이호선 = "2호선";
-        String 신분당선 = "신분당선";
-        List<Long> stationIds = responseConverter.convertToIds(stationClient.createStations(List.of("강남역", "역삼역", "양재역")));
-        lineClient.createLine(이호선, "green", stationIds.get(0), stationIds.get(1), 10);
-        lineClient.createLine(신분당선, "red", stationIds.get(0), stationIds.get(2), 7);
+        lineClient.createLine(이호선_생성_요청());
+        lineClient.createLine(신분당선_생성_요청());
 
         // when
-        ExtractableResponse<Response> linesResponse = lineClient.fetchLines();
+        ExtractableResponse<Response> linesResponse =
+                statusValidator.validateOk(lineClient.fetchLines());
 
         // then
         assertThat(responseConverter.convertToList(linesResponse, "stations"))
-                .allMatch(stations -> stations.size() == stationSize);
+                .allMatch(fetchStations -> fetchStations.size() == stationSize);
         assertThat(responseConverter.convertToNames(linesResponse))
                 .hasSize(lineSize)
-                .containsExactly(이호선, 신분당선);
+                .containsExactly(이호선.getValue(), 신분당선.getValue());
     }
 
     /**
@@ -96,21 +147,18 @@ public class LineAcceptanceTest {
     @DirtiesContext
     void getLine() {
         // given
-        String 이호선 = "2호선";
-        List<String> stations = List.of("강남역", "역삼역");
-        List<Long> stationIds = responseConverter.convertToIds(stationClient.createStations(stations));
-        Long lineId = responseConverter.convertToId(lineClient.createLine(이호선, "green", stationIds.get(0), stationIds.get(1), 10));
+        long lineId = responseConverter.convertToId(lineClient.createLine(이호선_생성_요청()));
 
         // when
-        ExtractableResponse<Response> response = lineClient.fetchLine(lineId);
+        ExtractableResponse<Response> response =
+                statusValidator.validateOk(lineClient.fetchLine(lineId));
 
         // then
         assertThat(responseConverter.convert(response, "stations", List.class))
-                .hasSize(stations.size());
-        assertThat(responseConverter.convertToName(response)).isEqualTo(이호선);
+                .hasSize(이호선역_이름들.size());
+        assertThat(responseConverter.convertToName(response)).isEqualTo(이호선.getValue());
     }
 
-    /**
     /**
      * Given 지하철 노선을 생성하고
      * When 생성한 지하철 노선을 수정하면
@@ -121,19 +169,34 @@ public class LineAcceptanceTest {
     @DirtiesContext
     void modifyLine() {
         // given
-        String 이호선 = "2호선";
-        String 새로운이호선 = "새로운2호선";
-        String green = "green";
-        String coreGreen = "coreGreen";
-        List<Long> stationIds = responseConverter.convertToIds(stationClient.createStations(List.of("강남역", "역삼역")));
-        Long lineId = responseConverter.convertToId(lineClient.createLine(이호선, green, stationIds.get(0), stationIds.get(1), 10));
+        long lineId = responseConverter.convertToId(lineClient.createLine(이호선_생성_요청()));
 
         // when
-        lineClient.modifyLine(lineId, 새로운이호선, coreGreen);
+        statusValidator.validateOk(lineClient.modifyLine(lineId, 분당선.getValue(), YELLOW.getValue()));
 
         // then
         assertThat(responseConverter.convertToNames(lineClient.fetchLines()))
-                .containsExactly(새로운이호선);
+                .containsExactly(분당선.getValue());
+    }
+
+    /**
+     * When 존재하지 않는 지하철 노선을 수정하면
+     * Then 오류(EntityNotFoundException) 객체를 반환한다
+     */
+    @DisplayName("존재하지 않는 지하철 노선을 수정한다.")
+    @Test
+    @DirtiesContext
+    void modifyNoneExistentLine() {
+        // given
+        long lineId = 1L;
+
+        // when
+        ExtractableResponse<Response> response =
+                statusValidator.validateBadRequest(lineClient.modifyLine(lineId, 분당선.getValue(), YELLOW.getValue()));
+
+        // then
+        assertThat(responseConverter.convertToError(response))
+                .contains(EntityNotFoundException.class.getName());
     }
 
 
@@ -147,16 +210,55 @@ public class LineAcceptanceTest {
     @DirtiesContext
     void deleteLine() {
         // given
-        String 이호선 = "2호선";
-        List<Long> stationIds = responseConverter.convertToIds(stationClient.createStations(List.of("강남역", "역삼역")));
-        Long lineId = responseConverter.convertToId(lineClient.createLine(이호선, "green", stationIds.get(0), stationIds.get(1), 10));
+        long lineId = responseConverter.convertToId(lineClient.createLine(이호선_생성_요청()));
 
         // when
-        lineClient.deleteLine(lineId);
+        statusValidator.validateNoContent(lineClient.deleteLine(lineId));
 
         // then
         assertThat(responseConverter.convertToNames(lineClient.fetchLines()))
-                .doesNotContain(이호선);
+                .doesNotContain(이호선.getValue());
+    }
+
+    /**
+     * When 존재하지 않는 지하철 노선을 삭제하면
+     * Then 해당 지하철 노선 정보는 삭제된다
+     */
+    @DisplayName("존재하지 않는 지하철 노선을 제거한다.")
+    @Test
+    @DirtiesContext
+    void deleteNonExistentLine() {
+        // given
+        long lineId = 1L;
+
+        // when
+        ExtractableResponse<Response> response = statusValidator.validateBadRequest(lineClient.deleteLine(lineId));
+
+        // then
+        assertThat(responseConverter.convertToError(response))
+                .contains(EmptyResultDataAccessException.class.getName());
+    }
+
+    private LineCreationRequest 이호선_생성_요청() {
+        List<Long> stationIds = responseConverter.convertToIds(stationClient.createStations(이호선역_이름들));
+        return new LineCreationRequest(
+                이호선.getValue(),
+                GREEN.getValue(),
+                stationIds.get(0),
+                stationIds.get(1),
+                TEN.getValue()
+        );
+    }
+
+    private LineCreationRequest 신분당선_생성_요청() {
+        List<Long> stationIds = responseConverter.convertToIds(stationClient.createStations(신분당선역_이름들));
+        return new LineCreationRequest(
+                신분당선.getValue(),
+                RED.getValue(),
+                stationIds.get(0),
+                stationIds.get(1),
+                FIVE.getValue()
+        );
     }
 
 }
--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -1,36 +1,31 @@
 package nextstep.subway.acceptance;
 
-import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.acceptance.client.StationClient;
+import nextstep.subway.acceptance.util.HttpStatusValidator;
 import nextstep.subway.acceptance.util.JsonResponseConverter;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.annotation.DirtiesContext;
 
-import java.util.List;
-
+import static nextstep.subway.acceptance.fixture.StationFixtures.강남역;
+import static nextstep.subway.acceptance.fixture.StationFixtures.이호선역_이름들;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class StationAcceptanceTest {
-    @LocalServerPort
-    int port;
+public class StationAcceptanceTest extends AcceptanceTest {
 
     @Autowired
     StationClient stationClient;
 
     @Autowired
-    JsonResponseConverter responseConverter;
+    HttpStatusValidator statusValidator;
 
-    @BeforeEach
-    public void setUp() {
-        RestAssured.port = port;
-    }
+    @Autowired
+    JsonResponseConverter responseConverter;
 
     /**
      * When 지하철역을 생성하면
@@ -42,14 +37,34 @@ public class StationAcceptanceTest {
     @DirtiesContext
     void createStation() {
         // given
-        String stationName = "강남역";
 
         // when
-        stationClient.createStation(stationName);
+        statusValidator.validateCreated(강남역_생성());
 
         // then
         assertThat(responseConverter.convertToNames(stationClient.fetchStations()))
-                .containsExactly(stationName);
+                .containsExactly(강남역.getValue());
+    }
+
+    /**
+     * When 지하철역을 중복 생성하면
+     * Then 지하철역 목록 조회 시 생성한 역이 중복되지 않고 찾을 수 있다
+     */
+    @DisplayName("지하철역을 중복 생성한다.")
+    @Test
+    @DirtiesContext
+    void createStationTwice() {
+        // given
+        int expectedSize = 1;
+
+        // when
+        statusValidator.validateCreated(강남역_생성());
+        statusValidator.validateCreated(강남역_생성());
+
+        // then
+        assertThat(responseConverter.convertToNames(stationClient.fetchStations()))
+                .hasSize(expectedSize)
+                .containsExactly(강남역.getValue());
     }
 
     /**
@@ -60,17 +75,18 @@ public class StationAcceptanceTest {
     @DisplayName("지하철역 목록을 조회한다.")
     @Test
     @DirtiesContext
-    void getStations() {
+    void 강남역_생성s() {
         // given
-        List<String> stationNames = List.of("강남역", "역삼역");
+        stationClient.createStations(이호선역_이름들);
 
         // when
-        stationClient.createStations(stationNames);
+        ExtractableResponse<Response> response =
+                statusValidator.validateOk(stationClient.fetchStations());
 
         // then
-        assertThat(responseConverter.convertToNames(stationClient.fetchStations()))
-                .hasSize(stationNames.size())
-                .containsExactly(stationNames.toArray(String[]::new));
+        assertThat(responseConverter.convertToNames(response))
+                .hasSize(이호선역_이름들.size())
+                .containsExactly(이호선역_이름들.toArray(String[]::new));
     }
 
     /**
@@ -83,15 +99,38 @@ public class StationAcceptanceTest {
     @DirtiesContext
     void deleteStation() {
         // given
-        String stationName = "강남역";
-        Long stationId = responseConverter.convertToId(stationClient.createStation(stationName));
+        Long stationId = responseConverter.convertToId(강남역_생성());
 
         // when
-        stationClient.deleteStation(stationId);
+        statusValidator.validateNoContent(stationClient.deleteStation(stationId));
 
         // then
         assertThat(responseConverter.convertToNames(stationClient.fetchStations()))
-                .doesNotContain(stationName);
+                .doesNotContain(강남역.getValue());
+    }
+
+    /**
+     * When 존재하지 않는 지하철 역을 삭제하면
+     * Then 해당 지하철 역 정보는 삭제된다
+     */
+    @DisplayName("존재하지 않는 지하철 역을 제거한다.")
+    @Test
+    @DirtiesContext
+    void deleteNonExistentStation() {
+        // given
+        Long lineId = 1L;
+
+        // when
+        ExtractableResponse<Response> response =
+                statusValidator.validateBadRequest(stationClient.deleteStation(lineId));
+
+        // then
+        assertThat(responseConverter.convertToError(response))
+                .contains(EmptyResultDataAccessException.class.getName());
+    }
+
+    private ExtractableResponse<Response> 강남역_생성() {
+        return stationClient.createStation(강남역.getValue());
     }
 
 }
