15aeedc1b88877505f729d4dce1e13704b21e20f
chkim <cndghks0213@gmail.com>
2022-07-20 06:30:48 +0900

    refactor: 노선 거리 추가

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -1,40 +1,23 @@
 package nextstep.subway.applicaion.dto;
 
-import nextstep.subway.domain.Line;
+import lombok.Getter;
 
+@Getter
 public class LineRequest {
     private String name;
     private String color;
+    private int distance;
     private Long upStationId;
     private Long downStationId;
 
     public LineRequest() {
     }
 
-    public LineRequest(String name, String color, Long upStationId, Long downStationId) {
+    public LineRequest(String name, String color, int distance, Long upStationId, Long downStationId) {
         this.name = name;
         this.color = color;
+        this.distance = distance;
         this.upStationId = upStationId;
         this.downStationId = downStationId;
-    }
-
-    public Line toEntity() {
-        return new Line(name, color, upStationId, downStationId);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public Long getUpStationId() {
-        return upStationId;
-    }
-
-    public Long getDownStationId() {
-        return downStationId;
     }
 }
