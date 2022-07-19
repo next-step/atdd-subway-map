97ba3bff72b582a5e72ff5496f70e2d0283a799d
chkim <cndghks0213@gmail.com>
2022-07-20 06:35:39 +0900

    test: 잘못된 식별자를 전달하는 경우의 테스트 추가

--- src/test/java/nextstep/subway/acceptance/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/line/LineAcceptanceTest.java
@@ -1,36 +1,44 @@
 package nextstep.subway.acceptance.line;
 
-import io.restassured.RestAssured;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.acceptance.AcceptanceTest;
+import nextstep.subway.applicaion.dto.LineResponse;
 import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.jdbc.Sql;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
-import static nextstep.subway.acceptance.line.LinePrepare.지하철노선_생성_요청;
-import static nextstep.subway.acceptance.line.LinePrepare.지하철노선_조회_요청;
-import static nextstep.subway.acceptance.line.LinePrepare.지하철노선목록_조회_요청;
-import static nextstep.subway.acceptance.line.LinePrepare.지하철노선_수정_요청;
-import static nextstep.subway.acceptance.line.LinePrepare.지하철노선_삭제_요청;
+import static nextstep.subway.acceptance.line.LineAcceptanceStep.지하철노선_생성_요청;
+import static nextstep.subway.acceptance.line.LineAcceptanceStep.지하철노선_생성_결과;
+import static nextstep.subway.acceptance.line.LineAcceptanceStep.지하철노선_수정_요청;
+import static nextstep.subway.acceptance.line.LineAcceptanceStep.지하철노선_삭제_요청;
+import static nextstep.subway.acceptance.line.LineAcceptanceStep.지하철노선_조회_요청;
+import static nextstep.subway.acceptance.line.LineAcceptanceStep.지하철노선_조회_결과;
+import static nextstep.subway.acceptance.line.LineAcceptanceStep.지하철노선목록_조회_결과;
+import static nextstep.subway.acceptance.station.StationAcceptanceStep.지하철역_생성_결과;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Sql("classpath:/database-init.sql")
-@DisplayName("지하철역 관련 기능")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class LineAcceptanceTest {
+class LineAcceptanceTest extends AcceptanceTest {
+    private static final String NOT_EXIST_LINE_PATH = "/lines/1000";
+    private static final Long NOT_EXIST_STATION_ID = 1000L;
 
-    @LocalServerPort
-    int port;
+    private Station 잠실역;
+    private Station 선릉역;
+    private Station 강남역;
+    private Station 교대역;
 
     @BeforeEach
-    public void setUp() {
-        RestAssured.port = port;
+    void init() throws JsonProcessingException {
+        잠실역 = 지하철역_생성_결과("잠실역");
+        선릉역 = 지하철역_생성_결과("선릉역");
+        강남역 = 지하철역_생성_결과("강남역");
+        교대역 = 지하철역_생성_결과("교대역");
     }
 
     /**
@@ -39,12 +47,24 @@ class LineAcceptanceTest {
      */
     @DisplayName("지하철노선을 생성한다.")
     @Test
-    void createLine() {
-        ExtractableResponse<Response> 일호선 = 지하철노선_생성_요청("1호선", "br-red-600", 1L, 2L);
+    void createLine() throws JsonProcessingException {
+        ExtractableResponse<Response> 일호선 = 지하철노선_생성_요청("1호선", "br-red-600", 10, 잠실역.getId(), 선릉역.getId());
 
-        List<Line> 지하철노선목록 = 지하철노선목록_조회_요청();
+        List<LineResponse> 지하철노선목록 = 지하철노선목록_조회_결과();
 
-        assertThat(지하철노선목록).containsExactly(일호선.as(Line.class));
+        assertThat(지하철노선목록).containsExactly(일호선.as(LineResponse.class));
+    }
+
+    /**
+     * When 존재하지 않는 지하철 역으로 지하철 노선을 생성하면
+     * Then Internal Server Error가 발생한다.
+     */
+    @DisplayName("존재하지 않는 지하철역으로 지하철 노선을 생성할 수 없다.")
+    @Test
+    void createLineWithWrongStationId() throws JsonProcessingException {
+        ExtractableResponse<Response> 일호선 = 지하철노선_생성_요청("1호선", "br-red-600", 10, 잠실역.getId(), NOT_EXIST_STATION_ID);
+
+        assertThat(일호선.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
     /**
@@ -54,13 +74,13 @@ class LineAcceptanceTest {
      */
     @DisplayName("지하철노선 목록을 조회한다.")
     @Test
-    void getLines() {
-        ExtractableResponse<Response> 일호선 = 지하철노선_생성_요청("1호선", "br-red-600", 1L, 2L);
-        ExtractableResponse<Response> 이호선 = 지하철노선_생성_요청("2호선", "br-red-700", 3L, 4L);
+    void getLines() throws JsonProcessingException {
+        LineResponse 일호선 = 지하철노선_생성_결과("1호선", "br-red-600", 10, 잠실역.getId(), 선릉역.getId());
+        LineResponse 이호선 = 지하철노선_생성_결과("2호선", "br-red-800", 10, 강남역.getId(), 교대역.getId());
 
-        List<Line> 지하철노선목록 = 지하철노선목록_조회_요청();
+        List<LineResponse> 지하철노선목록 = 지하철노선목록_조회_결과();
 
-        assertThat(지하철노선목록).containsExactly(일호선.as(Line.class), 이호선.as(Line.class));
+        assertThat(지하철노선목록).containsExactly(일호선, 이호선);
     }
 
     /**
@@ -70,10 +90,25 @@ class LineAcceptanceTest {
      */
     @DisplayName("지하철노선을 조회한다.")
     @Test
-    void getLine() {
-        ExtractableResponse<Response> 일호선 = 지하철노선_생성_요청("1호선", "br-red-600", 1L, 2L);
+    void getLine() throws JsonProcessingException {
+        ExtractableResponse<Response> 일호선 = 지하철노선_생성_요청("1호선", "br-red-600", 10, 잠실역.getId(), 선릉역.getId());
 
-        assertThat(일호선.as(Line.class)).isEqualTo(지하철노선_조회_요청(일호선.header("Location")));
+        assertThat(일호선.as(LineResponse.class))
+                .isEqualTo(지하철노선_조회_결과(일호선.header("Location")));
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 존재하지 않는 지하철의 노선을 조회하면
+     * Then Internal Server Error가 발생한다.
+     */
+    @DisplayName("존재하지 않는 지하철노선은 조회할 수 없다.")
+    @Test
+    void getLineWithWrongId() throws JsonProcessingException {
+        지하철노선_생성_요청("1호선", "br-red-600", 10, 잠실역.getId(), 선릉역.getId());
+
+        assertThat(지하철노선_조회_요청(NOT_EXIST_LINE_PATH).statusCode())
+                .isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
     /**
@@ -83,12 +118,27 @@ class LineAcceptanceTest {
      */
     @DisplayName("지하철노선을 수정한다.")
     @Test
-    void updateLine() {
-        ExtractableResponse<Response> 일호선 = 지하철노선_생성_요청("1호선", "br-red-600", 1L, 2L);
+    void updateLine() throws JsonProcessingException {
+        ExtractableResponse<Response> 일호선 = 지하철노선_생성_요청("1호선", "br-red-600", 10, 잠실역.getId(), 선릉역.getId());
 
         지하철노선_수정_요청(일호선.header("Location"), "3호선", "br-blue-600");
 
-        assertThat(지하철노선_조회_요청(일호선.header("Location"))).isEqualTo(new Line(1L, "3호선", "br-blue-600", 1L, 2L));
+        assertThat(지하철노선_조회_결과(일호선.header("Location")))
+                .isEqualTo(LineResponse.from(new Line(1L, "3호선", "br-blue-600", 10, 잠실역, 선릉역)));
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 존재하지 않는 지하철 노선을 수정하면
+     * Then Internal Server Error가 발생한다.
+     */
+    @DisplayName("존재하지 않는 지하철노선은 수정할 수 없다.")
+    @Test
+    void updateLineWithWrongId() throws JsonProcessingException {
+        지하철노선_생성_요청("1호선", "br-red-600", 10, 잠실역.getId(), 선릉역.getId());
+
+        assertThat(지하철노선_수정_요청(NOT_EXIST_LINE_PATH, "3호선", "br-blue-600").statusCode())
+                .isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
     /**
@@ -98,11 +148,26 @@ class LineAcceptanceTest {
      */
     @DisplayName("지하철노선을 삭제한다.")
     @Test
-    void deleteLine() {
-        ExtractableResponse<Response> 일호선 = 지하철노선_생성_요청("1호선", "br-red-600", 1L, 2L);
+    void deleteLine() throws JsonProcessingException {
+        ExtractableResponse<Response> 일호선 = 지하철노선_생성_요청("1호선", "br-red-600", 10, 잠실역.getId(), 선릉역.getId());
 
         지하철노선_삭제_요청(일호선.header("Location"));
 
-        assertThat(지하철노선목록_조회_요청()).doesNotContain(new Line(1L, "1호선", "br-red-600", 1L, 2L));
+        assertThat(지하철노선목록_조회_결과())
+                .doesNotContain(LineResponse.from(new Line(1L, "3호선", "br-blue-600", 10, 잠실역, 선릉역)));
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 존재하지 않는 지하철 노선을 삭제하면
+     * Then Internal Server Error가 발생한다.
+     */
+    @DisplayName("존재하지 않는 지하철 노선은 삭제할 수 없다.")
+    @Test
+    void deleteLineWithWrongId() throws JsonProcessingException {
+        지하철노선_생성_요청("1호선", "br-red-600", 10, 잠실역.getId(), 선릉역.getId());
+
+        assertThat(지하철노선_삭제_요청(NOT_EXIST_LINE_PATH).statusCode())
+                .isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 }
