6bcef02b3ac8bfb9c03a82e917e6cace7fd048aa
iiaii <iiaii@gwoowahan.com>
2022-07-20 01:21:40 +0900

    refactor: 불필요한 클래스 제거

--- src/main/java/nextstep/subway/repository/DistanceRepository.java
+++
@@ -1,7 +0,0 @@
-package nextstep.subway.repository;
-
-import nextstep.subway.domain.Distance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DistanceRepository extends JpaRepository<Distance, Long> {
-}
