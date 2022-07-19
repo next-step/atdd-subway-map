22ee3bf279da98ba51d0b996a1511c957ff95670
chkim <cndghks0213@gmail.com>
2022-07-20 06:32:44 +0900

    refactor: Station List를 응답하도록 변경

--- src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -1,20 +1,28 @@
 package nextstep.subway.applicaion.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import nextstep.subway.domain.Line;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
 public class LineResponse {
     private Long id;
     private String name;
     private String color;
-    private Long upStationId;
-    private Long downStationId;
+    private int distance;
+    private List<StationResponse> stations;
 
-    public LineResponse(Long id, String name, String color, Long upStationId, Long downStationId) {
+    private LineResponse(Long id, String name, String color, int distance, List<StationResponse> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
-        this.upStationId = upStationId;
-        this.downStationId = downStationId;
+        this.distance = distance;
+        this.stations = stations;
     }
 
     public static LineResponse from(Line line) {
@@ -22,28 +30,21 @@ public class LineResponse {
                 line.getId(),
                 line.getName(),
                 line.getColor(),
-                line.getUpStationId(),
-                line.getDownStationId()
+                line.getDistance(),
+                Arrays.asList(StationResponse.from(line.getUpStation()), StationResponse.from(line.getDownStation()))
         );
     }
 
-    public Long getId() {
-        return id;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LineResponse that = (LineResponse) o;
+        return getDistance() == that.getDistance() && Objects.equals(getId(), that.getId()) && Objects.equals(getName(), that.getName()) && Objects.equals(getColor(), that.getColor()) && Objects.equals(getStations(), that.getStations());
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public Long getUpStationId() {
-        return upStationId;
-    }
-
-    public Long getDownStationId() {
-        return downStationId;
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getColor(), getDistance(), getStations());
     }
 }
