e3d4a48e447bf41253a492e721041591ae2f93b3
sangnyyy <dltkdals2202@gmail.com>
2022-07-20 22:11:26 +0900

    chore : 에러와 익셉션 용어 통일

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -81,7 +81,7 @@ class SectionAcceptanceTest {
      * When 지하철 노선에 구간을 등록할 때, 노선의 하행 종점역과 새로운 구간의 상행역이 일치하지 않으면
      * Then 에러가 발생한다
      */
-    @DisplayName("지하철 노선에 구간을 생성 시 노선의 하행 종점역과 새로운 구간의 상행역이 일치하지 않으면 익셉션 발생한다.")
+    @DisplayName("지하철 노선에 구간을 생성 시 노선의 하행 종점역과 새로운 구간의 상행역이 일치하지 않으면 에러가 발생한다.")
     @Test
     void createSectionStationMismatchException() {
         // given
@@ -109,7 +109,7 @@ class SectionAcceptanceTest {
      * When 지하철 노선에 구간을 등록할 때, 새로운 구간의 하행역이 노선에 등록되어 있다면
      * Then 에러가 발생한다
      */
-    @DisplayName("지하철 노선에 구간을 생성 시 새로운 구간의 하행역이 노선에 등록되어 있다면 익셉션 발생한다.")
+    @DisplayName("지하철 노선에 구간을 생성 시 새로운 구간의 하행역이 노선에 등록되어 있다면 에러가 발생한다.")
     @Test
     void createSectionAlreadyExistStationException() {
         // given
@@ -165,7 +165,7 @@ class SectionAcceptanceTest {
      * When 구간에 등록된 하행 종점역이 아닌 역을 삭제하면
      * Then 에러가 발생한다
      */
-    @DisplayName("지하철 노선 구간에서 지하철역을 삭제 시 하행 종점역이 아니면 익셉션이 발생한다.")
+    @DisplayName("지하철 노선 구간에서 지하철역을 삭제 시 하행 종점역이 아니면 에러가 발생한다.")
     @Test
     void deleteStationDownTerminalStationException() {
         // given
@@ -195,7 +195,7 @@ class SectionAcceptanceTest {
      * When 구간에 등록된 상행 종점역과 하행 종점역만 있는 경우 삭제 시
      * Then 에러가 발생한다
      */
-    @DisplayName("지하철 노선 구간에서 지하철역을 삭제 시 상행 종점역과 하행 종점역만 있는 경우 익셉션이 발생한다.")
+    @DisplayName("지하철 노선 구간에서 지하철역을 삭제 시 상행 종점역과 하행 종점역만 있는 경우 에러가 발생한다.")
     @Test
     void deleteStationInOnlyOneStationSectionException() {
         // given
