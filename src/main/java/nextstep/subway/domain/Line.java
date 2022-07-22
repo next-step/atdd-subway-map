29798c3bc2bd7f48490ebc294a86e4bfb2130b08
kiljaewon <kiljw316@gmail.com>
2022-07-22 17:40:14 +0900

    feat: 노선 엔티티 및 리포지토리 추가

---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,40 @@
+package nextstep.subway.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Line {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String color;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "line")
+    private final List<Station> stations = new ArrayList<>();
+
+    public Line() {}
+
+    public Line(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+
+}
