041e268be1f3ebb27189a9210c02286a54e6a47f
kiljaewon <kiljw316@gmail.com>
2022-07-22 17:39:13 +0900

    feat: 지하철 노선 생성 인수테스트에 맞춰 service 추가

---
+++ src/main/java/nextstep/subway/application/LineService.java
@@ -0,0 +1,48 @@
+package nextstep.subway.application;
+
+import nextstep.subway.application.dto.LineRequest;
+import nextstep.subway.application.dto.LineResponse;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+public class LineService {
+
+    private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
+
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
+        this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
+    }
+
+    @Transactional
+    public LineResponse saveLine(LineRequest lineRequest) {
+        Line line = lineRepository.save(new Line(lineRequest.getName(), lineRequest.getColor()));
+        List<Station> stations = stationRepository.findAllById(List.of(lineRequest.getUpStationId(), lineRequest.getDownStationId()));
+        for (Station station : stations) {
+            station.changeLine(line);
+        }
+        return createLineResponse(line);
+
+    }
+
+    public List<LineResponse> findAllLines() {
+        return lineRepository.findAll().stream()
+                .map(this::createLineResponse)
+                .collect(Collectors.toList());
+    }
+
+    private LineResponse createLineResponse(Line line) {
+        return new LineResponse(line);
+
+    }
+}
