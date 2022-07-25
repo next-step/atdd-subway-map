cb592aaea3f436bd6e537a3f7ad24d5c0f5a216c
94peterchoi <94peterchoi@gmail.com>
2022-07-26 01:32:06 +0900

    [fix15] 각 테스트마다 나오는 동일한 변수에 대해 중복 줄이기

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -26,6 +26,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("노선 구간 관리")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SectionAcceptanceTest {
+
+    private Long 구일_아이디;
+    private Long 구로_아이디;
+    private Long 신도림_아이디;
+    private Long 영등포_아이디;
+
+    private String 일호선 = "1호선";
+
+    private int 구일_구로_거리 = 10;
+    private int 구로_신도림_거리 = 15;
+    private int 신도림_영등포_거리 = 13;
+
     @LocalServerPort
     int port;
 
@@ -36,6 +48,13 @@ public class SectionAcceptanceTest {
     public void setUp() {
         RestAssured.port = port;
         databaseUitl.tableClear();
+
+        List<ExtractableResponse<Response>> stationCreationResponses = StationApiCall.createStations("구일역", "구로역", "신도림역", "영등포역");
+
+        구일_아이디 = getId(stationCreationResponses.get(0));
+        구로_아이디 = getId(stationCreationResponses.get(1));
+        신도림_아이디 = getId(stationCreationResponses.get(2));
+        영등포_아이디 = getId(stationCreationResponses.get(3));
     }
 
     /**
@@ -46,26 +65,9 @@ public class SectionAcceptanceTest {
     @DisplayName("지하철 구간을 노선에 등록한다.")
     @Test
     void registerSection() {
+
         // given
-        final String 구일 = "구일역";
-        final String 구로 = "구로역";
-        final String 신도림 = "신도림역";
-        final String 영등포 = "영등포역";
-
-        final String 일호선 = "1호선";
-
-        List<ExtractableResponse<Response>> stationCreationResponses = StationApiCall.createStations(구일, 구로, 신도림, 영등포);
-        Long 구일_아이디 = getId(stationCreationResponses.get(0));
-        Long 구로_아이디 = getId(stationCreationResponses.get(1));
-        Long 신도림_아이디 = getId(stationCreationResponses.get(2));
-        Long 영등포_아이디 = getId(stationCreationResponses.get(3));
-
-        int 구일_구로_거리 = 10;
-        int 구로_신도림_거리 = 15;
-        int 신도림_영등포_거리 = 13;
-
         ExtractableResponse<Response> lineCreationResponse = LineApiCall.createLine(new LineRequest(일호선, "bg-blue-600",구일_아이디, 구로_아이디, 구일_구로_거리));
-
         Long 일호선_아이디 = lineCreationResponse.jsonPath().getLong("id");
 
         // when
@@ -89,24 +91,8 @@ public class SectionAcceptanceTest {
     @DisplayName("새로운 구간 등록 시 상행역이 하행종점역이 아닐 때 발생하는 예외 테스트")
     @Test
     void 새로운구간의_상행역이_하행종점역이_아님() {
+
         // given
-        final String 구일 = "구일역";
-        final String 구로 = "구로역";
-        final String 신도림 = "신도림역";
-        final String 영등포 = "영등포역";
-
-        final String 일호선 = "1호선";
-
-        List<ExtractableResponse<Response>> stationCreationResponses = StationApiCall.createStations(구일, 구로, 신도림, 영등포);
-
-        Long 구일_아이디 = getId(stationCreationResponses.get(0));
-        Long 구로_아이디 = getId(stationCreationResponses.get(1));
-        Long 신도림_아이디 = getId(stationCreationResponses.get(2));
-        Long 영등포_아이디 = getId(stationCreationResponses.get(3));
-
-        int 구일_구로_거리 = 10;
-        int 신도림_영등포_거리 = 15;
-
         ExtractableResponse<Response> lineCreationResponse = LineApiCall.createLine(new LineRequest(일호선, "bg-blue-600",구일_아이디, 구로_아이디, 구일_구로_거리));
         final Long 일호선_아이디 = lineCreationResponse.jsonPath().getLong("id");
 
@@ -125,21 +111,8 @@ public class SectionAcceptanceTest {
     @DisplayName("새로운 구간의 상행종점역이 기존 구간에 존재할 때 발생하는 예외 테스트")
     @Test
     void 새로운구간의_하행역이_기존구간에_존재함() {
+
         // given
-        final String 구일 = "구일역";
-        final String 구로 = "구로역";
-        final String 신도림 = "신도림역";
-        final String 영등포 = "영등포역";
-
-        final String 일호선 = "1호선";
-
-        List<ExtractableResponse<Response>> stationCreationResponses = StationApiCall.createStations(구일, 구로, 신도림, 영등포);
-        Long 구일_아이디 = getId(stationCreationResponses.get(0));
-        Long 구로_아이디 = getId(stationCreationResponses.get(1));
-
-        int 구일_구로_거리 = 10;
-        int 신도림_영등포_거리 = 15;
-
         ExtractableResponse<Response> lineCreationResponse = LineApiCall.createLine(new LineRequest(일호선, "bg-blue-600",구일_아이디, 구로_아이디, 구일_구로_거리));
         final Long 일호선_아이디 = lineCreationResponse.jsonPath().getLong("id");
 
@@ -159,30 +132,13 @@ public class SectionAcceptanceTest {
     @DisplayName("노선에서 지하철 구간을 삭제한다.")
     @Test
     void deleteSection() {
+
         // given
-        final String 구일 = "구일역";
-        final String 구로 = "구로역";
-        final String 신도림 = "신도림역";
-        final String 영등포 = "영등포역";
-
-        final String 일호선 = "1호선";
-
-        List<ExtractableResponse<Response>> stationCreationResponses = StationApiCall.createStations(구일, 구로, 신도림, 영등포);
-        Long 구일_아이디 = getId(stationCreationResponses.get(0));
-        Long 구로_아이디 = getId(stationCreationResponses.get(1));
-        Long 신도림_아이디 = getId(stationCreationResponses.get(2));
-        Long 영등포_아이디 = getId(stationCreationResponses.get(3));
-
-        int 구일_구로_거리 = 10;
-        int 구로_신도림역_거리 = 15;
-        int 신도림역_영등포역_거리 = 13;
-
         ExtractableResponse<Response> lineCreationResponse = LineApiCall.createLine(new LineRequest(일호선, "bg-blue-600",구일_아이디, 구로_아이디, 구일_구로_거리));
-
         final Long 일호선_아이디 = lineCreationResponse.jsonPath().getLong("id");
 
-        LineApiCall.registerSection(일호선_아이디, new SectionRequest(구로_아이디, 신도림_아이디, 구로_신도림역_거리));
-        LineApiCall.registerSection(일호선_아이디, new SectionRequest(신도림_아이디, 영등포_아이디, 신도림역_영등포역_거리));
+        LineApiCall.registerSection(일호선_아이디, new SectionRequest(구로_아이디, 신도림_아이디, 구로_신도림_거리));
+        LineApiCall.registerSection(일호선_아이디, new SectionRequest(신도림_아이디, 영등포_아이디, 신도림_영등포_거리));
 
         // when
         ExtractableResponse<Response> deleteSectionResponse = LineApiCall.deleteSection(일호선_아이디, new SectionRequest(신도림_아이디, 영등포_아이디));
@@ -205,26 +161,9 @@ public class SectionAcceptanceTest {
     @DisplayName("노선에서 마지막 구간이 아닌 구간을 삭제한다.")
     @Test
     void 구간삭제시_마지막구간이_아닐때() {
+
         // given
-        final String 구일 = "구일역";
-        final String 구로 = "구로역";
-        final String 신도림 = "신도림역";
-        final String 영등포 = "영등포역";
-
-        final String 일호선 = "1호선";
-
-        List<ExtractableResponse<Response>> stationCreationResponses = StationApiCall.createStations(구일, 구로, 신도림, 영등포);
-        Long 구일_아이디 = getId(stationCreationResponses.get(0));
-        Long 구로_아이디 = getId(stationCreationResponses.get(1));
-        Long 신도림_아이디 = getId(stationCreationResponses.get(2));
-        Long 영등포_아이디 = getId(stationCreationResponses.get(3));
-
-        int 구일_구로_거리 = 10;
-        int 구로_신도림_거리 = 15;
-        int 신도림_영등포_거리 = 13;
-
         ExtractableResponse<Response> lineCreationResponse = LineApiCall.createLine(new LineRequest(일호선, "bg-blue-600",구일_아이디, 구로_아이디, 구일_구로_거리));
-
         final Long 일호선_아이디 = lineCreationResponse.jsonPath().getLong("id");
 
         LineApiCall.registerSection(일호선_아이디, new SectionRequest(구로_아이디, 신도림_아이디, 구로_신도림_거리));
@@ -247,24 +186,9 @@ public class SectionAcceptanceTest {
     @DisplayName("구간이 하나뿐인 노선에서 구간을 삭제한다.")
     @Test
     void 구간이_하나남았을때_삭제() {
+
         // given
-        final String 구일 = "구일역";
-        final String 구로 = "구로역";
-        final String 신도림 = "신도림역";
-        final String 영등포 = "영등포역";
-
-        final String 일호선 = "1호선";
-
-        List<ExtractableResponse<Response>> stationCreationResponses = StationApiCall.createStations(구일, 구로, 신도림, 영등포);
-        Long 구일_아이디 = getId(stationCreationResponses.get(0));
-        Long 구로_아이디 = getId(stationCreationResponses.get(1));
-        Long 신도림_아이디 = getId(stationCreationResponses.get(2));
-
-        int 구일_구로_거리 = 10;
-        int 구로_신도림_거리 = 15;
-
         ExtractableResponse<Response> lineCreationResponse = LineApiCall.createLine(new LineRequest(일호선, "bg-blue-600",구일_아이디, 구로_아이디, 구일_구로_거리));
-
         final Long 일호선_아이디 = lineCreationResponse.jsonPath().getLong("id");
 
         LineApiCall.registerSection(일호선_아이디, new SectionRequest(구로_아이디, 신도림_아이디, 구로_신도림_거리));
@@ -276,7 +200,6 @@ public class SectionAcceptanceTest {
         // then
         assertThat(deleteSectionResponse.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
-
 
     // 응답객체 id 데이터 조회
     private Long getId(ExtractableResponse<Response> response) {
