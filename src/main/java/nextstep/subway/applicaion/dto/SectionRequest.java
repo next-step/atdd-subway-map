9fd40995b516a9a4e8b69b9c944c129cbdad26c9
chkim <cndghks0213@gmail.com>
2022-07-28 17:10:31 +0900

    feat: 구간 생성 요청에 사용할 SectionRequest 생성

---
+++ src/main/java/nextstep/subway/applicaion/dto/SectionRequest.java
@@ -0,0 +1,19 @@
+package nextstep.subway.applicaion.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SectionRequest {
+    private Long upStationId;
+    private Long downStationId;
+    private int distance;
+
+    public SectionRequest() {
+    }
+
+    public SectionRequest(Long upStationId, Long downStationId, int distance) {
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+}
