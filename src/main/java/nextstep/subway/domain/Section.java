630e38cc88ea7e43ad9c8c0518a0eb7525fb297b
jess <official1126@naver.com>
2022-08-02 03:34:30 +0900

    refactor import 문 제거

--- src/main/java/nextstep/subway/domain/Section.java
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -2,9 +2,7 @@ package nextstep.subway.domain;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 public class Section {
