f2230d9a153db0f77a18d5e05d327a8205aba257
cafitac <cafitac99@gmail.com>
2022-08-04 05:42:27 +0900

    feat: 구간 등록, 삭제에 대한 controller 코드를 작성합니다

---
+++ src/main/java/nextstep/subway/ui/SectionController.java
@@ -0,0 +1,41 @@
+package nextstep.subway.ui;
+
+import nextstep.subway.applicaion.SectionService;
+import nextstep.subway.applicaion.dto.section.CreateSectionRequest;
+import nextstep.subway.applicaion.dto.section.SectionResponse;
+import nextstep.subway.domain.Section;
+import nextstep.subway.exception.NotFoundException;
+import nextstep.subway.exception.ValidationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/lines")
+public class SectionController {
+
+    private final SectionService sectionService;
+
+    public SectionController(SectionService sectionService) {
+        this.sectionService = sectionService;
+    }
+
+    @PostMapping("/{lineId}/sections")
+    public ResponseEntity<SectionResponse> createSection(
+            @PathVariable Long lineId,
+            @RequestBody CreateSectionRequest createSectionRequest) throws ValidationException, NotFoundException {
+        final Long sectionId = sectionService.create(lineId, createSectionRequest);
+        final Section findSection = sectionService.findOne(sectionId);
+        final SectionResponse sectionResponse = new SectionResponse(findSection);
+
+        return ResponseEntity.created(URI.create("/lines/" + lineId + "/sections")).body(sectionResponse);
+    }
+
+    @DeleteMapping("/{lineId}/sections")
+    public ResponseEntity<Void> deleteSection(@PathVariable Long lineId, @RequestParam Long stationId) throws NotFoundException, ValidationException {
+        sectionService.delete(lineId, stationId);
+
+        return ResponseEntity.noContent().build();
+    }
+}
