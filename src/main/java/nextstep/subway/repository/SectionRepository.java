85d918fc63f79f75d22a31f4e0a3048c2d8d94e8
cafitac <cafitac99@gmail.com>
2022-08-04 05:43:13 +0900

    feat: 서비스 코드에 필요한 repository 쿼리를 등록합니다

--- src/main/java/nextstep/subway/repository/SectionRepository.java
+++ src/main/java/nextstep/subway/repository/SectionRepository.java
@@ -1,13 +1,20 @@
 package nextstep.subway.repository;
 
-import nextstep.subway.domain.Station;
-import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SectionRepository extends JpaRepository<Section, Long> {
 
     Optional<Section> findByUpStationAndDownStationAndLine(Station upStation, Station downStation, Line line);
+
+    Optional<Section> findByLineAndDownStation(Line line, Station downStation);
+
+    Optional<Section> findByLineAndUpStation(Line line, Station upStation);
+
+    List<Section> findAllByLine(Line line);
 }
