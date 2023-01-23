853fb7ff2c4387d22ab125c5a4f5eb33d3535771
TaeWoong Lee <doorwasopen@gmail.com>
2023-01-23 13:33:19 +0900

    test: 구글 페이지 요청 구현 테스트 (#505)

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,11 +1,13 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,8 +17,12 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when().get("https://google.com")
+                        .then().log().all()
+                        .extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
