e875d98cb1c802960036079e6effefd29c905510
codeleesh(이상호) <38518639+codeleesh@users.noreply.github.com>
2023-01-23 17:30:09 +0900

    refactor(test): 구글 호출의 대한 요청 / 응답 처리 (#508)

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,5 +1,6 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -8,16 +9,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class RestAssuredTest {
+class RestAssuredTest {
 
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        final ExtractableResponse<Response> 구글_호출_응답 = 구글_호출_요청();
+
+        assertAll(
+                () -> assertThat(구글_호출_응답.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(구글_호출_응답.body()).isNotNull()
+        );
+    }
+
+    private ExtractableResponse<Response> 구글_호출_요청() {
+
+        return RestAssured
+                .given().baseUri("https://google.com").log().all()
+                .when().get()
+                .then().log().all()
+                .extract();
     }
 }
