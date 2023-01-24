33e52e6c6aaffb0caa7c5c5b83ddec2d1d29db88
wooseok <rnjsdntjr26@daum.net>
2023-01-24 23:44:44 +0900

    test: 리뷰 사이클 연습 (#567)

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,5 +1,6 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +17,10 @@ public class RestAssuredTest {
     @Test
     void accessGoogle() {
         // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        ExtractableResponse<Response> response = RestAssured
+                                                    .get("https://google.com")
+                                                    .then().extract();
+
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
