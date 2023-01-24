a9b56bedc40b5f870e171a9644f525d9b5f0f7b6
hungrytech <74886812+hungrytech@users.noreply.github.com>
2023-01-24 13:51:39 +0900

    test: 구글 페이지 접근 테스트 작성 (#517)

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,23 +1,33 @@
 package subway;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
 
-    @DisplayName("구글 페이지 접근 테스트")
-    @Test
-    void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+	@DisplayName("구글 페이지 접근 테스트")
+	@Test
+	void accessGoogle() {
+		String googleUrl = "https://www.google.com";
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
+		//@formatter:off
+		ExtractableResponse<Response> response = given()
+			.log().all()
+				.when()
+				.get(googleUrl)
+			.then()
+				.log().all()
+			.extract();
+
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+	}
 }
