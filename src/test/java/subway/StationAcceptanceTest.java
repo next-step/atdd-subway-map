7c732931305e680257a9e37c7a73132fe4ab801c
dhlee3994 <dhlee3994@gmail.com>
2023-01-24 10:40:31 +0900

    Test: 지하철역 목록 조회 인수 테스트 작성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -14,10 +14,15 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class StationAcceptanceTest {
+
+    private final String stationName = "name";
+
     /**
      * When 지하철역을 생성하면
      * Then 지하철역이 생성된다
@@ -55,7 +60,39 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다.")
+    @Test
+    void testGetStations() {
+        // given
+        String 강남역 = "강남역";
+        String 서울대입구역 = "서울대입구역";
+
+        insertStation(강남역);
+        insertStation(서울대입구역);
+
+        // when & then
+        RestAssured
+                .given()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/stations")
+                .then()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .assertThat()
+                .body(stationName, hasSize(2))
+                .and()
+                .body(stationName, containsInAnyOrder(equalTo(서울대입구역), equalTo(강남역)));
+
+        // when
+        List<String> stationNames = getStations();
+
+        // then
+        assertAll(
+                () -> assertEquals(2, stationNames.size()),
+                () -> assertTrue(stationNames.containsAll(List.of(서울대입구역, 강남역)))
+        );
+    }
+
 
     /**
      * Given 지하철역을 생성하고
@@ -64,4 +101,25 @@ public class StationAcceptanceTest {
      */
     // TODO: 지하철역 제거 인수 테스트 메서드 생성
 
+
+    private List<String> getStations() {
+        return RestAssured
+                .given()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/stations")
+                .then()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .extract()
+                .jsonPath().getList(stationName, String.class);
+    }
+
+    private void insertStation(String station) {
+        Map<String, String> param = Map.of(stationName, station);
+
+        RestAssured.given()
+                .body(param)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations");
+    }
 }
