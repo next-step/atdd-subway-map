a63c7760d69c7b8590ed52a0f766477f9f86bf4f
dhlee3994 <121185072+dhlee3994@users.noreply.github.com>
2023-01-24 14:19:19 +0900

    Step1 (#549)
    * Docs: 요구사항 정리
    
    * Test: 지하철역 목록 조회 인수 테스트 작성
    
    * Test: 지하철역 삭제 인수 테스트 작성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -14,10 +14,15 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class StationAcceptanceTest {
+
+    private final String stationName = "name";
+
     /**
      * When 지하철역을 생성하면
      * Then 지하철역이 생성된다
@@ -55,13 +60,95 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다.")
+    @Test
+    void testGetStations() {
+        // given
+        String 강남역 = "강남역";
+        String 서울대입구역 = "서울대입구역";
+
+        insertStation(강남역);
+        insertStation(서울대입구역);
+
+        // when & then
+        RestAssured
+                .given()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/stations")
+                .then()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .assertThat()
+                .body(stationName, hasSize(2))
+                .and()
+                .body(stationName, containsInAnyOrder(equalTo(서울대입구역), equalTo(강남역)));
+
+        // when
+        List<String> stationNames = getStations();
+
+        // then
+        assertAll(
+                () -> assertEquals(2, stationNames.size()),
+                () -> assertTrue(stationNames.containsAll(List.of(서울대입구역, 강남역)))
+        );
+    }
+
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 삭제한다.")
+    @Test
+    void testDeleteStation() {
+        String 서울대입구역 = "서울대입구역";
+        insertStation(서울대입구역);
 
+        // when
+        RestAssured
+                .given()
+                .when()
+                .delete("/stations/{id}", 1)
+                .then()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+
+
+        // when & then
+        RestAssured
+                .given()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/stations")
+                .then()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(stationName, not(hasItem(서울대입구역)));
+
+        // when
+        List<String> stations = getStations();
+
+        // then
+        assertFalse(stations.contains(서울대입구역));
+    }
+
+    private List<String> getStations() {
+        return RestAssured
+                .given()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/stations")
+                .then()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .extract()
+                .jsonPath().getList(stationName, String.class);
+    }
+
+    private void insertStation(String station) {
+        Map<String, String> param = Map.of(stationName, station);
+
+        RestAssured.given()
+                .body(param)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations");
+    }
 }
