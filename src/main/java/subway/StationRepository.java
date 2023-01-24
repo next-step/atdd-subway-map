e1fcdb0ed40f4332f73a009f5da1e6061c76737d
Yunsik-Choi <choiys0311@gmail.com>
2023-01-24 17:24:17 +0900

    ⚡feat : Station Table Truncate Query 추가

--- src/main/java/subway/StationRepository.java
+++ src/main/java/subway/StationRepository.java
@@ -1,6 +1,17 @@
 package subway;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
+
+    @Transactional
+    @Modifying
+    @Query(
+            value = "TRUNCATE TABLE Station",
+            nativeQuery = true
+    )
+    void truncateTableStation();
 }
