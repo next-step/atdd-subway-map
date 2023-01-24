db1a3a1b656a3ca5c2a38ec6318ef8c5c4ad2a48
bada.kang <kangbada0728@gmail.com>
2023-01-24 10:22:09 +0900

    add: http://google.com RestAsssuredTest 추가 (#533)

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,11 +1,14 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+
+import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,9 +18,13 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
-
+        ExtractableResponse<Response> response = RestAssured
+                .given()
+                .when()
+                .get(URI.create("https://google.com"))
+                .then()
+                .statusCode(200)
+                .extract();
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
