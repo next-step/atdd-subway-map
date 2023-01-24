d3021b032bb50aa8d3cec9edeced1651f253753c
TaeWoong Lee <doorwasopen@gmail.com>
2023-01-24 10:13:10 +0900

    test: 지하철역 인수 테스트 작성 (#521)
    - 지하철역 목록 조회 인수 테스트 작성하기
    - 지하철역 삭제 인수 테스트 작성하기

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -9,9 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -55,13 +57,82 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다.")
+    @Test
+    void getStations() {
+        // given
+        String[] stationNames = new String[]{"강남역", "망포역"};
+        requestSaveStation(stationNames);
+        // when
+        List<String> findStationNames =
+                RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract().jsonPath().getList("name", String.class);
+        // then
+        assertThat(findStationNames.size()).isEqualTo(stationNames.length);
+        assertThat(findStationNames).containsAnyOf(stationNames);
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 제거한다.")
+    @Test
+    void deleteStation() {
+        // given
+        Station station = requestSaveStation("강남역");
+        // when
+        RestAssured.given().log().all()
+                .when().delete("/stations/" + station.getId())
+                .then().log().all()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+        // then
+        assertDoseNotContainsStation("강남역");
+    }
 
+    private List<Station> requestSaveStation(String... stationNames) {
+        return Arrays.stream(stationNames)
+                .map(this::requestSaveStation)
+                .collect(Collectors.toList());
+    }
+
+    private Station requestSaveStation(String stationName) {
+        // when
+        return RestAssured.given().log().all()
+                .body(new StationSaveRequest(stationName))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract()
+                .body()
+                .as(Station.class);
+    }
+
+    private void assertDoseNotContainsStation(String... stationNames) {
+        List<String> findStationNames = getStationNames();
+        assertThat(findStationNames).doesNotContain(stationNames);
+    }
+
+    private List<String> getStationNames() {
+        return RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+    }
+
+    static class StationSaveRequest {
+        private final String name;
+
+        public StationSaveRequest(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 }
