7d44191ad32839d4f1241f245c6667b97723610b
김영중 <idfever@naver.com>
2023-01-25 14:08:56 +0900

    review1 completed

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
@@ -16,8 +17,7 @@ public class RestAssuredTest {
     @Test
     void accessGoogle() {
         // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
-
+        Response response = given().when().get("https://google.com");
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
