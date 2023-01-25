f90330f964b5f6c97ceeaa4fd3a9f66f68cb9513
Dongle <sk1737030@naver.com>
2023-01-25 12:32:07 +0900

    test: 구글 페이지 접근 테스트 작성 (#572)
    Co-authored-by: dongho.geum <dongho.geum@myrealtrip.com>

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,11 +1,14 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+
+import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,7 +19,12 @@ public class RestAssuredTest {
     @Test
     void accessGoogle() {
         // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        ExtractableResponse<Response> response = RestAssured.
+            when()
+                .get(URI.create("https://www.google.com"))
+            .then()
+                .statusCode(HttpStatus.OK.value())
+            .extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
