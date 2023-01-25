39292f1422e178ec66526c16be2493bb0b2af337
DoubleK <airvw14@gmail.com>
2023-01-25 19:24:15 +0900

    test: request test for google page

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -2,21 +2,30 @@ package subway;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
+import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
+    private String url;
+
+    @BeforeEach
+    void setUp(){
+        url = "https://google.com";
+    }
 
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
         // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+//        ExtractableResponse<Response> response = get(url).then().extract();
+        ExtractableResponse<Response> response = when().get(url).then().extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
