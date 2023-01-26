9532e8bf68f38d2b0e3797388d10dd49f91c77e5
seungwoo.song <seungwoo.song@nhnpayco.com>
2023-01-26 19:38:43 +0900

    refactor(LineAcceptanceTest.java): 지하털 노선 목록 조회 기능 개선

--- src/test/java/subway/line/LineAcceptanceTest.java
+++ src/test/java/subway/line/LineAcceptanceTest.java
@@ -13,15 +13,14 @@ import subway.line.repository.LineRepository;
 import subway.station.StationAcceptanceTest;
 import subway.station.repository.StationRepository;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("지하철노선 관련 기능")
+@DisplayName("지하철 노선 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class LineAcceptanceTest {
 
@@ -38,14 +37,9 @@ public class LineAcceptanceTest {
     }
 
     @Test
-    void 지하철노선을_생성한다() {
+    void 지하철_노선을_생성한다() {
         //given
-        String name = "신분당선";
-        String color = "bg-red-600";
-        long upStationId = StationAcceptanceTest.createStation("상행역");
-        long downStationId = StationAcceptanceTest.createStation("하행역");
-        long distance = 10;
-        LineRequest lineRequest = new LineRequest(name, color, upStationId, downStationId, distance);
+        LineRequest lineRequest = createLineRequestFixture("신분당선");
 
         // when 지하철 노선을 생성하면
         long id = requestCreateLine(lineRequest).getId();
@@ -57,11 +51,36 @@ public class LineAcceptanceTest {
 
         assertThat(lineList).hasSize(1);
         assertThat(created.getId()).isEqualTo(id);
-        assertThat(created.getName()).isEqualTo(name);
-        assertThat(created.getColor()).isEqualTo(color);
+        assertThat(created.getName()).isEqualTo(lineRequest.getName());
+        assertThat(created.getColor()).isEqualTo(lineRequest.getColor());
         assertThat(created.getStations()).hasSize(2);
-        assertThat(created.getStations().get(0).getId()).isEqualTo(upStationId);
-        assertThat(created.getStations().get(1).getId()).isEqualTo(downStationId);
+        assertThat(created.getStations().get(0).getId()).isEqualTo(lineRequest.getUpStationId());
+        assertThat(created.getStations().get(1).getId()).isEqualTo(lineRequest.getDownStationId());
+    }
+
+    @Test
+    void 지하철_노선_목록_조회() {
+        //given 2개의 지하철노선을 생성하고
+        long id1 = requestCreateLine(createLineRequestFixture("1호선")).getId();
+        long id2 = requestCreateLine(createLineRequestFixture("2호선")).getId();
+
+        //when 지하철노선 목록을 조회하면
+        List<LineResponse> findLines = requestFindAllLines();
+        List<Long> ids = findLines.stream()
+                .map(LineResponse::getId)
+                .collect(Collectors.toList());
+
+        //then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+        assertThat(findLines).hasSize(2);
+        assertThat(ids).contains(id1, id2);
+    }
+
+    private LineRequest createLineRequestFixture(String name) {
+        String color = "bg-red-600";
+        long upStationId = StationAcceptanceTest.createStation(name + "-상행역");
+        long downStationId = StationAcceptanceTest.createStation(name + "-하행역");
+        long distance = 10;
+        return new LineRequest(name, color, upStationId, downStationId, distance);
     }
 
     private LineResponse requestCreateLine(LineRequest lineRequest) {
@@ -77,20 +96,25 @@ public class LineAcceptanceTest {
     }
 
     @Test
-    void 지하철노선_목록_조회() {
-        //given 2개의 지하철노선을 생성하고
-        createLine("강남역");
-        createLine("역삼역");
+    void 지하철_노선_조회() {
+        //Given 지하철 노선을 생성하고
+        //createLine("강남역");
+        //When 생성한 지하철 노선을 조회하면
+        //Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+    }
 
-        //when 지하철노선 목록을 조회하면
-        List<String> stationNames = when().get("/stations")
-                .then()
-                .extract()
-                .jsonPath()
-                .getList("name", String.class);
+    @Test
+    void 지하철_노선_수정() {
+        //Given 2개의 지하철 노선을 생성하고
+        //When 지하철 노선 목록을 조회하면
+        //Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+    }
 
-        //then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
-        assertThat(stationNames).contains("강남역", "역삼역"); // contains 를 이용해 두 역이 모두 포함되어 있는지 검증
+    @Test
+    void 지하철_노선_삭제() {
+        //Given 지하철 노선을 생성하고
+        //When 생성한 지하철 노선을 삭제하면
+        //Then 해당 지하철 노선 정보는 삭제된다
     }
 
     private List<LineResponse> requestFindAllLines() {
@@ -98,21 +122,5 @@ public class LineAcceptanceTest {
                 .then()
                 .extract()
                 .jsonPath().getList(".", LineResponse.class);
-    }
-
-    private String createLine(String stationName) {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", stationName);
-
-        return given()
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .post("/stations")
-                .then()
-                .statusCode(HttpStatus.CREATED.value())
-                .extract()
-                .jsonPath()
-                .getString("id"); // 응답 객체에서 id값을 추출해 리턴
     }
 }
