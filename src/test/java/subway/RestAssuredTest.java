ab0c9dc79cef3317d043d3921102273a91e55c7c
hyun6ik <fkanwm@naver.com>
2023-01-26 18:33:42 +0900

    feat : 리뷰 사이클 연습

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -16,7 +16,6 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
         String GOOGLE_URL = "https://google.com";
 
         ExtractableResponse<Response> response =
