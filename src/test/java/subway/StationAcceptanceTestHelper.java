ebf4f204dcc76fa537016cac5f29fd96866ec87f
cafitac <cafitac99@gmail.com>
2023-01-26 22:01:18 +0900

    refactor: 테스트 코드의 중복 코드를 개선합니다

--- src/test/java/subway/StationAcceptanceTestHelper.java
+++ src/test/java/subway/StationAcceptanceTestHelper.java
@@ -9,6 +9,8 @@ import org.springframework.http.MediaType;
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StationAcceptanceTestHelper {
 
     static ExtractableResponse<Response> 지하철역_생성_요청(final Map<String, Object> params) {
@@ -21,14 +23,8 @@ public class StationAcceptanceTestHelper {
     }
 
     static String 지하철역_생성함(final Map<String, Object> params) {
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .assertThat().statusCode(HttpStatus.CREATED.value())
-                        .extract();
+        final ExtractableResponse<Response> response = 지하철역_생성_요청(params);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         return response.header("location");
     }
 
@@ -40,12 +36,8 @@ public class StationAcceptanceTestHelper {
     }
 
     static List<String> 지하철역_목록_조회함() {
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .assertThat().statusCode(HttpStatus.OK.value())
-                        .extract();
+        final ExtractableResponse<Response> response = 지하철역_목록_조회_요청();
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         return response.jsonPath().getList("name", String.class);
     }
 
