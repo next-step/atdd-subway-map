bac2568f8d9cff2d03c36c0580e238489f8cc3b4
hyunssooo <jhs695@gmail.com>
2023-01-26 22:31:28 +0900

    refactor: 테스트 코드 정리

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -42,7 +42,6 @@ class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
     @DisplayName("지하철역 목록 조회")
     @Test
     void 지하철역_목록_조회() {
@@ -51,10 +50,9 @@ class StationAcceptanceTest {
 
         final List<String> stationNameResponses = 지하철역_이름들을_조회한다();
 
-        assertAll(
-            () -> assertThat(stationNameResponses.size()).isEqualTo(stationNames.size()),
-            () -> assertThat(stationNameResponses).containsAll(stationNames)
-        );
+        assertThat(stationNameResponses)
+            .hasSize(stationNames.size())
+            .containsAll(stationNames);
     }
 
     /**
@@ -62,7 +60,6 @@ class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
     @DisplayName("지하철역 제거")
     @Test
     void 지하철역_제거() {
@@ -83,13 +80,15 @@ class StationAcceptanceTest {
 
         final ExtractableResponse<Response> deleteResponse = 지하철역을_삭제한다(강남역Id);
 
+        assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
         final List<String> stationNameResponses = 지하철역_이름들을_조회한다();
 
         assertAll(
-            () -> assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
             () -> assertThat(stationNameResponses.size()).isNotEqualTo(stationNames.size()),
-            () -> assertThat(stationNameResponses).doesNotContain(강남역),
-            () -> assertThat(stationNameResponses).contains(잠실역)
+            () -> assertThat(stationNameResponses)
+                .doesNotContain(강남역)
+                .contains(잠실역)
         );
     }
 
