53c5816c6b45d0f0438186f04793955f6164b8f2
michol.lee <lovethefeel@gmail.com>
2023-01-27 02:11:08 +0900

    refactor(test): entity 어노테이션 붙은 테이블 자동 삭제 처리

--- src/test/java/subway/DatabaseStoreCleanup.java
+++ src/test/java/subway/DatabaseStoreCleanup.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.metamodel.EntityType;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @ActiveProfiles("local")
@@ -15,14 +19,24 @@ public class DatabaseStoreCleanup implements InitializingBean {
     @PersistenceContext
     private EntityManager entityManager;
 
+    private List<String> tableNames;
+
     @Override
     public void afterPropertiesSet() {
+        tableNames = entityManager.getMetamodel().getEntities().stream()
+                .filter(entity -> entity.getJavaType().getAnnotation(Entity.class) != null)
+                .map(EntityType::getName)
+                .collect(Collectors.toList());
     }
 
     @Transactional
     public void cleanStore() {
         entityManager.flush();
-        entityManager.createNativeQuery("TRUNCATE TABLE station").executeUpdate();
-        entityManager.createNativeQuery("ALTER TABLE station ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+        for (String tableName : tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+        }
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
     }
 }
