bc46ed8a2dcc6a3feae59a71b4bedc1325b60427
hiblue02 <hiblue02@gmail.com>
2023-01-26 22:59:46 +0900

    feat(SectionRepository): SectionRepository 추가

--- src/main/java/subway/StationService.java
+++ src/main/java/subway/StationService.java
@@ -2,6 +2,8 @@ package subway;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import subway.domain.Station;
+import subway.repository.StationRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
---
+++ src/main/java/subway/repository/SectionRepository.java
@@ -0,0 +1,8 @@
+package subway.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import subway.domain.Section;
+
+public interface SectionRepository extends JpaRepository<Section, Long> {
+
+}
