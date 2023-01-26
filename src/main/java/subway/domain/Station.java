badb6938f607656a3e305fdcbdd7aeeb85bcdab4
michol.lee <lovethefeel@gmail.com>
2023-01-27 02:16:35 +0900

    refactor: 역 객체 연관관계 및 생성자 추가

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,20 +1,31 @@
-package subway;
+package subway.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Station {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(length = 20, nullable = false)
     private String name;
 
-    public Station() {
+    @ManyToMany(mappedBy = "stations")
+    private List<Line> lines = new ArrayList<>();
+
+    protected Station() {
     }
 
-    public Station(String name) {
+    public Station(final Long id, final String name) {
+        this.id = id;
         this.name = name;
+    }
+
+    public Station(final String name) {
+        this(null, name);
     }
 
     public Long getId() {
