4359b5eb3ff8ba85a1f066a7ad58c0d1cba9ea0f
서민정 <seominjeong.dev@gmail.com>
2023-01-28 08:39:59 +0900

    🚀 1단계 - 지하철역 인수 테스트 작성 (#590)
    * docs: 요구사항을 작성해요.
    
    * feat: 지하철역 목록 조회 인수 테스트를 구현해요.
    
    * feat: 지하철역 삭제 인수테스트를 구현해요.
    
    * feat: 지하철역 생성에서 repository 의존을 제거해요.

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -3,11 +3,14 @@ package subway;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,8 +19,19 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StationAcceptanceTest {
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    private static final String ENDPOINT = "/stations";
+
     /**
      * When 지하철역을 생성하면
      * Then 지하철역이 생성된다
@@ -50,18 +64,80 @@ public class StationAcceptanceTest {
         assertThat(stationNames).containsAnyOf("강남역");
     }
 
-    /**
-     * Given 2개의 지하철역을 생성하고
-     * When 지하철역 목록을 조회하면
-     * Then 2개의 지하철역을 응답 받는다
-     */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("등록된 모든 지하철역을 조회한다.")
+    @Test
+    void findAllStations() {
+        // Given 2개의 지하철역을 생성하고
+        StationApi.create(ENDPOINT, new JsonBodyParams("name", "강남역"));
+        StationApi.create(ENDPOINT, new JsonBodyParams("name", "서울대입구역"));
 
-    /**
-     * Given 지하철역을 생성하고
-     * When 그 지하철역을 삭제하면
-     * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
-     */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+        // When 지하철역 목록을 조회하면
+        List<String> stationNames =
+                RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract().jsonPath().getList("name", String.class);
 
+        // Then 2개의 지하철역을 응답받는다.
+        assertThat(stationNames).hasSize(2);
+        assertThat(stationNames).containsExactly("강남역", "서울대입구역");
+    }
+
+    @DisplayName("등록된 지하철을 삭제하면 삭제된 역은 조회할 수 없다.")
+    @Test
+    void deleteAllStations() {
+        // Given 지하철역을 생성하고
+        ExtractableResponse<Response> 강남역_생성_응답 = StationApi.create(ENDPOINT, new JsonBodyParams("name", "강남역"));
+        ExtractableResponse<Response> 서울대입구역_생성_응답 = StationApi.create(ENDPOINT, new JsonBodyParams("name", "서울대입구역"));
+
+        Long 강남역_아이디 = 강남역_생성_응답.jsonPath().getLong("id");
+        Long 서울대입구역_아이디 = 서울대입구역_생성_응답.jsonPath().getLong("id");
+
+        // When 그 지하철역을 삭제하면
+        RestAssured.given().log().all()
+                .when().delete(String.format("/stations/%s", 강남역_아이디))
+                .then().log().all();
+
+        // Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
+        List<Long> stationIds =
+                RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract().jsonPath().getList("id", Long.class);
+
+        assertThat(stationIds).containsExactly(서울대입구역_아이디);
+        assertThat(stationIds).doesNotContain(강남역_아이디);
+    }
+}
+
+class JsonBodyParams {
+    String key;
+    String value;
+
+    public JsonBodyParams(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
+
+class StationApi {
+    static ExtractableResponse<Response> create(String endpoint, JsonBodyParams jsonBodyParams) {
+        Map<String, String> params = new HashMap<>();
+        params.put(jsonBodyParams.getKey(), jsonBodyParams.getValue());
+
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post(endpoint)
+                .then().log().all()
+                .extract();
+    }
 }
