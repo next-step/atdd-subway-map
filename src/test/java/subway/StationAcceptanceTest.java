0963268fc64f782bfbce4671e84a8ad223b83075
sangwon lee <231rollo@gmail.com>
2023-01-27 11:40:21 +0900

    1단계 - 지하철역 인수 테스트 작성
    리뷰 사항 반영
    
    - TODO 주석 삭제
    
    - 인수 조건에 부합한 테스트만 검증

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -58,7 +58,6 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
     @DisplayName("지하철역 목록 조회")
     @Test
     void showStations() {
@@ -88,7 +87,6 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
     @DisplayName("지하철역 삭제")
     @Test
     void deleteStation() {
@@ -113,10 +111,6 @@ public class StationAcceptanceTest {
                         .when().get("/stations")
                         .then().log().all()
                         .extract().jsonPath().getList("name", String.class);
-        String yorkSamStation = yorkSam.jsonPath().getString("name");
-        assertThat(stationNames).containsExactly(yorkSamStation);
-
-        // then
         String gangNamStation = gangNam.jsonPath().getString("name");
         assertThat(stationNames).doesNotContain(gangNamStation);
     }
