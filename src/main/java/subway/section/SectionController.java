e91081bafe2e3e110187c7221fcbaa00c3118712
swsong <ssw890720@gmail.com>
2023-01-27 22:46:36 +0900

    fix(SectionController.java): 구간 저장 오류 수정
    request 객체에 누락된 lineId 세팅

--- src/main/java/subway/section/SectionController.java
+++ src/main/java/subway/section/SectionController.java
@@ -2,10 +2,12 @@ package subway.section;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import subway.line.dto.SectionCreateRequest;
+import subway.line.repository.LineRepository;
 import subway.section.dto.SectionResponse;
 import subway.section.entity.Section;
 import subway.section.service.SectionService;
@@ -17,9 +19,11 @@ import java.net.URI;
 public class SectionController {
 
     private final SectionService sectionService;
+    private final LineRepository lineRepository;
 
     @PostMapping("/lines/{lineId}/sections")
-    public ResponseEntity<SectionResponse> saveSection(@RequestBody SectionCreateRequest request) {
+    public ResponseEntity<SectionResponse> saveSection(@PathVariable long lineId, @RequestBody SectionCreateRequest request) {
+        request.setLineId(lineId);
         Section saved = sectionService.save(request);
         return ResponseEntity.created(URI.create("lines/" + request.getLineId() + "/sections")).body(SectionResponse.from(saved));
     }
