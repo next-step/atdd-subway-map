5ea59c756b6ab63fd3466ae87e59d856147ce3bd
michol.lee <lovethefeel@gmail.com>
2023-01-27 23:55:59 +0900

    refactor: 노선 응답 중 역 목록 응답 Json 문자열 변경

--- src/main/java/subway/ui/dto/LineResponse.java
+++ src/main/java/subway/ui/dto/LineResponse.java
@@ -1,15 +1,18 @@
 package subway.ui.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import subway.domain.Line;
 import subway.domain.Station;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LineResponse {
     private final long id;
     private final String name;
     private final String color;
+    @JsonProperty("stations")
     private final List<StationResponse> stationResponses;
 
     public LineResponse(final long id, final String name, final String color, final List<StationResponse> stationResponses) {
@@ -20,11 +23,11 @@ public class LineResponse {
     }
 
     public static LineResponse createResponse(final Line saveLine) {
-        return new LineResponse(saveLine.getId(), saveLine.getName(), saveLine.getColor(), convertToStationResponse(saveLine.getStations()));
+        return new LineResponse(saveLine.getId(), saveLine.getName(), saveLine.getColor(), convertToStationResponse(saveLine.getUpStation(), saveLine.getDownStation()));
     }
 
-    private static List<StationResponse> convertToStationResponse(final List<Station> stations) {
-        return stations.stream()
+    private static List<StationResponse> convertToStationResponse(final Station...station) {
+        return Stream.of(station[0], station[1])
                 .map(StationResponse::of)
                 .collect(Collectors.toList());
     }
