dd49c162b1d647d9a56391ed92de54bedbbc5ed2
Jaeyoon <wodbs4338@naver.com>
2023-01-28 00:16:31 +0900

    step1 - 지하철역 조회, 삭제 인수테스트 구현 및 지하철역 생성 테스트 코드 리팩토링

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -25,29 +25,20 @@ public class StationAcceptanceTest {
      */
     @DisplayName("지하철역을 생성한다.")
     @Test
-    void createStation() {
-        // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "강남역");
+    void create() {
 
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+        // given
+        String station = "강남역";
+
+        // when
+        ExtractableResponse<Response> response = create(station);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
-        assertThat(stationNames).containsAnyOf("강남역");
+        List<String> stationNames = getAllStations().jsonPath().getList("name", String.class);
+        assertThat(stationNames).containsAnyOf(station);
     }
 
     /**
@@ -55,13 +46,72 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("생성된 지하철역들을 조회한다.")
+    @Test
+    void getStationList() {
+
+        // given
+        List<String> stations = List.of("강남역","양재역");
+        for (String station : stations) {
+            create(station);
+        }
+
+        // when
+        List<String> stationNames = getAllStations().jsonPath().getList("name", String.class);
+
+        // then
+        assertThat(stationNames).containsAll(stations);
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("생성된 지하철역을 제거한다.")
+    @Test
+    void deleteStation() {
+
+        // given
+        List<String> stations = List.of("강남역","양재역");
+        for (String station : stations) {
+            create(station);
+        }
+
+        // when
+        List<Long> stationIds = getAllStations().jsonPath().getList("id", Long.class);
+        Long deleteId = stationIds.get(0);
+        delete(deleteId);
+
+        // then
+        List<Long> result = getAllStations().jsonPath().getList("id", Long.class);
+        assertThat(result).doesNotContain(deleteId);
+    }
+
+    private ExtractableResponse<Response> create(String stationName) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", stationName);
+
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> getAllStations() {
+        return RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> delete(Long stationId) {
+        return RestAssured.given().log().all()
+                .when().delete("/stations/"+stationId)
+                .then().log().all()
+                .extract();
+    }
 
 }
