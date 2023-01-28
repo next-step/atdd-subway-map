5b70e5bac7f66071e7c4e09ea9e54f2108497304
boradol2 <boradol0902@gmail.com>
2023-01-28 18:39:59 +0900

    test(LineAcceptanceTest): Create Line test

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -1,10 +1,18 @@
 package subway;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
+import subway.line.LineRequest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 
 @DisplayName("지하철 노선 관련 기능")
@@ -12,4 +20,24 @@ import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFOR
 @Sql("/stations.sql")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class LineAcceptanceTest {
+    @DisplayName("지하철 노선을 생성한다")
+    @Test
+    void createLine() {
+        //when
+        ExtractableResponse<Response> response = createLineExtractableResponse(
+                new LineRequest("신분당선", "bg-red-600", (long)1, (long)2, 10)
+        );
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.jsonPath().getString("name")).contains("신분당선");
+    }
+
+    private static ExtractableResponse<Response> createLineExtractableResponse(LineRequest lineRequest) {
+        return RestAssured.given().log().all()
+                .body(lineRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+    }
 }
