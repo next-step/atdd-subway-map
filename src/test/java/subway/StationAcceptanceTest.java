eb64a5d771d17b7131d5c5c4375d7d2a1b60f944
JungTag <ytaek@gmarket.com>
2023-01-28 19:36:57 +0900

    1단계 - 지하철역 인수 테스트 작성 (#612)
    * [test] 리뷰 사이클 연습용 테스트 코드 추가
    
    * [test] 1단계 - 지하철역 인수 테스트 작성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -55,13 +55,54 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @Test
+    @DisplayName("지하철역을 조회한다.")
+    void getStations() {
+        // given
+        createStation("역삼역");
+        createStation("선릉역");
+
+        // when
+        List<String> stationNames = RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+
+        // then
+        assertThat(stationNames).containsExactlyInAnyOrder("역삼역", "선릉역");
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @Test
+    @DisplayName("지하철역을 제거한다.")
+    void deleteStation() {
+        // given
+        String id = createStation("삼성역").jsonPath().getString("id");
 
+        // when
+        RestAssured.given().log().all()
+                .when().delete("/stations/" + id)
+                .then().log().all()
+                .extract();
+
+        // then
+        List<String> stationNames = RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+        assertThat(stationNames).doesNotContain("삼성역");
+    }
+
+    private ExtractableResponse<Response> createStation(String name) {
+        return RestAssured.given()
+                .body(Map.of("name", name))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
 }
