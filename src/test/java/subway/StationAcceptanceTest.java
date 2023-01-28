e2610e117485a0a70753a371efc9f03cb59c99ff
hyungkiShin <tkaqkeldk@nate.com>
2023-01-28 17:09:21 +0900

    refactor: 코드리뷰 반영

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -1,10 +1,15 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 import java.util.HashMap;
@@ -14,8 +19,19 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class StationAcceptanceTest extends RestMethods {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    private static final String STATION_PATH = "/stations";
+
     /**
      * When 지하철역을 생성하면
      * Then 지하철역이 생성된다
@@ -28,7 +44,7 @@ class StationAcceptanceTest extends RestMethods {
         createStation("강남역");
 
         // when
-        final ExtractableResponse<Response> response = get("/stations");
+        final ExtractableResponse<Response> response = get(STATION_PATH);
         final List<String> stationNames = response.jsonPath()
                 .getList("name", String.class);
 
@@ -42,19 +58,22 @@ class StationAcceptanceTest extends RestMethods {
      * Then 2개의 지하철역을 응답 받는다
      */
     @DisplayName("두개의 지하철역을 입력받아 생성하고, 목록을 조회하면 입력한 두 지하철역이 존재해야 한다.")
-    @Test
-    void requestAndResponseForSubwayStationTest() {
+    @ParameterizedTest
+    @ValueSource(strings = {"강남역", "역삼역"})
+    void requestAndResponseForSubwayStationTest(String stationName) {
         // given
-        createStation("인덕원역");
-        createStation("안양역");
+        createStation(stationName);
+        createStation(stationName);
 
         // when
-        ExtractableResponse<Response> response = get("/stations");
+        ExtractableResponse<Response> response = get(STATION_PATH);
         List<String> stationNames = response.jsonPath()
                 .getList("name", String.class);
 
+
         // then
-        assertThat(stationNames).containsAnyOf("인덕원역", "안양역");
+        assertThat(stationNames)
+                .containsAnyOf(stationName);
     }
 
     /**
@@ -62,21 +81,23 @@ class StationAcceptanceTest extends RestMethods {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
-    @Test
-    @DisplayName("지하철역을 생성한뒤 생성한 지하철역을 생성하면 처음에 생성한 지하철역은 목록에 없어야 한다")
-    void createAndDeleteAfterNotFoundStation() {
+    @DisplayName("지하철역을 생성한뒤 생성한 지하철역을 삭제하면 처음에 생성한 지하철역은 목록에 없어야 한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"강남역", "역삼역"})
+    void createAndDeleteAfterFindNoContentsStation(String stationName) {
         // given
-        final Integer id = createStation("안양역")
+        final Integer id = createStation(stationName)
                 .jsonPath()
                 .get("id");
 
         // when
-        final ExtractableResponse<Response> response = delete("/station" + id);
+        final String deletePath = String.format(STATION_PATH + "/%s", id);
+
+        final ExtractableResponse<Response> response = delete(deletePath);
 
         // then
         assertThat(response.statusCode())
-                .isEqualTo(HttpStatus.NOT_FOUND.value());
+                .isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
     /**
@@ -88,7 +109,7 @@ class StationAcceptanceTest extends RestMethods {
         Map<String, String> params = new HashMap<>();
         params.put("name", name);
 
-        return post("/stations", params);
+        return post(STATION_PATH, params);
     }
 
 }
