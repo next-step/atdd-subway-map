485a24c692b046f17c72708f455eece73abc771a
michol.lee <lovethefeel@gmail.com>
2023-01-28 21:18:51 +0900

    refactor(test): 인수 테스트 검증 개선 - 응답 헤더에서 ID 추출 - 응답 바디 목록에서 ID 목록 추출 - 추출된 ID로 검증

--- src/test/java/subway/acceptance/TestFixtureLine.java
+++ src/test/java/subway/acceptance/TestFixtureLine.java
@@ -6,9 +6,12 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import subway.ui.dto.LineResponse;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -45,36 +48,43 @@ public class TestFixtureLine {
     static ExtractableResponse<Response> 지하철_노선_목록_조회_요청() {
         return RestAssured
                 .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().get("/lines")
                 .then().log().all()
                 .extract();
     }
 
-    static void 지하철_노선_목록_조회됨(ExtractableResponse<Response> response, final String lineName, final String lineColor, final int countOfStation) {
-        final JsonPath jsonPath = response.response().body().jsonPath();
+    static void 지하철_노선_목록_중_생성한_노선_조회됨(final ExtractableResponse<Response> lineResponse, final ExtractableResponse<Response> createResponse) {
+        final List<Long> 지하철_노선_목록_ID = 지하철_노선_목록_중_ID_목록_추출함(lineResponse);
+        final Long 지하철_노선_생성_ID = 응답_헤더_ID_추출함(createResponse);
 
         assertAll(
-                () -> assertThat(response.response().statusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(jsonPath.getString("[0].name")).isEqualTo(lineName),
-                () -> assertThat(jsonPath.getString("[0].color")).isEqualTo(lineColor),
-                () -> assertThat(jsonPath.getList("[0].stations")).hasSize(countOfStation)
+                () -> assertThat(lineResponse.response().statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(지하철_노선_목록_ID).contains(지하철_노선_생성_ID)
         );
     }
 
-    static void 지하철_노선_목록_조회됨(final ExtractableResponse<Response> lineResponse) {
+    private static List<Long> 지하철_노선_목록_중_ID_목록_추출함(ExtractableResponse<Response> lineResponse) {
+        return lineResponse.jsonPath().getList(".", LineResponse.class).stream()
+                .map(LineResponse::getId)
+                .collect(Collectors.toList());
+    }
+
+    private static long 응답_헤더_ID_추출함(final ExtractableResponse<Response> createResponse) {
+        return Long.parseLong(createResponse.header("Location").split("/")[2]);
+    }
+
+    static void 지하철_노선_목록_조회됨(final ExtractableResponse<Response> lineResponse, final int countOfLine) {
         final JsonPath 지하철_목록_응답_경로 = lineResponse.response().body().jsonPath();
 
         assertAll(
                 () -> assertThat(lineResponse.response().statusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(지하철_목록_응답_경로.getList("")).hasSize(2)
+                () -> assertThat(지하철_목록_응답_경로.getList("")).hasSize(countOfLine)
         );
     }
 
     static ExtractableResponse<Response> 지하철_노선_조회_요청(final ExtractableResponse<Response> response) {
         return RestAssured
                 .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().get(response.header("location"))
                 .then().log().all()
                 .extract();
