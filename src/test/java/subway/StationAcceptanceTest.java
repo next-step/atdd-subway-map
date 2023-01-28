48dfdea7deae931c6c1780a76e5a26e2097973c1
bada.kang <kangbada0728@gmail.com>
2023-01-29 00:52:07 +0900

    update: 지하철 역 삭제 테스트 추가

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -80,6 +80,19 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철 역을 삭제한다.")
+    @Test
+    void deleteStation() {
+        // given
+        createStation("강남역");
 
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when().delete("/stations/1")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
 }
