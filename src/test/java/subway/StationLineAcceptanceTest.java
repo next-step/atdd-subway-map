449c8d04e73f64bd2999524ca32f6ecb4adb9fa2
bada.kang <kangbada0728@gmail.com>
2023-01-29 21:45:22 +0900

    add: 지하철 노선 등록, 목록조회, 조회, 수정, 삭제 테스트 메서드 추가

---
+++ src/test/java/subway/StationLineAcceptanceTest.java
@@ -0,0 +1,72 @@
+package subway;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DisplayName("지하철 역 노선 관련 기능")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class StationLineAcceptanceTest {
+    @BeforeEach
+    void setup() {
+        StationUtils.createStation("강남역");
+        StationUtils.createStation("신사역");
+        StationUtils.createStation("판교역");
+    }
+
+    /**
+     * When 지하철 노선을 생성하면
+     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+     */
+    @Test
+    void createStationLine() {
+
+    }
+
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @Test
+    void selectStationLineList() {
+
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 조회하면
+     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+     */
+    @Test
+    void selectStationLine() {
+
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 수정하면
+     * Then 해당 지하철 노선 정보는 수정된다
+     */
+    @Test
+    void updateStationLine() {
+
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 삭제하면
+     * Then 해당 지하철 노선 정보는 삭제된다
+     */
+    @Test
+    void deleteStationLine() {
+
+    }
+
+
+
+}
