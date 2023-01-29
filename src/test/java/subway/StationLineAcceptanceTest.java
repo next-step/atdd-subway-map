4e5010ba024e9a81cb6a2c2ebd515d21d54d5ebd
Sanghou <tkdgh97531@gmail.com>
2023-01-29 20:00:20 +0900

    Add test body

--- src/test/java/subway/StationLineAcceptanceTest.java
+++ src/test/java/subway/StationLineAcceptanceTest.java
@@ -1,9 +1,13 @@
 package subway;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import subway.stationLine.StationLine;
 
 @DisplayName("지하철 노선 관리 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -15,8 +19,11 @@ public class StationLineAcceptanceTest {
    */
   @DisplayName("지하철노선 생성 테스트")
   @Test
-  void 지하철노선_생성_테스트() {
+  void 지하철_노선_생성_테스트() {
+    // when
+    StationLine line = 지하철_노선_생성();
 
+    assertThat(지하철_노선_조회()).isEqualTo(line);
   }
 
   /**
@@ -26,8 +33,11 @@ public class StationLineAcceptanceTest {
    */
   @DisplayName("지하철노선 목록 조회 테스트")
   @Test
-  void 지하철노선_목록_조회_테스트() {
+  void 지하철_노선_목록_조회_테스트() {
+    StationLine line1 = 지하철_노선_생성();
+    StationLine line2 = 지하철_노선_생성();
 
+    assertThat(지하철_노선_목록_조회()).containsAll(List.of(line1,line2));
   }
 
   /**
@@ -37,8 +47,15 @@ public class StationLineAcceptanceTest {
    */
   @DisplayName("지하철노선 조회 테스트")
   @Test
-  void 지하철노선_조회_테스트() {
+  void 지하철_노선_조회_테스트() {
+    // given
+    StationLine created = 지하철_노선_생성();
 
+    // when
+    StationLine show = 지하철_노선_조회();
+
+    // then
+    assertThat(show.getId()).isEqualTo(created.getId());
   }
 
   /**
@@ -48,8 +65,15 @@ public class StationLineAcceptanceTest {
    */
   @DisplayName("지하철노선 수정 테스트")
   @Test
-  void 지하철노선_수정_테스트() {
+  void 지하철_노선_수정_테스트() {
+    // given
+    StationLine created = 지하철_노선_생성();
 
+    // when
+    StationLine updated = 지하철_노선_수정();
+
+    // then
+    assertThat(지하철_노선_조회()).isEqualTo(updated);
   }
 
   /**
@@ -59,7 +83,34 @@ public class StationLineAcceptanceTest {
    */
   @DisplayName("지하철노선 삭제 테스트")
   @Test
-  void 지하철노선_삭제_테스트() {
+  void 지하철_노선_삭제_테스트() {
+    // given
+    StationLine line = 지하철_노선_생성();
+
+    // when
+    지하철_노선_삭제();
+
+    // then
+    assertThat(지하철_노선_조회()).isNull();
+  }
+
+  private StationLine 지하철_노선_생성() {
+    return null;
+  }
+
+  private StationLine 지하철_노선_조회() {
+    return null;
+  }
+
+  private List<StationLine> 지하철_노선_목록_조회() {
+    return null;
+  }
+
+  private StationLine 지하철_노선_수정() {
+    return null;
+  }
+
+  private void 지하철_노선_삭제() {
 
   }
 }
