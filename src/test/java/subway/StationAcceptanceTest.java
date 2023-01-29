ab0e56a8d126c4ed1f9527674c7e629aae09596b
최현웅 <76509935+choi1204@users.noreply.github.com>
2023-01-29 16:03:26 +0900

    test : 삭제 추가

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -37,7 +37,7 @@ public class StationAcceptanceTest {
     @DisplayName("강남역 지하철역을 생성한다.")
     @Test
     void createStation() {
-        // when 
+        // when
         createStation("강남역");
 
         // then
@@ -85,20 +85,29 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
+    @DisplayName("지하철역을 삭제합니다.")
+    @Test
     public void deleteStation() {
         // Given
         Long id = createStation("강남역");
         createStation("역삼역");
 
         // When
-        var response = RestAssured.given().log().all()
-                .when().delete("/station/" + id)
+        var deleteResponse = RestAssured.given().log().all()
+                .when().delete("/stations/" + id)
                 .then().log().all()
                 .extract();
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
 
         // Then
+
+        var response = RestAssured.given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/stations")
+                .then().log().all()
+                .extract();
+
         List<String> stationNameList = response.jsonPath().getList("name", String.class);
 
         assertThat(stationNameList).hasSize(1);
