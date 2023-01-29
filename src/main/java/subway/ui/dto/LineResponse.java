ca8af9869a87d7cc4bcb54f65ff07425e80e278d
michol.lee <lovethefeel@gmail.com>
2023-01-29 19:31:05 +0900

    refactor: 연관관계 설정 변경으로 인한 응답 데이터 처리 변경

--- src/main/java/subway/ui/dto/LineResponse.java
+++ src/main/java/subway/ui/dto/LineResponse.java
@@ -2,6 +2,7 @@ package subway.ui.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import subway.domain.Line;
+import subway.domain.Section;
 import subway.domain.Station;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LineResponse {
-    private long id;
+    private Long id;
     private String name;
     private String color;
     @JsonProperty("stations")
@@ -17,7 +18,7 @@ public class LineResponse {
 
     private LineResponse() {}
 
-    public LineResponse(final long id, final String name, final String color, final List<StationResponse> stationResponses) {
+    public LineResponse(final Long id, final String name, final String color, final List<StationResponse> stationResponses) {
         this.id = id;
         this.name = name;
         this.color = color;
@@ -25,16 +26,25 @@ public class LineResponse {
     }
 
     public static LineResponse createResponse(final Line saveLine) {
-        return new LineResponse(saveLine.getId(), saveLine.getName(), saveLine.getColor(), convertToStationResponse(saveLine.getUpStation(), saveLine.getDownStation()));
+        List<StationResponse> stationResponses = convertToStationResponse(saveLine.getSections().getSections());
+        return new LineResponse(saveLine.getId(), saveLine.getName(), saveLine.getColor(), stationResponses);
     }
 
-    private static List<StationResponse> convertToStationResponse(final Station upStation, final Station downStation) {
-        return Stream.of(upStation, downStation)
-                .map(StationResponse::of)
+    private static List<StationResponse> convertToStationResponse(final List<Section> sections) {
+        final List<Station> stations = convertToStation(sections);
+        return stations.stream()
+                .map(StationResponse::from)
                 .collect(Collectors.toList());
     }
 
-    public long getId() {
+    private static List<Station> convertToStation(final List<Section> sections) {
+        return sections.stream()
+                .flatMap(section -> Stream.of(section.getUpStation(), section.getDownStation()))
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public Long getId() {
         return id;
     }
 
