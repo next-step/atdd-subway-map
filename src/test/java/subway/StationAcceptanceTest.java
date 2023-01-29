ea9690181b896c1c2da9ab7762534ffad9355af6
wisotge <rnalstn0507@gmail.com>
2023-01-29 15:33:26 +0900

    feat: 지하철역 삭제 인수 테스트 작성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -49,12 +49,22 @@ public class StationAcceptanceTest {
         assertThat(stationNames).containsExactly("양재역", "교대역");
     }
 
-    /**
-     * Given 지하철역을 생성하고
-     * When 그 지하철역을 삭제하면
-     * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
-     */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 삭제한다.")
+    @Test
+    void deleteStation() {
+        // Given 지하철역을 생성하고
+        ExtractableResponse<Response> createResponse = 지하철역_생성("잠실역");
+
+        // When 그 지하철역을 삭제하면
+        Long id = createResponse.jsonPath().getLong("id");
+        ExtractableResponse<Response> deleteResponse = 지하철역_삭제(id);
+        assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        // Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다.
+        ExtractableResponse<Response> findResponse = 지하철역_목록_조회();
+        List<String> stationNames = findResponse.jsonPath().getList("name", String.class);
+        assertThat(stationNames).doesNotContain("잠실역");
+    }
 
 
     private ExtractableResponse<Response> 지하철역_목록_조회() {
@@ -73,6 +83,13 @@ public class StationAcceptanceTest {
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> 지하철역_삭제(Long id){
+        return given()
+                .when().delete("/stations/{id}", id)
                 .then().log().all()
                 .extract();
     }
