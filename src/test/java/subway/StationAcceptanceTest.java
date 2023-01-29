b7e0980a2a5c0894e7288b70e1c49fa2c3c4d130
Sangmin <sm9171@nate.com>
2023-01-29 21:37:10 +0900

    test: 지하철역 삭제 테스트 작성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -77,7 +77,23 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 삭제한다.")
+    @Test
+    void deleteStation() {
+        //given
+        Map<String, String> station = new HashMap<>();
+        station.put("name", "강남역");
+        ExtractableResponse<Response> request = createStationResponse(station);
+        Long id = request.jsonPath().getLong("id");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when().delete("/stations/" + id)
+                .then().log().all().extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
 
     private static ExtractableResponse<Response> createStationResponse(Map<String, String> param) {
         return RestAssured.given().log().all()
