f62daeeaf59948739e437cc74ba29d8169010aaf
dongho.geum <dongho.geum@myrealtrip.com>
2023-01-29 22:33:46 +0900

    feat: 지하철 노선 목록 조회

--- src/main/java/subway/web/LineController.java
+++ src/main/java/subway/web/LineController.java
@@ -2,12 +2,11 @@ package subway.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import subway.application.service.LineUseCase;
 import subway.web.request.LineCreateRequest;
 import subway.web.response.CreateLineResponse;
+import subway.web.response.LineResponse;
 
 @RestController
 public class LineController {
@@ -23,6 +22,12 @@ public class LineController {
         Long createdLineId = lineUseCase.createLine(stationRequest.toDomain());
         CreateLineResponse response = CreateLineResponse.from(lineUseCase.loadLine(createdLineId));
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/lines/{lineId}")
+    public ResponseEntity<LineResponse> lines(@PathVariable Long lineId) {
+        LineResponse line = LineResponse.from(lineUseCase.loadLine(lineId));
+        return ResponseEntity.status(HttpStatus.OK).body(line);
     }
 
 }
