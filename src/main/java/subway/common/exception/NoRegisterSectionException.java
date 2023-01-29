c8848137827582461e9dae907971bd8ba92a0583
michol.lee <lovethefeel@gmail.com>
2023-01-29 19:37:04 +0900

    feat: 공통 에러 객체, Custom Exception, Advice 생성

---
+++ src/main/java/subway/common/exception/NoRegisterSectionException.java
@@ -0,0 +1,8 @@
+package subway.common.exception;
+
+public class NoRegisterSectionException extends RuntimeException {
+
+    public NoRegisterSectionException(final String message) {
+        super(message);
+    }
+}
