9ab63c64004d156dcd51681c7877988b86cc25eb
NA YOONJI(Mangos) <yjna2316@naver.com>
2023-01-29 20:51:00 +0900

    [1단계] 지하철역 인수 테스트 작성 (#634)
    * chore: 사소한 수정
    
    * feat: [STEP1] 인수테스트 추가
    
    - 지하철 목록 조회
    - 지하철역 제거

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,5 +1,6 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
