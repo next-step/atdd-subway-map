29b488bfdb34b3fe1317d4038e964281e4005e90
michol.lee <lovethefeel@gmail.com>
2023-01-29 19:36:18 +0900

    feat: 노선 기능 컨트롤러 생성

---
+++ src/main/java/subway/ui/LineSectionController.java
@@ -0,0 +1,38 @@
+package subway.ui;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import subway.application.LineSectionService;
+import subway.ui.dto.LineSectionRequest;
+import subway.ui.dto.LineSectionResponse;
+
+@RestController
+@RequestMapping("/lines")
+public class LineSectionController {
+
+    private final LineSectionService lineSectionService;
+
+    public LineSectionController(final LineSectionService lineSectionService) {
+        this.lineSectionService = lineSectionService;
+    }
+
+    @PostMapping("/{id}/sections")
+    public ResponseEntity<Void> createLine(@PathVariable final Long id, @RequestBody final LineSectionRequest lineSectionRequest) {
+
+        lineSectionService.addSection(id, lineSectionRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}/sections")
+    public ResponseEntity<Void> createLine(@PathVariable final Long id, @RequestParam final Long stationId) {
+
+        lineSectionService.removeSection(id, stationId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}/sections")
+    public ResponseEntity<LineSectionResponse> findSectionByLine(@PathVariable final Long id) {
+
+        return ResponseEntity.ok().body(lineSectionService.findSectionByline(id));
+    }
+}
