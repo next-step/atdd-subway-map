a31829a68edc3d511911104a19602d6beb25bcd6
dongho.geum <dongho.geum@myrealtrip.com>
2023-01-29 23:58:15 +0900

    test: 지하철노선 조회 인수테스트 작성

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -65,6 +65,46 @@ class LineAcceptanceTest {
         assertThat(lineStations).containsExactlyInAnyOrder(new StationResponse(1L, "지하철역"), new StationResponse(2L, "새로운지하철역"));
     }
 
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("생성한 노선 목록을 조회 할 수 있다")
+    @Test
+    void loadStations() {
+        // Given
+        StationResponse 지하철역 = createStation("지하철역").as(StationResponse.class);
+        StationResponse 새로운지하철역 = createStation("새로운지하철역").as(StationResponse.class);
+        StationResponse 또다른지하철역 = createStation("또다른지하철역").as(StationResponse.class);
+
+        LineCreateRequest given_신분당선 = new LineCreateRequest("신분당선", "bg-red-600", 지하철역.getId(), 새로운지하철역.getId(), 10L);
+        ExtractableResponse<Response> given_신분당선_response = createLine(given_신분당선, 1L, 지하철역, 새로운지하철역);
+
+        LineCreateRequest given_분당선_ = new LineCreateRequest("분당선", "bg-green-600", 지하철역.getId(), 또다른지하철역.getId(), 10L);
+        ExtractableResponse<Response> given_분당선_response = createLine(given_분당선_, 2L, 지하철역, 또다른지하철역);
+
+        // when
+        ExtractableResponse<Response> actualResponse = RestAssured.given().spec(REQUEST_SPEC).log().all()
+            .when().get("/lines")
+            .then().log().all()
+            .extract();
+
+        // Then
+        assertThat(actualResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(actualResponse.jsonPath().getLong("[0].id")).isEqualTo(1L);
+        assertThat(actualResponse.jsonPath().getString("[0].name")).isEqualTo("신분당선");
+        assertThat(actualResponse.jsonPath().getString("[0].color")).isEqualTo("bg-red-600");
+        List<StationResponse> _0_indexStations = actualResponse.jsonPath().getList("[0].stations", StationResponse.class);
+        assertThat(_0_indexStations).containsExactlyInAnyOrder(지하철역, 새로운지하철역);
+
+        assertThat(actualResponse.jsonPath().getLong("[1].id")).isEqualTo(2L);
+        assertThat(actualResponse.jsonPath().getString("[1].name")).isEqualTo("분당선");
+        assertThat(actualResponse.jsonPath().getString("[1].color")).isEqualTo("bg-green-600");
+        List<StationResponse> _1_indexStations = actualResponse.jsonPath().getList("[1].stations", StationResponse.class);
+        assertThat(_1_indexStations).containsExactlyInAnyOrder(지하철역, 또다른지하철역);
+    }
+
     private static ExtractableResponse<Response> createLine(LineCreateRequest lineCreateRequest, Long lineId, StationResponse upStation, StationResponse downStation) {
 
         // when
