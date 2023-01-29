1469477dc6d88b54c74e6ec07129ecb9fc9b39c5
seovalue <seominjeong.dev@gmail.com>
2023-01-29 22:13:37 +0900

    feat: 지하철 노선 목록 조회 인수 테스트를 구현해요.

--- src/main/java/subway/common/Colors.java
+++ src/main/java/subway/common/Colors.java
@@ -2,4 +2,5 @@ package subway.common;
 
 public class Colors {
     public static final String RED = "bg-red-600";
+    public static final String BLUE = "bg-blue-600";
 }
--- src/test/java/subway/line/LineAcceptanceTest.java
+++ src/test/java/subway/line/LineAcceptanceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
 import subway.common.Colors;
 import subway.common.Endpoints;
@@ -16,6 +17,7 @@ import subway.utils.JsonBodyParam;
 import subway.utils.RestAssuredClient;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,7 +47,7 @@ public class LineAcceptanceTest {
         long 강남역_아이디 = 강남역_생성_응답.jsonPath().getLong("id");
         long 서울대입구역_아이디 = 서울대입구역_생성_응답.jsonPath().getLong("id");
 
-        RestAssuredClient.post(
+        var response = RestAssuredClient.post(
                 Endpoints.LINES,
                 new LineRequest(
                         "신분당선",
@@ -55,6 +57,7 @@ public class LineAcceptanceTest {
                         10L
                 )
         );
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다.
         List<LineResponse> lineResponses = RestAssuredClient.get(Endpoints.LINES).jsonPath().getList(".", LineResponse.class);
@@ -69,5 +72,53 @@ public class LineAcceptanceTest {
                         강남역_아이디,
                         서울대입구역_아이디
                 );
+    }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("여러 개의 지하철 노선을 조회한다.")
+    @Test
+    void findAllLines() {
+        ExtractableResponse<Response> 강남역_생성_응답 = RestAssuredClient.post(Endpoints.STATIONS, new JsonBodyParam("name", "강남역").toMap());
+        ExtractableResponse<Response> 서울대입구역_생성_응답 = RestAssuredClient.post(Endpoints.STATIONS, new JsonBodyParam("name", "서울대입구역").toMap());
+
+        // Given 지하철 노선을 생성하고
+        long 강남역_아이디 = 강남역_생성_응답.jsonPath().getLong("id");
+        long 서울대입구역_아이디 = 서울대입구역_생성_응답.jsonPath().getLong("id");
+
+        var 신분당선_생성 = RestAssuredClient.post(
+                Endpoints.LINES,
+                new LineRequest(
+                        "신분당선",
+                        Colors.RED,
+                        강남역_아이디,
+                        서울대입구역_아이디,
+                        10L
+                )
+        );
+        assertThat(신분당선_생성.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        var 이호선_생성 = RestAssuredClient.post(
+                Endpoints.LINES,
+                new LineRequest(
+                        "2호선",
+                        Colors.BLUE,
+                        강남역_아이디,
+                        서울대입구역_아이디,
+                        10L
+                )
+        );
+        assertThat(이호선_생성.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        // When 지하철 노선 목록을 조회하면
+        List<LineResponse> lineResponses = RestAssuredClient.get(Endpoints.LINES).jsonPath().getList(".", LineResponse.class);
+
+        // Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+        assertThat(lineResponses).hasSize(2);
+        var lineNames = lineResponses.stream().map(LineResponse::getName).collect(Collectors.toList());
+        assertThat(lineNames).containsExactly("신분당선", "2호선");
     }
 }
