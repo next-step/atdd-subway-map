ae4332484513105df59440e0a4146679fd847f9c
dhlee3994 <dhlee3994@gmail.com>
2023-01-30 12:19:37 +0900

    Test: 테스트에 사용할 변수를 BeforeEach에서 매번 초기화 하는 것이 아닌 static final 변수로 정의

--- src/test/java/subway/LineAcceptConstants.java
+++ src/test/java/subway/LineAcceptConstants.java
@@ -1,7 +1,5 @@
 package subway;
 
-import org.junit.jupiter.api.BeforeEach;
-
 import java.util.Map;
 
 public class LineAcceptConstants {
@@ -11,25 +9,14 @@ public class LineAcceptConstants {
     protected static final String LINE_DOWN_STATION_ID = "downStationId";
     protected static final String LINE_DISTANCE = "distance";
 
-    protected Map<String, Object> 이호선;
-    protected Map<String, Object> 신분당선;
-
-    @BeforeEach
-    void setUp() {
-        이호선 = Map.of(
-                LINE_NAME, "2호선",
-                LINE_COLOR, "bg-green-600",
-                LINE_UP_STATION_ID, 1,
-                LINE_DOWN_STATION_ID, 2,
-                LINE_DISTANCE, 24
-        );
-
-        신분당선 = Map.of(
-                LINE_NAME, "신분당선",
-                LINE_COLOR, "bg-red-600",
-                LINE_UP_STATION_ID, 3,
-                LINE_DOWN_STATION_ID, 4,
-                LINE_DISTANCE, 10
-        );
-    }
+    protected static final Map<String, Object> 이호선 = Map.of(LINE_NAME, "2호선",
+                                                              LINE_COLOR, "bg-green-600",
+                                                              LINE_UP_STATION_ID, 1,
+                                                              LINE_DOWN_STATION_ID, 2,
+                                                              LINE_DISTANCE, 24);
+    protected static final Map<String, Object> 신분당선 = Map.of(LINE_NAME, "신분당선",
+                                                                LINE_COLOR, "bg-red-600",
+                                                                LINE_UP_STATION_ID, 3,
+                                                                LINE_DOWN_STATION_ID, 4,
+                                                                LINE_DISTANCE, 10);
 }
