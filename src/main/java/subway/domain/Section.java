4da128d169f33ab49cb44088bc886d9cb6f8418d
hiblue02 <hiblue02@gmail.com>
2023-01-31 00:42:45 +0900

    feat(Domain): Section 추가

---
+++ src/main/java/subway/domain/Section.java
@@ -0,0 +1,35 @@
+package subway.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Section {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(targetEntity = Station.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "up_station_id")
+    private Station upStation;
+
+    @OneToOne(targetEntity = Station.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "down_station_id")
+    private Station downStation;
+
+    private Long distance;
+
+    protected Section() {
+    }
+
+    public Section(Station upStation, Station downStation, Long distance) {
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
+    public boolean isDownStation(Station station){
+        return this.downStation.equals(station);
+    }
+
+
+}
