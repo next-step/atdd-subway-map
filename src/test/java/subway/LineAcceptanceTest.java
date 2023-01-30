d929899ce18cd08c2efb45b37452be842bc50ebf
bactoria(황준오) <bactoria.dev@kakaocorp.com>
2023-01-31 00:31:57 +0900

    test:: [지하철노선 목록 조회하기] 인수테스트 추가

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -79,5 +78,49 @@ public class LineAcceptanceTest {
                 .extract();
 
         return response.body().jsonPath().getString("id");
+    }
+
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철노선 목록을 조회한다.")
+    @Test
+    void readLines() {
+        // given
+        String station1Id = createStation("강남역");
+        String station2Id = createStation("신논현역");
+        String station3Id = createStation("논현역");
+
+        createLine("신분당선", station1Id, station2Id);
+        createLine("신분당선", station2Id, station3Id);
+
+        // when
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .when().get("/lines")
+                        .then().log().all()
+                        .extract();
+
+        // then
+        assertThat(response.body().jsonPath().getList("id")).hasSize(2);
+    }
+
+    private void createLine(String lineName, String upStationId, String downStationId) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", lineName);
+        params.put("color", "bg-red-600");
+        params.put("upStationId", upStationId);
+        params.put("downStationId", downStationId);
+        params.put("distance", "10");
+
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
     }
 }
