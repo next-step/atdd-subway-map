ac643e3c01052b1400bf6b8e2ee885fec5b75f79
Jaepil Yu <jaepilyu1324@gmail.com>
2023-01-30 17:39:29 +0900

    test: 구간 생성/삭제 실패 케이스 작성

--- src/test/java/subway/SectionAcceptanceTest.java
+++ src/test/java/subway/SectionAcceptanceTest.java
@@ -33,7 +33,6 @@ public class SectionAcceptanceTest {
     @DisplayName("지하철 구간을 생성한다.")
     void createSection() {
         // when
-        client.createStation("지하철역3");
         client.createSection(lineId, new SectionRequest(3L, 2L, 10L));
 
         // then
@@ -44,10 +43,29 @@ public class SectionAcceptanceTest {
     }
 
     @Test
+    @DisplayName("새로운 구간의 상행역이 해당 노선에 등록되어 있는 하행 종점역이 아닐 경우 생성에 실패한다.")
+    void createSectionWhenNewUpStationIsNotDownStationOfLine() {
+        // when
+        client.createStation("지하철역4");
+        ExtractableResponse<Response> response = client.createSection(lineId, new SectionRequest(3L, 4L, 10L));
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    @Test
+    @DisplayName("새로운 구간의 하행역은 해당 노선에 등록된 역일 수 없다.")
+    void createSectionWhenNewDownStationExistsInLine() {
+        // when
+        ExtractableResponse<Response> response = client.createSection(lineId, new SectionRequest(2L, 3L, 10L));
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    @Test
     @DisplayName("특정 지하철 구간을 삭제한다.")
     void deleteSection() {
         // given
-        client.createStation("지하철역3");
         client.createSection(lineId, new SectionRequest(3L, 2L, 10L));
 
         // when
@@ -58,5 +76,25 @@ public class SectionAcceptanceTest {
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getList("stations.id", Long.class)).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("노선에 등록된 하행 종점역이 아닐 경우, 해당 노선을 삭제할 수 없다.")
+    void deleteSectionWhenGivenStationIsNotDownStationOfLine() {
+        // when
+        ExtractableResponse<Response> response = client.deleteSection(1L, 3L);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    @Test
+    @DisplayName("노선에 구간이 1개일 경우, 구간을 삭제할 수 없다.")
+    void deleteSectionWhenThereIsOnlyOneSectionInLine() {
+        // when
+        ExtractableResponse<Response> response = client.deleteSection(1L, 2L);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 }
