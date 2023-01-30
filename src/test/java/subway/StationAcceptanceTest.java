5990433c7d554b499a7a476688d8293c17ca175c
ChanYung Park <50781066+cyPark95@users.noreply.github.com>
2023-01-30 10:06:58 +0900

    🚀 1단계 - 지하철역 인수 테스트 작성 (#650)
    * test: 지하철역 목록 조회
    
    * refactor: 메소드 추출
    
     - RestAssured Api 요청 메소드
     - 지하철역 저장 메소드
    
    * test: 지하철역 제거
    
    * refactor: RestAssured Api 요청과 assert 메소드 추출

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -6,7 +6,6 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
@@ -14,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.*;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -27,26 +27,13 @@ public class StationAcceptanceTest {
     @Test
     void createStation() {
         // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "강남역");
-
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+        ExtractableResponse<Response> response = saveStation("강남역");
 
         // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertSuccessCreate(response);
 
         // then
-        List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
+        List<String> stationNames = assertSuccessShow(showStationsResponse());
         assertThat(stationNames).containsAnyOf("강남역");
     }
 
@@ -55,13 +42,82 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @Test
+    @DisplayName("지하철역 목록 조회")
+    void showStations() {
+        // given
+        saveStation("강남역");
+        saveStation("역삼역");
+
+        // when
+        List<String> stationNames = assertSuccessShow(showStationsResponse());
+
+        // then
+        assertThat(stationNames.size()).isEqualTo(2);
+        assertThat(stationNames).containsExactly("강남역", "역삼역");
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @Test
+    @DisplayName("지하철역 제거")
+    void deleteStation() {
+        // given
+        Long id = assertSuccessCreate(saveStation("강남역"));
 
+        // when
+        assertSuccessDelete(deleteStationResponse(id));
+
+        // then
+        List<String> stationNames = assertSuccessShow(showStationsResponse());
+        assertThat(stationNames.size()).isEqualTo(0);
+    }
+
+    private ExtractableResponse<Response> showStationsResponse() {
+        return RestAssured
+                .given().log().all().contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private static List<String> assertSuccessShow(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(OK.value());
+        return response.jsonPath().getList("name", String.class);
+    }
+
+    private ExtractableResponse<Response> saveStation(String stationName) {
+        Map<String, String> param = new HashMap<>();
+        param.put("name", stationName);
+
+        return createStationResponse(param);
+    }
+
+    private ExtractableResponse<Response> createStationResponse(Map<String, String> params) {
+        return RestAssured
+                .given().log().all().body(params).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private Long assertSuccessCreate(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(CREATED.value());
+        return response.body().jsonPath().getLong("id");
+    }
+
+    private ExtractableResponse<Response> deleteStationResponse(Long id) {
+        return RestAssured
+                .given().log().all().contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete("/stations/" + id)
+                .then().log().all()
+                .extract();
+    }
+
+    private void assertSuccessDelete(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(NO_CONTENT.value());
+    }
 }
