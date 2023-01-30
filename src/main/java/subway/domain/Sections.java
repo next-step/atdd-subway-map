b59f7446b288b003e215e81417b0e1d9cc5b631c
michol.lee <lovethefeel@gmail.com>
2023-01-30 21:01:32 +0900

    refactor: 요구사항 참고하여 아래 내용 수정 - 가장 마지막 구간의 종점역만 삭제 - 가장 최종 하행 종점역 뒤에만 구간을 추가

--- src/main/java/subway/domain/Sections.java
+++ src/main/java/subway/domain/Sections.java
@@ -30,7 +30,8 @@ public class Sections {
     }
 
     public void addSection(final Line line, final Section section) {
-        validateAddStation(section);
+        final Section findSection = findLastSectionByStationRefactor();
+        validateAddStation(findSection, section);
         section.addLine(line);
         this.sections.add(section);
     }
@@ -45,8 +46,8 @@ public class Sections {
         return sections;
     }
 
-    private void validateAddStation(final Section section) {
-        validateMatchDownStation(section.getUpStation());
+    private void validateAddStation(final Section findSection, final Section section) {
+        validateMatchDownStation(findSection, section.getUpStation());
         validateNoneMatchStation(section.getDownStation());
     }
 
@@ -57,8 +58,8 @@ public class Sections {
         }
     }
 
-    private void validateMatchDownStation(final Station upStation) {
-        if (canMatchDownStation(upStation)) {
+    private void validateMatchDownStation(final Section findSection, final Station upStation) {
+        if (canNotMatchDownStation(findSection, upStation)) {
             throw new NoRegisterStationException("노선에 등록된 하행종점역이 없습니다.");
         }
     }
@@ -74,16 +75,22 @@ public class Sections {
                 .anyMatch(section -> section.matchUpStation(station) || section.matchDownStation(station));
     }
 
-    private boolean canMatchDownStation(final Station station) {
-        return this.sections.stream()
-                .noneMatch(section -> section.matchDownStation(station));
+    private boolean canNotMatchDownStation(final Section findSection, final Station station) {
+        return !findSection.matchDownStation(station);
     }
 
     private Section findLastSectionByStation(final Station station) {
         return this.sections.stream()
                 .sorted(Comparator.comparing(Section::getId).reversed())
-                .filter(section -> section.getDownStation().equals(station))
                 .findFirst()
+                .filter(section -> section.getDownStation().equals(station))
                 .orElseThrow(() -> new NoRegisterStationException("해당 역으로 등록된 마지막 구간이 존재하지 않습니다."));
+    }
+
+    private Section findLastSectionByStationRefactor() {
+        return this.sections.stream()
+                .sorted(Comparator.comparing(Section::getId).reversed())
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("노선에 등록된 역이 존재하지 않습니다."));
     }
 }
