b92554402ef36cc97b5f9cd9268e9a3b211bcb56
wkwon <rnjsdntjr26@daum.net>
2023-01-30 16:17:40 +0900

    1주차 2단계: 인수테스트 뼈대 작성

---
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -0,0 +1,69 @@
+package subway;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@DisplayName("지하철노선 관련 기능")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LineAcceptanceTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeAll
+    private void setup(){
+        RestAssured.port = port;
+    }
+
+    @DisplayName("지하철노선을 생성한다.")
+    @Test
+    void createLineTest(){
+        // when
+
+        // then
+    }
+
+    @DisplayName("지하철노선 목록을 조회한다.")
+    @Test
+    void getLineListTest(){
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("지하철노선을 조회한다.")
+    @Test
+    void getLineTest(){
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("지하철노선을 수정한다.")
+    @Test
+    void updateLineTest(){
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("지하철노선을 삭제한다.")
+    @Test
+    void deleteLineTest() {
+        // given
+
+        // when
+
+        // then
+    }
+}
