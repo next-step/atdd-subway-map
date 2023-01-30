174180af75a9dcb348b4365229b1fa67cfaf5df8
michol.lee <lovethefeel@gmail.com>
2023-01-30 11:28:11 +0900

    refactor(test): 불필요한 주석 제거

--- src/test/java/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/subway/acceptance/LineSectionAcceptanceTest.java
@@ -32,11 +32,6 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         이호선 = 지하철노선_생성_요청("2호선", "bg-green-600", 강남역, 잠실역, 10);
     }
 
-    /**
-     * Given 지하철 노선을 생성하고
-     * When  지하철 노선 구간을 추가하면
-     * Then  지하철 노선 구간 목록 조회 시 생성한 구간을 찾을 수 있다
-     */
     @DisplayName("노선 구간을 추가 등록한다.")
     @Test
     void addSection() {
@@ -50,11 +45,6 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         지하철_노선_구간_목록_중_등록_구간_조회됨(지하철_노선_구간_목록_조회_응답, 2);
     }
 
-    /**
-     * Given 지하철 노선을 생성하고
-     * When  지하철 노선 구간의 등록되지 않은 상행종점역을 추가하여 구간 등록하면
-     * Then  지하철 노선 구간 추가는 실패한다.
-     */
     @DisplayName("노선 구간 추가 시 상행종점역은 노선에 등록되어 있지 않아서 구간 등록이 불가능하다.")
     @Test
     void error_addSection() {
@@ -64,11 +54,6 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         지하철_노선_구간_추가_실패됨(이호선_응답, HttpStatus.NOT_FOUND, "노선에 등록된 하행종점역이 없습니다.");
     }
 
-    /**
-     * Given 지하철 노선을 생성하고
-     * When  지하철 노선 구간의 이미 등록된 하행종점역을 추가하여 구간 등록하면
-     * Then  지하철 노선 구간 추가는 실패한다.
-     */
     @DisplayName("노선 구간 추가 시 하행종점역은 노선에 등록되어 있어서 구간 등록이 불가능하다.")
     @Test
     void error_addSection_2() {
@@ -78,11 +63,6 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         지하철_노선_구간_추가_실패됨(이호선_응답, HttpStatus.NOT_FOUND, "노선에 등록된 하행종점역이 없습니다.");
     }
 
-    /**
-     * Given 지하철 노선을 생성하고 1개의 구간을 추가 등록하고
-     * When  등록된 지하철 노선 구간 1개를 삭제하면
-     * Then  해당 노선 구간 정보는 삭제된다
-     */
     @DisplayName("노선 구간을 제거한다.")
     @Test
     void removeSection() {
@@ -100,11 +80,6 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         지하철_노선_구간_목록_중_등록_구간_조회됨(지하철_노선_구간_목록_조회_응답, 1);
     }
 
-    /**
-     * Given 지하철 노선을 생성하고
-     * When  등록된 지하철 노선 구간 1개를 삭제하면
-     * Then  지하철 노선 구간 추가는 실패한다.
-     */
     @DisplayName("노선 구간 제거 시 구간이 하나인 경우 삭제 불가로 구간 삭제가 불가능하다.")
     @Test
     void error_removeSection() {
