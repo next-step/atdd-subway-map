e849c68b6684415f20d1d95fa140552474a27914
bada.kang <kangbada0728@gmail.com>
2023-01-30 23:30:16 +0900

    refactor: 코드 리포매팅

--- src/main/java/subway/stationline/dto/StationLineInterface.java
+++ src/main/java/subway/stationline/dto/StationLineInterface.java
@@ -2,8 +2,12 @@ package subway.stationline.dto;
 
 public interface StationLineInterface {
     String name();
+
     String color();
+
     Long upStationId();
+
     Long downStationId();
+
     Long distance();
 }
