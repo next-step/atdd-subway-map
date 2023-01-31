7c9827b3431c7ee1f42ec3d62929e12ed724c32c
최현웅 <76509935+choi1204@users.noreply.github.com>
2023-01-31 15:23:45 +0900

    test : 2개의 지하철 노선을 생성하고 목록 조회 테스트

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -3,6 +3,7 @@ package subway;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,22 @@ public class LineAcceptanceTest {
     @Autowired
     private LineRepository lineRepository;
 
+    StationAcceptanceTest stationAcceptanceTest = new StationAcceptanceTest();
+
+    Long firstStationId;
+    Long secondStationId;
+    Long thirdStationId;
+
+    LineRequest lineRequest;
+
+    @BeforeEach
+    public void setup() {
+        firstStationId = stationAcceptanceTest.createStation("지하철역1");
+        secondStationId = stationAcceptanceTest.createStation("지하철역2");
+        thirdStationId = stationAcceptanceTest.createStation("지하철역3");
+        lineRequest = new LineRequest("신분당선", "bg-red-600", firstStationId, secondStationId, 10L);
+    }
+
     /**
      * When 지하철 노선을 생성하면
      * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
@@ -31,12 +48,8 @@ public class LineAcceptanceTest {
     @DisplayName("지하철 노선을 등록하고 생성한 노선을 찾을 수 있다..")
     @Test
     public void createLineTest() {
-        StationAcceptanceTest stationAcceptanceTest = new StationAcceptanceTest();
-        Long upStationId = stationAcceptanceTest.createStation("지하철역1");
-        Long downStationId = stationAcceptanceTest.createStation("지하철역2");
 
         // When
-        LineRequest lineRequest = new LineRequest("신분당선", "bg-red-600", upStationId, downStationId, 10L);
         createLine(lineRequest);
 
         // Then
@@ -54,12 +67,31 @@ public class LineAcceptanceTest {
      * When 지하철 노선 목록을 조회하면
      * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
      */
+    @DisplayName("2개의 지하철 노선을 생성하고 목록을 조회하면 지하철 목록 조회시 2개의 노선을 조회할 수 있다.")
+    @Test
+    void getLineList() {
+
+        // When
+        LineRequest secondLineRequest = new LineRequest("분당선", "bg-red-600", firstStationId, thirdStationId, 10L);
+        createLine(lineRequest);
+        createLine(secondLineRequest);
+
+        // Then
+        var response = getLineResponseList();
+        List<String> name = response.jsonPath().getList("name", String.class);
+
+        assertThat(name).hasSize(2);
+        assertThat(name).containsAnyOf("신분당선");
+        assertThat(name).containsAnyOf("분당선");
+    }
 
     /**
      * Given 지하철 노선을 생성하고
      * When 생성한 지하철 노선을 조회하면
      * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
      */
+
+
 
     /**
      * Given 지하철 노선을 생성하고
@@ -88,7 +120,7 @@ public class LineAcceptanceTest {
     }
 
     @DisplayName("지하철 노선을 등록할 수 있다.")
-    private void createLine(LineRequest lineRequest) {
+    private Long createLine(LineRequest lineRequest) {
         var response = RestAssured.given().log().all()
                 .body(lineRequest)
                 .accept(MediaType.APPLICATION_JSON_VALUE)
@@ -99,5 +131,7 @@ public class LineAcceptanceTest {
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(response.contentType()).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
+
+        return response.jsonPath().getLong("id");
     }
 }
