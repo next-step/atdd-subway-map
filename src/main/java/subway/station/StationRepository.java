bdf673f77974710732933fe6013d4de556a9cab5
Sanghou <tkdgh97531@gmail.com>
2023-01-31 09:55:19 +0900

    Remove unused method

--- src/main/java/subway/station/StationRepository.java
+++ src/main/java/subway/station/StationRepository.java
@@ -1,9 +1,6 @@
 package subway.station;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
-
-  Station findByName(@Param("name") String name);
 }
