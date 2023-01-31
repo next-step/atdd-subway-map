22c270acf5e8ad04dfb3d4641d5de2f416ddc897
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-31 23:33:08 +0900

    refactor: 지하철역 엔티티 보강
    - 기본생성자의 접근제어자 수정
    - equals() and hashCode() 재정의

--- src/main/java/subway/station/Station.java
+++ src/main/java/subway/station/Station.java
@@ -1,16 +1,28 @@
 package subway.station;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
+@Table(name = "station")
 public class Station {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "station_id")
     private Long id;
+
     @Column(length = 20, nullable = false)
     private String name;
 
-    public Station() {
+    /**
+     * JPA를 위한 기본 생성자
+     */
+    protected Station() {
     }
 
     public Station(String name) {
@@ -23,5 +35,18 @@ public class Station {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Station station = (Station) o;
+        return id.equals(station.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
