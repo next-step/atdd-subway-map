12042dee3ee421c477598cf676df6cc36ea66fec
heoseungyeon <dia0312@naver.com>
2023-01-31 20:17:59 +0900

    test: 지하철역 제거 인수 테스트 메서드 생성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -85,13 +85,37 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @Test
+    void deleteStation() {
+        // Given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "강남역");
 
-    void createStation(Map<String, String> params) {
+        Long stationId = createStation(params).jsonPath().getLong("id");
+        System.out.println(stationId);
+
+        // When
         RestAssured.given().log().all()
+            .when().delete("/stations/{id}", stationId)
+            .then().log().all();
+
+        // Then
+        ExtractableResponse<Response> retrieveResponse =
+            RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract();
+
+        List<String> result = retrieveResponse.jsonPath().getList("$");
+        assertThat(result).isEmpty();
+    }
+
+    ExtractableResponse<Response> createStation(Map<String, String> params) {
+        return RestAssured.given().log().all()
             .body(params)
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .when().post("/stations")
-            .then().log().all();
+            .then().log().all()
+            .extract();
     }
 }
