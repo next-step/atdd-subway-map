355884c6905811de47bc5242d76759116998e78f
Yunsik-Choi <choiys0311@gmail.com>
2023-01-31 14:44:47 +0900

    ⛑️ test : 지하철 노선 생성, 목록 조회 인수 테스트 작성

---
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -0,0 +1,135 @@
+package subway;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestConstructor.AutowireMode;
+import subway.domain.StationRepository;
+
+@TestConstructor(autowireMode = AutowireMode.ALL)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class LineAcceptanceTest {
+
+    @LocalServerPort
+    private int port;
+    private Long upStationId;
+    private Long downStationId;
+
+    private final StationRepository stationRepository;
+    private final LineRepository lineRepository;
+
+    public LineAcceptanceTest(final StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        stationRepository.truncateTableStation();
+        lineRepository.truncateTableLine();
+        this.upStationId = createStationResponseBy("강남역").jsonPath().getLong("id");
+        this.downStationId = createStationResponseBy("양재역").jsonPath().getLong("id");
+    }
+
+    private ExtractableResponse<Response> createStationResponseBy(final String stationName) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", stationName);
+
+        return RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/stations")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
+    }
+
+    @DisplayName("지하철 노선 생성한다.")
+    @Test
+    void createLine() {
+        // when
+        String name = "신분당선";
+        String color = "bg-red-600";
+        int distance = 10;
+
+        createLine(name, color, upStationId, downStationId, distance);
+
+        // then
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .when()
+                .get("/lines")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        Assertions.assertAll(
+                () -> assertThat(jsonPath.getList("name")).contains(name),
+                () -> assertThat(jsonPath.getList("color")).contains(color),
+                () -> assertThat(jsonPath.getList("stations.upStationId")).contains(upStationId),
+                () -> assertThat(jsonPath.getList("stations.downStationId")).contains(downStationId),
+                () -> assertThat(jsonPath.getList("distance")).contains(distance)
+        );
+    }
+
+    @DisplayName("지하철 노선 목록을 조회한다.")
+    @Test
+    void showLines() {
+        // given
+        createLine("신분당선", "bg-red-600", upStationId, downStationId, 10);
+        createLine("2호선", "bg-green-600", upStationId, downStationId, 20);
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .when()
+                .get("/lines")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+
+        // then
+        assertThat(response.jsonPath().getList("id")).hasSize(2);
+    }
+
+    private static void createLine(final String name, final String color, final Long upStationId,
+                                   final Long downStationId, final int distance) {
+        Map body = Map.of(
+                "name", name,
+                "color", color,
+                "upStationId", upStationId,
+                "downStationId", downStationId,
+                "distance", distance
+        );
+
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(body)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
+    }
+}
