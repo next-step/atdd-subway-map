d238c490a0250f55decd86df220ff6987ada52d6
hungrytech <xorals9448@gmail.com>
2023-01-31 22:22:09 +0900

    refactor: 예외사항 로그 레벨을 info로 남길 수 있도록 변경

--- src/main/java/subway/presentation/GlobalApiExceptionHandler.java
+++ src/main/java/subway/presentation/GlobalApiExceptionHandler.java
@@ -15,7 +15,7 @@ public class GlobalApiExceptionHandler {
 	@ExceptionHandler(SubwayException.class)
 	public ResponseEntity<ApiErrorResponse> handleSubwayException(SubwayException exception) {
 		ErrorCode errorCode = exception.getErrorCode();
-		log.error("message: {} code: {}", exception.getMessage(), errorCode.getCode());
+		log.info("message: {} code: {}", exception.getMessage(), errorCode.getCode());
 		return ResponseEntity.status(errorCode.getStatusCode())
 			.body(ApiErrorResponse.of(errorCode));
 	}
