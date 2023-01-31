b5a025e6dc97fb287cc382de8be0fdc005188122
JohnPrk <uo3641493@gmail.com>
2023-01-31 16:13:45 +0900

    test : 지하철 노선 생성 인수 테스트 작성

---
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -0,0 +1,49 @@
+package subway;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LineAcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    void createLine() {
+        //when
+        String lineName = "1호선";
+        String lineColor = "파란색";
+
+        Map<String, String> params = new HashMap<>();
+        params.put("name", lineName);
+        params.put("color", lineColor);
+
+        //then
+        ExtractableResponse<Response> saveResponse = RestAssured
+                .given()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+
+        Assertions.assertThat(saveResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+}
