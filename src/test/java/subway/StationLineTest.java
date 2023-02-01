492ce672160ce350ab481730d2afba6c6fecf19a
김영중 <idfever@naver.com>
2023-02-01 14:38:14 +0900

    지하철라인 인수테스트 작성

--- src/test/java/subway/StationLineTest.java
+++ src/test/java/subway/StationLineTest.java
@@ -1,2 +1,87 @@
-package subway;public class StationLineTest {
+package subway;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("지하철노선 관련 기능")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class StationLineTest {
+    /**
+     * When 지하철노선을 생성하면
+     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+     */
+    @DisplayName("지하철노선 생성")
+    @Test
+    void createStationLine() {
+        //When 지하철노선을 생성하면
+
+        //Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+
+    }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철노선 목록 조회")
+    @Test
+    void createStationLineAndGetStationLine() {
+        // Given 2개의 지하철 노선을 생성하고
+
+        // When 지하철 노선 목록을 조회하면
+
+        // Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 조회하면
+     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+     */
+    @DisplayName("지하철노선 조회")
+    @Test
+    void getStationLine() {
+        // Given 지하철 노선을 생성하고
+
+        // When 생성한 지하철 노선을 조회하면
+
+        // Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 수정하면
+     * Then 해당 지하철 노선 정보는 수정된다
+     */
+    @DisplayName("지하철노선 수정")
+    @Test
+    void updateStationLine() {
+        // Given 지하철 노선을 생성하고
+
+        // When 생성한 지하철 노선을 수정하면
+
+        // Then 해당 지하철 노선 정보는 수정된다
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 삭제하면
+     * Then 해당 지하철 노선 정보는 삭제된다
+     */
+    @DisplayName("지하철노선 삭제")
+    @Test
+    void deleteStationLine() {
+        // Given 지하철 노선을 생성하고
+
+        // When 생성한 지하철 노선을 삭제하면
+
+        // Then 해당 지하철 노선 정보는 삭제된다
+        
+    }
 }
