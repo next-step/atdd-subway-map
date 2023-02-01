5d24250afc129c5cdc411acb16d0c90c5e562f98
slogan-choi <84657207+slogan-choi@users.noreply.github.com>
2023-02-01 17:01:04 +0900

    feat: 1단계 - 지하철역 인수 테스트 작성 (#678)
    * feat: [1단계 - 지하철역 인수 테스트 작성] 지하철역 목록 조회 인수 테스트 메서드 생성
    
    * feat: [1단계 - 지하철역 인수 테스트 작성] 지하철역 제거 인수 테스트 메서드 생성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -56,6 +56,37 @@ public class StationAcceptanceTest {
      * Then 2개의 지하철역을 응답 받는다
      */
     // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("2개의 지하철역을 생성한다.")
+    @Test
+    void creationStations() {
+        // Given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "강남역");
+
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all();
+
+        params.put("name", "안양역");
+
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all();
+
+        // then
+        List<String> stationNames =
+                RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract().jsonPath().getList("name", String.class);
+
+        assertThat(stationNames).containsAnyOf("강남역");
+        assertThat(stationNames).containsAnyOf("안양역");
+    }
 
     /**
      * Given 지하철역을 생성하고
@@ -63,5 +94,43 @@ public class StationAcceptanceTest {
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
     // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 삭제한다.")
+    @Test
+    void deleteStation() {
+        // Given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "역");
+
+        ExtractableResponse<Response> CreateResponse =
+            RestAssured.given().log().all()
+                    .body(params)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().post("/stations")
+                    .then().log().all()
+                    .extract();
+
+        assertThat(CreateResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        String location = CreateResponse.header("Location");
+
+        // when
+        ExtractableResponse<Response> DeleteResponse =
+            RestAssured.given().log().all()
+                    .body(params)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().delete(location)
+                    .then().log().all()
+                    .extract();
+
+        assertThat(DeleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        // then
+        List<String> stationNames =
+                RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract().jsonPath().getList("name", String.class);
+
+        assertThat(stationNames).doesNotContain("역");
+    }
 
 }
