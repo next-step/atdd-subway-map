d35b58529866db8f60e1978f835dcbcc4be2a76a
Yunsik-Choi <choiys0311@gmail.com>
2023-02-01 12:12:58 +0900

    ⛑️ test : 지하철 노선 생성, 목록 조회 테스트 작성

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -20,10 +20,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestConstructor.AutowireMode;
+import subway.domain.LineRepository;
 import subway.domain.StationRepository;
 
+@DisplayName("지하철 노선 관련 기능")
 @TestConstructor(autowireMode = AutowireMode.ALL)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class LineAcceptanceTest {
 
     @LocalServerPort
@@ -34,15 +36,16 @@ public class LineAcceptanceTest {
     private final StationRepository stationRepository;
     private final LineRepository lineRepository;
 
-    public LineAcceptanceTest(final StationRepository stationRepository) {
+    public LineAcceptanceTest(final StationRepository stationRepository, final LineRepository lineRepository) {
         this.stationRepository = stationRepository;
+        this.lineRepository = lineRepository;
     }
 
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
         stationRepository.truncateTableStation();
-        lineRepository.truncateTableLine();
+        lineRepository.deleteAllAndRestartId();
         this.upStationId = createStationResponseBy("강남역").jsonPath().getLong("id");
         this.downStationId = createStationResponseBy("양재역").jsonPath().getLong("id");
     }
@@ -73,21 +76,16 @@ public class LineAcceptanceTest {
         createLine(name, color, upStationId, downStationId, distance);
 
         // then
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .when()
-                .get("/lines")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract();
+        ExtractableResponse<Response> response = showLinesResponse();
 
         JsonPath jsonPath = response.jsonPath();
 
         Assertions.assertAll(
+                () -> assertThat(jsonPath.getLong("id[0]")).isEqualTo(1),
                 () -> assertThat(jsonPath.getList("name")).contains(name),
                 () -> assertThat(jsonPath.getList("color")).contains(color),
-                () -> assertThat(jsonPath.getList("stations.upStationId")).contains(upStationId),
-                () -> assertThat(jsonPath.getList("stations.downStationId")).contains(downStationId),
+                () -> assertThat(jsonPath.getList("stations.id[0]")).contains(upStationId.intValue(),
+                        downStationId.intValue()),
                 () -> assertThat(jsonPath.getList("distance")).contains(distance)
         );
     }
@@ -100,20 +98,29 @@ public class LineAcceptanceTest {
         createLine("2호선", "bg-green-600", upStationId, downStationId, 20);
 
         // when
-        ExtractableResponse<Response> response = RestAssured
+        ExtractableResponse<Response> response = showLinesResponse();
+
+        // then
+        assertThat(response.jsonPath().getList("id")).hasSize(2).contains(1, 2);
+    }
+
+    private static ExtractableResponse<Response> showLinesResponse() {
+        return RestAssured
                 .given().log().all()
                 .when()
                 .get("/lines")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value())
                 .extract();
-
-        // then
-        assertThat(response.jsonPath().getList("id")).hasSize(2);
     }
 
-    private static void createLine(final String name, final String color, final Long upStationId,
-                                   final Long downStationId, final int distance) {
+    private static void createLine(
+            final String name,
+            final String color,
+            final Long upStationId,
+            final Long downStationId,
+            final int distance
+    ) {
         Map body = Map.of(
                 "name", name,
                 "color", color,
