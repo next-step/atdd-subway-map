d1b771d6992406671006b3b2c243630bcb96b2a0
Yunsik-Choi <choiys0311@gmail.com>
2023-02-01 22:34:35 +0900

    ♻️ refactor : 일급콜렉션 fetch join 적용

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -17,6 +17,6 @@ public interface LineRepository extends JpaRepository<Line, Long> {
     void deleteAllAndRestartId();
 
     @Override
-    @Query("select l from Line l join fetch l.stations")
+    @Query("select l from Line l join fetch l.stations.stations")
     List<Line> findAll();
 }
