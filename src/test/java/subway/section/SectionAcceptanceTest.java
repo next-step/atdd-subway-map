84f5a8767dc5f8582a63e24432e2f8a25cb85641
choongh2e <choonghee.lee@hotmail.com>
2023-02-02 08:57:47 +0900

    test(section): 구간 삭제 기능 - 실패 케이스

--- src/test/java/subway/section/SectionAcceptanceTest.java
+++ src/test/java/subway/section/SectionAcceptanceTest.java
@@ -59,7 +59,7 @@ public class SectionAcceptanceTest {
 
     /**
      * Given 지하철 노선을 생성하고
-     * When 지하철 구간을 등록하면
+     * When 등록 불가능한 지하철 구간을 등록하면
      * Then 지하철 노선 조회시 기존 노선과 동일하다
      */
     @DisplayName("구간 등록 기능 - 실패 케이스")
@@ -99,8 +99,58 @@ public class SectionAcceptanceTest {
 
         // when
         ExtractableResponse<Response> deleteSectionResponse = requestDeleteSection(lineOne.getId(), stationB.getId());
-
         assertThat(deleteSectionResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        // then
+        ExtractableResponse<Response> showLineResponse = LineApiClient.requestShowLine(lineOne.getId());
+        LineResponse updatedLineOne = showLineResponse.body().as(LineResponse.class);
+
+        assertThat(showLineResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(updatedLineOne.getStations()).containsExactly(stationA, stationC);
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * Given 지하철 구간 하나를 추가하고
+     * When 하행 종점역이 아닐때 지하철 구간을 삭제하면
+     * Then 지하철 노선 조회시 구간이 그대로 등록되어 있다.
+     */
+    @DisplayName("구간 삭제 기능 - 실패 케이스")
+    @Test
+    void deleteSectionFail() {
+        // given
+        LineResponse lineOne = LineApiClient.requestCreateLine("1호선", "#0052A4", stationA.getId(), stationC.getId(), 7)
+                .body().as(LineResponse.class);
+
+        requestAppendSection(lineOne.getId(), stationC.getId(), stationB.getId(), 3);
+
+        // when
+        ExtractableResponse<Response> deleteSectionResponse = requestDeleteSection(lineOne.getId(), stationA.getId());
+        assertThat(deleteSectionResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+
+        // then
+        ExtractableResponse<Response> showLineResponse = LineApiClient.requestShowLine(lineOne.getId());
+        LineResponse updatedLineOne = showLineResponse.body().as(LineResponse.class);
+
+        assertThat(showLineResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(updatedLineOne.getStations()).containsExactly(stationA, stationC, stationB);
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 지하철 구간을 삭제하면 구간이 한개 이므로
+     * Then 지하철 노선 조회시 처음 생성한 노선 그대로 조회된다.
+     */
+    @DisplayName("구간 삭제 기능 - 실패 케이스2")
+    @Test
+    void deleteSectionFail2() {
+        // given
+        LineResponse lineOne = LineApiClient.requestCreateLine("1호선", "#0052A4", stationA.getId(), stationC.getId(), 7)
+                .body().as(LineResponse.class);
+
+        // when
+        ExtractableResponse<Response> deleteSectionResponse = requestDeleteSection(lineOne.getId(), stationC.getId());
+        assertThat(deleteSectionResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
 
         // then
         ExtractableResponse<Response> showLineResponse = LineApiClient.requestShowLine(lineOne.getId());
