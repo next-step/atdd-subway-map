c8fa59072da4cf5ad4fbbfe4575a1ca87ddb6594
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-02-01 22:44:35 +0900

    style: 상수명을 대문자로 변경

--- src/main/java/subway/line/LineNotFoundException.java
+++ src/main/java/subway/line/LineNotFoundException.java
@@ -1,9 +1,9 @@
 package subway.line;
 
 public class LineNotFoundException extends RuntimeException {
-    private static final String message = "지하철노선을 찾을 수 없습니다.";
+    private static final String MESSAGE = "지하철노선을 찾을 수 없습니다.";
 
     public LineNotFoundException() {
-        super(message);
+        super(MESSAGE);
     }
 }
