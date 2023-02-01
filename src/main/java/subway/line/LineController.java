06b9afc94deed83094099c41914535bd1b2c64d5
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-02-02 02:02:17 +0900

    style: 지역변수 이름 변경

--- src/main/java/subway/line/LineController.java
+++ src/main/java/subway/line/LineController.java
@@ -24,30 +24,36 @@ public class LineController {
 
     @PostMapping
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
-        LineResponse line = lineService.saveLine(lineRequest);
-        return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+        LineResponse lineResponse = lineService.saveLine(lineRequest);
+
+        return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId())).body(lineResponse);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<LineResponse> showLine(@PathVariable(name = "id") long lineId) {
-        LineResponse lineById = lineService.findLineById(lineId);
-        return ResponseEntity.ok().body(lineById);
+        LineResponse lineResponse = lineService.findLineById(lineId);
+
+        return ResponseEntity.ok().body(lineResponse);
     }
 
     @GetMapping
     public ResponseEntity<List<LineResponse>> showLines() {
-        return ResponseEntity.ok().body(lineService.findAllLines());
+        List<LineResponse> lineResponses = lineService.findAllLines();
+
+        return ResponseEntity.ok().body(lineResponses);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateLine(@PathVariable(name = "id") long lineId, @RequestBody LineRequest lineRequest) {
         lineService.updateLine(lineId, lineRequest);
+
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteLine(@PathVariable(name = "id") long lineId) {
         lineService.deleteLineById(lineId);
+
         return ResponseEntity.noContent().build();
     }
 }
