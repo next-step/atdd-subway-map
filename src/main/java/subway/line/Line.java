bbb5810c4094b23827bea011becba4981b35b66b
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-02-02 02:23:56 +0900

    style: 불필요한 주석 제거

--- src/main/java/subway/line/Line.java
+++ src/main/java/subway/line/Line.java
@@ -9,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import java.util.Objects;
 
 import static javax.persistence.CascadeType.PERSIST;
