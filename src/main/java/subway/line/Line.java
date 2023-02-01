08f3ef24c8bd3c4be84f6c33bc4b9811ebabd8b9
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-02-01 22:48:55 +0900

    style: 자동생성된 코드에 코드블럭 감싸기

--- src/main/java/subway/line/Line.java
+++ src/main/java/subway/line/Line.java
@@ -94,8 +94,12 @@ public class Line {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Line line = (Line) o;
         return id.equals(line.id);
     }
--- src/main/java/subway/station/Station.java
+++ src/main/java/subway/station/Station.java
@@ -39,8 +39,12 @@ public class Station {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Station station = (Station) o;
         return id.equals(station.id);
     }
