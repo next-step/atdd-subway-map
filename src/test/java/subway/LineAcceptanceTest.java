c23b399b778fe7efa4865072156638c13e92f4d3
heoseungyeon <dia0312@naver.com>
2023-02-02 17:18:30 +0900

    test: 지하철 노선 인수 목록 조회 테스트 작성

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -48,4 +48,51 @@ class LineAcceptanceTest {
         assertThat(lineNames).containsAnyOf("신분당선");
     }
 
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철 노선 목록을 조회한다")
+    @Test
+    void 지하철_노선을_2개_생성하고_목록을_조회하면_2개의_노선을_찾을_수_있다() {
+        // Given
+        Map<String, Object> firstParams = new HashMap<>();
+        firstParams.put("name", "신분당선");
+        firstParams.put("color", "bg-red-600");
+        firstParams.put("upStationId", 1);
+        firstParams.put("downStationId", 2);
+        firstParams.put("distance", 10);
+
+        Map<String, Object> secondParams = new HashMap<>();
+        secondParams.put("name", "경의중앙선");
+        secondParams.put("color", "bg-teal-300");
+        secondParams.put("upStationId", 3);
+        secondParams.put("downStationId", 4);
+        secondParams.put("distance", 5);
+
+        RestAssured.given().log().all()
+            .body(firstParams)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/lines")
+            .then().log().all();
+
+        RestAssured.given().log().all()
+            .body(secondParams)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/lines")
+            .then().log().all();
+
+        // When
+        ExtractableResponse<Response> response =
+            RestAssured.given().log().all()
+                .when().get("/lines")
+                .then().log().all()
+                .extract();
+
+        // Then
+        List<String> lines = response.jsonPath()
+            .getList("$");
+        assertThat(lines).hasSize(2);
+    }
 }
