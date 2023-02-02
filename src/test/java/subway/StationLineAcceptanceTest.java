fb771e734523a5ef33edb34567c12015d0a6368c
wjdrbd96 <wjdrbs966@naver.com>
2023-02-02 19:19:54 +0900

    feat: 리뷰 반영

--- src/test/java/subway/StationLineAcceptanceTest.java
+++ src/test/java/subway/StationLineAcceptanceTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import subway.fixture.StationLineTestFixtures;
 import subway.line.StationLine;
 
 import java.util.HashMap;
