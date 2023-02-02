8f3fe8827dadc1703e9e850dcf27d9e9f8d5e184
choongh2e <choonghee.lee@hotmail.com>
2023-02-02 09:03:55 +0900

    refactor(import): 필요없는 import 제거

--- src/main/java/subway/common/CommonExceptionHandler.java
+++ src/main/java/subway/common/CommonExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class CommonExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
--- src/test/java/subway/section/SectionAcceptanceTest.java
+++ src/test/java/subway/section/SectionAcceptanceTest.java
@@ -3,7 +3,6 @@ package subway.section;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
