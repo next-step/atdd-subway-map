1855c161a7b6f5869601dd6e9ed2a7f7302033a8
giibeom <kpmyung@gmail.com>
2023-02-03 01:55:01 +0900

    refactor: RestAssured의 기본 헤더 값을 세팅하는 메서드를 생성합니다.

--- src/test/java/subway/acceptance/AcceptanceTest.java
+++ src/test/java/subway/acceptance/AcceptanceTest.java
@@ -1,22 +1,18 @@
 package subway.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import subway.common.util.DatabaseCleaner;
 
 import java.util.Map;
 
-import static io.restassured.RestAssured.UNDEFINED_PORT;
 import static io.restassured.RestAssured.given;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+import static subway.common.util.RestAssuredBuilder.기본_헤더값_설정;
 
 @SpringBootTest(webEnvironment = DEFINED_PORT)
 @ActiveProfiles("acceptanceTest")
@@ -28,19 +24,8 @@ class AcceptanceTest {
     public static final String LINE_BASE_URL = "/lines";
     public static final String LINE_COMMAND_URL = LINE_BASE_URL + "/{lineId}";
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
     private DatabaseCleaner databaseCleaner;
-
-    @BeforeEach
-    void setUp() {
-        if (RestAssured.port == UNDEFINED_PORT) {
-            RestAssured.port = port;
-            databaseCleaner.afterPropertiesSet();
-        }
-    }
 
     @AfterEach
     void cleanUp() {
@@ -48,62 +33,59 @@ class AcceptanceTest {
     }
 
     public static ExtractableResponse<Response> 지하철역_생성_요청(Map<String, String> requestBody) {
-        return given().log().all()
+        return given(기본_헤더값_설정()).log().all()
                 .body(requestBody)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post(STATION_BASE_URL)
                 .then().log().all()
                 .extract();
     }
 
     public static ExtractableResponse<Response> 지하철역_목록_조회_요청() {
-        return given().log().all()
+        return given(기본_헤더값_설정()).log().all()
                 .when().get(STATION_BASE_URL)
                 .then().log().all()
                 .extract();
     }
 
     public static void 지하철역_삭제_요청(String stationId) {
-        given().log().all()
+        given(기본_헤더값_설정()).log().all()
                 .when().delete(STATION_COMMAND_URL, stationId)
                 .then().log().all()
                 .extract();
     }
 
     public static ExtractableResponse<Response> 지하철_노선_생성_요청(Map<String, String> responseBody) {
-        return given().log().all()
+        return given(기본_헤더값_설정()).log().all()
                 .body(responseBody)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post(LINE_BASE_URL)
                 .then().log().all()
                 .extract();
     }
 
     public static ExtractableResponse<Response> 지하철_노선_목록_조회_요청() {
-        return given().log().all()
+        return given(기본_헤더값_설정()).log().all()
                 .when().get(LINE_BASE_URL)
                 .then().log().all()
                 .extract();
     }
 
     public static ExtractableResponse<Response> 지하철_노선_단건_조회_요청(String lineId) {
-        return given().log().all()
+        return given(기본_헤더값_설정()).log().all()
                 .when().get(LINE_COMMAND_URL, lineId)
                 .then().log().all()
                 .extract();
     }
 
     public static ExtractableResponse<Response> 지하철_노선_수정_요청(String lineId, Map<String, String> requestBody) {
-        return given().log().all()
+        return given(기본_헤더값_설정()).log().all()
                 .body(requestBody)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().put(LINE_COMMAND_URL, lineId)
                 .then().log().all()
                 .extract();
     }
 
     public static ExtractableResponse<Response> 지하철_노선_삭제_요청(String lineId) {
-        return given().log().all()
+        return given(기본_헤더값_설정()).log().all()
                 .when().delete(LINE_COMMAND_URL, lineId)
                 .then().log().all()
                 .extract();
