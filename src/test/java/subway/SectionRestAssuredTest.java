0784916150dfecc37f63bed1a63b99cb58194285
최현웅 <76509935+choi1204@users.noreply.github.com>
2023-02-02 21:22:03 +0900

    test : createSection RestAssured 추가

---
+++ src/test/java/subway/SectionRestAssuredTest.java
@@ -0,0 +1,26 @@
+package subway;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import subway.section.SectionCreateRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SectionRestAssuredTest {
+
+    public static Long createSection(SectionCreateRequest param) {
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(param)
+                .when().post()
+                .then().log().all()
+                .extract();
+
+        assertThat(response.contentType()).isEqualTo(HttpStatus.CREATED.value());
+
+        return response.jsonPath().getLong("id");
+    }
+}
