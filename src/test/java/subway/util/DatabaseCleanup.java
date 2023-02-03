e8d9a3f3cc21ca683902dc9e9ecfc2723cd97f79
Yunsik-Choi <choiys0311@gmail.com>
2023-02-03 18:05:40 +0900

    ♻️ refactor : Table 어노테이션 처리 로직 추가

--- src/test/java/subway/util/DatabaseCleanup.java
+++ src/test/java/subway/util/DatabaseCleanup.java
@@ -1,10 +1,14 @@
 package subway.util;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Table;
+import javax.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -21,10 +25,44 @@ public class DatabaseCleanup implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() {
-        this.tableNames = entityManager.getMetamodel().getEntities().stream()
-                .filter(entity -> entity.getJavaType().getAnnotation(Entity.class) != null)
-                .map(entity -> convertCamelToSnake(entity.getName()))
+        tableNames = new ArrayList<>();
+
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<String> entityAnnotationTableNames = entities.stream()
+                .filter(this::hasEntityAnnotation)
+                .filter(entityType -> !hasTableAnnotation(entityType))
+                .map(entityType -> convertCamelToSnake(entityType.getName()))
                 .collect(Collectors.toUnmodifiableList());
+
+        this.tableNames.addAll(entityAnnotationTableNames);
+
+        List<String> tableAnnotationTableNames = entities.stream()
+                .filter(this::hasEntityAnnotation)
+                .filter(this::hasTableAnnotation)
+                .map(entityType -> {
+                    String tableName = entityType.getJavaType().getAnnotation(Table.class).name();
+                    return tableName.isBlank() ? convertCamelToSnake(entityType.getName()) : tableName;
+                })
+                .collect(Collectors.toUnmodifiableList());
+
+        this.tableNames.addAll(tableAnnotationTableNames);
+    }
+
+    private boolean hasEntityAnnotation(final EntityType<?> entityType) {
+        return entityType.getJavaType().getAnnotation(Entity.class) != null;
+    }
+
+    private boolean hasTableAnnotation(final EntityType<?> entityType) {
+        return entityType.getJavaType().getAnnotation(Table.class) != null;
+    }
+
+    public static String convertCamelToSnake(String str) {
+        String regex = "([a-z])([A-Z])";
+        String replacement = "$1_$2";
+        String value = "";
+        value = str.replaceAll(regex, replacement).toUpperCase();
+        return value;
     }
 
     @Transactional
@@ -39,13 +77,5 @@ public class DatabaseCleanup implements InitializingBean {
         }
 
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
-    }
-
-    public static String convertCamelToSnake(String str) {
-        String regex = "([a-z])([A-Z])";
-        String replacement = "$1_$2";
-        String value = "";
-        value = str.replaceAll(regex, replacement).toUpperCase();
-        return value;
     }
 }
