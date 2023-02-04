b6eb99f18483abe4180a42a3ff9507131e832810
hyungkiShin <tkaqkeldk@nate.com>
2023-02-05 01:36:12 +0900

    refactor: test Fixture 추가

---
+++ src/test/java/subway/fixture/StationFixture.java
@@ -0,0 +1,15 @@
+package subway.fixture;
+
+import subway.repository.entity.Station;
+
+public abstract class StationFixture {
+    public static final long 강남역_ID = 1L;
+    public static final long 역삼역_ID = 2L;
+    public static final long 교대역_ID = 3L;
+    public static final long 양재역_ID = 4L;
+    public static final long 미존재역_ID = 1000L;
+    public static final Station 강남역 = new Station(강남역_ID, "강남역");
+    public static final Station 역삼역 = new Station(역삼역_ID, "역삼역");
+    public static final Station 교대역 = new Station(교대역_ID, "교대역");
+    public static final Station 양재역 = new Station(양재역_ID, "양재역");
+}
