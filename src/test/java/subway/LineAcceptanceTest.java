3a181ab91135df848f44ec9ae4bffebd2cd3022c
jisu <jisuuoo96@gmail.com>
2023-02-05 01:10:33 +0900

    test: 지하철 노선 목록 조회 인수 테스트 작성

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -25,6 +25,7 @@ class LineAcceptanceTest {
         RestAssured.port = port;
         StationAcceptanceTest.createStation("강남역");
         StationAcceptanceTest.createStation("양재역");
+        StationAcceptanceTest.createStation("서현역");
     }
 
 
@@ -46,7 +47,7 @@ class LineAcceptanceTest {
     }
 
 
-    private void createLine(String name, String color, long upStationId, long downStationId, int distance) {
+    public void createLine(String name, String color, long upStationId, long downStationId, int distance) {
         LineRequest param = new LineRequest(name, color, upStationId, downStationId, distance);
 
         RestAssured.given().log().all()
@@ -61,6 +62,27 @@ class LineAcceptanceTest {
                 .then().log().all().extract().jsonPath().getList("name", String.class);
     }
 
+    /*
+    * 지하철 노선 목록 조회
+    * given 2개의 지하철 노선을 생성하고
+    * when 지하철 노선 목록을 조회하면
+    * then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+    * */
+
+    @DisplayName("지하철 노선 목록 조회 인수테스트")
+    @Test
+    void getLines() {
+        //given
+        createLine("신분당선", "bg-red-600", 1, 2, 10);
+        createLine("분당선", "bg-green-600", 1, 3, 10);
+
+        //when
+        List<String> lineList = getLines("/lines");
+
+        //then
+        assertThat(lineList).containsExactly("신분당선", "분당선");
+
+    }
 
 
 
