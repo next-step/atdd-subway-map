eec4af502af7c4372b14dd4cb4d8b4ec6113522a
bactoria <bactoria@gmail.com>
2023-02-04 21:24:30 +0900

    review:: 인수테스트 수행 속도 개선
    관련 코멘트: https://github.com/next-step/atdd-subway-map/pull/672#discussion_r1092159398

--- src/test/java/subway/AcceptanceTest.java
+++ src/test/java/subway/AcceptanceTest.java
@@ -2,18 +2,21 @@ package subway;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
+    @Autowired
+    private DatabaseCleaner DatabaseCleaner;
+
     @LocalServerPort
     private int port;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
+        DatabaseCleaner.execute();
         RestAssured.port = port;
     }
 }
