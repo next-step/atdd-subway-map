c8bfe422ccff23ce4d3c851ab4126ed0b5311cca
hyungkiShin <tkaqkeldk@nate.com>
2023-02-04 23:42:20 +0900

    refactor: 💪 ErrorCode 추가

--- src/main/java/subway/exception/message/SubwayErrorCode.java
+++ src/main/java/subway/exception/message/SubwayErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum SubwayErrorCode {
 
-    NOT_FOUND_STATION("존재하지 않는 역입니다.");
+    NOT_FOUND_STATION("존재하지 않는 역입니다."),
+    NOT_POSSIBLE_TO_CREATE_STATION("상행역과 하행역이 같은 구간은 생성할 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
     String message;
     HttpStatus httpStatus;
