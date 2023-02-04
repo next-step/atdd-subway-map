d182066869c4b90644568073410ad0f21b7e7d21
wu2ee <gulb1602@gmail.com>
2023-02-04 13:59:53 +0900

    fix : AcceptanceTest 로 네이밍 변경 (명시적으로 변경)

--- src/test/java/subway/AcceptanceTest.java
+++ src/test/java/subway/AcceptanceTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class Acceptance {
+public class AcceptanceTest {
     @LocalServerPort
     int port;
 
