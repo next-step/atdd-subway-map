414fcea78d91b37b31395b3e32178a8ad3e83672
wu2ee <gulb1602@gmail.com>
2023-02-05 10:43:12 +0900

    fix : station 패키지 이동으로 인한 인수 테스트 import 문 변경

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import subway.station.StationResponse;
 
 import java.util.Arrays;
 import java.util.HashMap;
