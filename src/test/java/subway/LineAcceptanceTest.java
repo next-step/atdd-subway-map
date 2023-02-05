ef7c6299c7cce38063410a328b0b6cb4a9a9a805
jisu <jisuuoo96@gmail.com>
2023-02-05 22:06:37 +0900

    test: 지하철 노선 조회 인수 테스트 작성

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -1,9 +1,7 @@
 package subway;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
@@ -28,21 +26,20 @@ class LineAcceptanceTest {
         StationAcceptanceTest.createStation("서현역");
     }
 
-
     /*
     * 지하철 노선 생성
     * when 지하철 노선을 생성하면
     * then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다.
     * */
 
-    @DisplayName("지하철 노선 생성 인수 테스트")
+    @DisplayName("지하철노선 생성 인수 테스트")
     @Test
     void createLine() {
         //when
         createLine("신분당선", "bg-red-600", 1, 2, 10);
 
-        //then
         List<String> lineList = getLines("/lines");
+
         assertThat(lineList).containsAnyOf("신분당선");
     }
 
@@ -62,6 +59,7 @@ class LineAcceptanceTest {
                 .then().log().all().extract().jsonPath().getList("name", String.class);
     }
 
+
     /*
     * 지하철 노선 목록 조회
     * given 2개의 지하철 노선을 생성하고
@@ -69,7 +67,7 @@ class LineAcceptanceTest {
     * then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
     * */
 
-    @DisplayName("지하철 노선 목록 조회 인수테스트")
+    @DisplayName("지하철노선 목록 조회 인수테스트")
     @Test
     void getLines() {
         //given
@@ -81,7 +79,29 @@ class LineAcceptanceTest {
 
         //then
         assertThat(lineList).containsExactly("신분당선", "분당선");
+    }
 
+    /*
+    * given 지하철 노선을 생성하고
+    * when 생성한 지하철 노선을 조회하면
+    * then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+    * */
+
+    @DisplayName("지하철노선 조회 인수테스트")
+    @Test
+    void getLine() {
+        //given
+        createLine("신분당선", "bg-red-600", 1, 2, 10);
+        //when
+        List<String> line = getLine("/lines/1");
+        //then
+        assertThat(line).containsAnyOf("강남역");
+    }
+
+    public List<String> getLine(String path) {
+        return RestAssured.given().log().all()
+                .when().get(path)
+                .then().log().all().extract().jsonPath().getList("stations.name");
     }
 
 
