e55292504a9e248a4cc56543e6e6da328c5fa165
wu2ee <gulb1602@gmail.com>
2023-02-05 16:57:18 +0900

    feat : 지하철 인수 테스트 격리를 위한 DatabaseCleanup 추가
    - 테이블 이름 소문자로 통일
    - TRUNCATE 이용 (외래키 제약 임시 비활성화 한 뒤 데이터 삭제 후 다시 활성화)

--- src/test/java/subway/AcceptanceTest.java
+++ src/test/java/subway/AcceptanceTest.java
@@ -2,16 +2,22 @@ package subway;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import subway.utils.DatabaseCleanup;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
     @LocalServerPort
     int port;
 
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+        databaseCleanup.execute();
     }
 }
