c3e2cf0a90a595024a808ab60a30bfef139d8468
Choonghee Lee <choonghee.lee@hotmail.com>
2023-02-05 11:07:30 +0900

    refactor(line entity relationship): 상하행역 OneToOne 연관 관계 제거

--- src/main/java/subway/line/entity/Line.java
+++ src/main/java/subway/line/entity/Line.java
@@ -21,14 +21,6 @@ public class Line {
     @Column(length = 20, nullable = false)
     private String color;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "up_station_id")
-    private Station upStation;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "down_station_id")
-    private Station downStation;
-
     @OneToMany(cascade = CascadeType.PERSIST)
     @JoinTable(name = "line_sections", joinColumns = @JoinColumn(name = "line_id"), inverseJoinColumns = @JoinColumn(name = "section_id"))
     private final List<Section> sections = new ArrayList<>();
@@ -39,8 +31,6 @@ public class Line {
     public Line(LineRequest lineRequest, Station upStation, Station downStation) {
         this.name = lineRequest.getName();
         this.color = lineRequest.getColor();
-        this.upStation = upStation;
-        this.downStation = downStation;
         this.sections.add(new Section(lineRequest.getDistance(), upStation, downStation));
     }
 
@@ -51,13 +41,27 @@ public class Line {
 
     public void appendSection(Section section) {
         this.sections.add(section);
-        this.downStation = section.getDownStation();
     }
 
     public void removeSection() {
         this.sections.remove(this.sections.size() - 1);
+    }
 
-        Section lastSection = this.sections.get(this.sections.size() - 1);
-        this.downStation = lastSection.getDownStation();
+    public Station getUpStation() {
+        mustContainSection();
+
+        return sections.get(0).getUpStation();
+    }
+
+    public Station getDownStation() {
+        mustContainSection();
+
+        return sections.get(sections.size() - 1).getDownStation();
+    }
+
+    private void mustContainSection() {
+        if (sections.isEmpty()) {
+            throw new EntityNotFoundException("등록된 구간이 없습니다");
+        }
     }
 }
