c4fa19cab9d85acf819436aaadf1b4f52d3806e7
Sangmin <sm9171@nate.com>
2023-02-05 10:12:07 +0900

    test: 지하철 구간 삭제 테스트 작성

--- src/test/java/subway/SectionAcceptanceTest.java
+++ src/test/java/subway/SectionAcceptanceTest.java
@@ -38,10 +38,10 @@ public class SectionAcceptanceTest extends AcceptanceTest {
 
     /**
      * given 새로운 역을 생성하고
-     * when 지하철 구간을 생성하면
-     * Then 지하철 구간을 조회 시 생성한 구간을 찾을 수 있다
+     * when 지하철 구간을 등록하면
+     * Then 지하철 구간을 조회 시 등록한 구간을 찾을 수 있다
      */
-    @DisplayName("지하철 구간을 생성한다.")
+    @DisplayName("지하철 구간을 등록한다.")
     @Test
     void addSection() {
         //given
@@ -55,6 +55,39 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(station1Id, station2Id, station3Id);
 
+    }
+
+    /**
+     * given 새로운 역을 생성하고 지하철 구간을 등록하고
+     * when 지하철 구간을 삭제하면
+     * Then 지하철 구간을 조회 시 제거된 구간을 찾을 수 있다
+     */
+    @DisplayName("지하철 구간을 삭제한다.")
+    @Test
+    void deleteSection() {
+        //given
+        Long station3Id = createStation("청계산입구역").jsonPath().getLong("id");
+        createSectionResponse(line1Id, createSectionCreateParams(station2Id, station3Id));
+
+        //when
+        Map<String, Object> map = new HashMap<>();
+        map.put("stationId",station3Id);
+        ExtractableResponse<Response> deleteSectionResponse = deleteSectionResponse(line1Id, map);
+
+        //then
+        assertThat(deleteSectionResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        ExtractableResponse<Response> response = getLineResponse(line1Id);
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).doesNotContain(station3Id);
+
+    }
+
+    private ExtractableResponse<Response> deleteSectionResponse(Long lineId, Map<String, Object> params) {
+        return RestAssured.given().log().all()
+                .given().pathParam("id", lineId)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().delete("/lines/{id}/sections")
+                .then().log().all().extract();
     }
 
     private ExtractableResponse<Response> createSectionResponse(Long lineId, Map<String, String> params) {
