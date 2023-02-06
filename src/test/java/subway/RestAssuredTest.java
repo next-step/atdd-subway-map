6486ed89d2791210275cc63c24f0591c585b82e6
Yunsik-Choi <choiys0311@gmail.com>
2023-02-06 19:10:01 +0900

    ⛑️ test : 구글 페이지 접근 테스트 포트 설정

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -8,15 +8,16 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpStatus;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class RestAssuredTest {
 
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        String url = "https://google.com";
+        String url = "https://google.com:443";
 
         ExtractableResponse<Response> response = when()
                 .get(url)
