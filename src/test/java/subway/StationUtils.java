d6638ded16b48aea15b4e19ff9e36e2f50daad3e
Airvw <airvw14@gmail.com>
2023-02-06 22:00:58 +0900

    test: 1단계 피드백 적용

---
+++ src/test/java/subway/StationUtils.java
@@ -0,0 +1,40 @@
+package subway;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+public class StationUtils {
+    static ExtractableResponse<Response> createStation(String stationName){
+        Station station = new Station(stationName);
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .body(station)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when().post("/stations")
+                        .then().log().all()
+                        .extract();
+        return response;
+    }
+
+    static List<String> getStationNames(){
+        List<String> stationNames =
+                RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract().jsonPath().getList("name", String.class);
+
+        return stationNames;
+    }
+
+    static ExtractableResponse<Response> deleteStation(ExtractableResponse<Response> response){
+        int id = response.body().jsonPath().getInt("id");
+        return  RestAssured.given().log().all()
+                .when().delete("/stations/" + id)
+                .then().log().all()
+                .extract();
+    }
+}
