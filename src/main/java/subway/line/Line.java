6d5f27aa11c7da458da43d719ce373b6989963da
wu2ee <gulb1602@gmail.com>
2023-02-08 07:41:15 +0900

    feat : 구간 등록 & 제거 예외 메시지 추가

--- src/main/java/subway/line/Line.java
+++ src/main/java/subway/line/Line.java
@@ -87,13 +87,13 @@ public class Line {
 
     private void validAddUpStation(Station upStation) {
         if (!isFinalStation(upStation)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("등록하고자 하는 구간의 상행역이 현재 노선의 하행종점역이 아니면 구간을 등록할 수 없습니다.");
         }
     }
 
     private void validAddDownStation(Station downStation) {
         if (getStations().contains(downStation)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("등록하고자 하는 구간의 하행역이 현재 노선에 이미 포함되어 있다면 구간을 등록할 수 없습니다.");
         }
     }
 
@@ -114,13 +114,13 @@ public class Line {
 
     private void validSectionSize() {
         if (this.sections.size() <= MIN_SECTION_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("현재 구간 갯수가 하나이므로 구간을 삭제할 수 없습니다.");
         }
     }
 
     private void validRemoveStation(Long stationId) {
         if (!isFinalStation(stationId)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("하행종점역이 아닌 역은 삭제할 수 없습니다.");
         }
     }
 }
