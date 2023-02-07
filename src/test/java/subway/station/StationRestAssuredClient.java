ac12dc08992794c1c5aa7620ab4e067cbb8faa63
yjna2316 <yjna2316@gmail.com>
2023-02-08 00:39:35 +0900

    [Step3] refac: RestAssuredClient 메소드 사용시 상속 통해 하도록 수정

--- src/test/java/subway/station/StationRestAssuredClient.java
+++ src/test/java/subway/station/StationRestAssuredClient.java
@@ -6,16 +6,16 @@ import subway.utils.RestAssuredClient;
 
 import java.util.Map;
 
-public class StationRestAssuredClient {
+public class StationRestAssuredClient extends RestAssuredClient {
     public static ExtractableResponse<Response> createStation(Map<String, Object> requestParam) {
-        return RestAssuredClient.post("/stations", requestParam);
+        return post("/stations", requestParam);
     }
 
     public static void deleteStation(Long id) {
-        RestAssuredClient.delete(String.format("/stations/%d", id));
+        delete(String.format("/stations/%d", id));
     }
 
     public static ExtractableResponse<Response> listStation() {
-        return RestAssuredClient.get("/stations");
+        return get("/stations");
     }
 }
