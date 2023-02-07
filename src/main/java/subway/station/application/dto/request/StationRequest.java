aa3fc0f7823c718569319c4aa593fbf12d536f04
giibeom <kpmyung@gmail.com>
2023-02-07 19:42:47 +0900

    fix(station): RequestDto 객체의 단일 매개변수 생성자에서 데이터 바인딩 실패 상황을 `@JsonCreator`로 해결합니다.

--- src/main/java/subway/station/application/dto/request/StationRequest.java
+++ src/main/java/subway/station/application/dto/request/StationRequest.java
@@ -1,11 +1,19 @@
 package subway.station.application.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Builder;
 import lombok.Getter;
 import subway.station.domain.Station;
 
 @Getter
 public class StationRequest {
-    private String name;
+    private final String name;
+
+    @JsonCreator
+    @Builder
+    private StationRequest(final String name) {
+        this.name = name;
+    }
 
     public Station toEntity() {
         return Station.builder()
