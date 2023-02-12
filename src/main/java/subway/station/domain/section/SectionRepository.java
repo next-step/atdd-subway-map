113dc0cb2dc206112c5fcbf9bce3ac91a67994a5
JohnPrk <uo3641493@gmail.com>
2023-02-13 00:43:50 +0900

    refactor : SectionRepository 삭제(Section 객체는 Line 객체에 종속적으로 설계)

--- src/main/java/subway/station/domain/section/SectionRepository.java
+++
@@ -1,6 +0,0 @@
-package subway.station.domain.section;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SectionRepository extends JpaRepository<Section, Long> {
-}
