ea5cde766409d2c220ccc9c447d0742e86ab70f7
sjhello <sjhello235@gmail.com>
2023-02-12 17:32:34 +0900

    refactor: @BeforeEach를 사용하여 노선 생성 요청 전 역 생성 메서드 분리

--- src/test/java/subway/line/LineAcceptanceTest.java
+++ src/test/java/subway/line/LineAcceptanceTest.java
@@ -3,11 +3,14 @@ package subway.line;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import subway.stations.StationRestAssured;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +20,21 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class LineAcceptanceTest {
 
+    private ExtractableResponse<Response> stationResponse1;
+    private ExtractableResponse<Response> stationResponse2;
+
+    @BeforeEach
+    void stationCreate() {
+        // given - 역 생성
+        Map<String, String> station1 = new HashMap<>();
+        station1.put("name", "논현역");
+        stationResponse1 = StationRestAssured.createStation(station1);
+
+        Map<String, String> station2 = new HashMap<>();
+        station2.put("name", "강남역");
+        stationResponse2 = StationRestAssured.createStation(station2);
+    }
+
     /**
      * When 지하철 노선을 생성하면
      * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
@@ -24,11 +42,8 @@ class LineAcceptanceTest {
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createRoute() {
-        // given - 역 생성
-        createStation();
-
         // given - 노선 생성
-        LineRequest lineRequest = new LineRequest("신분당선", "red", 1L, 2L, 10L);
+        LineRequest lineRequest = new LineRequest("신분당선", "red", stationResponse1.jsonPath().getLong("id"), stationResponse2.jsonPath().getLong("id"), 10L);
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
@@ -40,29 +55,6 @@ class LineAcceptanceTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-
-    }
-
-    private static void createStation() {
-        Map<String, String> param1 = new HashMap<>();
-        param1.put("name", "논현역");
-
-        Map<String, String> param2 = new HashMap<>();
-        param2.put("name", "강남역");
-
-        RestAssured.given()
-                .body(param1)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/stations")
-                .then().log().all()
-                .extract();
-
-        RestAssured.given()
-                .body(param2)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/stations")
-                .then().log().all()
-                .extract();
     }
 
     /**
