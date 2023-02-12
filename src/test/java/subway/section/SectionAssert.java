b43d86c668144182b268695aa96855e98795dd8e
Yunsik-Choi <choiys0311@gmail.com>
2023-02-12 14:46:07 +0900

    ⛑️ test : 구간 테스트 검증 수정

--- src/test/java/subway/section/SectionAssert.java
+++ src/test/java/subway/section/SectionAssert.java
@@ -2,14 +2,14 @@ package subway.section;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static subway.line.LineRestAssured.Location_조회;
-import static subway.section.SectionRestAssured.구간_조회;
+import static subway.line.LineRestAssured.노선_조회;
 
 import io.restassured.path.json.JsonPath;
 
 public class SectionAssert {
     
     public static void 구간_등록_검증(final Long lineId, final Long stationId) {
-        JsonPath jsonPath = 구간_조회(lineId, stationId).jsonPath();
+        JsonPath jsonPath = 노선_조회(lineId).jsonPath();
 
         assertThat(jsonPath.getLong("downStationId")).isEqualTo(stationId);
     }
