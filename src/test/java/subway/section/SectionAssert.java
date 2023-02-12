c0664009f88db6e36d7050cda0a4b818673c068d
Yunsik-Choi <choiys0311@gmail.com>
2023-02-12 14:49:47 +0900

    ⛑️ test : 구간 등록 검증 수정

--- src/test/java/subway/section/SectionAssert.java
+++ src/test/java/subway/section/SectionAssert.java
@@ -11,7 +11,7 @@ public class SectionAssert {
     public static void 구간_등록_검증(final Long lineId, final Long stationId) {
         JsonPath jsonPath = 노선_조회(lineId).jsonPath();
 
-        assertThat(jsonPath.getLong("downStationId")).isEqualTo(stationId);
+        assertThat(jsonPath.getLong("stations.id[1]")).isEqualTo(stationId);
     }
 
     public static void 구간_조회_검증(final String location, final Long downStationId) {
