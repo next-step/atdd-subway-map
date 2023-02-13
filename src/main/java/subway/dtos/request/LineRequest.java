165467ae08018626450120fa71c4626bdc08961a
Airvw <airvw14@gmail.com>
2023-02-13 22:25:25 +0900

    feat: request, response 추가

---
+++ src/main/java/subway/dtos/request/LineRequest.java
@@ -0,0 +1,24 @@
+package subway.dtos.request;
+
+public class LineRequest {
+    private String name;
+    private String color;
+    private Long upStationId;
+    private Long downStationId;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+}
