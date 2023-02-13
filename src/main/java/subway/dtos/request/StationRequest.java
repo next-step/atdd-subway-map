9d667d140d55c182797aa243404ef30c631630e3
Airvw <airvw14@gmail.com>
2023-02-13 22:20:35 +0900

    refactor: 패키지 경로 수정

--- src/main/java/subway/dtos/request/StationRequest.java
+++ src/main/java/subway/dtos/request/StationRequest.java
@@ -1,4 +1,4 @@
-package subway;
+package subway.dtos.request;
 
 public class StationRequest {
     private String name;
--- src/main/java/subway/service/StationService.java
+++ src/main/java/subway/service/StationService.java
@@ -1,7 +1,11 @@
-package subway;
+package subway.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import subway.domain.Station;
+import subway.domain.repository.StationRepository;
+import subway.dtos.request.StationRequest;
+import subway.dtos.response.StationResponse;
 
 import java.util.List;
 import java.util.stream.Collectors;
