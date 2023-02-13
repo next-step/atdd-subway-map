f8c83a9417d2aea1e4db4a9c3052bd9db7673561
sjhello <sjhello235@gmail.com>
2023-02-14 08:00:10 +0900

    refactor: 404를 반환하게 애노테이션 추가

--- src/main/java/subway/station/exception/StationNotFoundException.java
+++ src/main/java/subway/station/exception/StationNotFoundException.java
@@ -1,5 +1,9 @@
 package subway.station.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class StationNotFoundException extends RuntimeException {
 
     private static final String notFoundMessage = "역이 존재하지 않습니다";
