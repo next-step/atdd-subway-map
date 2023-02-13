8158dc5a93a38077583b22dd38f74048cffbe649
JohnPrk <uo3641493@gmail.com>
2023-02-13 17:59:23 +0900

    fix : 병합 관련 충돌 해결

--- src/test/java/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/subway/acceptance/StationAcceptanceTest.java
@@ -1,53 +1,118 @@
-package subway.acceptanceSetting;
+package subway.acceptance;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
-import subway.restAssured.LineRestAssured;
-import subway.restAssured.StationRestAssured;
+import subway.acceptanceSetting.StationAcceptanceTestSetting;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("지하철역 관련 기능")
 @ActiveProfiles("acceptance")
-@Transactional(readOnly = true)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class LineAcceptanceTestSetting {
+public class StationAcceptanceTest extends StationAcceptanceTestSetting {
 
-    protected static final int LENGTH_TWO = 2;
-    protected static final String SHINBUNDANG_LINE = "신분당선";
-    protected static final String BUNDANG_LINE = "분당선";
-    protected static final String ANOTHER_BUNDANG_LINE = "다른분당선";
-    protected static final String RED = "bg-red-600";
-    protected static final String GREEN = "bg-green-600";
-    protected static final String YELLOW = "bg-yellow-600";
-    protected static final long DISTANCE_TEN = 10L;
-    protected static final long DISTANCE_FIFTEEN = 15L;
-    protected static final String PATH_NAME = "name";
-    protected static final String PATH_COLOR = "color";
-    protected static final String PATH_LINE = "line";
+    /**
+     * When 지하철역을 생성하면
+     * Then 지하철역이 생성된다
+     * Then 지하철역 목록 조회 시 생성한 역을 찾을 수 있다
+     */
+    @DisplayName("지하철역을 생성한다.")
+    @Test
+    void createStation() {
+        // when+then
+        saveStation(STATION_NAME);
+        saveStation(NEW_STATION_NAME);
+        saveStation(ANOTHER_STATION_NAME);
 
-    @LocalServerPort
-    int port;
+        // then
+        List<String> stationNames = findAllStations().jsonPath().getList("name", String.class);
+        assertThat(stationNames).contains(STATION_NAME);
+    }
 
-    @Autowired
-    private DatabaseCleanUp databaseCleanUp;
+    /**
+     * Given 2개의 지하철역을 생성하고
+     * When 지하철역 목록을 조회하면
+     * Then 2개의 지하철역을 응답 받는다
+     */
+    @DisplayName("지하철역을 조회한다.")
+    @Test
+    void readStations() {
+        // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
 
-    private StationRestAssured stationRestAssured = new StationRestAssured();
-    protected LineRestAssured lineRestAssured = new LineRestAssured();
-    protected Long firstStationId;
-    protected Long secondStationId;
-    protected Long thirdStationId;
+        //given
+        saveStation(STATION_NAME);
+        saveStation(NEW_STATION_NAME);
 
+        //when
+        ExtractableResponse<Response> readResponse = findAllStations();
 
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-        databaseCleanUp.execute();
+        //then
+        List<String> stationNames = readResponse.jsonPath().get("name");
+        Assertions.assertThat(stationNames).containsExactly(STATION_NAME, NEW_STATION_NAME);
+    }
 
-        firstStationId = stationRestAssured.save("지하철역");
-        secondStationId = stationRestAssured.save("새로운지하철역");
-        thirdStationId = stationRestAssured.save("또다른지하철역");
+    /**
+     * Given 지하철역을 생성하고
+     * When 그 지하철역을 삭제하면
+     * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
+     */
+    @DisplayName("지하철역을 삭제한다.")
+    @Test
+    void deleteStation() {
+        // TODO: 지하철역 제거 인수 테스트 메서드 생성
+
+        //given
+        Long id = saveStation(STATION_NAME);
+
+        //when
+        deleteStationById(id);
+
+        //then
+        Assertions.assertThat(findAllStations().jsonPath().getList("name")).doesNotContain(STATION_NAME);
+    }
+
+    private void deleteStationById(Long id) {
+        RestAssured
+                .given().log().all()
+                .when()
+                .delete("/stations/" + id)
+                .then().log().all()
+                .assertThat().statusCode(HttpStatus.NO_CONTENT.value())
+                .extract();
+    }
+
+    private ExtractableResponse<Response> findAllStations() {
+        return RestAssured
+                .given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .assertThat().statusCode(HttpStatus.OK.value())
+                .extract();
+    }
+
+    Long saveStation(String stationName) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", stationName);
+        return RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/stations")
+                .then().log().all()
+                .assertThat().statusCode(HttpStatus.CREATED.value())
+                .extract().jsonPath().getObject("id", Long.class);
     }
 }
