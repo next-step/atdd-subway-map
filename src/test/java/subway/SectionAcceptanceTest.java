ed2962d06248cea452e59a253b503ff59b1573bd
hoo <tbnsok40@gmail.com>
2023-02-14 16:08:49 +0900

    feedback: 역id 명시적 의미 지니도록 수정

--- src/test/java/subway/SectionAcceptanceTest.java
+++ src/test/java/subway/SectionAcceptanceTest.java
@@ -27,33 +27,38 @@ class SectionAcceptanceTest {
     String 서귀포역 = LineAcceptanceTest.서귀포역;
     String 한라산역 = LineAcceptanceTest.한라산역;
     String 백두산역 = LineAcceptanceTest.백두산역;
-    String 새로운역 = "금강산역";
-    String 또새로운역 = "러시아역";
+    String 금강산역 = "금강산역";
+    String 러시아역 = "러시아역";
     LineRequest 제주선 = LineAcceptanceTest.제주선_생성(); // upStation : 1, downStation : 2
     LineRequest 반도선 = LineAcceptanceTest.반도선_생성(); // upStation : 2, downStation : 3
+
+    long 한라산역ID = 1L;
+    long 백두산역ID = 2L;
+    long 서귀포역ID = 3L;
+    long 금강산역ID = 4L;
+    long 러시아역ID = 5L;
 
     @BeforeEach
     void setUp() {
         StationAcceptanceTest.createOneStation(한라산역);
         StationAcceptanceTest.createOneStation(백두산역);
         StationAcceptanceTest.createOneStation(서귀포역);
-        StationAcceptanceTest.createOneStation(새로운역);
-        StationAcceptanceTest.createOneStation(또새로운역);
+        StationAcceptanceTest.createOneStation(금강산역);
+        StationAcceptanceTest.createOneStation(러시아역);
         LineAcceptanceTest.노선_생성(제주선);
     }
 
     @Test
     void 구간_제거_테스트() {
-        long lineId = 1L;
-        long stationId = 3L;
-        SectionRequest sectionRequest = new SectionRequest(2L, 3L, 10);
+        long lineId = 한라산역ID;
+        SectionRequest sectionRequest = new SectionRequest(백두산역ID, 서귀포역ID, 10);
         createSection(lineId, sectionRequest);
         LineAcceptanceTest.노선_조회(lineId);
 
         ExtractableResponse<Response> extract = RestAssured
             .given().log().all()
             .pathParam("id", lineId)
-            .params("stationId", stationId)
+            .params("stationId", 서귀포역ID)
             .contentType(APPLICATION_JSON_VALUE)
             .when()
             .delete("/lines/{id}/sections")
@@ -66,15 +71,15 @@ class SectionAcceptanceTest {
 
     @Test
     void 구간_등록_테스트() {
-        long id = 1L;
-        SectionRequest sectionRequest = new SectionRequest(2L, 3L, 10);
+        long id = 한라산역ID;
+        SectionRequest sectionRequest = new SectionRequest(백두산역ID, 서귀포역ID, 10);
         assertEquals(createSection(id, sectionRequest).statusCode(), HttpStatus.CREATED.value());
     }
 
     @Test
     void 하행종점_상행역_다르면_예외를_던진다() {
-        long id = 1L;
-        SectionRequest sectionRequest = new SectionRequest(4L, 5L, 10);
+        long id = 한라산역ID;
+        SectionRequest sectionRequest = new SectionRequest(금강산역ID, 러시아역ID, 10);
         assertAll(
             () -> assertEquals(createSection(id, sectionRequest).statusCode(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value())
