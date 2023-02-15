a17bb452bb86722dba415ea8092cce12669e690b
sjhello <sjhello235@gmail.com>
2023-02-15 21:51:48 +0900

    refactor: 한글 메서드명 사용, application/json 상수 선언

--- src/test/java/subway/line/LineRestAssured.java
+++ src/test/java/subway/line/LineRestAssured.java
@@ -4,30 +4,42 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.MediaType;
+import subway.line.presentation.LinePatchRequest;
 import subway.line.presentation.LineRequest;
 
 public class LineRestAssured {
 
     private static final String API_PATH = "/lines";
 
-    public static ExtractableResponse<Response> createLine(LineRequest lineRequest) {
+    private static final String APPLICATION_JSON_VALUE = MediaType.APPLICATION_JSON_VALUE;;
+
+    public static ExtractableResponse<Response> 지하철_노선_생성_요청(LineRequest lineRequest) {
         return RestAssured.given().log().all()
                 .body(lineRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(APPLICATION_JSON_VALUE)
                 .when().post(API_PATH)
                 .then().log().all()
                 .extract();
     }
-    public static ExtractableResponse<Response> readLines() {
+    public static ExtractableResponse<Response> 지하철_노선_목록_조회_요청() {
         return RestAssured.given().log().all()
                 .when().get(API_PATH)
                 .then().log().all()
                 .extract();
     }
 
-    public static ExtractableResponse<Response> readLine(Long id) {
+    public static ExtractableResponse<Response> 지하철_노선_조회_요청(Long id) {
         return RestAssured.given().log().all()
                 .when().get(API_PATH + "/" + id)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 지하철_노선_수정_요청(LinePatchRequest linePatchRequest, Long id) {
+        return RestAssured.given()
+                .body(linePatchRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().patch(API_PATH + "/" + id)
                 .then().log().all()
                 .extract();
     }
