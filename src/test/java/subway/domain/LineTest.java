3e281bcacee9d1b9421a5a85a86b7e612e9f8f38
Yunsik-Choi <choiys0311@gmail.com>
2023-02-15 14:36:04 +0900

    ⛑️ test : 노선 수정 관련 테스트 작성

---
+++ src/test/java/subway/domain/LineTest.java
@@ -0,0 +1,83 @@
+package subway.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import subway.exception.LineModifyException;
+
+@DisplayName("노선 관련 기능")
+class LineTest {
+
+    private Station 강남역;
+    private Station 역삼역;
+    private Station 정자역;
+    private Station 미금역;
+
+    @BeforeEach
+    void setUp() {
+        this.강남역 = new Station("강남역");
+        this.역삼역 = new Station("역삼역");
+        this.정자역 = new Station("정자역");
+        this.미금역 = new Station("미금역");
+    }
+
+
+    @DisplayName("수정 관련 기능")
+    @Nested
+    class Modify {
+        @DisplayName("구간 이름 수정")
+        @Test
+        void modifyEditName() {
+            String expected = "수정 후 이름";
+            Line line = new Line("수정 전 이름", "색깔", 강남역, 정자역, 10);
+
+            line.modify(expected, "색깔", 5);
+
+            assertThat(line.getName()).isEqualTo(expected);
+        }
+
+        @DisplayName("구간 이름 수정시 이름은 null일 수 없습니다.")
+        @Test
+        void modifyEditNameNull() {
+            Line line = new Line("수정 전 이름", "색깔", 강남역, 정자역, 10);
+
+            assertThatThrownBy(() -> line.modify(null, "색깔", 5))
+                    .isInstanceOf(LineModifyException.class);
+        }
+
+        @DisplayName("구간 색깔 수정")
+        @Test
+        void modifyEditColor() {
+            String expected = "수정 후 색깔";
+            Line line = new Line("이름", "색깔", 강남역, 정자역, 10);
+
+            line.modify("이름", expected, 5);
+
+            assertThat(line.getColor()).isEqualTo(expected);
+        }
+
+        @DisplayName("구간 이름 수정시 색깔은 null일 수 없습니다.")
+        @Test
+        void modifyEditColorNull() {
+            Line line = new Line("수정 전 이름", "색깔", 강남역, 정자역, 10);
+
+            assertThatThrownBy(() -> line.modify("이름", null, 5))
+                    .isInstanceOf(LineModifyException.class);
+        }
+
+        @DisplayName("구간 거리 수정")
+        @Test
+        void modifyEditDistance() {
+            int expected = 15;
+            Line line = new Line("이름", "색깔", 강남역, 정자역, 10);
+
+            line.modify("이름", "색깔", expected);
+
+            assertThat(line.getDistance()).isEqualTo(expected);
+        }
+    }
+}
