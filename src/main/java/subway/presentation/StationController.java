2462412389b094e0a48e96a2030e6533fdae6207
Yunsik-Choi <choiys0311@gmail.com>
2023-02-16 14:30:40 +0900

    ♻️ refactor : StationController base url 설정

--- src/main/java/subway/presentation/StationController.java
+++ src/main/java/subway/presentation/StationController.java
@@ -9,27 +9,30 @@ import subway.dto.StationRequest;
 import subway.dto.StationResponse;
 import subway.application.StationService;
 
+@RequestMapping("/stations")
 @RestController
 public class StationController {
 
     private final StationService stationService;
+    private final String baseUrl;
 
     public StationController(final StationService stationService) {
         this.stationService = stationService;
+        baseUrl = this.getClass().getAnnotation(RequestMapping.class).value()[0];
     }
 
-    @PostMapping("/stations")
+    @PostMapping
     public ResponseEntity<StationResponse> createStation(@RequestBody final StationRequest stationRequest) {
         StationResponse station = stationService.saveStation(stationRequest);
-        return ResponseEntity.created(URI.create("/stations/" + station.getId())).body(station);
+        return ResponseEntity.created(URI.create(baseUrl + "/" + station.getId())).body(station);
     }
 
-    @GetMapping(value = "/stations")
+    @GetMapping
     public ResponseEntity<List<StationResponse>> showStations() {
         return ResponseEntity.ok().body(stationService.findAllStations());
     }
 
-    @DeleteMapping("/stations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteStation(@PathVariable final Long id) {
         stationService.deleteStationById(id);
         return ResponseEntity.noContent().build();
