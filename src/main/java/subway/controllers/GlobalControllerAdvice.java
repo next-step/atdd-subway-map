3d8f2d190a6bcb2f5295821e68b0764f8d921568
jykim <wodusl1103@naver.com>
2023-02-16 21:48:11 +0900

    chore: exception 로깅

--- src/main/java/subway/controllers/GlobalControllerAdvice.java
+++ src/main/java/subway/controllers/GlobalControllerAdvice.java
@@ -1,21 +1,25 @@
 package subway.controllers;
 
 import javax.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
 	@ExceptionHandler({EntityNotFoundException.class})
 	public ResponseEntity<Void> exceptionHandler(EntityNotFoundException e) {
-		e.printStackTrace();
+		logger.error(e.toString());
 		return ResponseEntity.notFound().build();
 	}
 
 	@ExceptionHandler({IllegalArgumentException.class})
 	public ResponseEntity<Void> exceptionHandler(IllegalArgumentException e) {
-		e.printStackTrace();
+		logger.error(e.toString());
 		return ResponseEntity.badRequest().build();
 	}
 }
