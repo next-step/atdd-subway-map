06d4a2d0d0947221216a4486924ee630ab94d7a8
sjhello <sjhello235@gmail.com>
2023-02-22 23:21:33 +0900

    refactor: 응답 결과에서 Json을 사용하도록 변경

--- src/test/java/subway/util/ResponseUtils.java
+++ src/test/java/subway/util/ResponseUtils.java
@@ -13,16 +13,4 @@ public class ResponseUtils {
     public static JsonPath Json_추출(ExtractableResponse<Response> response) {
         return response.jsonPath();
     }
-
-    public static Long Json_ID_추출(ExtractableResponse<Response> response) {
-        return Json_추출(response).getLong("id");
-    }
-
-    public static String Json_이름_추출(ExtractableResponse<Response> response) {
-        return Json_추출(response).getString("name");
-    }
-
-    public static String Json_색_추출(ExtractableResponse<Response> response) {
-        return Json_추출(response).getString("color");
-    }
 }
