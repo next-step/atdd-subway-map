7746f3fda812f80bc03e165ffd2ff713db9d3dcb
jisu <jisuuoo96@gmail.com>
2023-02-28 02:55:56 +0900

    feat: 지하철 구간 생성 예외 처리 handler 구현

---
+++ src/main/java/subway/common/constants/ErrorConstant.java
@@ -0,0 +1,10 @@
+package subway.common.constants;
+
+public class ErrorConstant {
+
+    public ErrorConstant() {
+    }
+
+    public static final String NOT_LAST_STATION = "새로운 구간의 상행 역은 노선에 등록된 하행 종점 역이어야 합니다.";
+    public static final String ALREADY_ENROLL_STATION = "새로운 구간의 하행 역은 노선에 등록된 역일 수 없습니다.";
+}
