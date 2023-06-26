eb213e7e22c235012d0ca9d6b4aed495a58a976d
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2023-06-26 21:34:57 +0900

    test : verify 200 ok response when request get google index page (#767)

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,23 +1,39 @@
 package subway;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
+import static io.restassured.RestAssured.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class RestAssuredTest {
+class RestAssuredTest {
+
+    private final static String GOOGLE_INDEX_PAGE_URL = "https://google.com";
 
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        givenBase()
+                .when()
+                .get()
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.HTML);
+    }
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    private RequestSpecification givenBase() {
+        return given()
+                .log().all()
+                .baseUri(GOOGLE_INDEX_PAGE_URL);
     }
 }
