fc657b3443be90cc23d5a8d3bb097b2bae8b6ed2
지널 <hyeok7777@gmail.com>
2023-06-27 00:51:24 +0900

    🚀 0단계 - 리뷰 사이클 연습 (#778)
    * [docs] edit README.md
    
    * [test] modify access success test to google
    add failure test

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,5 +1,6 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -7,7 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.net.UnknownHostException;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
@@ -15,9 +18,31 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+
+        ExtractableResponse<Response> response = RestAssured
+                .given()
+                .when()
+                .get("https://www.google.com")
+                .then().statusCode(200)
+                .extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
+
+    @DisplayName("구글 페이지 접근 실패 테스트")
+    @Test
+    void accessFailGoogle() {
+        try {
+            ExtractableResponse<Response> response = RestAssured
+                    .given()
+                    .when().get("https://www.failaccesstogoogle.com")
+                    .then().statusCode(500)
+                    .extract();
+        } catch (Exception e) {
+            if (e instanceof UnknownHostException) {
+                return;
+            }
+        }
+    }
+
 }
