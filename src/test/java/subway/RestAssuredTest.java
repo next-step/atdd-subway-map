96ff49c3f966894a5620eeac0190dd5c508fa822
JiSun Lim <jisunlim818@gmail.com>
2023-06-28 00:28:25 +0900

    :white_check_mark: 구글 페이지 접근 테스트 작성 (#788)

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -7,16 +7,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
+import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
+    private static final String GOOGLE_PAGE_URL = "https://www.google.com";
 
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        ExtractableResponse<Response> response = when().get(GOOGLE_PAGE_URL).then().extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
