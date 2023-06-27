f56e9afd6bdf9d824da07000b5dd5340f96f7ddc
MintKim1990 <91534593+MintKim1990@users.noreply.github.com>
2023-06-27 10:34:42 +0900

    test: 구글 페이지 접근 테스트 코드 추가 (#760)
    Co-authored-by: MintKim1990 <rlaalsxo1990@gmail.com>

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,5 +1,6 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -12,11 +13,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
 
+    private static final String GOOGLE_URL = "https://google.com";
+
+
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+
+        ExtractableResponse<Response> response = RestAssured.given()
+                .when()
+                .get(GOOGLE_URL)
+                .then()
+                .extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
