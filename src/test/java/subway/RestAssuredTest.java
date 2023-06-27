cdb2af8c81da8d73124c4560dfcecd35b84591d3
wonseok5893 <57287086+wonseok5893@users.noreply.github.com>
2023-06-27 21:40:47 +0900

    STEP0 구글 페이지 접근 테스트 (#759)
    Co-authored-by: zedd c <zedd.c@kakaocorp.com>

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,13 +1,24 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+
+import static io.restassured.RestAssured.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
@@ -15,9 +26,10 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        final String PATH = "https://google.com";
+        given().baseUri(PATH)
+                .when().get()
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
     }
 }
