95409c84199c08d0ab693b24ea5bec326e145e4e
vvithnne <dear__yoon@naver.com>
2023-06-28 22:50:36 +0900

    🚀 0단계: 리뷰 사이클 연습 (#789)
    * test: test to verify that ok http status code(200) is received when sending request
    
    * refactor: clear unused packages and add newline at end of file

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,13 +1,10 @@
 package subway;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
@@ -15,9 +12,12 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        RestAssured
+                // when: https://google.com 로 요청을 보낼 때
+                .when()
+                    .get("https://google.com")
+                // then: 200 응답 코드가 오는지 검증
+                .then()
+                    .statusCode(HttpStatus.OK.value());
     }
 }
