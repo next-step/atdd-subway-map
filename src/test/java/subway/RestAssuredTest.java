ca51b320035e8c2f00231e4adc054db453817cd3
ChaeHyeon <cohys7@gmail.com>
2023-06-29 01:05:44 +0900

    RestAssured 테스트

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,28 +1,21 @@
 package subway;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class RestAssuredTest {
-    RestTemplate restTemplate = new RestTemplate();
 
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        String url = "https://google.com";
-
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        RestAssured
+                .given()
+                .when()
+                    .get("http://www.google.com")
+                .then()
+                    .statusCode(200);
     }
 }
