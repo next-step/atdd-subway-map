b41ef0585ac3d9b57d32b2a0f9c67a821ff211d4
윤지민(Jimin Yun)/Biz플랫폼개발팀/SKP <jimin.yun@sk.com>
2023-06-28 10:37:32 +0900

    0. 리뷰사이클 연습

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
@@ -15,9 +16,11 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        given()
+                .baseUri("https://google.com")
+                .when().get()
+                .then()
+                .statusCode(HttpStatus.OK.value());
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
