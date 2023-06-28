836f98b2005e604bc4408aec17481b55884329cb
Gangbean <xbox2537@gmail.com>
2023-06-28 21:07:36 +0900

    Refactor: remove hardcoding

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,15 +1,11 @@
 package subway;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import static io.restassured.RestAssured.when;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
@@ -21,6 +17,6 @@ public class RestAssuredTest {
         when().
             get("https://google.com").
         then().
-            statusCode(200);
+            statusCode(HttpStatus.OK.value());
     }
 }
