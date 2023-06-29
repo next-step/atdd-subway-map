c95802d08c095e8fa25e59e3b00540e9e2da165e
garden-iee <garden.iee@navercorp.com>
2023-06-29 18:13:20 +0900

    feature: 구글 페이지 접근 테스트 추가

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,23 +1,35 @@
 package subway;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
+
+    private static final String GOOGLE_PAGE_HOST = "https://google.com";
 
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
         // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
 
+        // given
+        ExtractableResponse<Response> response;
+        RestAssured.baseURI = GOOGLE_PAGE_HOST;
+
+        // when
+        response = given().when().get().then().extract();
+
+        // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
