d75ea6544181e89d2deb755082637c713f1d4c45
hae-gun <newchk610@gmail.com>
2023-06-29 23:58:30 +0900

    test: 0단계 구글 이지 접근 테스트 (#811)

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,11 +1,15 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+
+import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,8 +20,14 @@ public class RestAssuredTest {
     @Test
     void accessGoogle() {
         // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
-
+        ExtractableResponse<Response> response = 구글페이지요청("https://google.com");
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private ExtractableResponse<Response> 구글페이지요청(String url) {
+        return RestAssured
+                .given()
+                .when().get(url)
+                .then().statusCode(200).extract();
     }
 }
