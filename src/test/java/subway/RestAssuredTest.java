d155cef74d614693e5282890a025ebfa9ab9a715
George <oh980225@gmail.com>
2023-06-29 19:04:45 +0900

    "https://google.com"로 요청을 보낼 경우 200응답 코드가 오는지를 검증 (#804)

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,13 +1,10 @@
 package subway;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
@@ -15,9 +12,12 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        RestAssured
+                .given()
+                .log()
+                .all()
+                .when().get("https://google.com")
+                .then()
+                .statusCode(HttpStatus.OK.value());
     }
 }
