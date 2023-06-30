9125a37b249d2ae61c4f04d981f460401fbda868
김설영 <kimsy8979@mrblue.com>
2023-06-30 18:14:02 +0900

    test: 지하철역 삭제 인수테스트 작성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,7 +33,7 @@ public class StationAcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames = 지하철역_목록을_조회한다();
+        List<String> stationNames = 지하철역_목록_이름을_조회한다();
 
         assertThat(stationNames).containsAnyOf(강남역);
     }
@@ -66,14 +65,14 @@ public class StationAcceptanceTest {
         지정된_이름의_지하철역을_생성한다(여의도역);
 
         //when
-        List<String> stationNames = 지하철역_목록을_조회한다();
+        List<String> stationNames = 지하철역_목록_이름을_조회한다();
 
         //then
         assertThat(stationNames).hasSize(2);
         assertThat(stationNames).contains(가양역, 여의도역);
     }
 
-    private List<String> 지하철역_목록을_조회한다() {
+    private List<String> 지하철역_목록_이름을_조회한다() {
         return RestAssured.given().log().all()
                 .when().get("/stations")
                 .then().log().all()
@@ -89,11 +88,42 @@ public class StationAcceptanceTest {
                 .when().post("/stations")
                 .then().log().all();
     }
+
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역 제거")
+    @Test
+    void deleteStationById() {
+        //given
+        String 가양역 = "가양역";
+        지정된_이름의_지하철역을_생성한다(가양역);
 
+        List<Long> stationIds = 지하철역_목록_Id를_조회한다();
+        Long stationId = stationIds.get(0);
+
+        //when
+        지하철역을_삭제한다(stationId);
+
+        //then
+        List<Long> stationIdsAfterDelete = 지하철역_목록_Id를_조회한다();
+
+        assertThat(stationIdsAfterDelete).hasSize(0);
+    }
+
+    private List<Long> 지하철역_목록_Id를_조회한다() {
+        return RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("id", Long.class);
+    }
+
+    private void 지하철역을_삭제한다(Long stationId) {
+        RestAssured.given().log().all()
+                .pathParam("id", stationId)
+                .when().delete("/stations/{id}")
+                .then().log().all();
+    }
 }
