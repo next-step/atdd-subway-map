b634fb2656986c41884890ac5e2d12881680ee63
hellonayeon <hellonykwon@gmail.com>
2023-07-01 00:18:04 +0900

    Feat: RestAssured 를 사용하여 구글 페이지 접근 테스트 작성

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,5 +1,6 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -15,8 +16,15 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
+        RestAssured.baseURI = "https://www.google.com";
+
         // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        ExtractableResponse<Response> response = RestAssured
+            .given()
+            .when()
+            .get("/")
+            .then()
+            .extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
