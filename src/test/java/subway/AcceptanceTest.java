c0f6e88f2489081e2948fb1f6a1a7b0e2a250737
ktae23 <pktpkt8917@gmail.com>
2023-06-30 21:24:12 +0900

    test : add AcceptanceTest Annotation for common setting for RestAssured

---
+++ src/test/java/subway/AcceptanceTest.java
@@ -0,0 +1,26 @@
+package subway;
+
+import io.restassured.RestAssured;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(AcceptanceTest.SetUpRestAssured.class)
+public @interface AcceptanceTest {
+
+    @TestComponent
+    class SetUpRestAssured implements ApplicationListener<ServletWebServerInitializedEvent> {
+
+        @Override
+        public void onApplicationEvent(ServletWebServerInitializedEvent event) {
+            RestAssured.port = event.getWebServer().getPort();
+        }
+    }
+}
