30e35326021f16fbbaa2da680e32976428af1eb3
유기훈 <98267772+62hoon99@users.noreply.github.com>
2023-07-02 07:10:12 +0900

    1단계 지하철역 인수 테스트 작성 (#820)
    * test: 지하철역 목록 조회 인수 테스트 구현
    
    * test: 지하철역 제거 인수 테스트 구현
    
    * refactor: 인수 테스트 중복 되는 기능 메서드로 분리
    
    * refactor: 인수 테스트 지하철역 삭제 메서드 파라미터 station id 값으로 변경

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -27,27 +27,14 @@ public class StationAcceptanceTest {
     @Test
     void createStation() {
         // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "강남역");
-
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+        final String 강남역 = "강남역";
+        ExtractableResponse<Response> response = generateSubwayStation(강남역);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
-        assertThat(stationNames).containsAnyOf("강남역");
+        assertThat(getSubwayStations()).containsAnyOf("강남역");
     }
 
     /**
@@ -56,6 +43,25 @@ public class StationAcceptanceTest {
      * Then 2개의 지하철역을 응답 받는다
      */
     // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 조회한다.")
+    @Test
+    void 지하철역_조회() {
+        //given
+        final String 마들역 = "마들역";
+        final String 노원역 = "노원역";
+
+        ExtractableResponse<Response> 마들역_생성 = generateSubwayStation(마들역);
+        ExtractableResponse<Response> 노원역_생성 = generateSubwayStation(노원역);
+
+        assertThat(마들역_생성.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(노원역_생성.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        //when
+        List<String> names = getSubwayStations();
+
+        //then
+        assertThat(names.size()).isEqualTo(2);
+    }
 
     /**
      * Given 지하철역을 생성하고
@@ -63,5 +69,51 @@ public class StationAcceptanceTest {
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
     // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역 제거한다.")
+    @Test
+    void 지하철역_제거() {
+        //given
+        final String 마들역 = "마들역";
+        ExtractableResponse<Response> 마들역_생성 = generateSubwayStation(마들역);
+
+        assertThat(마들역_생성.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        //when
+        ExtractableResponse<Response> 마들역_삭제 =
+                deleteSubwayStation(마들역_생성.body().jsonPath().getObject("id", Long.class));
+
+        assertThat(마들역_삭제.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        //then
+        assertThat(getSubwayStations()).doesNotContain(마들역);
+    }
+
+    private ExtractableResponse<Response> deleteSubwayStation(Long stationId) {
+        return RestAssured
+                .given().log().all()
+                .pathParam("id", stationId)
+                .when().delete("/stations/{id}")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> generateSubwayStation(String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private List<String> getSubwayStations() {
+        return RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+    }
 
 }
