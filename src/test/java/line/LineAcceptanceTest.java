b1d3952b61e9bf7bf623204f1e28fab27fab7779
owen.qq <owen.qq@kakaocorp.com>
2023-07-01 16:35:41 +0900

    test: 노선 조회 테스트 작성

--- src/test/java/line/LineAcceptanceTest.java
+++ src/test/java/line/LineAcceptanceTest.java
@@ -25,25 +25,54 @@ import subway.line.LineCreateRequest;
 @SpringBootTest(classes = SubwayApplication.class, webEnvironment = WebEnvironment.DEFINED_PORT)
 public class LineAcceptanceTest {
     private static final String API_CREATE_LINE = "/lines";
+    private static final String API_GET_LINE = "/lines";
 
     @DisplayName("노선을 생성한다")
     @Test
     void createLine() {
-        // given
-        LineCreateRequest request = new LineCreateRequest("신분당선", "bg-red-600", 1, 2, 10);
-
-        // when
-        ExtractableResponse<Response> createdResponse = RestAssured.given().log().all()
-                                                                   .body(request)
-                                                                   .contentType(MediaType.APPLICATION_JSON_VALUE)
-                                                                   .when().post(API_CREATE_LINE)
-                                                                   .then().log().all()
-                                                                   .extract();
+        ExtractableResponse<Response> createdResponse = 노선생성("신분당선", "bg-red-600", 1, 2, 10);
 
         // then
         assertThat(createdResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(createdResponse.jsonPath().getString("name")).isEqualTo("신분당선");
         assertThat(createdResponse.jsonPath().getString("color")).isEqualTo("bg-red-600");
         assertThat(createdResponse.jsonPath().getList("stations.id")).containsSequence(List.of(1, 2));
+    }
+
+    private ExtractableResponse<Response> 노선생성(String name, String color, long upStationId, long downStationId, int distance) {
+        LineCreateRequest request = new LineCreateRequest(name, color, upStationId, downStationId, distance);
+
+        return RestAssured.given().log().all()
+                          .body(request)
+                          .contentType(MediaType.APPLICATION_JSON_VALUE)
+                          .when().post(API_CREATE_LINE)
+                          .then().log().all()
+                          .extract();
+    }
+
+    @DisplayName("지하철 노선을 조회한다")
+    @Test
+    void getLine() {
+        // given
+        ExtractableResponse<Response> createResponse = 노선생성("신분당선", "bg-red-600", 1, 2, 10);
+
+        Long createdLineId = createResponse.jsonPath().getLong("id");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                   .contentType(MediaType.APPLICATION_JSON_VALUE)
+                   .when().get(getLineRequestUrl(createdLineId))
+                   .then().log().all()
+                   .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("name")).isEqualTo("신분당선");
+        assertThat(response.jsonPath().getString("color")).isEqualTo("bg-red-600");
+        assertThat(response.jsonPath().getList("stations.id")).containsSequence(List.of(1, 2));
+    }
+
+    private String getLineRequestUrl(long id) {
+        return API_GET_LINE + "/" + id;
     }
 }
