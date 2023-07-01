325ce3c2b5001c4e053ed96cb0a489be56c50232
pawoo0211 <pawoo0211@naver.com>
2023-07-01 21:38:14 +0900

    [1단계] 지하철 역 목록 조회 인수 테스트 코드 구현

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -3,15 +3,17 @@ package subway;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,19 +24,23 @@ public class StationAcceptanceTest {
     private final String CREATE_STATION_API_URL = "/stations";
     private final String SHOW_STATIONS_API_URL = "/stations";
 
+    static Stream stationNames() {
+        return Stream.of("강남역");
+    }
+
     /**
      * When 지하철역을 생성하면
      * Then 지하철역이 생성된다
      * Then 지하철역 목록 조회 시 생성한 역을 찾을 수 있다
      */
     @DisplayName("지하철역을 생성한다.")
-    @Test
-    void createStation() {
+    @ParameterizedTest
+    @MethodSource("stationNames")
+    void createStation(String stationName) {
         // when
         Map<String, String> parameter = new HashMap<>();
-        String defaultStationName = "강남역";
 
-        parameter.put("name", defaultStationName);
+        parameter.put("name", stationName);
 
         ExtractableResponse<Response> response =
                 RestAssured
@@ -68,7 +74,7 @@ public class StationAcceptanceTest {
                             .jsonPath()
                                 .getList("name", String.class);
 
-        assertThat(stationNames).containsAnyOf(defaultStationName);
+        assertThat(stationNames).containsAnyOf(stationName);
     }
 
     /**
@@ -76,10 +82,31 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
     @DisplayName("지하철역 목록을 조회한다.")
     @Test
     void showStations() {
+        // given
+        List<String> stationNames = Arrays.asList("구디역", "봉천역");
+        for (String stationName : stationNames) {
+            createStation(stationName);
+        }
+
+        // when
+        List<String> response = RestAssured
+                        .given()
+                            .log()
+                                .all()
+                        .when()
+                            .get(SHOW_STATIONS_API_URL)
+                        .then()
+                            .log()
+                                .all()
+                        .extract()
+                            .jsonPath()
+                                .getList("name", String.class);
+
+        // then
+        Assertions.assertThat(response).contains("구디역", "봉천역");
     }
 
     /**
