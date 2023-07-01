9ef0a59827f7aff970179c44a870a9ad01abf81a
Dong Min Seol <sdm3285@gmail.com>
2023-07-02 00:57:22 +0900

    test: 지하철역 목록 조회 인수 테스트 메서드 생성 & 지하철역 제거 인수 테스트 메서드 생성 (#830)

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -3,6 +3,7 @@ package subway;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ import org.springframework.http.MediaType;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import subway.step.StationStep;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,11 +44,7 @@ public class StationAcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
+        List<String> stationNames = StationStep.지하철역_이름_전체조회_요청();
         assertThat(stationNames).containsAnyOf("강남역");
     }
 
@@ -55,13 +53,53 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 전체 목록을 조회한다.")
+    @Test
+    void 지하철역_목록조회() {
+        // given
+        List<String> 역_생성_목록 = List.of("강남역", "사당역");
+        StationStep.지하철역_다중_생성_요청(역_생성_목록);
+
+        // when
+        List<String> 역_목록 =
+            RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/stations")
+                .then().log().all()
+                .extract()
+                .jsonPath()
+                .getList("name", String.class);
+
+        // then
+        Assertions.assertThat(역_목록)
+            .asList()
+            .isEqualTo(역_생성_목록);
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역 삭제")
+    @Test
+    void 지하철역_삭제 () {
+        // given
+        String 역_이름 = "판교역";
+        StationResponse 역 = StationStep.지하철역_생성_요청(역_이름);
+
+        // when
+        RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .pathParam("id", 역.getId())
+            .when().delete("/stations/{id}")
+            .then().log().all();
+
+        // then
+        List<String> stationNames = StationStep.지하철역_이름_전체조회_요청();
+        Assertions.assertThat(stationNames)
+            .asList()
+            .doesNotContain(역_이름);
+    }
 
 }
---
+++ src/test/java/subway/step/StationStep.java
@@ -0,0 +1,53 @@
+package subway.step;
+
+import io.restassured.RestAssured;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import subway.StationResponse;
+
+public class StationStep {
+
+  public static StationResponse 지하철역_생성_요청 (String 역_이름) {
+      return RestAssured.given()
+          .body(Map.of("name", 역_이름))
+          .contentType(MediaType.APPLICATION_JSON_VALUE)
+          .post("/stations")
+          .then()
+          .assertThat()
+          .statusCode(HttpStatus.CREATED.value())
+          .extract()
+          .as(StationResponse.class);
+  }
+
+  public static List<StationResponse> 지하철역_다중_생성_요청 (List<String> 역_목록) {
+    List<StationResponse> responses = new ArrayList<>();
+
+    for (String 역이름 : 역_목록) {
+      responses.add(RestAssured.given()
+          .body(Map.of("name", 역이름))
+          .contentType(MediaType.APPLICATION_JSON_VALUE)
+          .post("/stations")
+          .then()
+          .assertThat()
+          .statusCode(HttpStatus.CREATED.value())
+          .extract()
+          .as(StationResponse.class)
+      );
+    }
+
+    return responses;
+  }
+
+  public static List<String> 지하철역_이름_전체조회_요청 () {
+    return RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().get("/stations")
+            .then().log().all()
+            .extract()
+            .jsonPath()
+            .getList("name", String.class);
+  }
+}
