f53521c55c358cd5676eeafa30a61b2d7f32076c
ALbertIM0427 <guang7226961@naver.com>
2023-07-02 22:05:43 +0900

    refactor: 리뷰 반영

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -13,19 +13,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 
+@DirtiesContext
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class StationAcceptanceTest {
 
-    private static List<String> 지하철역_목록_조회() {
+    private static ExtractableResponse<Response> 지하철역_목록_조회() {
         return RestAssured.given().log().all()
                 .when().get("/stations")
                 .then().log().all()
-                .extract().jsonPath().getList("name", String.class);
+                .extract();
     }
 
-    private static ExtractableResponse<Response> 지할철역을_생성한다(String 역이름) {
+    private static ExtractableResponse<Response> 지하철역을_생성한다(String 역이름) {
         Map<String, String> params = new HashMap<>();
         params.put("name", 역이름);
         return RestAssured.given().log().all()
@@ -36,15 +38,26 @@ public class StationAcceptanceTest {
                 .extract();
     }
 
-    private static void 지할척역_제거한다(int stationId) {
-        RestAssured.given().log().all()
+    private static ExtractableResponse<Response> 지할척역_제거한다(int stationId) {
+        return RestAssured.given().log().all()
                 .when().delete("/stations/" + stationId)
-                .then().log().all();
+                .then().log().all()
+                .extract();
     }
 
-    private static int 지하철역_아이디(ExtractableResponse<Response> response) {
-        return response.jsonPath().get("id");
+
+    private static List<String> 지하철역_목록의_이름을_파싱한다(ExtractableResponse<Response> 지하철역_목록_조회_결과) {
+        return 지하철역_목록_조회_결과.jsonPath().getList("name", String.class);
     }
+
+    private static int 지하철역의_아이디를_파싱한다(ExtractableResponse<Response> 지하철역_조회_결과) {
+        return 지하철역_조회_결과.jsonPath().getInt("id");
+    }
+
+    private static List<Integer> 지하철역_목록에서_아이디를_파싱한다(ExtractableResponse<Response> 지하철역_목록_조회_결과) {
+        return 지하철역_목록_조회_결과.jsonPath().getList("id", Integer.class);
+    }
+
 
     /**
      * When 지하철역을 생성하면
@@ -55,15 +68,16 @@ public class StationAcceptanceTest {
     @Test
     void createStation() {
         // when
-        var response = 지할철역을_생성한다("강남역");
+        var 강남역 = 지하철역을_생성한다("강남역");
 
         // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(강남역.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames = 지하철역_목록_조회();
-        assertThat(stationNames).containsAnyOf("강남역");
+        List<String> 지하철역_목록_이름 = 지하철역_목록의_이름을_파싱한다(지하철역_목록_조회());
+        assertThat(지하철역_목록_이름).containsAnyOf("강남역");
     }
+
 
     /**
      * Given 2개의 지하철역을 생성하고
@@ -74,14 +88,14 @@ public class StationAcceptanceTest {
     @Test
     void searchSubwayStationList() {
         // given
-        지할철역을_생성한다("강남역");
-        지할철역을_생성한다("이수역");
+        지하철역을_생성한다("강남역");
+        지하철역을_생성한다("이수역");
 
         // when
-        List<String> stationNames = 지하철역_목록_조회();
+        List<String> 지하철역_목록_이름 = 지하철역_목록의_이름을_파싱한다(지하철역_목록_조회());
 
         // then
-        assertThat(stationNames).contains("강남역", "이수역");
+        assertThat(지하철역_목록_이름).contains("강남역", "이수역");
     }
 
     /**
@@ -89,21 +103,19 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
     @DisplayName("지하철역 제거")
     @Test
     public void removeSubwayStation() {
         // given
-        var response = 지할철역을_생성한다("강남역");
+        var 강남역 = 지하철역을_생성한다("강남역");
+        int 강남역_아이디 = 지하철역의_아이디를_파싱한다(강남역);
 
         // when
-        지할척역_제거한다(지하철역_아이디(response));
+        지할척역_제거한다(강남역_아이디);
 
         // then
-        List<String> stationNames = 지하철역_목록_조회();
-        assertThat(stationNames).doesNotContain("강남역");
-
+        List<Integer> 지할철역_목록_아이디 = 지하철역_목록에서_아이디를_파싱한다(지하철역_목록_조회());
+        assertThat(지할철역_목록_아이디).doesNotContain(강남역_아이디);
     }
-
 
 }
