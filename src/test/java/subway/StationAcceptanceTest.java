6d60a577071068e1c474abc30a5a1f362816bab5
deserve82 <inth8204@gmail.com>
2023-07-02 21:01:41 +0900

    test : 지하철 노선도 삭제 테스트 추가

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -66,7 +66,30 @@ class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철 역을 삭제한다")
+    @Test
+    void deleteStation() {
+        //given
+        Long savedId = 지하철_노선도_등록_Id_획득("판교역");
+
+        //when
+        ExtractableResponse<Response> response = 지하철_노선도_삭제(savedId);
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        List<StationResponse> stations = 지하철_노선도_리스트_반환();
+        assertThat(stations).isEmpty();
+    }
+
+    private ExtractableResponse<Response> 지하철_노선도_삭제(Long id) {
+        String pathVariable = "/" + id;
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete("/stations" + pathVariable)
+                .then().log().all()
+                .extract();
+    }
+
     private ExtractableResponse<Response> 지하철_노선도_등록(String name) {
         Map<String, String> params = new HashMap<>();
         params.put("name", name);
@@ -76,6 +99,10 @@ class StationAcceptanceTest {
                 .when().post("/stations")
                 .then().log().all()
                 .extract();
+    }
+
+    private Long 지하철_노선도_등록_Id_획득(String name) {
+        return 지하철_노선도_등록(name).jsonPath().getLong("id");
     }
 
     private ExtractableResponse<Response> 지하철_노선도_조회() {
