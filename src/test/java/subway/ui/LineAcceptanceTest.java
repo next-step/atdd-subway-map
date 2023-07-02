0a2c8b799d7a24f91a10f017c4c843049fdd449e
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-07-02 23:17:32 +0900

    test: 지하철노선 목록 조회 테스트 구현

--- src/test/java/subway/ui/LineAcceptanceTest.java
+++ src/test/java/subway/ui/LineAcceptanceTest.java
@@ -47,6 +47,48 @@ public class LineAcceptanceTest {
         assertThat(getSubwayLines()).containsExactly(신분당선);
     }
 
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철노선 목록 조회")
+    @Test
+    void showLines() {
+        //given
+        final String 지하철역 = "지하철역";
+        final String 새로운지하철역 = "새로운지하철역";
+        final String 또다른지하철역 = "또다른지하철역";
+
+        final String 신분당선 = "신분당선";
+        LineCreateRequest 신분당선_요청 = new LineCreateRequest(
+                신분당선,
+                "bg-red-600",
+                getIdFromResponse(StationAcceptanceTest.generateSubwayStation(지하철역)),
+                getIdFromResponse(StationAcceptanceTest.generateSubwayStation(새로운지하철역)),
+                10L
+        );
+        ExtractableResponse<Response> 신분당선_응답 = generateSubwayLine(신분당선_요청);
+        assertThat(신분당선_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        final String 분당선 = "분당선";
+        LineCreateRequest 분당선_요청 = new LineCreateRequest(
+                분당선,
+                "bg-green-600",
+                getIdFromResponse(StationAcceptanceTest.generateSubwayStation(지하철역)),
+                getIdFromResponse(StationAcceptanceTest.generateSubwayStation(또다른지하철역)),
+                10L
+        );
+        ExtractableResponse<Response> 분당선_응답 = generateSubwayLine(분당선_요청);
+        assertThat(분당선_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        //when
+        List<String> subwayLines = getSubwayLines();
+
+        //then
+        assertThat(subwayLines).containsOnly(분당선, 신분당선);
+    }
+
     private Long getIdFromResponse(ExtractableResponse<Response> response) {
         return response.jsonPath().getObject("id", Long.class);
     }
