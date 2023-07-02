de9a7c9858ac8853b174ba674aafa7109585a9cf
Jun Jeong <46917538+yyy9942@users.noreply.github.com>
2023-07-02 18:29:41 +0900

    test. 1단계 지하철역 인수테스트 작성 (#841)
    * feat. 1단계 구글 페이지 접근 테스트 작성
    
    * test. 1단계 지하철역 인수테스트 작성
    
    * refactoring: 불필요한 메타 어노테이션 제거
    
    * refactoring: 불필요한 메타 어노테이션 제거

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -1,53 +1,39 @@
 package subway;
 
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
-import java.util.HashMap;
+import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Map;
 
+import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Transactional
 public class StationAcceptanceTest {
     /**
      * When 지하철역을 생성하면
      * Then 지하철역이 생성된다
      * Then 지하철역 목록 조회 시 생성한 역을 찾을 수 있다
      */
-    @DisplayName("지하철역을 생성한다.")
-    @Test
-    void createStation() {
+    @DisplayName("지하철역을 생성하면 생성된 지하철역이 조회된다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"강남역"})
+    void createStation(String name) {
         // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "강남역");
-
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+        TestHelper.createStation(name);
 
         // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-
-        // then
-        List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
-        assertThat(stationNames).containsAnyOf("강남역");
+        assertThat(TestHelper.selectAllStations())
+                .extracting(StationResponse::getName)
+                .contains(name);
     }
 
     /**
@@ -55,13 +41,46 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @Test
+    @DisplayName("지하철역을 생성한 후 목록을 조회하면 생성한 지하철 역이 조회된다.")
+    void selectStations() {
+        // given
+        TestHelper.createStation("강남역");
+        TestHelper.createStation("역삼역");
+
+        // when
+        List<StationResponse> stationResponses = TestHelper.selectAllStations();
+
+        // then
+        assertThat(stationResponses).hasSize(2)
+                .map(StationResponse::getName)
+                .contains("강남역", "역삼역");
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @ParameterizedTest
+    @ValueSource(strings = {"강남역"})
+    @DisplayName("지하철역을 제거하면 목록 조회시 조회되지 않는다")
+    void deleteStation(String name) {
+        // given
+        final StationResponse station = TestHelper.createStation(name);
+
+        //when
+        RestAssured.given().log().all()
+                .when().log().all()
+                    .delete(format("/stations/%d", station.getId()))
+                .then()
+                    .log().all()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+
+        //then
+        assertThat(TestHelper.selectAllStations())
+                .extracting(StationResponse::getName)
+                .allMatch(it -> !it.equals(name));
+    }
 
 }
