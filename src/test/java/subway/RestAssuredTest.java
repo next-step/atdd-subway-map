66bba1cbda444a54c5139005b564f352a183cec6
toongri <37677423+toongri@users.noreply.github.com>
2023-07-02 19:11:58 +0900

    0단계 리뷰사이클 연습 (#848)

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,11 +1,13 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,9 +17,17 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        //given
+        String url = "https://www.google.com";
 
+        //when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get(url)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value()).extract();
+
+        //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
