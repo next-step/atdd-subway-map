a620c64d19633f74ecf55f7c41047b01a611fa14
George <oh980225@gmail.com>
2023-07-03 10:54:27 +0900

    지하철역 목록 조회 인수 테스트 작성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -3,6 +3,7 @@ package subway;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.assertj.core.api.AbstractIntegerAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -55,7 +56,19 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다.")
+    @Test
+    void showStations() {
+        // given
+        지하철역을_생성한다("강남역");
+        지하철역을_생성한다("건대입구역");
+
+        // when
+        List<String> stationNames = 지하철역_목록을_조회한다();
+
+        // then
+        생성한_갯수의_지하철역_목록을_응답한다(stationNames, 2);
+    }
 
     /**
      * Given 지하철역을 생성하고
@@ -64,4 +77,23 @@ public class StationAcceptanceTest {
      */
     // TODO: 지하철역 제거 인수 테스트 메서드 생성
 
+    private static void 생성한_갯수의_지하철역_목록을_응답한다(List<String> stationNames, int createdCount) {
+        assertThat(stationNames.size()).isEqualTo(createdCount);
+    }
+    private static List<String> 지하철역_목록을_조회한다() {
+        return RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+    }
+
+    private static void 지하철역을_생성한다(String stationName) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", stationName);
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all();
+    }
 }
