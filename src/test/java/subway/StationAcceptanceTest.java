a15a7cc61dd393c64dc526a5a6b90f2eb1c19a2c
George <oh980225@gmail.com>
2023-07-03 18:56:31 +0900

    [Step1] 1단계 - 지하철역 인수 테스트 작성 🚀 (#862)
    * 지하철역 목록 조회 인수 테스트 작성
    
    * 지하철역 삭제 인수 테스트 작성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -1,8 +1,10 @@
 package subway;
 
 import io.restassured.RestAssured;
+import io.restassured.config.HeaderConfig;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.assertj.core.api.AbstractIntegerAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -55,13 +57,68 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다.")
+    @Test
+    void showStations() {
+        // given
+        지하철역을_생성한다("강남역");
+        지하철역을_생성한다("건대입구역");
+
+        // when
+        List<String> stationNames = 지하철역_목록을_조회한다();
+
+        // then
+        생성한_갯수의_지하철역_목록을_응답한다(stationNames, 2);
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 삭제한다.")
+    @Test
+    void deleteStation() {
+        // given
+        ExtractableResponse<Response> createdResponse = 지하철역을_생성한다("건대입구역");
 
+        // when
+        지하철역을_삭제한다(createdResponse.header("Location"));
+
+        // then
+        List<String> stationNames = 지하철역_목록을_조회한다();
+        삭제한_역은_조회되지_않는다(stationNames, "건대입구역");
+    }
+
+    private static void 지하철역을_삭제한다(String createdResourceUrl) {
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete(createdResourceUrl)
+                .then().log().all();
+    }
+
+    private static void 삭제한_역은_조회되지_않는다(List<String> stationNames, String deletedName) {
+        assertThat(deletedName).isNotIn(stationNames);
+    }
+
+    private static void 생성한_갯수의_지하철역_목록을_응답한다(List<String> stationNames, int createdCount) {
+        assertThat(stationNames.size()).isEqualTo(createdCount);
+    }
+    private static List<String> 지하철역_목록을_조회한다() {
+        return RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+    }
+
+    private static ExtractableResponse<Response> 지하철역을_생성한다(String stationName) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", stationName);
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
 }
