d9c438bba6a84bb12db142e40c5be490c043cb3b
동성 서 <sds901234@tradlinx.com>
2023-07-03 22:30:57 +0900

    test: 지하철 노선 목록 조회 인수 테스트 추가

--- src/test/java/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/subway/acceptance/LineAcceptanceTest.java
@@ -33,20 +33,7 @@ public class LineAcceptanceTest {
         Long downStationId = 지하철_역_생성_요청("양재역").jsonPath().getLong("id");
 
         // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "신분당선");
-        params.put("color", "bg-red-600");
-        params.put("upStationId", upStationId.toString());
-        params.put("downStationId", downStationId.toString());
-        params.put("distance", "10");
-
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/lines")
-                        .then().log().all()
-                        .extract();
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청("신분당선", "bg-red-600", upStationId, downStationId, 10L);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
@@ -60,4 +47,46 @@ public class LineAcceptanceTest {
         assertThat(lineNames).containsAnyOf("신분당선");
     }
 
+    private static ExtractableResponse<Response> 지하철_노선_생성_요청(String name, String color, Long upStationId, Long downStationId, Long distance) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+        params.put("upStationId", upStationId.toString());
+        params.put("downStationId", downStationId.toString());
+        params.put("distance", distance.toString());
+
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .body(params)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when().post("/lines")
+                        .then().log().all()
+                        .extract();
+        return response;
+    }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철 노선을 목록을 조회 한다.")
+    @Test
+    void getLines() {
+        // given
+        Long station1 = 지하철_역_생성_요청("강남역").jsonPath().getLong("id");
+        Long station2 = 지하철_역_생성_요청("양재역").jsonPath().getLong("id");
+        Long station3 = 지하철_역_생성_요청("역삼역").jsonPath().getLong("id");
+        Long station4 = 지하철_역_생성_요청("선릉역").jsonPath().getLong("id");
+
+        지하철_노선_생성_요청("신분당선", "bg-red-600", station1, station2, 10L);
+        지하철_노선_생성_요청("2호선", "bg-green-600", station3, station4, 10L);
+
+        List<String> lineNames =
+                RestAssured.given().log().all()
+                        .when().get("/lines")
+                        .then().log().all()
+                        .extract().jsonPath().getList("name", String.class);
+        assertThat(lineNames).containsAnyOf("신분당선", "2호선");
+    }
 }
