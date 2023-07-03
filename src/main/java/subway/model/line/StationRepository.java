16bef207a8621edbd19ba8742d7294720e9ddecf
wade.song <wade.song@kakaocorp.com>
2023-07-03 18:58:57 +0900

    [step3] 일부 warning 제거

--- src/main/java/subway/model/line/StationRepository.java
+++ src/main/java/subway/model/line/StationRepository.java
@@ -1,7 +1,6 @@
 package subway.model.line;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import subway.model.line.Station;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
 }
