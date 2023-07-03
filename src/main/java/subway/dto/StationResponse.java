053e8309cfde67b9c33ba168928811d63d584aa4
kimsan <cloudhat17@limited40.com>
2023-07-03 16:53:17 +0900

    레이어드 아키텍처 패턴에 맞게 패키지 생성 및 클래스 이동

--- src/main/java/subway/dto/StationResponse.java
+++ src/main/java/subway/dto/StationResponse.java
@@ -1,4 +1,4 @@
-package subway;
+package subway.dto;
 
 public class StationResponse {
     private Long id;
--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -16,6 +16,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import subway.CommonStep.StationStep;
+import subway.dto.StationResponse;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
