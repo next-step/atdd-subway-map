a119498793f8e624737c4fe99774caa6f3f32c6f
pawoo0211 <pawoo0211@naver.com>
2023-07-03 22:08:58 +0900

    [2단계] 지하철 노션 테스트 코드 격리 코드 추가

--- src/test/java/subway/line/SubwayLineAcceptanceTest.java
+++ src/test/java/subway/line/SubwayLineAcceptanceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import subway.helper.SubwayHelper;
 
 import java.util.ArrayList;
@@ -18,8 +19,10 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 
 @DisplayName("지하철 노선 관련 기능")
+@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SubwayLineAcceptanceTest {
 
