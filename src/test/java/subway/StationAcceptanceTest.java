9c29419892cadad236a090c557551eda0a7a831d
ChaeHyeon <86863306+cohys7@users.noreply.github.com>
2023-07-03 19:17:55 +0900

    🚀 1단계 - 지하철역 인수 테스트 작성 (#863)
    * 지하철역 목록 조회, 지하철역 삭제 인수 테스트 작성
    
    * 리팩토링

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -17,7 +17,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class StationAcceptanceTest {
+class StationAcceptanceTest {
+
     /**
      * When 지하철역을 생성하면
      * Then 지하철역이 생성된다
@@ -26,28 +27,17 @@ public class StationAcceptanceTest {
     @DisplayName("지하철역을 생성한다.")
     @Test
     void createStation() {
-        // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "강남역");
+        String stationName = "강남역";
 
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+        // when
+        ExtractableResponse<Response> response = 지하철역을_생성한다(stationName);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
-        assertThat(stationNames).containsAnyOf("강남역");
+        List<String> stationNames = 지하철역_목록을_조회한다();
+        assertThat(stationNames).containsAnyOf(stationName);
     }
 
     /**
@@ -55,13 +45,81 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다.")
+    @Test
+    void searchStations() {
+        List<String> addStationNames = List.of("강남역", "선릉역");
+
+        // given
+        addStationNames.forEach(stationName -> {
+            지하철역을_생성한다(stationName);
+        });
+
+        // when
+        List<String> stationNames = 지하철역_목록을_조회한다();
+
+        // then
+        assertThat(stationNames).hasSize(2);
+    }
+
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 제거한다.")
+    @Test
+    void removeStations() {
+        // given
+        String stationId = 지하철역을_생성한다("신논현역")
+                .jsonPath()
+                .get("id")
+                .toString();
 
+        // when
+        ExtractableResponse<Response> response = 지하철역을_삭제한다(stationId);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        // then
+        List<String> stationNames = 지하철역_목록을_조회한다();
+        assertThat(stationNames).doesNotContain("신논현역");
+    }
+
+    private static ExtractableResponse<Response> 지하철역을_생성한다(String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+
+        ExtractableResponse<Response> response =
+                RestAssured.given()
+                        .body(params)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when()
+                        .post("/stations")
+                        .then()
+                        .log().all()
+                        .extract();
+        return response;
+    }
+
+    private static ExtractableResponse<Response> 지하철역을_삭제한다(String stationId) {
+        return RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .delete("/stations/" + stationId)
+                .then()
+                .log().all()
+                .extract();
+    }
+
+    private static List<String> 지하철역_목록을_조회한다() {
+        return RestAssured
+                .given()
+                .when().get("/stations")
+                .then()
+                .log().all()
+                .extract()
+                .jsonPath()
+                .getList("name", String.class);
+    }
 }
