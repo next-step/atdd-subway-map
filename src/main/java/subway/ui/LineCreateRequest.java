d3afaec89d3d2af79eb7c80776aadc13540c0b94
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-07-04 22:15:54 +0900

    refactor: RequestBody dto 에 setter 메서드 제거

--- src/main/java/subway/ui/LineCreateRequest.java
+++ src/main/java/subway/ui/LineCreateRequest.java
@@ -19,26 +19,6 @@ public class LineCreateRequest {
         this.distance = distance;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setUpStationId(Long upStationId) {
-        this.upStationId = upStationId;
-    }
-
-    public void setDownStationId(Long downStationId) {
-        this.downStationId = downStationId;
-    }
-
-    public void setDistance(Long distance) {
-        this.distance = distance;
-    }
-
     public String getName() {
         return name;
     }
--- src/main/java/subway/ui/LineUpdateRequest.java
+++ src/main/java/subway/ui/LineUpdateRequest.java
@@ -17,15 +17,8 @@ public class LineUpdateRequest {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getColor() {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
 }
