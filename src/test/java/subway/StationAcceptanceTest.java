214275dbf3a0faf496840ad2d1f3dd1a6594ae97
NaYeon Kwon <hellonykwon@gmail.com>
2023-07-04 21:13:02 +0900

    🚀 1단계 - 지하철역 인수 테스트 작성 (#880)
    * Test: 테스트 포트 설정
    - RestAssured 요청 시 스프링이 기동된 랜덤포트 번호로 요청을 보낼 수 있도록 @BeforeEach 에서 port 설정
    
    * Test: 지하철 목록 조회 인수 테스트 작성
    
    * Test: 지하철 삭제 인수 테스트 작성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -1,23 +1,33 @@
 package subway;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @DisplayName("지하철역 관련 기능")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StationAcceptanceTest {
+
+    @LocalServerPort
+    int port;;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
     /**
      * When 지하철역을 생성하면
      * Then 지하철역이 생성된다
@@ -55,13 +65,75 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다.")
+    @Test
+    void getStations() {
+        // given
+        Map<String, String> station1 = new HashMap<>() {{ put("name", "강남역"); }};
+        Map<String, String> station2 = new HashMap<>() {{ put("name", "잠실역"); }};
+
+        RestAssured.given().log().all()
+            .body(station1)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/stations")
+            .then().log().all()
+            .statusCode(HttpStatus.CREATED.value());
+
+        RestAssured.given().log().all()
+            .body(station2)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/stations")
+            .then().log().all()
+            .statusCode(HttpStatus.CREATED.value());
+
+        // when
+        List<String> stationNames =
+            RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+
+        // then
+        assertThat(stationNames).containsExactly("강남역", "잠실역");
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 제거한다.")
+    @Test
+    void deleteStation() {
+        // given
+        Map<String, String> station = new HashMap<>() {{ put("name", "강남역"); }};
+
+        ExtractableResponse<Response> stationCreateResponse =
+            RestAssured.given().log().all()
+                .body(station)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+
+        assertThat(stationCreateResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        // when
+        RestAssured.given().log().all()
+            .body(station)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().delete(stationCreateResponse.header("Location"))
+            .then().log().all()
+            .statusCode(HttpStatus.NO_CONTENT.value());
+
+        // then
+        List<String> stationNames =
+            RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+
+        assertThat(stationNames).doesNotContainSequence("강남역");
+    }
 
 }
