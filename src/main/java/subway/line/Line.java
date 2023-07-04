ba61d7c1ad081e7413bb1257e30b78513ab4027d
YooInKeun <keun0390@naver.com>
2023-07-04 23:18:51 +0900

    chore: 지하철 이름 unique 제약 조건 추가

--- src/main/java/subway/line/Line.java
+++ src/main/java/subway/line/Line.java
@@ -14,7 +14,7 @@ public class Line {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
