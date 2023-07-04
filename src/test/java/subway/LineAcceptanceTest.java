28d72eea2d5593f8c775a657b48810fb3c9febc6
hellonayeon <hellonykwon@gmail.com>
2023-07-04 23:24:36 +0900

    Test: 지하철 노선 인수 테스트 기본 환경설정

---
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -0,0 +1,21 @@
+package subway;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@DisplayName("지하철 노선 관련 기능")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LineAcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+}
