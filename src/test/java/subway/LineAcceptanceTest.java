fd6c3029d3f849ec2d68d3f5c8e9a285dd0a7598
George <oh980225@gmail.com>
2023-07-05 01:29:13 +0900

    test: 지하철 노선을 조회하는 Test
    "지하철 노선을 조회한다" 인수 테스트 작성했습니다.
    현재 구현은 되어있지 않기에 실패하는 테스트입니다.

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -1,6 +1,8 @@
 package subway;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -49,6 +51,30 @@ public class LineAcceptanceTest {
         생성한_갯수의_지하철_노선_목록을_응답한다(lineNames, 2);
     }
 
+    //    Given 지하철 노선을 생성하고
+    //    When 생성한 지하철 노선을 조회하면
+    //    Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+    @DisplayName("지하철 노선을 조회한다.")
+    @Test
+    void showLine() {
+        // given
+        ExtractableResponse<Response> createdResponse = 지하철_노선을_생성한다("신분당선", "bg-red-600", 1L, 2L, 10L);
+
+        // when
+        ExtractableResponse<Response> selectedResponse = 지하철_노선을_조회한다(createdResponse.header("Location"));
+
+        // then
+        생성한_노선의_정보를_응답한다(createdResponse.as(LineResponse.class), selectedResponse.as(LineResponse.class));
+    }
+
+    private static void 생성한_노선의_정보를_응답한다(LineResponse createdLine, LineResponse selectedLine) {
+        assertThat(createdLine).isEqualTo(selectedLine);
+    }
+
+    private ExtractableResponse<Response> 지하철_노선을_조회한다(String location) {
+        return null;
+    }
+
     private static void 생성한_갯수의_지하철_노선_목록을_응답한다(List<String> lineNames, int createdCount) {
         assertThat(lineNames.size()).isEqualTo(createdCount);
     }
@@ -64,7 +90,7 @@ public class LineAcceptanceTest {
                 .extract().jsonPath().getList("name", String.class);
     }
 
-    private static void 지하철_노선을_생성한다(String name, String color, Long upStationId, Long downStationId, Long distance) {
+    private static ExtractableResponse<Response> 지하철_노선을_생성한다(String name, String color, Long upStationId, Long downStationId, Long distance) {
         LineRequest request = new LineRequest(
                 name,
                 color,
@@ -73,18 +99,14 @@ public class LineAcceptanceTest {
                 distance
         );
 
-        RestAssured.given().log().all()
+        return RestAssured.given().log().all()
                 .body(request)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/lines")
                 .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
     }
-
-    //    Given 지하철 노선을 생성하고
-    //    When 생성한 지하철 노선을 조회하면
-    //    Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
-    //    TODO: 지하철노선 조회 인수 테스트 작성
 
     //    Given 지하철 노선을 생성하고
     //    When 생성한 지하철 노선을 수정하면
