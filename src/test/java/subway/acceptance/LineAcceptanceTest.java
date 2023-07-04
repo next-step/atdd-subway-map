0bbd3b94fbdeefb3dac794f39fd8f46022a07e81
deserve82 <inth8204@gmail.com>
2023-07-04 19:56:14 +0900

    test : 지하철 노선 목록 조회 인수 테스트 추가

--- src/test/java/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/subway/acceptance/LineAcceptanceTest.java
@@ -1,5 +1,6 @@
 package subway.acceptance;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
@@ -17,6 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import subway.line.controller.dto.LineResponse;
+import subway.line.domain.Line;
 
 @DisplayName("지하철 노선 관련 기능")
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -27,6 +29,7 @@ class LineAcceptanceTest {
     void initStations() {
         지하철_역_등록("첫번째역");
         지하철_역_등록("두번째역");
+        지하철_역_등록("세번째역");
     }
 
     /**
@@ -49,6 +52,37 @@ class LineAcceptanceTest {
         assertThat(신분당선.getColor()).isEqualTo("bg-red-600");
         assertThat(신분당선.getStations()).hasSize(2);
     }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철 노선 목록을 조회한다")
+    @Test
+    void showLines() {
+        //given
+        지하철_노선도_등록("신분당선",  "bg-red-600", 1L, 2L, 10);
+        지하철_노선도_등록("7호선",  "bg-red-100", 1L, 3L, 10);
+
+        //when
+        List<LineResponse> lines = 지하철_노선도_조회();
+
+        //then
+        assertThat(lines).hasSize(2);
+        LineResponse 신분당선 = lines.get(0);
+        assertThat(신분당선.getId()).isEqualTo(1L);
+        assertThat(신분당선.getName()).isEqualTo("신분당선");
+        assertThat(신분당선.getColor()).isEqualTo("bg-red-600");
+        assertThat(신분당선.getStations()).hasSize(2);
+
+        LineResponse line7 = lines.get(1);
+        assertThat(line7.getId()).isEqualTo(2L);
+        assertThat(line7.getName()).isEqualTo("7호선");
+        assertThat(line7.getColor()).isEqualTo("bg-red-100");
+        assertThat(line7.getStations()).hasSize(2);
+    }
+
 
     private ExtractableResponse<Response> 지하철_노선도_등록(String name, String color, Long upStationId, Long downStationId, Integer distance) {
         HashMap<String, Object> params = new HashMap<>();
