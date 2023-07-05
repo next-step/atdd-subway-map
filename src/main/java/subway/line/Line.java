ed7bfc5c782ccd37e10336032b77bad899b3d436
bean.kong <kongbean@kakao.com>
2023-07-05 18:16:48 +0900

    line entity, repository  정의

---
+++ src/main/java/subway/line/Line.java
@@ -0,0 +1,22 @@
+package subway.line;
+
+import subway.station.Station;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Line {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String color;
+
+    @OneToMany(mappedBy = "line")
+    private List<Station> stations;
+}
--- src/main/java/subway/station/Station.java
+++ src/main/java/subway/station/Station.java
@@ -1,5 +1,7 @@
 package subway.station;
 
+import subway.line.Line;
+
 import javax.persistence.*;
 
 @Entity
@@ -7,8 +9,13 @@ public class Station {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(length = 20, nullable = false)
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "line_id")
+    private Line line;
 
     public Station() {
     }
