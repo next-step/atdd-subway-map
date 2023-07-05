0fe104d222060ae1af80ad79f373f5ca1127870d
Minsuk kim <alstjrdl951@gmail.com>
2023-07-05 13:48:21 +0900

    feat: 인수 테스트 템플릿 작성

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -40,6 +40,57 @@ public class LineAcceptanceTest extends AcceptanceTest {
         지하철_노선_생성_여부_검증(createdRes, getLineListRes);
     }
 
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다
+     */
+    @DisplayName("지하철 노선 목록을 조회한다")
+    @Test
+    void getLines() {
+        // given
+        // when
+        // then
+    }
+
+    /**
+     * When 지하철 노선을 생성하면
+     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+     */
+    @DisplayName("지하철 노선을 조회한다")
+    @Test
+    void getLine() {
+        // given
+        // when
+        // then
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 수정하면
+     * Then 해당 지하철 노선 정보는 수정된다
+     */
+    @DisplayName("지하철 노선을 수정한다")
+    @Test
+    void changeLine() {
+        // given
+        // when
+        // then
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 삭제하면
+     * Then 해당 지하철 노선 정보는 삭제된다
+     */
+    @DisplayName("지하철 노선을 삭제한다")
+    @Test
+    void deleteLine() {
+        // given
+        // when
+        // then
+    }
+
     private static void 지하철_노선_생성_여부_검증(ExtractableResponse<Response> createdRes, ExtractableResponse<Response> getLineListRes) {
         String lineName = createdRes.jsonPath().getString("name");
         List<String> lineList = getLineListRes.jsonPath().getList("name", String.class);
