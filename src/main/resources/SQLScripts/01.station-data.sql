a03a8b63e7d509fc846f361ce5ab11afa48d03aa
hoon25 <cchoon95@hanmail.net>
2023-07-05 17:00:31 +0900

    fix (fixture-sql): truncate table sql didn't work

--- src/main/resources/SQLScripts/01.station-data.sql
+++ src/main/resources/SQLScripts/01.station-data.sql
@@ -1,3 +1,4 @@
-insert into "STATION" (id, name) values (1, 'station1');
-insert into "STATION" (id, name) values (2, 'station2');
-insert into "STATION" (id, name) values (3, 'station3');
+-- truncate table station;
+insert into station (id, name) values (1, 'station1');
+insert into station (id, name) values (2, 'station2');
+insert into station (id, name) values (3, 'station3');
