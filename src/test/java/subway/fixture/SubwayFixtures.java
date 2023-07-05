b4f516b5b704211924685719c4ec523d4d4830a3
garden-iee <garden.iee@navercorp.com>
2023-07-05 23:19:52 +0900

    인수테스트 격리

--- src/test/java/subway/fixture/SubwayFixtures.java
+++ src/test/java/subway/fixture/SubwayFixtures.java
@@ -8,22 +8,17 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-
 public class SubwayFixtures {
 
-    public static Response createSubwayStation(String name) {
+    public static void createSubwayStation(String name) {
         Map<String, String> params = new HashMap<>();
         params.put("name", name);
 
-        ExtractableResponse<Response> response = given()
+        given()
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .body(params)
             .when().post("/stations")
             .then().statusCode(HttpStatus.CREATED.value())
             .and().extract();
-
-        return (Response) response.body();
     }
 }
