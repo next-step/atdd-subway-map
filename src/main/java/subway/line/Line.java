64df11794c54cfc8e23de0593e2fe5d22fc7b04a
bean.kong <kongbean@kakao.com>
2023-07-06 01:50:37 +0900

    line entity field update 코드 구현

--- src/main/java/subway/line/Line.java
+++ src/main/java/subway/line/Line.java
@@ -1,11 +1,19 @@
 package subway.line;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import subway.station.Station;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Line {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +27,14 @@ public class Line {
 
     @OneToMany(mappedBy = "line")
     private List<Station> stations;
+
+    private int distance;
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public void updateColor(String color) {
+        this.color = color;
+    }
 }
