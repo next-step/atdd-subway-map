8a7266034f24ccd43096ddb291d06384321bffd3
janeljs <jisunlim818@gmail.com>
2023-07-06 06:47:15 +0900

    :sparkles: FixtureMonkeyWrapper 추가
    FixtureMonkey 기본 기능으로는 validation을 적용한 객체 여러개를 생성할 수 없어 custom wrapper를 추가함

---
+++ src/test/java/subway/FixtureMonkeyWrapper.java
@@ -0,0 +1,26 @@
+package subway;
+
+import com.navercorp.fixturemonkey.ArbitraryBuilder;
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import lombok.experimental.UtilityClass;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@UtilityClass
+public class FixtureMonkeyWrapper {
+    private static final FixtureMonkey FIXTURE_MONKEY = FixtureMonkey.builder()
+            .defaultNotNull(true)
+            .build();
+
+    public static <T> List<T> giveMe(ArbitraryBuilder<T> builder, int size) {
+        return Stream.generate(builder::sample)
+                .limit(size)
+                .collect(Collectors.toList());
+    }
+
+    public static FixtureMonkey create() {
+        return FIXTURE_MONKEY;
+    }
+}
