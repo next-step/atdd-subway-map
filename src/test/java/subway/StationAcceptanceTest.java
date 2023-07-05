0a96c16bbd2e7919a9150f284f4e1f9971ec341a
SeokyungKim <kskyung0624@gmail.com>
2023-07-05 14:16:25 +0900

    refactor : 테스트 시나리오 인자로 넣는 값을 상수로 추출한다

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -15,6 +15,10 @@ import static subway.SubwayTestFixture.*;
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class StationAcceptanceTest {
+
+    private static final String 강남역 = "강남역";
+    private static final String 신논현역 = "신논현역";
+
     /**
      * When 지하철역을 생성하면
      * Then 지하철역이 생성된다
@@ -24,7 +28,7 @@ public class StationAcceptanceTest {
     @Test
     void createStation() {
         // when
-        ExtractableResponse<Response> stationCreateResponse = 지하철_역_생성_요청("강남역");
+        ExtractableResponse<Response> stationCreateResponse = 지하철_역_생성_요청(강남역);
 
         // then
         assertThat(stationCreateResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
@@ -34,7 +38,7 @@ public class StationAcceptanceTest {
         List<String> stationNames = stationListResponse
                 .jsonPath().getList("name", String.class);
 
-        assertThat(stationNames).containsAnyOf("강남역");
+        assertThat(stationNames).containsAnyOf(강남역);
     }
 
     /**
@@ -42,13 +46,12 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
     @DisplayName("지하철역 목록을 조회한다")
     @Test
     void getStationList() {
         // when
-        지하철_역_생성_요청("강남역");
-        지하철_역_생성_요청("신논현역");
+        지하철_역_생성_요청(강남역);
+        지하철_역_생성_요청(신논현역);
 
         // given
         ExtractableResponse<Response> response = 지하철_역_목록_조회_요청();
@@ -57,7 +60,7 @@ public class StationAcceptanceTest {
         List<String> stationNames = response
                 .jsonPath().getList("name", String.class);
 
-        assertThat(stationNames).containsExactly("강남역", "신논현역");
+        assertThat(stationNames).containsExactly(강남역, 신논현역);
     }
 
     /**
@@ -65,12 +68,11 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
     @DisplayName("지하철역을 삭제 후 확인한다")
     @Test
     void deleteStation() {
         // when
-        ExtractableResponse<Response> stationCreateResponse = 지하철_역_생성_요청("강남역");
+        ExtractableResponse<Response> stationCreateResponse = 지하철_역_생성_요청(강남역);
 
         // given
         지하철_역_삭제_요청(stationCreateResponse.header("location"));
