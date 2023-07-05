a41ca8de579779a5b65f82c691c4023a5df2c385
George <oh980225@gmail.com>
2023-07-05 16:18:05 +0900

    test: StationAcceptanceTest 테스트 수정
    StationAcceptanceTest 인수테스트가 모든 테스트 메서드를 한번에 돌리면 실패하였습니다.
    @DirtiesContext 를 통해 각 테스트가 더욱 독립적인 환경에서 실행될 수 있도록 수정하였습니다.

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class StationAcceptanceTest {
     /**
      * When 지하철역을 생성하면
