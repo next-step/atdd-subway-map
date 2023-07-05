d786cea38c3d1ba58282c17026625bf5fb277efd
MintKim1990 <91534593+MintKim1990@users.noreply.github.com>
2023-07-05 11:10:05 +0900

    Step1: 지하철역 인수 테스트 작성 (#887)
    * docs: 1단계 요구사항 추가
     * 지하철역 목록 조회 인수 테스트 작성하기
     * 지하철역 삭제 인수 테스트 작성하기
    
    * fix: 패키지 구조 변경
    
    * feat: 지하철역 인수 테스트 코드 추가
     * 지하철역 목록 조회 인수 테스트 코드 추가
     * 지하철 역 삭제 인수 테스트 코드 추가
    
    ---------
    
    Co-authored-by: MintKim1990 <rlaalsxo1990@gmail.com>

--- src/main/java/subway/service/StationService.java
+++ src/main/java/subway/service/StationService.java
@@ -1,7 +1,11 @@
-package subway;
+package subway.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import subway.service.request.StationRequest;
+import subway.service.response.StationResponse;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
