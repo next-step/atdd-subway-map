ff8d07a82f1050af2999d496be1a68a8111e37d8
hellonayeon <hellonykwon@gmail.com>
2023-07-05 22:50:53 +0900

    Test: 지하철 노선을 생성하는 팩토리 메서드 구현 - 노선 이름 리스트 관리 - 테스트에서 사용할 노선 파라미터를 생성해서 리턴

---
+++ src/test/java/subway/factory/LineFactory.java
@@ -0,0 +1,39 @@
+package subway.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LineFactory {
+
+    public static final String[] LINE_NAMES = { "신분당선", "우이신설선", "공항철도선" };
+
+    // {key: 구간 이름, value: 구간값 { key: 구간 필드, value: 구간 필드값 }}
+    private static final Map<String, HashMap<String, String>> LINE_MAP = new HashMap<> ()
+    {{
+            put(LINE_NAMES[0], new HashMap<>() {{
+                put("name", LINE_NAMES[0]);
+                put("color", "bg-red-600");
+                put("upStationId", "1");
+                put("downStationId", "2");
+                put("distance", "10");
+            }});
+            put(LINE_NAMES[1], new HashMap<>() {{
+                put("name", LINE_NAMES[1]);
+                put("color", "bg-green-200");
+                put("upStationId", "2");
+                put("downStationId", "3");
+                put("distance", "20");
+            }});
+            put(LINE_NAMES[2], new HashMap<>() {{
+                put("name", LINE_NAMES[2]);
+                put("color", "bg-blue-200");
+                put("upStationId", "4");
+                put("downStationId", "5");
+                put("distance", "30");
+            }});
+        }};
+
+    public static Map<String, String> create(String name) {
+        return LINE_MAP.get(name);
+    }
+}
