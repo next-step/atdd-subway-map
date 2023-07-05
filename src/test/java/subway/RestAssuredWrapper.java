2d433220a16b228ab77c0b66e9496e81ebd093f7
janeljs <jisunlim818@gmail.com>
2023-07-06 07:46:37 +0900

    :sparkles: 테스트 편의성을 위해 RestAssuredWrapper 추가

---
+++ src/test/java/subway/RestAssuredWrapper.java
@@ -0,0 +1,49 @@
+package subway;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class RestAssuredWrapper {
+    public static ExtractableResponse<Response> get(String path) {
+        return request(path, null, null, HttpMethod.GET);
+    }
+
+    public static ExtractableResponse<Response> get(String path, Map<String, String> queryParams) {
+        return request(path, null, queryParams, HttpMethod.GET);
+    }
+
+    public static ExtractableResponse<Response> post(String path, Object body) {
+        return request(path, body, null, HttpMethod.POST);
+    }
+
+    public static ExtractableResponse<Response> delete(String path) {
+        return request(path, null, null, HttpMethod.DELETE);
+    }
+
+    private static ExtractableResponse<Response> request(String path, Object body, Map<String, String> queryParams, HttpMethod method) {
+        RequestSpecification requestSpecification = given()
+                .log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE);
+
+        if (queryParams != null) {
+            requestSpecification.queryParams(queryParams);
+        }
+
+        if (body != null) {
+            requestSpecification.body(body);
+        }
+
+        return requestSpecification
+                .when().request(method.name(), path)
+                .then()
+                .log().all()
+                .extract();
+    }
+}
