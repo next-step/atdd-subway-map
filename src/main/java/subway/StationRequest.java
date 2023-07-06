a5a981e95ea3e772411b7fb48bf281ee00079891
JiSun Lim <jisunlim818@gmail.com>
2023-07-06 09:38:19 +0900

    🚀 1단계 - 지하철역 인수 테스트 작성 (#913)
    * :wrench: h2 데이터베이스 설정 추가
    
    * :heavy_plus_sign: Lombok, FixtureMonkey, Guava 의존성 추가
    
    * :sparkles: 테스트 베이스 클래스, DB 초기화 클래스 추가
    
    * :sparkles: FixtureMonkeyWrapper 추가
    
    FixtureMonkey 기본 기능으로는 validation을 적용한 객체 여러개를 생성할 수 없어 custom wrapper를 추가함
    
    * :memo: step 1 요구사항 업데이트
    
    * :sparkles: 테스트 편의성을 위해 RestAssuredWrapper 추가
    
    * :art: 개행 추가
    
    * :white_check_mark: 지하철역 목록 조회, 삭제 인수테스트 추가
    
    * :recycle: RestAssuredWrapper를 유틸리티 클래스로 변경
    
    * :recycle: stationRequestArbitraryBuilder를 static 메서드로 변경
    
    * :pencil2: path에 슬래시 빠진 것 추가

--- src/main/java/subway/StationRequest.java
+++ src/main/java/subway/StationRequest.java
@@ -1,9 +1,10 @@
 package subway;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class StationRequest {
     private String name;
-
-    public String getName() {
-        return name;
-    }
 }
