6964614500228250e828f5ecf9c11bc6e418cd9f
YooInKeun <keun0390@naver.com>
2023-07-08 23:45:23 +0900

    chore: 불필요한 코드 제거

--- src/main/java/subway/section/SectionRepository.java
+++ src/main/java/subway/section/SectionRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SectionRepository extends JpaRepository<Section, Long> {
     void deleteAllByLineId(Long lineId);
-
-    Section findSectionByLineIdAndUpStationIdAndDownStationId(Long upStationId, Long downStationId, Long lineId);
 }
