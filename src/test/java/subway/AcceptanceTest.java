c6a2ce5872f019c1fec7e05e152efd38114eff40
SeolYoungKim <nasur4da@gmail.com>
2023-07-09 15:26:33 +0900

    comment: TODO 태그 추가 - EntityManager를 통한 Truncate 시도

--- src/test/java/subway/AcceptanceTest.java
+++ src/test/java/subway/AcceptanceTest.java
@@ -3,6 +3,7 @@ package subway;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
+//TODO: EntityManager를 이용한 Truncate 시도 해보기
 @Sql(scripts = "/beforeclass.sql")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public abstract class AcceptanceTest {
