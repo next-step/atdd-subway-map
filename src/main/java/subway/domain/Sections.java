67a47968926cfbf830bfbb6b71a93c0b6ae5dffa
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-07-09 22:04:07 +0900

    feat: 지하철 노선의 하행 종점역만 삭제 가능하도록 구현

--- src/main/java/subway/domain/Sections.java
+++ src/main/java/subway/domain/Sections.java
@@ -14,6 +14,7 @@ public class Sections {
 
     private static final String ADD_ERROR_MESSAGE = "새로운 구간의 상행역은 해당 노선에 등록되어있는 하행 종점역이어야 합니다.";
     private static final String ADD_DUPLICATE_ERROR_MESSAGE = "새로운 구간의 하행역이 해당 노선에 등록되어있으면 안됩니다.";
+    private static final String NOT_EQUALS_DOWN_END_SECTION_ERROR_MESSAGE = "하행 종점역 구간만 삭제 가능합니다.";
 
     @OneToMany(mappedBy = "lineId", cascade = {CascadeType.ALL}, orphanRemoval = true)
     private List<Section> sections = new ArrayList<>();
@@ -37,6 +38,10 @@ public class Sections {
     }
 
     public void delete(Long stationId) {
-        sections.remove(sections.get(sections.size() - 1));
+        Section downEndSection = sections.get(sections.size() - 1);
+        if (!downEndSection.getDownStation().matchId(stationId)) {
+            throw new IllegalArgumentException(NOT_EQUALS_DOWN_END_SECTION_ERROR_MESSAGE);
+        }
+        sections.remove(downEndSection);
     }
 }
