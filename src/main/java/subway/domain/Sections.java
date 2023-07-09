49f84e1dcf6537b424be6c26d078fedaa73d5177
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-07-09 21:33:21 +0900

    feat: 새로운 구간의 하행역이 해당 노선에 등록되어 있지 않은 역만 가능하도록 구현

--- src/main/java/subway/domain/Sections.java
+++ src/main/java/subway/domain/Sections.java
@@ -13,13 +13,19 @@ import java.util.stream.Stream;
 public class Sections {
 
     private static final String ADD_ERROR_MESSAGE = "새로운 구간의 상행역은 해당 노선에 등록되어있는 하행 종점역이어야 합니다.";
+    private static final String ADD_DUPLICATE_ERROR_MESSAGE = "새로운 구간의 하행역이 해당 노선에 등록되어있으면 안됩니다.";
 
     @OneToMany(mappedBy = "lineId", cascade = {CascadeType.ALL}, orphanRemoval = true)
     private List<Section> sections = new ArrayList<>();
 
     public void add(Section section) {
-        if (!sections.isEmpty() && !sections.get(sections.size() - 1).isDownStation(section.getUpStation())) {
-            throw new IllegalArgumentException(ADD_ERROR_MESSAGE);
+        if (!sections.isEmpty()) {
+            if (!sections.get(sections.size() - 1).isDownStation(section.getUpStation())) {
+                throw new IllegalArgumentException(ADD_ERROR_MESSAGE);
+            }
+            if (getStations().contains(section.getDownStation())) {
+                throw new IllegalArgumentException();
+            }
         }
         sections.add(section);
     }
