afa6fadb880fc0ef173a4114e2e6aa7706cb3759
SeolYoungKim <nasur4da@gmail.com>
2023-07-11 21:45:39 +0900

    test: 테이블 이름 획득하는 메서드가 1회만 수행되도록 변경

--- src/test/java/subway/AcceptanceTest.java
+++ src/test/java/subway/AcceptanceTest.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-import javax.persistence.metamodel.Metamodel;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,14 +13,25 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public abstract class AcceptanceTest {
+    private static List<String> tableNames;
+
     @Autowired
     private EntityManager entityManager;
 
     @Autowired
     private PlatformTransactionManager transactionManager;
 
+    void setTableNames() {
+        if (tableNames == null) {
+            tableNames = entityManager.getMetamodel().getEntities().stream()
+                    .map(entityType -> entityType.getName().toLowerCase())
+                    .collect(Collectors.toList());
+        }
+    }
+
     @BeforeEach
     void setUp() {
+        setTableNames();
         TransactionStatus transaction = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
         try {
@@ -29,9 +39,8 @@ public abstract class AcceptanceTest {
             referentialIntegrityFalse.executeUpdate();
 
             String truncateQuery = "TRUNCATE TABLE %s";
-            Metamodel metamodel = entityManager.getMetamodel();
-            List<Query> truncateQueries = metamodel.getEntities().stream()
-                    .map(entity -> String.format(truncateQuery, entity.getName().toLowerCase()))
+            List<Query> truncateQueries = tableNames.stream()
+                    .map(tableName -> String.format(truncateQuery, tableName))
                     .map(query -> entityManager.createNativeQuery(query))
                     .collect(Collectors.toList());
 
