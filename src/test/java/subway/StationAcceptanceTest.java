d9c8861a53553405ea223cb7dbca3d71bfd157b4
gkfgran5037 <42172353+gkfgran5037@users.noreply.github.com>
2023-07-11 17:22:19 +0900

    fix : create 메서드 생성 후 id 반환하는 방식으로 변경

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -74,23 +74,35 @@ public class StationAcceptanceTest {
         assertThat(stationNames).contains(station1, station2);
     }
 
-    private void create(String name) {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", name);
-
-        RestAssured.given().log().all()
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/stations")
-                .then().log().all()
-                .extract();
-    }
-
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
     // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역 삭제")
+    @Test
+    void deleteStation() {
+        // when
+        String stationName = "강남역";
+        Long stationId = create(stationName);
 
+        Map<String, String> params = new HashMap<>();
+        params.put("name", stationName);
+
+
+
+    }
+
+    private Long create(String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract().jsonPath().getLong("id");
+    }
 }
