15161be1dece28c7b14f9236ad31aecd1870b240
SeokyungKim <kskyung0624@gmail.com>
2023-07-12 17:16:53 +0900

    test : 구간 기능 인수테스트에 대한 Step을 정의한다

--- src/test/java/subway/line/LineAcceptanceTest.java
+++ src/test/java/subway/line/LineAcceptanceTest.java
@@ -15,6 +15,10 @@ import subway.station.StationResponse;
 import static org.assertj.core.api.Assertions.assertThat;
 import static common.Constants.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static subway.line.SectionTestStepDefinition.지하철_구간_생성_요청;
+import static subway.line.SectionTestStepDefinition.지하철_구간_생성_요청_상태_코드_반환;
+import static subway.line.SectionTestStepDefinition.지하철_구간_제거_요청;
+import static subway.line.SectionTestStepDefinition.지하철_구간_제거_요청_상태_코드_반환;
 import static subway.station.StationTestStepDefinition.*;
 import static subway.line.LineTestStepDefinition.*;
 
@@ -140,7 +144,7 @@ public class LineAcceptanceTest {
     // Given 지하철 노선을 생성하고
     // When 해당 노선의 하행 종점역이 아닌 역을 상행선으로 가지는 구간을 추가하면
     // Then 지하철 노선 조회시 추가한 구간을 확인할 수 없다
-    @DisplayName("해당 노선의 하행 종점이 아닌 역을 상행선으로 가지는 구간 추가시 실패한다.")
+    @DisplayName("지하철 노선에 구간 추가시 해당 노선의 하행 종점이 아닌 역을 상행선으로 가진다면 실패한다.")
     @Test
     void createSection_fail_upStationOfSectionDoesNotMatchWithDownEndStationOfLine() {
         // given
@@ -159,12 +163,11 @@ public class LineAcceptanceTest {
     // Given 지하철 노선을 생성하고
     // When 해당 노선에 이미 등록된 역을 하행선으로 가지는 구간을 추가하면
     // Then 응답에서 409 Conflict 상태코드를 받는다
-    @DisplayName("해당 노선에 이미 등록된 역을 하행선으로 가지는 구간 추가시 실패한다.")
+    @DisplayName("지하철 노선에 구간 추가시 해당 노선에 이미 등록된 역을 하행선으로 가지면 실패한다.")
     @Test
     void createSection_fail_anyStationOfSectionDoesNotMatchWithDownEndStationOfLine() {
         // given
         var lineCreateResponse = 지하철_노선_생성_요청(신분당선, "bg-red-600", 지하철역, 새로운지하철역, 10);
-        var stationResponse = 지하철_역_생성_요청(또다른지하철역);
 
         // when
         var statusCode = 지하철_구간_생성_요청_상태_코드_반환(lineCreateResponse.getId(), getDownEndStationId(lineCreateResponse), getUpEndStationId(lineCreateResponse));
@@ -186,7 +189,7 @@ public class LineAcceptanceTest {
             stationResponse.getId());
 
         // when
-        지하철_구간_제거_요청(stationResponse.getId());
+        지하철_구간_제거_요청(lineCreateResponse.getId(), stationResponse.getId());
 
         // then
         var lineResponse = 지하철_노선_조회_요청(lineCreateResponse.getId());
@@ -196,7 +199,7 @@ public class LineAcceptanceTest {
     // Given 지하철 노선에 구간을 추가하고
     // When 하행 종점역이 아닌 역을 제거 요청하면
     // Then 지하철 노선 조회시 추가한 구역이 제거되지 않은 것을 확인할 수 있다.
-    @DisplayName("하행 종점역이 아닌 역을 제거 요청시 실패한다.")
+    @DisplayName("지하철 노선에 구간 제거시 하행 종점역이 아닌 역을 제거 요청하면 실패한다.")
     @Test
     void deleteSection_fail_sectionDoesNotMatchWithDownEndStationOfLine() {
         // given
@@ -206,7 +209,7 @@ public class LineAcceptanceTest {
             stationResponse.getId());
 
         // when
-        지하철_구간_제거_요청(getUpEndStationId(lineCreateResponse));
+        지하철_구간_제거_요청(lineCreateResponse.getId(), getUpEndStationId(lineCreateResponse));
 
         // then
         var lineResponse = 지하철_노선_조회_요청(lineCreateResponse.getId());
@@ -216,14 +219,14 @@ public class LineAcceptanceTest {
     // Given 지하철 노선을 생성하고
     // When 상행 종점역과 하행 종점역만 있는 노선에 구간 삭제 요청하면
     // Then 지하철 노선 조회시 추가한 구역이 제거되지 않은 것을 확인할 수 있다.
-    @DisplayName("상행 종점역과 하행 종점역만 있는 노선에 구간 삭제 요청시 실패한다.")
+    @DisplayName("지하철 노선에 구간 제거시 상행 종점역과 하행 종점역만 있는 노선에 구간이라면 실패한다.")
     @Test
     void deleteSection_fail_sectionOnlyExistsUpAndDownEndStationInLine() {
         // given
         var lineCreateResponse = 지하철_노선_생성_요청(신분당선, "bg-red-600", 지하철역, 새로운지하철역, 10);
 
         // when
-        var statusCode = 지하철_구간_제거_실패_요청_상태_코드_반환(lineCreateResponse.getId(), getUpEndStationId(lineCreateResponse));
+        var statusCode = 지하철_구간_제거_요청_상태_코드_반환(lineCreateResponse.getId(), getUpEndStationId(lineCreateResponse));
 
         // then
         var lineResponse = 지하철_노선_조회_요청(lineCreateResponse.getId());
