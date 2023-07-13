ed668d2aa3cbe2b5608d7f4916769bc11a7202fa
janeljs <jisunlim818@gmail.com>
2023-07-13 10:42:47 +0900

    :bug: 테스트가 실패하여 builder 추가, mapstruct mapper 정상 동작 위해 필요

--- src/main/java/subway/LineResponse.java
+++ src/main/java/subway/LineResponse.java
@@ -1,12 +1,14 @@
 package subway;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class LineResponse {
     private Long id;
     private String name;
