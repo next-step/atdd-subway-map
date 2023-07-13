5679523488ad7cf79dc76b143f81c9fcb002d68d
james.jin <james.jin@kakaocorp.com>
2023-07-13 18:20:53 +0900

    3단계 - 지하철 구간 관리 - 피드백 반영 -- 머지 잘못한 부분 제거

--- src/main/java/subway/entity/Line.java
+++ src/main/java/subway/entity/Line.java
@@ -1,7 +1,6 @@
 package subway.entity;
 
 import javax.persistence.*;
-import java.util.List;
 
 
 @Entity
@@ -39,30 +38,4 @@ public class Line {
     public String getColor() {
         return color;
     }
-<<<<<<< HEAD
-=======
-
-    public Long getUpStationId() {
-        return upStationId;
-    }
-
-    public Long getDownStationId() {
-        return downStationId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Line)) return false;
-        Line line = (Line) o;
-        return Objects.equals(getId(), line.getId()) && Objects.equals(getName(), line.getName()) && Objects.equals(color, line.color) && Objects.equals(upStationId, line.upStationId) && Objects.equals(downStationId, line.downStationId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), color, upStationId, downStationId);
-    }
-
-    public static Line EMPTY = new Line();
->>>>>>> kakao-jamesjin
 }
