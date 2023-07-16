8de39c20d9b869f4d27fe298322b0412fb005cc5
hellonayeon <hellonykwon@gmail.com>
2023-07-16 18:16:43 +0900

    Feature: 구간(Section) 요청응답 DTO 생성

---
+++ src/main/java/subway/dto/SectionResponse.java
@@ -0,0 +1,25 @@
+package subway.dto;
+
+public class SectionResponse {
+
+    private Long id;
+    private Long lineId;
+    private Long upStationId;
+    private Long downStationId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getLineId() {
+        return lineId;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+}
