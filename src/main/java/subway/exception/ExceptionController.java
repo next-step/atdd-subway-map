26d41c0db64e4aeeda30b996801a7454f537264f
pawoo0211 <pawoo0211@naver.com>
2023-07-16 14:56:24 +0900

    [3단계] 예외 처리 Production 코드 초기 구현

---
+++ src/main/java/subway/exception/ExceptionController.java
@@ -0,0 +1,16 @@
+package subway.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity customExceptionHandler(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+}
