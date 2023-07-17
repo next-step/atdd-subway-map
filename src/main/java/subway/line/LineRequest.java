814712e2559cf0cbeab91a7c50a537b05c75b7c0
오진혁 <hyeok7777@gmail.com>
2023-07-17 23:59:24 +0900

    [refactor]  add constructor for updateLine

--- src/main/java/subway/line/LineRequest.java
+++ src/main/java/subway/line/LineRequest.java
@@ -2,13 +2,14 @@ package subway.line;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LineRequest {
 
     private String name;
-
     private String color;
 
     private Long upStationId;
@@ -18,4 +19,8 @@ public class LineRequest {
     private int distance;
 
 
+    public LineRequest(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
 }
