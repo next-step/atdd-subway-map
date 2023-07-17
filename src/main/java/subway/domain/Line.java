810f30b854f0c29c14db1c58741bdad6d32c2a36
DongWooE <ehddn977@naver.com>
2023-07-17 23:46:23 +0900

    [add] 지하철노선 생성 테스트 코드 추가

---
+++ src/main/java/subway/domain/Line.java
@@ -0,0 +1,64 @@
+package subway.domain;
+
+import subway.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Line {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    private String color;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "up_station_id")
+    private Station upStation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "down_station_id")
+    private Station downStation;
+    private Integer distance;
+
+    protected Line() {}
+
+    public Line(String name, String color, Integer distance) {
+        this.name = name;
+        this.color = color;
+        this.distance = distance;
+    }
+
+    public void changeStations(Station upStation, Station downStation){
+        this.upStation = upStation;
+        this.downStation = downStation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+}
---
+++ src/main/java/subway/packet/LineRequest.java
@@ -0,0 +1,35 @@
+package subway.packet;
+
+import subway.domain.Line;
+
+public class LineRequest {
+    private String name;
+    private String color;
+    private Long upStationId;
+    private Long downStationId;
+    private Integer distance;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Line toEntity(){
+        return new Line(this.name, this.color, this.distance);
+    }
+}
