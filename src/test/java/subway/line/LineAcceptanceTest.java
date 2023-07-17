291a695a55f6d467b1125aa581263537655c8473
George <oh980225@gmail.com>
2023-07-17 17:58:51 +0900

    refactor: 특정 response 로 변환해주는 메서드 뽑기
    특정 Response 로 변환해주는 메서드를 제네릭을 이용해 만들었습니다.
    그리고 이 메서드는 모든 범위의 테스트에서 사용가능할 것 같아서,
    따로 공통 사용을 위한 SubwayTestUtil를 만들었습니다.

--- src/test/java/subway/line/LineAcceptanceTest.java
+++ src/test/java/subway/line/LineAcceptanceTest.java
@@ -20,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static subway.SubwayTestUtil.toSpecificResponse;
 
 @DisplayName("지하철 노선 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -70,7 +71,7 @@ class LineAcceptanceTest {
         ExtractableResponse<Response> selectedResponse = 지하철_노선을_조회한다(createdResponse.header("Location"));
 
         // then
-        생성한_노선의_정보를_응답한다(createdResponse.as(LineResponse.class), selectedResponse.as(LineResponse.class));
+        생성한_노선의_정보를_응답한다(createdResponse, selectedResponse);
     }
 
     //    Given 지하철 노선을 생성하고
@@ -87,7 +88,11 @@ class LineAcceptanceTest {
 
         // then
         ExtractableResponse<Response> selectedResponse = 지하철_노선을_조회한다(createdResponse.header("Location"));
-        조회한_노선은_요청한_정보로_수정된_상태이다(selectedResponse.as(LineResponse.class), "다른분당선", "bg-red-600");
+        조회한_노선은_요청한_정보로_수정된_상태이다(
+                selectedResponse,
+                "다른분당선",
+                "bg-red-600"
+        );
     }
 
     //    Given 지하철 노선을 생성하고
@@ -118,10 +123,10 @@ class LineAcceptanceTest {
                 .statusCode(HttpStatus.NO_CONTENT.value());
     }
 
-    private static void 조회한_노선은_요청한_정보로_수정된_상태이다(LineResponse selectedLineResponse, String updatedName, String updatedColor) {
+    private static void 조회한_노선은_요청한_정보로_수정된_상태이다(ExtractableResponse<Response> selectedResponse, String updatedName, String updatedColor) {
         assertAll(
-                () -> assertThat(updatedName).isEqualTo(selectedLineResponse.getName()),
-                () -> assertThat(updatedColor).isEqualTo(selectedLineResponse.getColor())
+                () -> assertThat(updatedName).isEqualTo(toSpecificResponse(selectedResponse, LineResponse.class).getName()),
+                () -> assertThat(updatedColor).isEqualTo(toSpecificResponse(selectedResponse, LineResponse.class).getColor())
         );
     }
 
@@ -134,8 +139,8 @@ class LineAcceptanceTest {
                 .statusCode(HttpStatus.OK.value());
     }
 
-    private static void 생성한_노선의_정보를_응답한다(LineResponse createdLine, LineResponse selectedLine) {
-        assertThat(createdLine).isEqualTo(selectedLine);
+    private static void 생성한_노선의_정보를_응답한다(ExtractableResponse<Response> createdResponse, ExtractableResponse<Response> selectedResponse) {
+        assertThat(toSpecificResponse(createdResponse, LineResponse.class)).isEqualTo(toSpecificResponse(selectedResponse, LineResponse.class));
     }
 
     private ExtractableResponse<Response> 지하철_노선을_조회한다(String createdResourceUrl) {
--- src/test/java/subway/section/SectionAcceptanceTest.java
+++ src/test/java/subway/section/SectionAcceptanceTest.java
@@ -17,6 +17,7 @@ import subway.section.dto.CreateSectionRequest;
 import subway.station.dto.StationResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static subway.SubwayTestUtil.toSpecificResponse;
 
 @DisplayName("지하철 구간 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -42,7 +43,7 @@ public class SectionAcceptanceTest {
 
         ExtractableResponse<Response> selectedResponse = 지하철_노선을_조회한다();
 
-        추가한_구간이_노선에_포함된다(createdResponse.as(LineResponse.class), selectedResponse.as(LineResponse.class));
+        추가한_구간이_노선에_포함된다(createdResponse, selectedResponse);
     }
 
     /**
@@ -121,12 +122,12 @@ public class SectionAcceptanceTest {
     }
 
     private static Long 노선의_마지막_구간이_아닌_구간의_하행역_Id이다(ExtractableResponse<Response> createdResponse) {
-        return createdResponse.as(LineResponse.class).getStations().get(0).getId();
+        return toSpecificResponse(createdResponse, LineResponse.class).getStations().get(0).getId();
     }
 
     private static ExtractableResponse<Response> 구간이_1개_뿐인_노선이_있다() {
         ExtractableResponse<Response> selectedResponse = 지하철_노선을_조회한다();
-        assertThat(selectedResponse.as(LineResponse.class).getStations().size()).isEqualTo(2);
+        assertThat(toSpecificResponse(selectedResponse, LineResponse.class).getStations().size()).isEqualTo(2);
         return selectedResponse;
     }
 
@@ -140,7 +141,7 @@ public class SectionAcceptanceTest {
     private static void 삭제한_구간의_역이_노선에서_제외된다() {
         ExtractableResponse<Response> selectedResponse = 지하철_노선을_조회한다();
         assertThat(new StationResponse(3L, "또 다른 지하철역"))
-                .isNotIn(selectedResponse.as(LineResponse.class).getStations());
+                .isNotIn(toSpecificResponse(selectedResponse, LineResponse.class).getStations());
     }
 
     private static void 지하철_구간을_삭제한다(String resourceUrl) {
@@ -161,18 +162,18 @@ public class SectionAcceptanceTest {
 
     private static CreateSectionRequest 상행역이_노선의_하행_종점역이_아닌_구간에_대한_요청이_존재한다() {
         ExtractableResponse<Response> selectedResponse = 지하철_노선을_조회한다();
-        Long lineUpStationId = selectedResponse.as(LineResponse.class).getStations().get(0).getId();
+        Long lineUpStationId = toSpecificResponse(selectedResponse, LineResponse.class).getStations().get(0).getId();
         return new CreateSectionRequest(lineUpStationId, 3L, 5L);
     }
 
     private static CreateSectionRequest 하행역이_지하철_노선에_등록된_구간에_대한_요청이_존재한다() {
         ExtractableResponse<Response> selectedResponse = 지하철_노선을_조회한다();
-        Long alreadyRegisteredStationId = selectedResponse.as(LineResponse.class).getStations().get(0).getId();
+        Long alreadyRegisteredStationId = toSpecificResponse(selectedResponse, LineResponse.class).getStations().get(0).getId();
         return new CreateSectionRequest(2L, alreadyRegisteredStationId, 5L);
     }
 
-    private static void 추가한_구간이_노선에_포함된다(LineResponse createdLine, LineResponse selectedLine) {
-        assertThat(createdLine).isEqualTo(selectedLine);
+    private static void 추가한_구간이_노선에_포함된다(ExtractableResponse<Response> createdResponse, ExtractableResponse<Response> selectedResponse) {
+        assertThat(toSpecificResponse(createdResponse, LineResponse.class)).isEqualTo(toSpecificResponse(selectedResponse, LineResponse.class));
     }
 
     private static ExtractableResponse<Response> 지하철_노선을_조회한다() {
