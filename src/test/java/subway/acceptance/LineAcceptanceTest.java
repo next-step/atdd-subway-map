85c85267f93c277c6f642488846be35304144d8e
jjisun <135081458+jjisun@users.noreply.github.com>
2023-07-18 18:14:30 +0900

    🚀 3단계 - 지하철 구간 관리 (#954)
    * test: 지하철 구간 추가, 삭제 인수테스트 작성, 기존 테스트 수정
    
    * feat: 지하철 구간 추가, 삭제 구현
    
    * refactor: 지하철 구간 관리 리팩토링 리뷰 반영

--- src/test/java/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/subway/acceptance/LineAcceptanceTest.java
@@ -1,4 +1,4 @@
-package subway;
+package subway.acceptance;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -11,16 +11,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
-import subway.request.LineModifyRequest;
-import subway.request.LineRequest;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import subway.dto.request.LineModifyRequest;
+import subway.dto.request.LineRequest;
 
 import java.util.ArrayList;
 import java.util.List;
+import subway.fixture.LineFixture;
+import subway.fixture.StationFixture;
 
-import static org.springframework.test.annotation.DirtiesContext.MethodMode.AFTER_METHOD;
-
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 @DisplayName("지하철 노선 관련 기능")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LineAcceptanceTest {
 
     @LocalServerPort
@@ -35,9 +37,9 @@ public class LineAcceptanceTest {
     }
 
     private static void addStationCache() {
-        Long id1 = StationAcceptanceTest.createStation("지하철역").jsonPath().getLong("id");
-        Long id2 = StationAcceptanceTest.createStation("새로운지하철역").jsonPath().getLong("id");
-        Long id3 = StationAcceptanceTest.createStation("또다른지하철역").jsonPath().getLong("id");
+        Long id1 = StationFixture.createStation("지하철역").jsonPath().getLong("id");
+        Long id2 = StationFixture.createStation("새로운지하철역").jsonPath().getLong("id");
+        Long id3 = StationFixture.createStation("또다른지하철역").jsonPath().getLong("id");
 
         stationIds.add(id1);
         stationIds.add(id2);
@@ -48,25 +50,16 @@ public class LineAcceptanceTest {
      * When 지하철 노선을 생성하면
      * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
      */
-    @DirtiesContext(methodMode = AFTER_METHOD)
+
     @DisplayName("지하철노선을 생성한다.")
     @Test
     void createLineAndFindList() {
         //when
-        LineRequest request = getLineRequest1();
-        this.createLine(request);
+        LineRequest request = mockRequest_신분당선();
+        LineFixture.createLine(request);
         //then
         List<String> names = this.findLines().jsonPath().getList("name", String.class);
         Assertions.assertThat(names).contains(request.getName());
-    }
-
-    private static ExtractableResponse<Response> createLine(LineRequest dto) {
-        return RestAssured.given().log().all()
-                .body(dto)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/lines")
-                .then().log().all()
-                .extract();
     }
 
     private static ExtractableResponse<Response> findLines() {
@@ -82,13 +75,12 @@ public class LineAcceptanceTest {
      * When 지하철 노선 목록을 조회하면
      * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.있다
      */
-    @DirtiesContext(methodMode = AFTER_METHOD)
     @DisplayName("지하철노선 목록을 조회한다.")
     @Test
     void create2LineAndFindLines() {
         //when
-        this.createLine(getLineRequest1());
-        this.createLine(getLineRequest2());
+        LineFixture.createLine(mockRequest_신분당선());
+        LineFixture.createLine(mockRequest_분당선());
 
         //then
         List<String> names = this.findLines().jsonPath().getList("name", String.class);
@@ -100,40 +92,30 @@ public class LineAcceptanceTest {
      * When 생성한 지하철 노선을 조회하면
      * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
      */
-    @DirtiesContext(methodMode = AFTER_METHOD)
     @DisplayName("지하철노선 목록을 조회한다.")
     @Test
     void create2LineAndfindLine() {
         //when
-        LineRequest request = getLineRequest1();
-        Long id = this.createLine(request).jsonPath().getLong("id");
+        LineRequest request = mockRequest_신분당선();
+        Long id = LineFixture.createLine(request).jsonPath().getLong("id");
 
         //then
-        String name = this.findLine(id).jsonPath().get("name");
+        String name = LineFixture.findLine(id).jsonPath().get("name");
         Assertions.assertThat(name).isEqualTo(request.getName());
     }
 
-
-    private static ExtractableResponse<Response> findLine(Long id) {
-        return RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/lines/{id}", id)
-                .then().log().all()
-                .extract();
-    }
 
     /**
      * Given 지하철 노선을 생성하고
      * When 생성한 지하철 노선을 수정하면
      * Then 해당 지하철 노선 정보는 수정된다
      */
-    @DirtiesContext(methodMode = AFTER_METHOD)
     @DisplayName("지하철노선을 수정한다.")
     @Test
     void createLineAndModifyLine() {
         //given
-        LineRequest request = getLineRequest1();
-        Long id = this.createLine(request).jsonPath().getLong("id");
+        LineRequest request = mockRequest_신분당선();
+        Long id = LineFixture.createLine(request).jsonPath().getLong("id");
 
         //when
         LineModifyRequest modify = LineModifyRequest.builder()
@@ -141,20 +123,11 @@ public class LineAcceptanceTest {
                 .color("bg-red-600")
                 .build();
 
-        this.modifyLine(id, modify);
+        LineFixture.modifyLine(id, modify);
 
         //then
-        String name = findLine(id).jsonPath().get("name");
+        String name = LineFixture.findLine(id).jsonPath().get("name");
         Assertions.assertThat(name).isEqualTo(modify.getName());
-    }
-
-    private static ExtractableResponse<Response> modifyLine(Long id, LineModifyRequest request) {
-        return RestAssured.given()
-                .body(request).log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().put("/lines/{id}", id)
-                .then().log().all()
-                .extract();
     }
 
     /**
@@ -162,13 +135,12 @@ public class LineAcceptanceTest {
      * When 생성한 지하철 노선을 삭제하면
      * Then 해당 지하철 노선 정보는 삭제된다
      */
-    @DirtiesContext(methodMode = AFTER_METHOD)
     @DisplayName("지하철노선을 삭제한다.")
     @Test
     void createLineAndDeleteLine() {
         //when
-        LineRequest request = getLineRequest1();
-        Long id = this.createLine(request).jsonPath().getLong("id");
+        LineRequest request = mockRequest_신분당선();
+        Long id = LineFixture.createLine(request).jsonPath().getLong("id");
 
         deleteLine(id);
 
@@ -185,7 +157,7 @@ public class LineAcceptanceTest {
                 .extract();
     }
 
-    private LineRequest getLineRequest1() {
+    private LineRequest mockRequest_신분당선() {
         String 신분당선 = "신분당선";
         return LineRequest.builder()
                 .name(신분당선)
@@ -196,7 +168,7 @@ public class LineAcceptanceTest {
                 .build();
     }
 
-    private LineRequest getLineRequest2() {
+    private LineRequest mockRequest_분당선() {
         String 분당선 = "분당선";
         return LineRequest.builder()
                 .name(분당선)
