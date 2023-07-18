c8723430a4f24701a8c94a5d905195a8fda88a46
YooInKeun <keun0390@naver.com>
2023-07-19 07:42:29 +0900

    refactor: station 조회, 메소드로 분리

--- src/main/java/subway/line/LineService.java
+++ src/main/java/subway/line/LineService.java
@@ -21,14 +21,10 @@ public class LineService {
 
     @Transactional
     public LineResponse saveLine(LineRequest request) {
-        Station upStation = stationRepository.findById(request.getUpStationId())
-                .orElseThrow(() -> new IllegalStateException(String.format("상행종점역을 찾을 수 없습니다. (upStationId: %d)", request.getUpStationId())));
-
-        Station downStation = stationRepository.findById(request.getDownStationId())
-                .orElseThrow(() -> new IllegalStateException(String.format("하행종점역을 찾을 수 없습니다. (downStationId: %d)", request.getDownStationId())));
+        Station upStation = findStation(request.getUpStationId());
+        Station downStation = findStation(request.getDownStationId());
 
         Line line = lineRepository.save(new Line(request.getName(), request.getColor(), upStation, downStation, request.getDistance()));
-
         return LineResponse.of(line);
     }
 
@@ -48,11 +44,8 @@ public class LineService {
         Line line = lineRepository.findById(id)
                 .orElseThrow(() -> new IllegalStateException(String.format("지하철 노선을 찾을 수 없습니다. (id: %d)", id)));
 
-        Station upStation = stationRepository.findById(request.getUpStationId())
-                .orElseThrow(() -> new IllegalStateException(String.format("상행종점역을 찾을 수 없습니다. (upStationId: %d)", request.getUpStationId())));
-
-        Station downStation = stationRepository.findById(request.getDownStationId())
-                .orElseThrow(() -> new IllegalStateException(String.format("하행종점역을 찾을 수 없습니다. (downStationId: %d)", request.getDownStationId())));
+        Station upStation = findStation(request.getUpStationId());
+        Station downStation = findStation(request.getDownStationId());
 
         line.update(request.getName(), request.getColor(), upStation, downStation, request.getDistance());
     }
@@ -67,15 +60,13 @@ public class LineService {
         Line line = lineRepository.findById(lineId)
                 .orElseThrow(() -> new IllegalStateException(String.format("지하철 노선을 찾을 수 없습니다. (id: %d)", lineId)));
 
-        Station upStation = stationRepository.findById(request.getUpStationId())
-                .orElseThrow(() -> new IllegalStateException(String.format("노선의 하행종점역을 찾을 수 없습니다. (downStationId: %d)", request.getUpStationId())));
+        Station upStation = findStation(request.getUpStationId());
 
         if (!line.getDownStationId().equals(upStation.getId())) {
             throw new IllegalStateException(String.format("새로운 구간의 상행역은 해당 노선에 등록되어있는 하행 종점역이 아닙니다. (구간의 upStationId: %d)", request.getUpStationId()));
         }
 
-        Station downStation = stationRepository.findById(request.getDownStationId())
-                .orElseThrow(() -> new IllegalStateException(String.format("새로운 하행종점역을 찾을 수 없습니다. (downStationId: %d)", request.getDownStationId())));
+        Station downStation = findStation(request.getDownStationId());
 
         if (line.contains(downStation)) {
             throw new IllegalStateException(String.format("새로운 구간의 하행역은 해당 노선에 등록되어있는 역일 수 없습니다. (downStationId: %d)", downStation.getId()));
@@ -93,5 +84,10 @@ public class LineService {
                 .orElseThrow(() -> new IllegalStateException(String.format("지하철 노선을 찾을 수 없습니다. (id: %d)", lineId)));
 
         line.deleteSectionByDownStationId(stationId);
+    }
+
+    private Station findStation(Long stationId) {
+        return stationRepository.findById(stationId)
+                .orElseThrow(() -> new IllegalStateException(String.format("역을 찾을 수 없습니다. (stationId: %d)", stationId)));
     }
 }
