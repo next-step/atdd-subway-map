ddaace72fc941e412503e827bdc89734f8e9dba4
Lee seung chan <dltmdcks702@gmail.com>
2024-01-22 17:40:52 +0900

    feat: add rest assured test

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,5 +1,6 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -7,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -15,8 +18,7 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        final ExtractableResponse<Response> response = get("https://google.com").then().extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
