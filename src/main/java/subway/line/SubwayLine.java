5390aa94833866743a676376b8f7a6322949a24d
ian <xonic789@naver.com>
2024-01-23 22:32:47 +0900

    SubwayLine Entity, Repository 구현

---
+++ src/main/java/subway/line/SubwayLine.java
@@ -0,0 +1,30 @@
+package subway.line;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class SubwayLine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private int distance;
+
+    public SubwayLine() {
+    }
+
+
+
+}
