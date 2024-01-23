fd4d2fbb321d9213dd5a4c75b8c8211be9104b10
perhona <perhona@kakao.com>
2024-01-23 12:48:05 +0900

    feat: RestAssured를 활용한 구글 페이지 요청 구현

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -7,17 +7,33 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
+import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
 
-    @DisplayName("구글 페이지 접근 테스트")
+    @DisplayName("응답 객체로 구글 페이지 접근 테스트")
     @Test
-    void accessGoogle() {
+    void accessGoogle_response() {
         // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        // Response 반환 (extract method 사용)
+        ExtractableResponse<Response> response = when()
+                .get("https://google.com")
+                .then()
+                .extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @DisplayName("statusCode method로 구글 페이지 접근 테스트")
+    @Test
+    void accessGoogle_statusCode() {
+        // TODO: 구글 페이지 요청 구현
+        // statusCode method로 검증
+        when()
+                .get("https://google.com")
+                .then()
+                .statusCode(200);
     }
 }
