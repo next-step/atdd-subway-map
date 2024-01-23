f8c4a1c930ee49bf1c5f9f649b04b6a2c43ea104
ian <xonic789@naver.com>
2024-01-23 09:03:55 +0900

    test: 구글 접근 페이지 테스트 로직 작성 - RestAssured 를 사용해서 구글 페이지 접근 했을 때 Http status 200이 나오는 로직 작성

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,5 +1,8 @@
 package subway;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -7,16 +10,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
 
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
         ExtractableResponse<Response> response = null;
+        response = RestAssured
+            .given().log().all()
+            .when()
+            .get("https://www.google.com")
+            .then().log().all()
+            .extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
