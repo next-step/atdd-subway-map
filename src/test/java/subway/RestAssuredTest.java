3749d894017bd7ea69a54fce9650a30e24143c13
LeeJuOh <56071126+LeeJuOh@users.noreply.github.com>
2024-01-23 22:56:50 +0900

    test: 구글 페이지 접근 테스트 (#995)
    주오님! 안녕하세요. 1주차 미션을 함께하게 된 이혜영입니다. 😄 
    한주간 잘부탁드려요!

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,11 +1,13 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,7 +18,16 @@ public class RestAssuredTest {
     @Test
     void accessGoogle() {
         // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        ExtractableResponse<Response> response = RestAssured.
+            given()
+                .log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE).
+            when()
+                .get("https://google.com").
+            then()
+                .log().status()
+                .statusCode(HttpStatus.OK.value()).
+            extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
