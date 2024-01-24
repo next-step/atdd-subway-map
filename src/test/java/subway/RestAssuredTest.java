eb6e0cd397d10fba9516fba34c34370364f6358c
Yoon <fmoni1306@gmail.com>
2024-01-24 20:48:01 +0900

    구글 페이지에 요청을 보내면 status 200을 응답받는 테스트코드 추가

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -15,16 +15,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
 
-    @DisplayName("구글 페이지 접근 테스트")
+    @DisplayName("구글 페이지에 요청을 보내면 status 200을 응답 받는다")
     @Test
     void accessGoogle() {
         // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response =
-            given()
-                .log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-            .when().get("https://www.google.com")
-            .then().log().status().statusCode(HttpStatus.OK.value()).extract();
+        ExtractableResponse<Response> response = when()
+                .get("https://www.google.com")
+                .then()
+                .extract();
+
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
