86a99b3adb57c2966162478729a6f9100684612f
Woony Kim <49712957+woonys@users.noreply.github.com>
2024-01-24 19:14:48 +0900

    [Step 1]1단계 - 지하철역 인수 테스트 작성 (#1002)
    * feat: 지하철역 목록 조회 인수 테스트 메서드 생성
    
    * feat: 지하철역 제거 인수 테스트 메서드 생성 - 실패 테스트
    
    * refactor: 조회 메서드 추가 - 컨트롤러
    
    * feat: id로 지하철역 조회 메서드 생성- 서비스 & 예외 처리
    
    * fix: NoStationException 발생시 http status code 수정
    
    * add: DisplayName 추가

--- src/main/java/subway/StationController.java
+++ src/main/java/subway/StationController.java
@@ -25,6 +25,11 @@ public class StationController {
         return ResponseEntity.ok().body(stationService.findAllStations());
     }
 
+    @GetMapping(value = "/station/{id}")
+    public ResponseEntity<StationResponse> showStation(@PathVariable Long id) {
+        return ResponseEntity.ok().body(stationService.findStationById(id));
+    }
+
     @DeleteMapping("/stations/{id}")
     public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
         stationService.deleteStationById(id);
