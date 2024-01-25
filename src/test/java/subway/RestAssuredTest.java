24e1967326425c11a92c9f39e738414adb419f97
Seyeong Lee <lys200209@naver.com>
2024-01-25 19:12:14 +0900

    test: connect Google (#1009)

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestAssuredTest {
@@ -17,6 +19,12 @@ public class RestAssuredTest {
     void accessGoogle() {
         // TODO: 구글 페이지 요청 구현
         ExtractableResponse<Response> response = null;
+
+        response = given()
+                .when()
+                .get("https://google.com")
+                .then()
+                .extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
