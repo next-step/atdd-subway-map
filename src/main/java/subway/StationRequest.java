b1662745c922124cc7e732036bd363e6a5cc7bf5
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-01-26 20:14:50 +0900

    Step1 (#1024)
    * Docs : 지하철 미션 시나리오 작성
    
    * test : 지하철역 목록 조회 인수 테스트 작성
    
    * test : 지하철역 삭제 인수 테스트 작성하기
    
    * fix : 지하철역 목록 조회 테스트 상태 코드 추가
    
    * refactor : 지하철역 삭제 테스트 상수 재사용

--- src/main/java/subway/StationRequest.java
+++ src/main/java/subway/StationRequest.java
@@ -3,6 +3,14 @@ package subway;
 public class StationRequest {
     private String name;
 
+    public StationRequest() {
+
+    }
+
+    public StationRequest(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -1,27 +1,25 @@
 package subway;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class StationAcceptanceTest {
     /**
-     * When 지하철역을 생성하면
-     * Then 지하철역이 생성된다
-     * Then 지하철역 목록 조회 시 생성한 역을 찾을 수 있다
+     * When 지하철역을 생성하면 Then 지하철역이 생성된다 Then 지하철역 목록 조회 시 생성한 역을 찾을 수 있다
      */
     @DisplayName("지하철역을 생성한다.")
     @Test
@@ -51,17 +49,70 @@ public class StationAcceptanceTest {
     }
 
     /**
-     * Given 2개의 지하철역을 생성하고
-     * When 지하철역 목록을 조회하면
-     * Then 2개의 지하철역을 응답 받는다
+     * Given 2개의 지하철역을 생성하고 When 지하철역 목록을 조회하면 Then 2개의 지하철역을 응답 받는다
      */
     // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @Test
+    @DisplayName("지하철역 목록을 조회한다.")
+    void find_station_list() {
+        // given
+        String 수원역 = "수원역";
+        String 서울역 = "서울역";
+        String 동탄역 = "동탄역";
+
+        createStation(수원역);
+        createStation(서울역);
+        createStation(동탄역);
+
+        // when
+        JsonPath jsonPath = RestAssured
+                .given().log().all()
+                .when().get("/stations")
+                .then().log().all().statusCode(HttpStatus.OK.value())
+                .extract().jsonPath();
+
+        List<String> stationNames = jsonPath.getList("name", String.class);
+
+        // then
+        assertThat(stationNames).hasSize(3);
+        assertThat(stationNames).contains(수원역, 서울역, 동탄역);
+    }
+
+    private void createStation(String name) {
+        RestAssured
+                .given().body(new StationRequest(name)).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().statusCode(HttpStatus.CREATED.value());
+    }
 
     /**
-     * Given 지하철역을 생성하고
-     * When 그 지하철역을 삭제하면
-     * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
+     * Given 지하철역을 생성하고 When 그 지하철역을 삭제하면 Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
     // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @Test
+    @DisplayName("원하는 지하철역을 제거을 제거한다.")
+    void delete_station() {
+        // given
+        String 수원역 = "수원역";
+        String 서울역 = "서울역";
+        long givenStationId = 1;
 
+        createStation(수원역);
+        createStation(서울역);
+
+        // when
+        RestAssured
+                .given().log().all()
+                .when().delete("/stations/" + givenStationId)
+                .then().log().all().statusCode(HttpStatus.NO_CONTENT.value());
+
+        // then
+        List<String> names = RestAssured
+                .given()
+                .when().get("/stations")
+                .then().extract().jsonPath().getList("name", String.class);
+
+        assertThat(names).hasSize(1);
+        assertThat(names).contains(서울역);
+    }
 }
