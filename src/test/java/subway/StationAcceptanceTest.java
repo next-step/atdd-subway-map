6660f9700fb6fba0e8b745abfa0fb5a25e2b5b8a
seonje1 <154968849+seonje1@users.noreply.github.com>
2024-01-26 18:02:44 +0900

    1단계 - 지하철역 인수 테스트 작성 PR (#1017)
    * test/지하철역 목록조회 인수 테스트 작성
    
    * 인수 테스트 시나리오 리팩토링
    
    - 지하철역 생성 api, 조회api 호출 메서드 추출로 중복제거
    
    * 생성한 지하철역을 삭제하는 인수 테스트 시나리오 작성
    
    * 테스트 케이스 리팩토링
    
    - 역 이름 변수 추출로 중복 제거

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -1,6 +1,5 @@
 package subway;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -13,11 +12,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static io.restassured.RestAssured.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class StationAcceptanceTest {
+
     /**
      * When 지하철역을 생성하면
      * Then 지하철역이 생성된다
@@ -26,28 +27,18 @@ public class StationAcceptanceTest {
     @DisplayName("지하철역을 생성한다.")
     @Test
     void createStation() {
-        // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "강남역");
+        //given
+        String stationName = "강남역";
 
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+        // when
+        ExtractableResponse<Response> response = createStationsApiCall(stationName);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
-        assertThat(stationNames).containsAnyOf("강남역");
+        List<String> stationNames = showStationsApiCall().jsonPath().getList("name", String.class);
+        assertThat(stationNames).containsAnyOf(stationName);
     }
 
     /**
@@ -55,13 +46,64 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("생성한 지하철역 목록 조회")
+    @Test
+    void showStations() {
+        //given
+        String stationNameA = "성수역";
+        createStationsApiCall(stationNameA);
+        String stationNameB = "잠실역";
+        createStationsApiCall(stationNameB);
+
+        //when
+        ExtractableResponse<Response> response = showStationsApiCall();
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("name", String.class))
+                .containsExactly(stationNameA, stationNameB);
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("생성한 지하철역 삭제")
+    @Test
+    void deleteStation() {
+        //given
+        String stationName = "언주역";
+        createStationsApiCall(stationName);
 
+        //when
+        ExtractableResponse<Response> response = given().log().all()
+                .pathParam("id", 1L)
+                .when().delete("/stations/{id}")
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        List<String> stationsNames = showStationsApiCall().jsonPath().getList("name", String.class);
+        assertThat(stationsNames).isEmpty();
+    }
+
+    private ExtractableResponse<Response> createStationsApiCall(String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+
+        return given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> showStationsApiCall() {
+        return given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract();
+    }
 }
