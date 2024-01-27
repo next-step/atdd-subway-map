534f0392eca5cb802a3351abd8ee8e9bfe0613c2
samkimpepper <62634206+samkimpepper@users.noreply.github.com>
2024-01-27 20:15:16 +0900

    [test] 지하철역 조회, 삭제 테스트코드 작성 (#1038)

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -25,7 +25,7 @@ public class StationAcceptanceTest {
      */
     @DisplayName("지하철역을 생성한다.")
     @Test
-    void createStation() {
+    void createStationSuccess() {
         // when
         Map<String, String> params = new HashMap<>();
         params.put("name", "강남역");
@@ -55,13 +55,62 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 조회한다.")
+    @Test
+    void getStationSuccess() {
+        // given
+        List<String> givenNames = List.of(
+                createStation("아차산역").getName(),
+                createStation("장한평역").getName()
+        );
+
+        // when
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("name", String.class)).containsAll(givenNames);
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 삭제한다.")
+    @Test
+    void deleteStationSuccess() {
+        // given
+        Long createdStationId = createStation("광나루역").getId();
 
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .when().delete("/stations/{id}", createdStationId)
+                        .then().log().all()
+                        .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        // when
+        response = RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract();
+
+        // then
+        assertThat(response.jsonPath().getList("id", Long.class)).doesNotContain(createdStationId);
+    }
+
+    private StationResponse createStation(String name) {
+        return RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(Map.of("name", name))
+                .when().post("/stations")
+                .then().extract().as(StationResponse.class);
+    }
 }
