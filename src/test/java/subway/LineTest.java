6dad3ca1a4b13b9ec7af09796ded8d78d914e618
domodazzi <domodazzi@gmail.com>
2024-01-27 22:22:25 +0900

    test : 지하철노선 목록 조회 테스트

--- src/test/java/subway/LineTest.java
+++ src/test/java/subway/LineTest.java
@@ -78,6 +78,34 @@ public class LineTest {
    * When 지하철 노선 목록을 조회하면
    * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
    */
+  @DisplayName("노선 목록을 조회한다.")
+  @Test
+  void getLinesSuccess() {
+    // given
+    final var createdLines = List.of(
+        createLine("신분당선", "bg-red-600", 1, 2, 10),
+        createLine("7호선", "bg-green-300", 3, 4, 20)
+    );
+
+    // when
+    final var response = RestAssured
+        .given().log().all()
+        .port(port)
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when().get("/lines")
+        .then().log().all()
+        .extract();
+
+    final var lineIds = response.jsonPath().getList("id", Long.class);
+
+    // then
+    assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+    // then
+    createdLines.forEach(
+        createdLine -> assertThat(createdLine.getId()).isIn(lineIds)
+    );
+  }
 
   /**
    * Given 지하철 노선을 생성하고
@@ -96,6 +124,29 @@ public class LineTest {
    * When 생성한 지하철 노선을 삭제하면
    * Then 해당 지하철 노선 정보는 삭제된다
    */
+
+  private LineResponse createLine(
+      final String name,
+      final String color,
+      final Integer upStationId,
+      final Integer downStationId,
+      final Integer distance
+  ) {
+    final var params = new HashMap<>();
+    params.put("name", name);
+    params.put("color", color);
+    params.put("upStationId", upStationId);
+    params.put("downStationId", downStationId);
+    params.put("distance", distance);
+
+    return RestAssured
+        .given()
+        .body(params)
+        .port(port)
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when().post("/lines")
+        .then().extract().as(LineResponse.class);
+  }
 
   private List<LineResponse> getLines() {
     return Arrays.asList(
