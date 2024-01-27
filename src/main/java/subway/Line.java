f0fa500ef94c03edd7f946b9660b731f8745aac8
g0rani <pjw0405@naver.com>
2024-01-27 15:38:35 +0900

    feat: line entity 작성

---
+++ src/main/java/subway/Line.java
@@ -0,0 +1,25 @@
+package subway;
+
+import javax.persistence.*;
+
+@Entity
+public class Line {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private Long upStationId;
+
+    @Column(nullable = false)
+    private Long downStationId;
+
+    @Column(nullable = false)
+    private Long distance;
+}
