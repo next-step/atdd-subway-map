4ebc9487ebf9ac9e379d4a18f6de5c2d9c30f0c0
g0rani <pjw0405@naver.com>
2024-01-27 17:46:30 +0900

    test: 지하철 노선 목록 조회 test code 작성

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -1,6 +1,7 @@
 package subway;
 
-import org.junit.jupiter.api.BeforeEach;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,19 +35,61 @@ public class LineAcceptanceTest {
         params.put("downStationId", "2");
         params.put("distance", "10");
 
-        given().log().all()
+        callApiCreateLines(params);
+
+        // then
+        List<String> actual = callApiFindLines().jsonPath().getList("name", String.class);
+        String expected = "신분당선";
+        assertThat(actual).containsAnyOf(expected);
+    }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철역들의 목록을 조회한다.")
+    @Test
+    void findLines() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "신분당선");
+        params.put("color", "bg-red-600");
+        params.put("upStationId", "1");
+        params.put("downStationId", "2");
+        params.put("distance", "10");
+        callApiCreateLines(params);
+
+        params = new HashMap<>();
+        params.put("name", "0호선");
+        params.put("color", "bg-red-100");
+        params.put("upStationId", "1");
+        params.put("downStationId", "3");
+        params.put("distance", "10");
+        callApiCreateLines(params);
+
+        // when
+        List<LineResponse> linesResponse = callApiFindLines().jsonPath().getList(".", LineResponse.class);
+
+        // then
+        int actual = linesResponse.size();
+        int expected = 2;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private ExtractableResponse<Response> callApiCreateLines(Map<String, String> params) {
+        return given().log().all()
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/lines")
                 .then().log().all()
                 .extract();
+    }
 
-        // then
-        List<String> actual = given().log().all()
+    private ExtractableResponse<Response> callApiFindLines() {
+        return given().log().all()
                 .when().get("/lines")
                 .then().log().all()
-                .extract().jsonPath().getList("name", String.class);
-        String expected = "신분당선";
-        assertThat(actual).containsAnyOf(expected);
+                .extract();
     }
 }
--- src/test/resources/test-sql/station-insert.sql
+++ src/test/resources/test-sql/station-insert.sql
@@ -1,2 +1,3 @@
 INSERT INTO station (name) VALUES ('강남역');
 INSERT INTO station (name) VALUES ('삼성역');
+INSERT INTO station (name) VALUES ('선릉역');
