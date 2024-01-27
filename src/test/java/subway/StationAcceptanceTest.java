8d966f6d6ec287f6342565ae7df427c474296cd3
kmando01 <ksumin201566@gmail.com>
2024-01-27 22:16:32 +0900

    feat : 지하철역 조회 인수테스트

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -17,7 +17,6 @@ import org.springframework.http.MediaType;
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class StationAcceptanceTest {
-	/** When 지하철역을 생성하면 Then 지하철역이 생성된다 Then 지하철역 목록 조회 시 생성한 역을 찾을 수 있다 */
 	@DisplayName("지하철역을 생성한다.")
 	@Test
 	void create_station() {
@@ -29,46 +28,23 @@ public class StationAcceptanceTest {
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
 		// then
-		List<String> stationNames =
-				RestAssured.given()
-						.log()
-						.all()
-						.when()
-						.get("/stations")
-						.then()
-						.log()
-						.all()
-						.extract()
-						.jsonPath()
-						.getList("name", String.class);
+		List<String> stationNames = getStations();
 		assertThat(stationNames).containsAnyOf("강남역");
 	}
 
-	/** Given 2개의 지하철역을 생성하고 When 지하철역 목록을 조회하면 Then 2개의 지하철역을 응답 받는다 */
-	// TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
-	@DisplayName("지하철역 2개를 생성한다.")
+	@DisplayName("생성한 지하철역을 조회한다.")
 	@Test
-	void createTwoStation() {
+	void get_stations() {
 		// given
 		createStation(createBody("강남역1"));
 		createStation(createBody("강남역2"));
 
 		// when
+		List<String> stationNames = getStations();
 
 		// then
-		List<String> stationNames =
-				RestAssured.given()
-						.log()
-						.all()
-						.when()
-						.get("/stations")
-						.then()
-						.log()
-						.all()
-						.extract()
-						.jsonPath()
-						.getList("name", String.class);
-		assertThat(stationNames).containsAnyOf("강남역");
+		assertThat(stationNames).containsAnyOf("강남역1");
+		assertThat(stationNames).containsAnyOf("강남역2");
 	}
 
 	/**
