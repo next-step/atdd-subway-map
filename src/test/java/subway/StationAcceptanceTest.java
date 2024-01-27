53cc73884cbc1d4cd358ba5ea1f3396531c7718c
Seyeong Lee <lys200209@naver.com>
2024-01-27 21:13:24 +0900

    🚀 1단계 - 지하철역 인수 테스트 작성 (#1042)
    * test: connect Google
    
    * test: mission step1

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -12,12 +12,15 @@ import org.springframework.http.MediaType;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class StationAcceptanceTest {
+
     /**
      * When 지하철역을 생성하면
      * Then 지하철역이 생성된다
@@ -54,8 +57,39 @@ public class StationAcceptanceTest {
      * Given 2개의 지하철역을 생성하고
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
+     * 인수 테스트란, End to End Test 인데, Controller -> Service -> Repository 까지 테스트 흐름이 움직이기 때문에 통합 테스트와 비슷한 성격을 띄는 것 같은데, 맞나...?
      */
     // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다.")
+    @Test
+    void findAllStations() {
+        // Given
+        saveStation("지하철역이름");
+        saveStation("새로운지하철역이름");
+        saveStation("또다른지하철역이름");
+
+        // 조회
+        ExtractableResponse<Response> response = RestAssured
+                .given()
+                .when().get("/stations")
+                .then()
+                .extract();
+
+        List<StationResponse> stationResponses = response.jsonPath().getList(".", StationResponse.class);
+        assertThat(stationResponses.size()).isEqualTo(3);
+    }
+
+    private static void saveStation(String name) {
+        Map<String, String> param = new HashMap<>();
+        param.put("name", name);
+
+        RestAssured.given()
+                .body(param)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
 
     /**
      * Given 지하철역을 생성하고
@@ -63,5 +97,27 @@ public class StationAcceptanceTest {
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
     // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 제거한다.")
+    @Test
+    void deleteStation() {
+        // Given
+        saveStation("지하철역이름");
+        saveStation("새로운지하철역이름");
+        saveStation("또다른지하철역이름");
 
+        // When
+        RestAssured
+                .given().log().all()
+                .when().delete("/stations/1")
+                .then().log().all();
+
+        // then
+        List<StationResponse> stationResponses = RestAssured
+                .given()
+                .when().get("/stations")
+                .then()
+                .extract().jsonPath().getList(".", StationResponse.class);
+
+        assertThat(stationResponses.size()).isEqualTo(2);
+    }
 }
