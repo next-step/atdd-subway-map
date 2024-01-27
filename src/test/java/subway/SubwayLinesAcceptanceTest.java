9da84129b816994929829a7986bc82eb8ba12dca
Lee seung chan <dltmdcks702@gmail.com>
2024-01-27 10:30:31 +0900

    test: add test for find subway line

--- src/test/java/subway/SubwayLinesAcceptanceTest.java
+++ src/test/java/subway/SubwayLinesAcceptanceTest.java
@@ -1,6 +1,7 @@
 package subway;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.ARRAY;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -61,7 +62,30 @@ public class SubwayLinesAcceptanceTest {
         assertThat(subwayLinesNameList).contains(일호선, 이호선);
     }
 
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 조회하면
+     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+     */
+    @Test
+    void 지하철노선_조회() {
+        // Given
+        final ExtractableResponse<Response> createdLineResponse = createSubwayLine(일호선);
+        SubwayLineResponse createdLine = createdLineResponse.as(SubwayLineResponse.class);
 
+        // When
+        final ExtractableResponse<Response> subwayLineGetResponse = RestAssured
+            .given().log().all()
+            .when()
+                .get("/subwayLines/" + createdLine.getId())
+            .then().log().all()
+            .extract();
+        SubwayLineResponse foundLine = subwayLineGetResponse.as(SubwayLineResponse.class);
+
+        // Then
+        assertThat(foundLine.getId()).isEqualTo(createdLine.getId());
+        assertThat(foundLine.getName()).isEqualTo(createdLine.getName());
+    }
 
 
 
