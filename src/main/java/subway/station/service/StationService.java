18070c6dddf1ea1f022adde293232b56e765d480
Jongmin Yoon <jongmin4943@gmail.com>
2024-01-27 23:13:07 +0900

    fix: StationService.createStationResponse 제거
    - StationResponse 에서 직접 변환으로 변경

--- src/main/java/subway/station/service/StationService.java
+++ src/main/java/subway/station/service/StationService.java
@@ -22,12 +22,12 @@ public class StationService {
     @Transactional
     public StationResponse saveStation(StationRequest stationRequest) {
         Station station = stationRepository.save(new Station(stationRequest.getName()));
-        return createStationResponse(station);
+        return StationResponse.from(station);
     }
 
     public List<StationResponse> findAllStations() {
         return stationRepository.findAll().stream()
-                .map(this::createStationResponse)
+                .map(StationResponse::from)
                 .collect(Collectors.toList());
     }
 
@@ -36,10 +36,4 @@ public class StationService {
         stationRepository.deleteById(id);
     }
 
-    private StationResponse createStationResponse(Station station) {
-        return new StationResponse(
-                station.getId(),
-                station.getName()
-        );
-    }
 }
