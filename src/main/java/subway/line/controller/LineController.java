e4b48f35a27f319ff0241c70e6f3078513d755a0
Jongmin Yoon <jongmin4943@gmail.com>
2024-01-28 00:16:29 +0900

    feat: Line 목록 조회 controller api 추가

--- src/main/java/subway/line/controller/LineController.java
+++ src/main/java/subway/line/controller/LineController.java
@@ -7,6 +7,7 @@ import subway.line.service.dto.LineResponse;
 import subway.line.service.LineService;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class LineController {
@@ -20,5 +21,10 @@ public class LineController {
     public ResponseEntity<LineResponse> createLine(@RequestBody final LineCreateRequest createRequest) {
         final LineResponse lineResponse = lineService.saveLine(createRequest);
         return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId())).body(lineResponse);
+    }
+
+    @GetMapping("/lines")
+    public ResponseEntity<List<LineResponse>> showLines() {
+        return ResponseEntity.ok().body(lineService.findAllLines());
     }
 }
