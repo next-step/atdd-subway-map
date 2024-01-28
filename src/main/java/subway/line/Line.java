94031c426110ae755e765d2f7ffe53cbc7a57a2e
ifi999 <kkwkjm4231@gmail.com>
2024-01-28 11:06:45 +0900

    feat: Line Entity 작성

---
+++ src/main/java/subway/line/Line.java
@@ -0,0 +1,72 @@
+package subway.line;
+
+import subway.station.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Line {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String color;
+
+    @ManyToOne
+    @JoinColumn(name = "up_station_id")
+    private Station upStation;
+
+    @ManyToOne
+    @JoinColumn(name = "down_station_id")
+    private Station downStation;
+
+    @Column
+    private Integer distance;
+
+    public Line() {
+    }
+
+    public Line(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", distance=" + distance +
+                '}';
+    }
+
+}
--- src/main/java/subway/station/Station.java
+++ src/main/java/subway/station/Station.java
@@ -1,6 +1,9 @@
 package subway.station;
 
+import subway.line.Line;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Station {
@@ -9,6 +12,12 @@ public class Station {
     private Long id;
     @Column(length = 20, nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "upStation", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Line> linesAsUpStation;
+
+    @OneToMany(mappedBy = "downStation", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Line> linesAsDownStation;
 
     public Station() {
     }
@@ -23,5 +32,21 @@ public class Station {
 
     public String getName() {
         return name;
+    }
+
+    public List<Line> getLinesAsUpStation() {
+        return linesAsUpStation;
+    }
+
+    public List<Line> getLinesAsDownStation() {
+        return linesAsDownStation;
+    }
+
+    @Override
+    public String toString() {
+        return "Station{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
