43d72b4236d22025c719733b5c8d1582d79104c7
qwe5507 <apvmffkdls@gmail.com>
2024-01-28 20:14:17 +0900

    fix: [step2 요구사항 반영] 전체 테스트 시, 실패하는 테스트 수정

--- src/main/resources/db/teardown.sql
+++ src/main/resources/db/teardown.sql
@@ -5,3 +5,4 @@ SET REFERENTIAL_INTEGRITY TRUE; -- 모든 제약조건 활성화
 insert into station (id, name) values (1, '지하철역');
 insert into station (id, name) values (2, '새로운지하철역');
 insert into station (id, name) values (3, '또다른지하철역');
+ALTER TABLE station ALTER COLUMN id RESTART WITH 4;
