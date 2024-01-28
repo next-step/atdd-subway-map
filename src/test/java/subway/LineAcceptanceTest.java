1ea264e7e222d29508b620f64fed2a986c8c0ed7
subin606 <gaemilee606@gmail.com>
2024-01-28 20:54:16 +0900

    지하철노선관리 인수테스트 리팩터링

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -11,10 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
+import subway.controller.dto.LineCreateRequest;
+import subway.controller.dto.LineUpdateRequest;
 
-import java.io.Serializable;
-import java.util.Map;
+import java.util.List;
 
+import static helper.JsonPathUtils.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관련 기능")
@@ -31,31 +33,21 @@ public class LineAcceptanceTest {
      */
     @DisplayName("지하철 노선 생성")
     @Nested
-    class CreateSubwayLine {
+    class 지하철_노선_생성_테스트 {
         // TODO: 유효하지 않은 값에 대한 검증 추가
         @DisplayName("지하철 노선을 생성하면, 생성한 노선을 찾을 수 있다")
         @Test
         void createSubwayLine() {
             // given
-            String lineName = "수인분당선";
-            String color = "bg-yellow-600";
-            long upStationId = 1L;
-            long downStationId = 2L;
-            int distance = 10;
-
-            Map<String, ? extends Serializable> requestBody = Map.of(
-                "name", lineName,
-                "color", color,
-                "upStationId", upStationId,
-                "downStationId", downStationId,
-                "distance", distance
+            LineCreateRequest 수인분당선 = new LineCreateRequest(
+                "수인분당선", "bg-yellow-600", 1L, 2L, 10
             );
 
             // when
-            ExtractableResponse<Response> response = LineAcceptanceTest.createSubwayLine(requestBody);
+            ExtractableResponse<Response> response = LineAcceptanceTest.createSubwayLine(수인분당선);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-            assertThat(response.body().jsonPath().getString("name")).isEqualTo(lineName);
+            assertThat(getStringPath(response.body(), "name")).isEqualTo(수인분당선.getName());
 
             long createdSubwayLineId = getSubwayLineId(response);
             assertThat(createdSubwayLineId).isNotNull();
@@ -75,51 +67,29 @@ public class LineAcceptanceTest {
      */
     @DisplayName("지하철 노선 목록 조회")
     @Nested
-    class GetSubwayLines {
+    class 지하철_노선_목록_조회_테스트 {
 
         @DisplayName("생성한 지하철 노선을 모두 조회할 수 있다")
         @Test
         void will_return_created_subway_lines() {
             // TODO: Request DTO 사용으로 변경?
             // given
-            String firstLineName = "수인분당선";
-            String firstLineColor = "bg-yellow-600";
-            long firstLineUpStationId = 1L;
-            long firstLineDownStationId = 2L;
-            int firstLineDistance = 10;
-
-            String secondLineName = "신분당선";
-            String secondLineColor = "bg-red-600";
-            long secondLineUpStationId = 11L;
-            long secondLineDownStationId = 12L;
-            int secondLineDistance = 5;
-
-            createSubwayLine(
-                Map.of(
-                    "name", firstLineName,
-                    "color", firstLineColor,
-                    "upStationId", firstLineUpStationId,
-                    "downStationId", firstLineDownStationId,
-                    "distance", firstLineDistance
-                )
+            LineCreateRequest 수인분당선 = new LineCreateRequest(
+                "수인분당선", "bg-yellow-600", 1L, 2L, 10
+            );
+            LineCreateRequest 신분당선 = new LineCreateRequest(
+                "신분당선", "bg-red-600", 11L, 12L, 5
             );
 
-            createSubwayLine(
-                Map.of(
-                    "name", secondLineName,
-                    "color", secondLineColor,
-                    "upStationId", secondLineUpStationId,
-                    "downStationId", secondLineDownStationId,
-                    "distance", secondLineDistance
-                )
-            );
+            createSubwayLine(수인분당선);
+            createSubwayLine(신분당선);
 
             // when
             ExtractableResponse<Response> getSubwayLinesResponse = getSubwayLines();
 
             assertThat(getSubwayLinesResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
-            assertThat(getSubwayLinesResponse.body().jsonPath().getList("name"))
-                .containsExactly(firstLineName, secondLineName);
+            assertThat(getListPath(getSubwayLinesResponse.body(), "name", String.class))
+                .containsExactly(수인분당선.getName(), 신분당선.getName());
         }
     }
 
@@ -130,28 +100,17 @@ public class LineAcceptanceTest {
      */
     @DisplayName("지하철 노선 조회")
     @Nested
-    class GetSubwayLine {
+    class 지하철_노선_조회_테스트 {
         // TODO: 찾을 수 없는 노선 ID가 주어지는 경우 검증
         @DisplayName("생성한 지하철 노선을 조회하면 해당 노선의 정보를 응답받는다")
         @Test
         void will_return_subway_line() {
             // given
-            String lineName = "수인분당선";
-            String color = "bg-yellow-600";
-            long upStationId = 1L;
-            long downStationId = 2L;
-            int distance = 10;
-
-            ExtractableResponse<Response> createSubwayResponse = createSubwayLine(
-                Map.of(
-                    "name", lineName,
-                    "color", color,
-                    "upStationId", upStationId,
-                    "downStationId", downStationId,
-                    "distance", distance
-                )
+            LineCreateRequest 수인분당선 = new LineCreateRequest(
+                "수인분당선", "bg-yellow-600", 1L, 2L, 10
             );
 
+            ExtractableResponse<Response> createSubwayResponse = createSubwayLine(수인분당선);
             long createdSubwayLineId = getSubwayLineId(createSubwayResponse);
 
             // when
@@ -159,7 +118,7 @@ public class LineAcceptanceTest {
 
             // then
             assertThat(getSubwayLineResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
-            assertThat(getSubwayLineResponse.response().body().jsonPath().getLong("id")).isEqualTo(createdSubwayLineId);
+            assertThat(getSubwayLineId(getSubwayLineResponse)).isEqualTo(createdSubwayLineId);
         }
     }
 
@@ -170,40 +129,24 @@ public class LineAcceptanceTest {
      */
     @DisplayName("지하철 노선 수정")
     @Nested
-    class UpdateSubwayLine {
+    class 지하철_노선_수정_테스트 {
         // TODO: 유효하지 않은 수정 데이터 검증
         @DisplayName("생성한 지하철 노선을 수정하면 해당 지하철 노선 정보는 수정된다")
         @Test
         void will_return_updated_subway_line_data() {
             // given
-            String lineName = "수인분당선";
-            String color = "bg-yellow-600";
-            long upStationId = 1L;
-            long downStationId = 2L;
-            int distance = 10;
-
-            Map<String, ? extends Serializable> requestBody = Map.of(
-                "name", lineName,
-                "color", color,
-                "upStationId", upStationId,
-                "downStationId", downStationId,
-                "distance", distance
+            LineCreateRequest 수인분당선 = new LineCreateRequest(
+                "수인분당선", "bg-yellow-600", 1L, 2L, 10
             );
 
-            ExtractableResponse<Response> createSubwayResponse = createSubwayLine(requestBody);
-
+            ExtractableResponse<Response> createSubwayResponse = createSubwayLine(수인분당선);
             long createdSubwayLineId = getSubwayLineId(createSubwayResponse);
 
-            String newLineName = "수인분당당선";
-            String newColor = "bg-brown-600";
             // when
-            Map<Object, Object> updateRequestBody = Map.of(
-                "name", newLineName,
-                "color", newColor
-            );
+            LineUpdateRequest updateRequest = new LineUpdateRequest("수인분당당선", "bg-brown-600");
 
             ExtractableResponse<Response> updateResponse = RestAssured.given().log().all()
-                .body(updateRequestBody)
+                .body(updateRequest)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .pathParam("lineId", createdSubwayLineId)
                 .when().put("/lines/{lineId}")
@@ -217,8 +160,10 @@ public class LineAcceptanceTest {
 
             // then
             assertThat(getSubwayLineResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
-            assertThat(getSubwayLineResponse.body().jsonPath().getString("name")).isEqualTo(newLineName);
-            assertThat(getSubwayLineResponse.body().jsonPath().getString("color")).isEqualTo(newColor);
+            assertThat(getStringPath(getSubwayLineResponse.body(), "name"))
+                .isEqualTo(updateRequest.getName());
+            assertThat(getStringPath(getSubwayLineResponse.body(), "color"))
+                .isEqualTo(updateRequest.getColor());
         }
     }
 
@@ -229,28 +174,17 @@ public class LineAcceptanceTest {
      */
     @DisplayName("지하철 노선 삭제")
     @Nested
-    class DeleteSubwayLine {
+    class 지하철_노선_삭제_테스트 {
 
         @DisplayName("생성한 지하철 노선을 삭제하면 해당 지하철 노선 정보는 삭제된다")
         @Test
         void will_delete_subway_line() {
             // given
-            String lineName = "수인분당선";
-            String color = "bg-yellow-600";
-            long upStationId = 1L;
-            long downStationId = 2L;
-            int distance = 10;
-
-            Map<String, ? extends Serializable> requestBody = Map.of(
-                "name", lineName,
-                "color", color,
-                "upStationId", upStationId,
-                "downStationId", downStationId,
-                "distance", distance
+            LineCreateRequest 수인분당선 = new LineCreateRequest(
+                "수인분당선", "bg-yellow-600", 1L, 2L, 10
             );
 
-            ExtractableResponse<Response> createSubwayResponse = createSubwayLine(requestBody);
-
+            ExtractableResponse<Response> createSubwayResponse = createSubwayLine(수인분당선);
             long createdSubwayLineId = getSubwayLineId(createSubwayResponse);
 
             // when
@@ -266,16 +200,16 @@ public class LineAcceptanceTest {
             ExtractableResponse<Response> getSubwayLinesResponse = getSubwayLines();
 
             assertThat(getSubwayLinesResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
-            assertThat(getSubwayLinesResponse.body().jsonPath().getList("id")).noneMatch(id -> id.equals(createdSubwayLineId));
+            assertThat(getSubwayLineIds(getSubwayLinesResponse)).noneMatch(id -> id.equals(createdSubwayLineId));
         }
     }
 
     /** 주어진 지하철 노선 정보를 통해 지하철 노선 생성 요청 후 응답값을 반환합니다 */
     private static ExtractableResponse<Response> createSubwayLine(
-        final Map<String, ? extends Serializable> requestBody
+        final LineCreateRequest request
     ) {
         return RestAssured.given().log().all()
-            .body(requestBody)
+            .body(request)
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .when().post("/lines")
             .then().log().all()
@@ -284,7 +218,12 @@ public class LineAcceptanceTest {
 
     /** 주어진 응답값에서 식별자를 추출해 반환합니다 */
     private long getSubwayLineId(final ExtractableResponse<Response> response) {
-        return response.body().jsonPath().getLong("id");
+        return getLongPath(response.body(), "id");
+    }
+
+    /** 주어진 응답값에서 식별자목록을 추출해 반환합니다 */
+    private List<Long> getSubwayLineIds(final ExtractableResponse<Response> response) {
+        return getListPath(response.body(), "id", Long.class);
     }
 
     /** 모든 지하철 노선을 조회하는 요청 후 응답값을 반환합니다 */
