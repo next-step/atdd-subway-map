3582212d8054dfa2f595cd1ff004c758c2cf2e85
kimjunbo <72437309+kimjunbo@users.noreply.github.com>
2024-01-28 21:58:34 +0900

    🚀 1단계 - 지하철역 인수 테스트 작성 (#1054)
    * test : 지하철역 목록 조회, 지하철역 삭제 테스트 코드 작성
    
    * refact: RestAssured import 추가

---
+++ src/test/java/subway/Endpoint.java
@@ -0,0 +1,39 @@
+package subway;
+
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class Endpoint {
+
+	// 지하철역 생성 Endpoint
+	static ExtractableResponse<Response> createStation(Map<String, String> param) {
+		return RestAssured.given().log().all()
+			.body(param)
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.when().post("/stations")
+			.then().log().all()
+			.extract();
+	}
+
+	// 지하철역 목록 조회 Endpoint
+	static ExtractableResponse<Response> showStations() {
+		return RestAssured.given().log().all()
+			.when().get("/stations")
+			.then().log().all()
+			.extract();
+	}
+
+	// 지하철역 삭제 Endpoint
+	static ExtractableResponse<Response> deleteStation(Long id) {
+		return RestAssured.given().log().all()
+			.pathParam("id", id)
+			.when().delete("/stations/{id}")
+			.then().log().all()
+			.extract();
+	}
+}
