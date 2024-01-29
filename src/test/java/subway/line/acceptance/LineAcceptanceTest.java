7bf44a325599b6646e4f0a3e33eecc6b55e130d3
Jongmin Yoon <jongmin4943@gmail.com>
2024-01-29 21:59:10 +0900

    refactor: LineAcceptanceTest 리팩토링
    - private method `LineApiHelper` 로 공통화
    - 리터럴 변수화

--- src/test/java/subway/line/acceptance/LineAcceptanceTest.java
+++ src/test/java/subway/line/acceptance/LineAcceptanceTest.java
@@ -12,11 +12,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
+import subway.common.LineApiHelper;
+import subway.common.StationApiHelper;
 import subway.line.service.dto.LineResponse;
-import subway.station.acceptance.StationAcceptanceTest;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -27,16 +27,21 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 @ContextConfiguration(classes = TestConfig.class)
 @ExtendWith(AcceptanceTestExtension.class)
 public class LineAcceptanceTest {
-    public static final String LINE_API_PATH = "/lines";
     private Long 지하철역_Id;
     private Long 새로운지하철역_Id;
     private Long 또다른지하철역_Id;
+    private final String 신분당선 = "신분당선";
+    private final String 신분당선_color = "bg-red-600";
+    private final int 신분당선_distance = 10;
+    private final String 분당선 = "분당선";
+    private final String 분당선_color = "bg-green-600";
+    private final int 분당선_distance = 15;
 
     @BeforeEach
     void setUp() {
-        지하철역_Id = createStation("지하철역");
-        새로운지하철역_Id = createStation("새로운지하철역");
-        또다른지하철역_Id = createStation("또다른지하철역");
+        지하철역_Id = RestAssuredHelper.getIdFrom(StationApiHelper.createStation("지하철역"));
+        새로운지하철역_Id = RestAssuredHelper.getIdFrom(StationApiHelper.createStation("새로운지하철역"));
+        또다른지하철역_Id = RestAssuredHelper.getIdFrom(StationApiHelper.createStation("또다른지하철역"));
     }
 
     /**
@@ -48,18 +53,17 @@ public class LineAcceptanceTest {
     @Test
     void createLineTest() {
         // when
-        final ExtractableResponse<Response> response = requestCreateLine("신분당선", "bg-red-600", 지하철역_Id, 새로운지하철역_Id, 10);
+        final ExtractableResponse<Response> response = LineApiHelper.createLine(신분당선, 신분당선_color, 지하철역_Id, 새로운지하철역_Id, 신분당선_distance);
 
         // then
         assertSoftly(softly -> {
             softly.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-            final LineResponse lineResponse = response.as(LineResponse.class);
-            assertLineResponse(lineResponse, "신분당선", "bg-red-600", 지하철역_Id, 새로운지하철역_Id);
+            assertLineResponse(response.as(LineResponse.class), 신분당선, 신분당선_color, 지하철역_Id, 새로운지하철역_Id);
         });
 
         // then
-        final List<String> lineNames = RestAssuredHelper.get(LINE_API_PATH).jsonPath().getList("name", String.class);
-        assertThat(lineNames).containsAnyOf("신분당선");
+        final List<String> lineNames = getLineNames(LineApiHelper.fetchLines());
+        assertThat(lineNames).containsAnyOf(신분당선);
     }
 
     /**
@@ -71,17 +75,17 @@ public class LineAcceptanceTest {
     @Test
     void fetchLinesTest() {
         // given
-        final ExtractableResponse<Response> 신분당선_response = requestCreateLine("신분당선", "bg-red-600", 지하철역_Id, 새로운지하철역_Id, 10);
-        final ExtractableResponse<Response> 분당선_response = requestCreateLine("분당선", "bg-green-600", 지하철역_Id, 또다른지하철역_Id, 10);
+        final ExtractableResponse<Response> 신분당선_response = LineApiHelper.createLine(신분당선, 신분당선_color, 지하철역_Id, 새로운지하철역_Id, 신분당선_distance);
+        final ExtractableResponse<Response> 분당선_response = LineApiHelper.createLine(분당선, 분당선_color, 지하철역_Id, 또다른지하철역_Id, 분당선_distance);
 
         // when
-        final ExtractableResponse<Response> response = RestAssuredHelper.get(LINE_API_PATH);
+        final ExtractableResponse<Response> response = LineApiHelper.fetchLines();
 
         // then
         assertAll(
                 () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
-                , () -> assertResponseData(response, RestAssuredHelper.getIdFrom(신분당선_response), "신분당선", "bg-red-600", 지하철역_Id, 새로운지하철역_Id)
-                , () -> assertResponseData(response, RestAssuredHelper.getIdFrom(분당선_response), "분당선", "bg-green-600", 지하철역_Id, 또다른지하철역_Id)
+                , () -> assertResponseData(response, RestAssuredHelper.getIdFrom(신분당선_response), 신분당선, 신분당선_color, 지하철역_Id, 새로운지하철역_Id)
+                , () -> assertResponseData(response, RestAssuredHelper.getIdFrom(분당선_response), 분당선, 분당선_color, 지하철역_Id, 또다른지하철역_Id)
         );
     }
 
@@ -94,17 +98,16 @@ public class LineAcceptanceTest {
     @Test
     void fetchLineByIdTest() {
         // given
-        final ExtractableResponse<Response> 신분당선_response = requestCreateLine("신분당선", "bg-red-600", 지하철역_Id, 새로운지하철역_Id, 10);
+        final ExtractableResponse<Response> 신분당선_response = LineApiHelper.createLine(신분당선, 신분당선_color, 지하철역_Id, 새로운지하철역_Id, 신분당선_distance);
         final Long 신분당선_id = RestAssuredHelper.getIdFrom(신분당선_response);
 
         // when
-        final ExtractableResponse<Response> response = RestAssuredHelper.getById(LINE_API_PATH, 신분당선_id);
+        final ExtractableResponse<Response> response = LineApiHelper.fetchLineById(신분당선_id);
 
         // then
         assertSoftly(softly -> {
             assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-            final LineResponse lineResponse = response.as(LineResponse.class);
-            assertLineResponse(lineResponse, "신분당선", "bg-red-600", 지하철역_Id, 새로운지하철역_Id);
+            assertLineResponse(response.as(LineResponse.class), 신분당선, 신분당선_color, 지하철역_Id, 새로운지하철역_Id);
         });
     }
 
@@ -117,17 +120,18 @@ public class LineAcceptanceTest {
     @Test
     void updateLineByIdTest() {
         // given
-        final ExtractableResponse<Response> 신분당선_response = requestCreateLine("신분당선", "bg-red-600", 지하철역_Id, 새로운지하철역_Id, 10);
+        final ExtractableResponse<Response> 신분당선_response = LineApiHelper.createLine(신분당선, 신분당선_color, 지하철역_Id, 새로운지하철역_Id, 신분당선_distance);
         final Long 신분당선_id = RestAssuredHelper.getIdFrom(신분당선_response);
 
         // when
-        final ExtractableResponse<Response> response = RestAssuredHelper.put(LINE_API_PATH, 신분당선_id, createLineUpdateRequestFixture("다른분당선", "bg-red-600"));
+        final String 다른분당선 = "다른분당선";
+        final ExtractableResponse<Response> response = LineApiHelper.modifyLine(신분당선_id, 다른분당선, 신분당선_color);
 
         // then
         assertSoftly(softly -> {
             assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-            final LineResponse lineResponse = RestAssuredHelper.getById(LINE_API_PATH, 신분당선_id).as(LineResponse.class);
-            assertLineResponse(lineResponse, "다른분당선", "bg-red-600", 지하철역_Id, 새로운지하철역_Id);
+            final LineResponse lineResponse = LineApiHelper.fetchLineById(신분당선_id).as(LineResponse.class);
+            assertLineResponse(lineResponse, 다른분당선, 신분당선_color, 지하철역_Id, 새로운지하철역_Id);
         });
     }
 
@@ -140,22 +144,27 @@ public class LineAcceptanceTest {
     @Test
     void deleteLineByIdTest() {
         // given
-        final ExtractableResponse<Response> 신분당선_response = requestCreateLine("신분당선", "bg-red-600", 지하철역_Id, 새로운지하철역_Id, 10);
-        final ExtractableResponse<Response> 분당선_response = requestCreateLine("분당선", "bg-green-600", 지하철역_Id, 또다른지하철역_Id, 10);
+        final ExtractableResponse<Response> 신분당선_response = LineApiHelper.createLine(신분당선, 신분당선_color, 지하철역_Id, 새로운지하철역_Id, 신분당선_distance);
+        final ExtractableResponse<Response> 분당선_response = LineApiHelper.createLine(분당선, 분당선_color, 지하철역_Id, 또다른지하철역_Id, 분당선_distance);
         final Long 신분당선_id = RestAssuredHelper.getIdFrom(신분당선_response);
 
         // when
-        final ExtractableResponse<Response> response = RestAssuredHelper.deleteById(LINE_API_PATH, 신분당선_id);
+        final ExtractableResponse<Response> response = LineApiHelper.removeLine(신분당선_id);
 
         // then
         assertSoftly(softly -> {
             softly.assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-            final List<Long> lineIds = RestAssuredHelper
-                    .get(LINE_API_PATH)
-                    .jsonPath()
-                    .getList("id", Long.class);
+            final List<Long> lineIds = getIds(LineApiHelper.fetchLines());
             softly.assertThat(lineIds).containsExactly(RestAssuredHelper.getIdFrom(분당선_response));
         });
+    }
+
+    private List<Long> getIds(final ExtractableResponse<Response> response) {
+        return response.jsonPath().getList("id", Long.class);
+    }
+
+    private List<String> getLineNames(final ExtractableResponse<Response> response) {
+        return response.jsonPath().getList("name", String.class);
     }
 
     private void assertResponseData(final ExtractableResponse<Response> response, final Long id, final String name, final String color, final Long upStationId, final Long downStationId) {
@@ -170,32 +179,6 @@ public class LineAcceptanceTest {
             softly.assertThat(lineResponse.getStations())
                     .extracting("id").containsExactly(upStationId, downStationId);
         });
-    }
-
-    private Long createStation(final String name) {
-        return RestAssuredHelper.getIdFrom(RestAssuredHelper.post(StationAcceptanceTest.STATION_API_PATH, Map.of("name", name)));
-    }
-
-    private ExtractableResponse<Response> requestCreateLine(final String name, final String color, final Long upStationId, final Long downStationId, final int distance) {
-        final Map<String, Object> createLineRequest = createLineRequestFixture(name, color, upStationId, downStationId, distance);
-        return RestAssuredHelper.post(LINE_API_PATH, createLineRequest);
-    }
-
-    private Map<String, Object> createLineRequestFixture(final String name, final String color, final Long upStationId, final Long downStationId, final int distance) {
-        return Map.of(
-                "name", name
-                , "color", color
-                , "upStationId", upStationId
-                , "downStationId", downStationId
-                , "distance", distance
-        );
-    }
-
-    private Map<String, Object> createLineUpdateRequestFixture(final String name, final String color) {
-        return Map.of(
-                "name", name
-                , "color", color
-        );
     }
 
 }
