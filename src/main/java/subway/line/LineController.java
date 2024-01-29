ff5118d584bf067b0f2331d7e8c5977b0adff34a
mincheolkk <kmc04080408@gmail.com>
2024-01-30 01:50:59 +0900

    refactor : 서비스 메서드 호출하게 컨트롤러 수정

--- src/main/java/subway/line/LineController.java
+++ src/main/java/subway/line/LineController.java
@@ -2,6 +2,7 @@ package subway.line;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -9,10 +10,16 @@ import java.net.URI;
 @RestController
 public class LineController {
 
+    private final LineService lineService;
+
+    public LineController(LineService lineService) {
+        this.lineService = lineService;
+    }
 
     @PostMapping("/lines")
-    public ResponseEntity<LineResponse> createLine() {
-        Long lineId = 1L;
-        return ResponseEntity.created(URI.create("/lines/" + lineId)).build();
+    public ResponseEntity<LineResponse> createLine(@RequestBody LineCreateRequest request) {
+        LineResponse lineResponse = lineService.create(request);
+        return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId()))
+                .body(lineResponse);
     }
 }
