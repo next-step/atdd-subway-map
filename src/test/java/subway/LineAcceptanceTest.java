7b894c991942077ea1b690e84b33a53bdc6ad6f7
woonys <wodns1324@gmail.com>
2024-01-29 21:29:47 +0900

    feat: 실패 테스트 작성 - 지하철 노선을 생성하면 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다.

---
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -0,0 +1,74 @@
+package subway;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+
+import subway.line.LineResponse;
+
+@DisplayName("지하철 노선 관련 기능")
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+public class LineAcceptanceTest {
+
+    @DisplayName("지하철 노선을 생성하면 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다.")
+    @Test
+    void test_canSearchLine_whenCreateLine() {
+        //given
+        String lineName = "신분당선";
+        String lineColor = "bg-red-600";
+        Long upStationId = 1L;
+        Long downStationId = 2L;
+        Integer distance = 10;
+
+        Map<String, String> requestParams = Map.of(
+            "name", lineName,
+            "color", lineColor,
+            "upStationId", upStationId.toString(),
+            "downStationId", downStationId.toString(),
+            "distance", distance.toString()
+        );
+
+        //when
+        given().body(requestParams)
+               .contentType(MediaType.APPLICATION_JSON_VALUE)
+               .when().post("/lines").then().log().all();
+
+        //then
+        List<LineResponse> responses = when().get("/lines").then().extract().jsonPath().getList(".", LineResponse.class);
+        assertThat(responses).extracting(LineResponse::getName).containsExactly(lineName);
+    }
+
+    @DisplayName("2개의 지하철 노선을 생성하고 지하철 노선 목록을 조회하면 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.")
+    @Test
+    void test_create() {
+
+    }
+
+    @DisplayName("지하철 노선을 생성하고 생성한 지하철 노선을 조회하면 생성한 지하철 노선의 정보를 응답받을 수 있다.")
+    @Test
+    void test3() {
+
+    }
+
+    @DisplayName("지하철 노선을 생성하고 생성한 지하철 노선을 수정하면 해당 지하철 노선 정보는 수정된다.")
+    @Test
+    void test4() {
+
+    }
+
+    @DisplayName("지하철 노선을 생성하고 생성한 지하철 노선을 삭제하면 해당 지하철 노선 정보는 삭제된다.")
+    @Test
+    void test5() {
+
+    }
+
+}
--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -1,23 +1,22 @@
 package subway;
 
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import javax.print.attribute.standard.Media;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import subway.line.LineResponse;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -29,28 +28,28 @@ public class StationAcceptanceTest {
      */
     @DisplayName("지하철역을 생성한다.")
     @Test
-    void createStation() {
+    void testCreateStation() {
         // when
         Map<String, String> params = new HashMap<>();
         params.put("name", "강남역");
 
         ExtractableResponse<Response> response =
-                given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+            given().log().all()
+                   .body(params)
+                   .contentType(MediaType.APPLICATION_JSON_VALUE)
+                   .when().post("/stations")
+                   .then().log().all()
+                   .extract();
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
         List<String> stationNames =
-                given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
+            given().log().all()
+                   .when().get("/stations")
+                   .then().log().all()
+                   .extract().jsonPath().getList("name", String.class);
         assertThat(stationNames).containsAnyOf("강남역");
     }
 
@@ -62,20 +61,14 @@ public class StationAcceptanceTest {
     // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
     @DisplayName("지하철을 조회한다.")
     @Test
-    void getStations() {
+    void testGetStations() {
         //given: 2개의 지하철역 생성
         Map<String, String> param1 = Map.of("name", "역삼역");
         Map<String, String> param2 = Map.of("name", "선릉역");
 
-        given().body(param1)
-               .contentType(MediaType.APPLICATION_JSON_VALUE).log().all()
-               .when().post("/stations")
-               .then().log().all();
+        createStation(param1);
 
-        given().body(param2)
-               .contentType(MediaType.APPLICATION_JSON_VALUE).log().all()
-               .when().post("/stations")
-               .then().log().all();
+        createStation(param2);
         //when
         List<StationResponse> response =
             when().get("/stations").then().log().all().extract().jsonPath().getList(".", StationResponse.class);
@@ -93,7 +86,7 @@ public class StationAcceptanceTest {
     // TODO: 지하철역 제거 인수 테스트 메서드 생성
     @DisplayName("지하철을 삭제한다.")
     @Test
-    void deleteStation() {
+    void testDeleteStation() {
         //given
         Map<String, String> params = Map.of("name", "왕십리역");
         given().body(params).contentType(MediaType.APPLICATION_JSON_VALUE).when().post("/stations").then().log().all();
@@ -106,4 +99,10 @@ public class StationAcceptanceTest {
         when().get("/station/{id}", 1).then().statusCode(HttpStatus.NOT_FOUND.value()).log().all();
     }
 
+    void createStation(Map<String, String> param1) {
+        given().body(param1)
+               .contentType(MediaType.APPLICATION_JSON_VALUE).log().all()
+               .when().post("/stations")
+               .then().log().all();
+    }
 }
