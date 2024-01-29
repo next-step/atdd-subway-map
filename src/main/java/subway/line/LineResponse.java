d42f9050bc26c00ba2ee9eefdc6f4c41254e0d3e
woonys <wodns1324@gmail.com>
2024-01-29 21:46:07 +0900

    feat: LineController & post method 추가

--- src/main/java/subway/line/LineResponse.java
+++ src/main/java/subway/line/LineResponse.java
@@ -1,4 +1,17 @@
 package subway.line;
 
+import subway.Stations;
+
 public class LineResponse {
+    private Long id;
+    private String name;
+    private String color;
+    private Stations stations;
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
 }
