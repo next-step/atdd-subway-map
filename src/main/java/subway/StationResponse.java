69ff85fc9b8a9bfafdf2a52dd0ebf1bb01c41f71
mincheolkk <kmc04080408@gmail.com>
2024-01-30 01:49:28 +0900

    feat : 팩토리 메서드 추가

--- src/main/java/subway/StationResponse.java
+++ src/main/java/subway/StationResponse.java
@@ -1,19 +1,23 @@
 package subway;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class StationResponse {
     private Long id;
     private String name;
 
+    @Builder
     public StationResponse(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
+    public static StationResponse of(Station station) {
+        return StationResponse.builder()
+                .id(station.getId())
+                .name(station.getName())
+                .build();
     }
 }
