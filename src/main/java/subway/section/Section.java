333b8c61d567742e23359ddc2f39c5d2f8948695
Lee seung chan <dltmdcks702@gmail.com>
2024-01-31 05:20:01 +0900

    chore: delete unused import

--- src/main/java/subway/section/Section.java
+++ src/main/java/subway/section/Section.java
@@ -1,14 +1,11 @@
 package subway.section;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import subway.lines.Line;
 
 @Entity()
