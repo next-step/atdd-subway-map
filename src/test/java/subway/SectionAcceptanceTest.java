d2df23f420497a27408626f90415280578541d05
hhhhhwi <aab555586@gmail.com>
2024-01-30 23:57:04 +0900

    test: 지하철 구간 등록 실패 테스트 구현

--- src/test/java/subway/SectionAcceptanceTest.java
+++ src/test/java/subway/SectionAcceptanceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 import subway.dto.LineRequest;
 import subway.dto.LineResponse;
@@ -41,5 +42,56 @@ public class SectionAcceptanceTest {
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 		assertThat(lineApiRequest.get(1L).as(LineResponse.class).getStaions().stream()
 				.map(StationResponse::getId)).contains(3L);
+	}
+
+	/**
+	 * Given 지하철 노선에 구간을 등록한다.
+	 * When 등록한 구간과 같은 구간을 등록하면
+	 * Then 등록되지 않고 코드값 500 (Internal sever Error) 을 반환한다.
+	 */
+	@DisplayName("지하철 노선에 등록되어 있는 역을 등록하면 실패한다.")
+	@DirtiesContext
+	@Test
+	void createSectionWithRegisteredStationThenFailTest() {
+		// given
+		sectionApiRequest.post(Map.of(
+				"downStationId", 4L
+				,"upStationId", 2L
+				,"distance", 10
+				,"line_id", 1L), 1L);
+
+		// when & then
+		ExtractableResponse<Response> response = sectionApiRequest.post(Map.of(
+				"downStationId", 2L
+				,"upStationId", 4L
+				,"distance", 10), 1L);
+
+		// then
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+	}
+
+	/**
+	 * Given 지하철 노선에 구간을 등록한다.
+	 * When 상행역이 해당 노선의 하행 종점역이 아닌 구간을 등록하면
+	 * Then 등록되지 않고 코드값 500 (Internal sever Error) 을 반환한다.
+	 */
+	@DisplayName("하행 종점역이 상행역이 아닌 구간을 등록하면 실패한다.")
+	@DirtiesContext
+	@Test
+	void createSectionWithUpStationIsNotEndStationThenFailTest() {
+		// given
+		sectionApiRequest.post(Map.of(
+				"downStationId", 4L
+				,"upStationId", 2L
+				,"distance", 10), 1L);
+
+		// when
+		ExtractableResponse<Response> response = sectionApiRequest.post(Map.of(
+				"downStationId", 5L
+				,"upStationId", 3L
+				,"distance", 10), 1L);
+
+		// then
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
 	}
 }
