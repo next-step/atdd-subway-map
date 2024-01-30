ba7cdbd94643da3c73fbf62a2164e6ecf937227d
10015387 <sunga08@wjtb.net>
2024-01-30 18:39:04 +0900

    feat : 지하철 노선 목록 조회 기능 추가

--- src/main/java/subway/line/LineController.java
+++ src/main/java/subway/line/LineController.java
@@ -1,11 +1,13 @@
 package subway.line;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class LineController {
@@ -19,4 +21,9 @@ public class LineController {
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
     }
 
+    @GetMapping("/lines")
+    public ResponseEntity<List<LineResponse>> showLine() {
+        List<LineResponse> lines = lineService.findAllLines();
+        return ResponseEntity.ok().body(lines);
+    }
 }
