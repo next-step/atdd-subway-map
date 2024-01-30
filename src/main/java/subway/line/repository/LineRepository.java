b8ad5ffe4dfbf17b9310d43f9993e8d0799f11a1
Jongmin Yoon <jongmin4943@gmail.com>
2024-01-30 23:56:58 +0900

    fix: Line findAll join fetch 구문 추가

--- src/main/java/subway/line/repository/LineRepository.java
+++ src/main/java/subway/line/repository/LineRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
-    @Query("select line from Line line left join fetch line.sections")
+    @Query("select line from Line line left join fetch line.sections.sections section join fetch section.upStation join fetch section.downStation")
     List<Line> findAllWithLines();
 
     @Query("select line from Line line left join fetch line.sections.sections section join fetch section.upStation join fetch section.downStation where line.id = :id")
