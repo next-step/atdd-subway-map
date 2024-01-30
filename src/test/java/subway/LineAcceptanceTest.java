5bdff79da9484b99128751f1c2f221574684269a
sunga <eert2260@naver.com>
2024-01-30 21:07:52 +0900

    test : 지하철 노선 생성 및 조회 인수 테스트 추가

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -33,7 +33,7 @@ public class LineAcceptanceTest {
      * When 지하철 노선을 생성하면
      * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
      */
-    @DisplayName("지하철 노선을 생성하고 조회한다")
+    @DisplayName("지하철 노선을 생성하고 노선 목록을 조회한다")
     @Test
     void createLine() {
 
@@ -78,9 +78,9 @@ public class LineAcceptanceTest {
      * When 지하철 노선 목록을 조회하면
      * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
      */
-    @DisplayName("2개의 지하철 노선을 생성하고 조회한다")
+    @DisplayName("2개의 지하철 노선을 생성하고 노선 목록을 조회한다")
     @Test
-    void showLines() {
+    void createAndShowTwoLines() {
 
         //given
         StationRestAssuredCRUD.createStation("미금역");
@@ -131,5 +131,51 @@ public class LineAcceptanceTest {
 
         // then
         assertThat(names).containsAll(List.of("신분당선", "수인분당선"));
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 조회하면
+     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+     */
+    @DisplayName("지하철 노선을 생성하고 노선을 조회한다")
+    @Test
+    void test() {
+
+        //given
+        StationRestAssuredCRUD.createStation("강남역");
+        StationRestAssuredCRUD.createStation("양재역");
+
+        Map<String, Object> param = new HashMap<>();
+        param.put("name", "신분당선");
+        param.put("color", "bg-red-600");
+        param.put("upStationId", 1);
+        param.put("downStationId", 2);
+        param.put("distance", 10);
+
+        ExtractableResponse<Response> createResponse = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(param)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        Long createId = createResponse.body().jsonPath().getLong("id");
+
+        //when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .param("id", createId)
+                .when()
+                    .get("/lines/{id}")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.jsonPath().getList("id")).containsExactly(createId);
+
     }
 }
