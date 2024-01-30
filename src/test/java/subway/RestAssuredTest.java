5864522c457f062826fb890831470d767fecc163
yu <yu@reverselab.co.kr>
2024-01-31 08:52:53 +0900

    앞의 테스트에서 설정된 포트가 영향을 미쳐, RestAssured.reset() 처리

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -2,15 +2,22 @@ package subway;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import subway.rest.Rest;
 
 class RestAssuredTest {
+    @BeforeAll
+    static void beforeAll() {
+        RestAssured.reset();
+    }
+
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
