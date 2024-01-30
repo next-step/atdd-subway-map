4dd47858519a748bac9870d3c36511df010b7412
NOJaeHyuk <jaehyuk7722@naver.com>
2024-01-31 00:10:22 +0900

    [Refactor] 기존 구현된 Station Builder 패턴으로 변경

--- src/main/java/subway/station/dto/response/StationResponse.java
+++ src/main/java/subway/station/dto/response/StationResponse.java
@@ -1,19 +1,24 @@
 package subway.station.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+import subway.station.domain.Station;
+
+@Getter
 public class StationResponse {
     private Long id;
     private String name;
 
-    public StationResponse(Long id, String name) {
+    @Builder
+    private StationResponse(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
+    public static StationResponse from(Station station) {
+        return StationResponse.builder()
+                .id(station.getId())
+                .name(station.getName())
+                .build();
     }
 }
--- src/main/java/subway/station/repository/StationRepository.java
+++ src/main/java/subway/station/repository/StationRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import subway.station.domain.Station;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
+    default Station getById(Long id) {
+        return findById(id).orElseThrow(() -> new IllegalArgumentException("해당 지하철이 존재하지 않습니다."));
+    }
 }
