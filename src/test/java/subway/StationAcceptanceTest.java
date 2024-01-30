e72a77f3043957e5cc563b9a4c29b25204865957
juricho <juri.cho@timf.co.kr>
2024-01-31 00:30:19 +0900

    fix/0131_step1 지하철역 인수 테스트

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -51,17 +52,81 @@ public class StationAcceptanceTest {
     }
 
     /**
+     * 지하철역 생성
+     */
+    private ExtractableResponse<Response> makeStation(String name) {
+        StationRequest newStation = new StationRequest(name);
+
+        return RestAssured.given().log().all()
+                .body(newStation)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    /**
+     * 지하철역 삭제
+     */
+    private ExtractableResponse<Response> deleteStation(Long id) {
+        return RestAssured.given().log().all()
+                .when().delete("/stations/" + id)
+                .then().log().all()
+                .extract();
+    }
+
+    /**
      * Given 2개의 지하철역을 생성하고
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다")
+    @Test
+    void getStations() {
+        // given
+        makeStation("구의역");
+        makeStation("건대역");
+
+        // when
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .statusCode(HttpStatus.OK.value())
+                        .extract();
+
+        // then
+        List<StationResponse> result = response.jsonPath().getList(".", StationResponse.class);
+        assertAll(
+                () -> assertThat(result.size()).isEqualTo(2),
+                () -> assertThat(result.stream().map(StationResponse::getName)).containsExactly("구의역", "건대역")
+        );
+    }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 삭제한다")
+    @Test
+    void deleteStation() {
+        // given
+        ExtractableResponse<Response> newStation = makeStation("구의역");
+        Long id = newStation.jsonPath().getLong("id");
 
+        // when
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .when().delete("/stations/{id}", id)
+                        .then().log().all()
+                        .statusCode(HttpStatus.NO_CONTENT.value())
+                        .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
+                () -> assertThat(response.body().asString()).isEmpty()
+        );
+    }
 }
