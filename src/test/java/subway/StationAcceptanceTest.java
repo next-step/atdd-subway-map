3822731436138ae70f2f171cbd4274f8695afc54
ksyk1205 <ksyk1205@naver.com>
2024-01-31 21:01:14 +0900

    코멘트 사항 수정

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -55,31 +55,22 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
     @DisplayName("지하철역 목록을 조회한다.")
     @Test
     void findStation() {
         //given
-        String subway1 = "강남역";
-        String subway2 = "교대역";
+        String 강남역 = "강남역";
+        String 교대역 = "교대역";
 
-        Map<String, String> params1 = new HashMap<>();
-        params1.put("name", subway1);
-        createSubway(params1);
-        Map<String, String> params2 = new HashMap<>();
-        params2.put("name", subway2);
-        createSubway(params2);
+        createSubway(강남역);
+        createSubway(교대역);
 
         //when
-        List<String> responseList =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
+        List<String> responseList = getStationNameList();
 
         //then
         assertThat(responseList.size()).isEqualTo(2);
-        assertThat(responseList).containsAnyOf(subway1, subway2);
+        assertThat(responseList).containsAnyOf(강남역, 교대역);
     }
 
     /**
@@ -87,41 +78,46 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
     @DisplayName("지하철역을 제거한다.")
     @Test
     void removeStation() {
         //given
-        String subway1 = "역삼역";
+        String 역삼역 = "역삼역";
 
-        Map<String, String> params1 = new HashMap<>();
-        params1.put("name", subway1);
-        ExtractableResponse<Response> response = createSubway(params1);
+        ExtractableResponse<Response> response = createSubway(역삼역);
 
         //when
         ExtractableResponse<Response> deleteResponse = RestAssured.given().log().all()
-                .when().delete("/stations/"+ response.jsonPath().getLong("id"))
+                .when().delete("/stations/" + response.jsonPath().getLong("id"))
                 .then().log().all()
                 .extract();
 
         assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
 
         //then
-        List<String> responseList = RestAssured.given().log().all()
+        List<String> responseList = getStationNameList();
+
+        assertThat(responseList).doesNotContain(역삼역);
+    }
+
+    private List<String> getStationNameList() {
+        return RestAssured.given().log().all()
                 .when().get("/stations")
                 .then().log().all()
                 .extract().jsonPath().get("name");
-
-        assertThat(responseList).doesNotContain(subway1);
     }
 
-    private ExtractableResponse<Response> createSubway(Map<String, String> params) {
+    private ExtractableResponse<Response> createSubway(String stationName) {
+        Map<String, String> requestParams = new HashMap<>();
+        requestParams.put("name", stationName);
+
         return RestAssured.given().log().all()
-                .body(params)
+                .body(requestParams)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/stations")
                 .then().log().all()
                 .extract();
 
     }
+
 }
