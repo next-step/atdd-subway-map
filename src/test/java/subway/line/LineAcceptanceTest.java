a84979ffd6c0f729a8760c522f39d07609592d50
Hyeon9mak <jinha3507@gmail.com>
2024-01-31 23:24:17 +0900

    test: 지하철 노선 구간 등록 테스트 코드 작성

--- src/test/java/subway/line/LineAcceptanceTest.java
+++ src/test/java/subway/line/LineAcceptanceTest.java
@@ -5,6 +5,7 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +19,7 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("지하철역 관련 기능")
+@DisplayName("지하철 노선 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class LineAcceptanceTest {
 
@@ -221,6 +222,149 @@ public class LineAcceptanceTest {
         assertThat(loadLine.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
+    @Nested
+    @DisplayName("지하철 구간 등록")
+    class AddSection {
+
+        /**
+         * Given 2개의 지하철 역(A, B)이 등록되어 있다.
+         * And 1개의 지하철 노선이 등록되어 있다.
+         * When 지하철 노선 하행 종착지(B)에 추가로 지하철 구간(B-C)을 등록한다.
+         * Then 새로운 지하철 구간이 등록된다. (A-B-C)
+         */
+        @DisplayName("지하철 노선에 신규 구간을 등록한다.")
+        @Test
+        void success() {
+            // given
+            ExtractableResponse<Response> 성수역 = createStation("성수역");
+            String 성수역_ID = 성수역.jsonPath().getString("id");
+            ExtractableResponse<Response> 건대입구역 = createStation("건대입구역");
+            String 건대입구역_ID = 건대입구역.jsonPath().getString("id");
+            ExtractableResponse<Response> 구의역 = createStation("구의역");
+            String 구의역_ID = 구의역.jsonPath().getString("id");
+
+            ExtractableResponse<Response> 이호선 = createLine(
+                    "2호선",
+                    "bg-green-000",
+                    성수역_ID,
+                    건대입구역_ID,
+                    "10"
+            );
+            String 이호선_ID = 이호선.jsonPath().getString("id");
+
+            // when
+            Map<String, String> params = new HashMap<>();
+            params.put("upStationId", 건대입구역_ID);
+            params.put("downStationId", 구의역_ID);
+            params.put("distance", "10");
+
+            ExtractableResponse<Response> response =
+                    RestAssured.given().log().all()
+                            .body(params)
+                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .when().post("/lines/{lineId}/sections", 이호선_ID)
+                            .then().log().all()
+                            .extract();
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+            assertThat(response.jsonPath().getList("stations")).hasSize(3);
+            assertThat(response.jsonPath().getList("stations.id", String.class)).containsExactly(성수역_ID, 건대입구역_ID, 구의역_ID);
+            assertThat(response.jsonPath().getList("stations.name", String.class)).containsExactly("성수역", "건대입구역", "구의역");
+        }
+
+        /**
+         * Given 2개의 지하철 역(A, C)이 등록되어 있다.
+         * And 1개의 지하철 노선이 등록되어 있다.
+         * When 지하철 노선 하행 종착지(C)에 추가로 지하철 구간(B-C)을 등록을 시도하면
+         * Then 추가 구간(B-C)의 하행역(C)이 이미 노선에 등록되어있어 에러가 발생한다.
+         */
+        @DisplayName("지하철 노선 구간에 이미 등록되어 있는 역을 추가하려 하면 에러가 발생한다.")
+        @Test
+        void duplicateStationError() {
+            // given
+            ExtractableResponse<Response> 성수역 = createStation("성수역");
+            String 성수역_ID = 성수역.jsonPath().getString("id");
+            ExtractableResponse<Response> 건대입구역 = createStation("건대입구역");
+            String 건대입구역_ID = 건대입구역.jsonPath().getString("id");
+            ExtractableResponse<Response> 구의역 = createStation("구의역");
+            String 구의역_ID = 구의역.jsonPath().getString("id");
+
+            ExtractableResponse<Response> 이호선 = createLine(
+                    "2호선",
+                    "bg-green-000",
+                    성수역_ID,
+                    구의역_ID,
+                    "10"
+            );
+            String 이호선_ID = 이호선.jsonPath().getString("id");
+
+            // when
+            Map<String, String> params = new HashMap<>();
+            params.put("upStationId", 건대입구역_ID);
+            params.put("downStationId", 구의역_ID);
+            params.put("distance", "10");
+
+            ExtractableResponse<Response> response =
+                    RestAssured.given().log().all()
+                            .body(params)
+                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .when().post("/lines/{lineId}/sections", 이호선_ID)
+                            .then().log().all()
+                            .extract();
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+            assertThat(response.jsonPath().getString("message")).isEqualTo("주어진 하행역은 이미 노선에 등록되어 있는 등록된 역입니다. downStationId: " + 구의역_ID);
+        }
+
+        /**
+         * Given 2개의 지하철 역(A, B)이 등록되어 있다.
+         * And 1개의 지하철 노선이 등록되어 있다.
+         * When 지하철 노선 하행 종착지(B)에 추가로 지하철 구간(C-D)을 등록을 시도하면
+         * Then 추가 구간(C-D)의 상행역(C)가 노선의 하행 종착역(B)과 다르기 때문에 에러가 발생한다.
+         */
+        @DisplayName("새로 추가하려는 구간의 상행역이 노선의 하행 종착역과 다른 역이라면 에러가 발생한다.")
+        @Test
+        void invalidUpStationError() {
+            // given
+            ExtractableResponse<Response> 성수역 = createStation("성수역");
+            String 성수역_ID = 성수역.jsonPath().getString("id");
+            ExtractableResponse<Response> 건대입구역 = createStation("건대입구역");
+            String 건대입구역_ID = 건대입구역.jsonPath().getString("id");
+            ExtractableResponse<Response> 구의역 = createStation("구의역");
+            String 구의역_ID = 구의역.jsonPath().getString("id");
+            ExtractableResponse<Response> 잠실역 = createStation("잠실역");
+            String 잠실역_ID = 잠실역.jsonPath().getString("id");
+
+            ExtractableResponse<Response> 이호선 = createLine(
+                    "2호선",
+                    "bg-green-000",
+                    성수역_ID,
+                    건대입구역_ID,
+                    "10"
+            );
+            String 이호선_ID = 이호선.jsonPath().getString("id");
+
+            // when
+            Map<String, String> params = new HashMap<>();
+            params.put("upStationId", 구의역_ID);
+            params.put("downStationId", 잠실역_ID);
+            params.put("distance", "10");
+
+            ExtractableResponse<Response> response =
+                    RestAssured.given().log().all()
+                            .body(params)
+                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .when().post("/lines/{lineId}/sections", 이호선_ID)
+                            .then().log().all()
+                            .extract();
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+            assertThat(response.jsonPath().getString("message")).isEqualTo("새로운 구간의 상행역은 노선의 하행 종착역과 같아야 합니다. upStationId: " + 구의역_ID);
+        }
+    }
 
     private ExtractableResponse<Response> createStation(String stationName) {
         StationRequest request = new StationRequest(stationName);
