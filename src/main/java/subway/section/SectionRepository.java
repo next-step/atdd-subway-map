992fe0f1a791cdf81bc578fceb5cf15812e2a823
seonje1 <re552@naver.com>
2024-02-01 01:01:25 +0900

    사용하지않는 메소드 삭제

--- src/main/java/subway/section/SectionRepository.java
+++ src/main/java/subway/section/SectionRepository.java
@@ -6,13 +6,4 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface SectionRepository extends JpaRepository<Section, Long> {
-
-    @Query(
-            "select s " +
-            "from Section s " +
-            "where s in :sections " +
-            "and s.upStation.id = :stationId " +
-            "or s.downStation.id = :stationId"
-    )
-    List<Section> findDeleteSections(List<Section> sections, Long stationId);
 }
