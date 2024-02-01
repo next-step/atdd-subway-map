c44a0f9f25fcfb366cebab1ff3f15bd04eb51569
kmando01 <ksumin201566@gmail.com>
2024-02-01 22:42:24 +0900

    docs : 인수조건 주석 추가

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -20,7 +20,9 @@ import subway.fixture.StationFixture;
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class LineAcceptanceTest {
-	/** When 지하철 노선을 생성하면 Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다 */
+	/** When 지하철 노선을 생성하면
+	 * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+	 */
 	@DisplayName("지하철 노선을 생성한다.")
 	@Test
 	void create_line() {
@@ -42,7 +44,9 @@ public class LineAcceptanceTest {
 		assertThat(responseBody.jsonPath().getList("stations")).hasSize(2);
 	}
 
-	/** Given 2개의 지하철 노선을 생성하고 When 지하철 노선 목록을 조회하면 Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다. */
+	/** Given 2개의 지하철 노선을 생성하고
+	 * When 지하철 노선 목록을 조회하면
+	 * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다. */
 	@DisplayName("지하철 노선의 목록을 조회한다.")
 	@Test
 	void get_lines() {
@@ -70,6 +74,12 @@ public class LineAcceptanceTest {
 		assertThat(responseBody.jsonPath().getList("stations").size()).isEqualTo(2);
 	}
 
+
+	/**
+	 * Given 지하철 노선을 생성하고
+	 * When 생성한 지하철 노선을 조회하면
+	 * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+	 */
 	@DisplayName("지하철 노선을 조회한다.")
 	@Test
 	void get_line() {
@@ -95,6 +105,11 @@ public class LineAcceptanceTest {
 						LineFixture.신분당선_생성().getDownStationId().intValue());
 	}
 
+	/**
+	 * Given 지하철 노선을 생성하고
+	 * When 생성한 지하철 노선을 수정하면
+	 * Then 해당 지하철 노선 정보는 수정된다
+	 */
 	@DisplayName("특정 지하철 노선 정보를 수정한다.")
 	@Test
 	void update_line() {
@@ -116,6 +131,11 @@ public class LineAcceptanceTest {
 		assertThat(getResponse.jsonPath().getString("color")).isEqualTo(requestBody.getColor());
 	}
 
+	/**
+	 * Given 지하철 노선을 생성하고
+	 * When 생성한 지하철 노선을 삭제하면
+	 * Then 해당 지하철 노선 정보는 삭제된다
+	 */
 	@DisplayName("특정 지하철 노선을 삭제한다")
 	@Test
 	void delete_line() {
