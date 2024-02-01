b1af18ba9d8656b587efb6b4177bdcc08d6a08dd
woonys <wodns1324@gmail.com>
2024-02-02 01:24:01 +0900

    2단계 - 지하철 노선 관리: 지하철 노선 목록 조회

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -22,7 +22,7 @@ public class LineAcceptanceTest {
 
     @DisplayName("지하철 노선을 생성하면 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다.")
     @Test
-    void test_canSearchLine_whenCreateLine() {
+    void test_지하철노선_생성() {
         //given
         // 역 저장
         Map<String, String> param1 = Map.of("name", "역삼역");
@@ -63,8 +63,62 @@ public class LineAcceptanceTest {
 
     @DisplayName("2개의 지하철 노선을 생성하고 지하철 노선 목록을 조회하면 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.")
     @Test
-    void test_create() {
+    void test_지하철_노선_목록_조회() {
+        //given
+        // 역 저장
+        Map<String, String> param1 = Map.of("name", "역삼역");
+        Map<String, String> param2 = Map.of("name", "선릉역");
+        Map<String, String> param3 = Map.of("name", "왕십리역");
+        Map<String, String> param4 = Map.of("name", "고색역");
 
+        createStation(param1);
+        createStation(param2);
+        createStation(param3);
+        createStation(param4);
+
+        String lineName1 = "신분당선";
+        String lineColor1 = "bg-red-600";
+        Long upStationId1 = 1L;
+        Long downStationId1 = 2L;
+        Integer distance1 = 10;
+
+        Map<String, String> requestParam1 = Map.of(
+            "name", lineName1,
+            "color", lineColor1,
+            "upStationId", upStationId1.toString(),
+            "downStationId", downStationId1.toString(),
+            "distance", distance1.toString()
+        );
+
+        given()
+            .body(requestParam1)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/lines").then().log().all();
+
+        String lineName2 = "분당선";
+        String lineColor2 = "bg-yellow-600";
+        Long upStationId2 = 3L;
+        Long downStationId2 = 4L;
+        Integer distance2 = 2;
+
+        Map<String, String> requestParam2 = Map.of(
+            "name", lineName2,
+            "color", lineColor2,
+            "upStationId", upStationId2.toString(),
+            "downStationId", downStationId2.toString(),
+            "distance", distance2.toString()
+        );
+
+        given()
+            .body(requestParam2)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/lines").then().log().all();
+
+        //when
+        List<LineResponse> lineResponses = when().get("/lines").then().extract().jsonPath().getList(".", LineResponse.class);
+        assertThat(lineResponses).hasSize(2);
+        assertThat(lineResponses).extracting(LineResponse::getName).containsExactly(lineName1, lineName2);
+        assertThat(lineResponses).extracting(LineResponse::getColor).containsExactly(lineColor1, lineColor2);
     }
 
     @DisplayName("지하철 노선을 생성하고 생성한 지하철 노선을 조회하면 생성한 지하철 노선의 정보를 응답받을 수 있다.")
