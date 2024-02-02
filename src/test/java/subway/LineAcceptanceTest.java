3caa44c12f2da55508f2074556bf0c2c8325897d
mincheolkk <kmc04080408@gmail.com>
2024-02-03 03:20:52 +0900

    test : 지하철 노선 목록 조회 구현

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -11,9 +11,11 @@ import org.springframework.test.annotation.DirtiesContext;
 import subway.line.LineCreateRequest;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.path.json.JsonPath.from;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_CLASS;
 
 @DirtiesContext(classMode = AFTER_CLASS)
@@ -50,6 +52,52 @@ public class LineAcceptanceTest {
         );
     }
 
+    /**
+     *  Given 2개의 지하철 노선을 생성하고
+     *  When 지하철 노선 목록을 조회하면
+     *  Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @Test
+    void 생성한_지하철노선을_모두_조회한다() {
+        // given
+        createStation("건대입구");
+        createStation("어린이대공원");
+        createStation("군자");
+
+        LineCreateRequest lineCreateRequest_1 = new LineCreateRequest(
+                "신분당선",
+                "bg-red-600",
+                1L,
+                2L,
+                10
+        );
+        createLine(lineCreateRequest_1);
+
+        LineCreateRequest lineCreateRequest_2 = new LineCreateRequest(
+                "분당선",
+                "bg-green-600",
+                1L,
+                3L,
+                20
+        );
+        createLine(lineCreateRequest_2);
+
+        // when
+        ExtractableResponse<Response> response = getLines();
+        String responseJson = response.body().asString();
+        List<Map<String, ?>> lines = from(responseJson).getList("");
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(200);
+        assertThat(lines.get(0).get("name")).isEqualTo("신분당선");
+        assertThat((List<Map<String, ?>>) lines.get(0).get("stations")).extracting("name").containsExactlyInAnyOrder("건대입구", "어린이대공원");
+
+        assertThat(lines.get(1).get("name")).isEqualTo("분당선");
+        assertThat((List<Map<String, ?>>) lines.get(1).get("stations")).extracting("name").containsExactlyInAnyOrder("건대입구", "군자");
+    }
+
+
+
     private ExtractableResponse<Response> createLine(LineCreateRequest request) {
         return RestAssured.given().log().all()
                 .body(request)
@@ -69,5 +117,13 @@ public class LineAcceptanceTest {
                 .when().post("/stations")
                 .then().log().all()
                 .extract();
+    }
+
+    private ExtractableResponse<Response> getLines() {
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when().get("/lines")
+                .then().log().all()
+                .extract();
+        return response;
     }
 }
