5cd38e378f00362b49f35e1220995a2490b5f3f3
mincheolkk <kmc04080408@gmail.com>
2024-02-03 03:23:17 +0900

    feat : 목록 조회 서비스 메서드 구현 및 중복 코드 분리

--- src/main/java/subway/line/LineService.java
+++ src/main/java/subway/line/LineService.java
@@ -6,6 +6,8 @@ import subway.StationRepository;
 import subway.StationResponse;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LineService {
@@ -20,12 +22,8 @@ public class LineService {
 
     public LineResponse create(LineCreateRequest request) {
 
-        Station upStation = stationRepository.findById(request.getUpStationId()).orElseThrow(
-                () -> new EntityNotFoundException("해당 엔티티를 찾을 수 없습니다.")
-        );
-        Station downStation = stationRepository.findById(request.getDownStationId()).orElseThrow(
-                () -> new EntityNotFoundException("해당 엔티티를 찾을 수 없습니다.")
-        );
+        Station upStation = getStation(request.getUpStationId());
+        Station downStation = getStation(request.getDownStationId());
 
         Line line = LineCreateRequest.toEntity(request);
         Line savedLine = lineRepository.save(line);
@@ -35,4 +33,31 @@ public class LineService {
 
         return LineResponse.of(savedLine, upStationResponse, downStationResponse);
     }
+
+    public List<LineResponse> getLines() {
+
+        List<Line> lines = lineRepository.findAll();
+        List<LineResponse> responses = new ArrayList<>();
+
+        for (Line line : lines) {
+            Station upStation = getStation(line.getUpStationId());
+            Station downStation = getStation(line.getDownStationId());
+
+            StationResponse upStationResponse = StationResponse.of(upStation);
+            StationResponse downStationResponse = StationResponse.of(downStation);
+
+            LineResponse response = LineResponse.of(line, upStationResponse, downStationResponse);
+            responses.add(response);
+        }
+
+        return responses;
+    }
+
+    private Station getStation(Long stationId) {
+        return stationRepository.findById(stationId).orElseThrow(
+                () -> new EntityNotFoundException("해당 엔티티를 찾을 수 없습니다.")
+        );
+    }
+
+
 }
