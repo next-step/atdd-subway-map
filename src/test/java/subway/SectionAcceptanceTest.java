b5335f7f8391d6d76fbc2e5dce72b6e8fdf4de50
woonys <wodns1324@gmail.com>
2024-02-06 02:53:50 +0900

    test: 공통 부분 메서드 분리

--- src/test/java/subway/SectionAcceptanceTest.java
+++ src/test/java/subway/SectionAcceptanceTest.java
@@ -14,22 +14,16 @@ import io.restassured.http.ContentType;
 import subway.domain.LineResponse;
 
 public class SectionAcceptanceTest extends BaseAcceptanceTest {
-    @Autowired
-    private LineRepository lineRepository;
 
     @Test
     void test_특정_노선에_구간을_등록하면_노선_조회시_등록한_구간을_확인할_수_있다() {
         //given
-        Map<String, String> 역삼역 = Map.of("name", "역삼역");
-        Map<String, String> 선릉역 = Map.of("name", "선릉역");
-        Map<String, String> 강남역 = Map.of("name", "강남역");
-        Map<String, String> 왕십리역 = Map.of("name", "왕십리역");
         createStation(역삼역);
         createStation(선릉역);
         createStation(강남역);
         createStation(왕십리역);
 
-        LineResponse lineResponse = createLine("이호선");
+        LineResponse lineResponse = createLine(getRequestParam_신분당선());
 
         //when
         Map<String, String> sectionPostRequest = Map.of(
@@ -47,7 +41,7 @@ public class SectionAcceptanceTest extends BaseAcceptanceTest {
                                 .then().log().all().extract().jsonPath().getObject(".", LineResponse.class);
         assertAll(
             () -> assertThat(response.countSections()).hasSize(1),
-            () -> assertThat(response.getUpStationId()).isEqualTo(2),
+            () -> assertThat(response.getUpStationId()).isEqualTo(1),
             () -> assertThat(response.getDownStationId()).isEqualTo(4),
             () -> assertThat(response.getDistance()).isEqualTo(10)
         );
