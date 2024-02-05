8cc2a88167e92f5c8d2a88f2bb6083ccf8e7dbda
yu <yu@reverselab.co.kr>
2024-02-05 12:59:12 +0900

    SectionRepository 삭제

--- src/main/java/subway/line/SectionRepository.java
+++
@@ -1,9 +0,0 @@
-package subway.line;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SectionRepository extends JpaRepository<Section, Long> {
-	List<Section> findAllByLineId(Long lineId);
-}
