36c3732e89e46dc712a6a3ad0c9d6d4eec6e89a4
moonstal1506 <moonstal1506@gmail.com>
2024-02-05 23:37:24 +0900

    refactor: StationSteps로 분리

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -1,17 +1,13 @@
 package subway;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,13 +23,13 @@ public class StationAcceptanceTest {
     @Test
     void createStation() {
         // when
-        ExtractableResponse<Response> response = createStation("강남역");
+        ExtractableResponse<Response> response = StationSteps.createStation("강남역");
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames = getStationNames();
+        List<String> stationNames = StationSteps.getStationNames();
         assertThat(stationNames).containsAnyOf("강남역");
     }
 
@@ -46,11 +42,11 @@ public class StationAcceptanceTest {
     @Test
     void getStations() {
         // given
-        createStation("강남역");
-        createStation("역삼역");
+        StationSteps.createStation("강남역");
+        StationSteps.createStation("역삼역");
 
         // when
-        List<String> stationNames = getStationNames();
+        List<String> stationNames = StationSteps.getStationNames();
 
         // then
         assertThat(stationNames).containsAnyOf("강남역", "역삼역");
@@ -65,37 +61,15 @@ public class StationAcceptanceTest {
     @Test
     void deleteStation() {
         // given
-        ExtractableResponse<Response> response = createStation("강남역");
+        ExtractableResponse<Response> response = StationSteps.createStation("강남역");
         String locationHeader = response.header("Location");
 
         // when
-        RestAssured.given().log().all()
-                .when().delete(locationHeader)
-                .then().log().all()
-                .statusCode(HttpStatus.NO_CONTENT.value());
+        StationSteps.deleteStation(locationHeader);
 
         // then
-        List<String> stationNames = getStationNames();
+        List<String> stationNames = StationSteps.getStationNames();
         assertThat(stationNames).doesNotContain("강남역");
     }
 
-    private static ExtractableResponse<Response> createStation(String name) {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", name);
-
-        return RestAssured.given().log().all()
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/stations")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value())
-                .extract();
-    }
-
-    private static List<String> getStationNames() {
-        return RestAssured.given().log().all()
-                .when().get("/stations")
-                .then().log().all()
-                .extract().jsonPath().getList("name", String.class);
-    }
 }
