bfa578b91eb0e718f3229c65d6734106c79cd3cb
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-02-06 09:08:24 +0900

    fix: StationLine Mock Data 수정

--- src/test/java/config/fixtures/subway/StationLineMockData.java
+++ src/test/java/config/fixtures/subway/StationLineMockData.java
@@ -7,7 +7,7 @@ public class StationLineMockData {
     public static final StationLineRequest 신분당선 =
             new StationLineRequest("신분당선", "bg-red-600", 1L, 2L, 10);
     public static final StationLineRequest 분당선 =
-            new StationLineRequest("신분당선", "bg-green-600", 2L, 4L, 20);
+            new StationLineRequest("분당선", "bg-green-600", 2L, 4L, 20);
 
     public static final StationLineRequest 수정된_신분당선 =
             new StationLineRequest("수정된_신분당선", "bg-blue-100", 1L, 2L, 10);
