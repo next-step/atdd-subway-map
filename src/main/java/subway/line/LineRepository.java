d4cd85e19aca48e3e7c458e35daf6c4ff09f68f2
moonstal1506 <moonstal1506@gmail.com>
2024-02-08 01:25:10 +0900

    fix: fetch join 추가

--- src/main/java/subway/line/LineRepository.java
+++ src/main/java/subway/line/LineRepository.java
@@ -1,6 +1,16 @@
 package subway.line;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LineRepository extends JpaRepository<Line, Long> {
+
+    @EntityGraph(attributePaths = {"upStation", "downStation", "sections", "sections.upStation", "sections.downStation"})
+    List<Line> findAll();
+
+    @EntityGraph(attributePaths = {"upStation", "downStation", "sections", "sections.upStation", "sections.downStation"})
+    Optional<Line> findById(Long lineId);
 }
