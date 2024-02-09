3a785b0a5116de7058bb37ffa853d9718bcc13dd
sunga <eert2260@naver.com>
2024-02-09 15:22:21 +0900

    test : 새로운 구간의 하행역이 지하철 노선에 등록된 역인 경우 구간 등록 에러 발생 인수 테스트 작성

--- src/test/java/subway/SectionAcceptanceTest.java
+++ src/test/java/subway/SectionAcceptanceTest.java
@@ -71,11 +71,37 @@ public class SectionAcceptanceTest extends CommonAcceptanceTest{
 
     /**
      * Given 지하철 노선을 생성하고
-     * When 노선에 등록된 역을 포함한 구간을 등록하면
+     * When 노선에 등록된 역이 하행역인 구간을 등록하면
      * Then 예외가 발생한다.
      */
+    @Test
+    @DisplayName("새로운 구간의 하행역이 지하철 노선에 등록된 역인 경우 구간 등록 오류 테스트")
     void addSectionExistStationException() {
 
+        //given
+        Long 강남역Id = extractResponseId(StationRestAssuredCRUD.createStation("강남역"));
+        Long 선릉역Id = extractResponseId(StationRestAssuredCRUD.createStation("선릉역"));
+        ExtractableResponse<Response> lineResponse = LineRestAssuredCRUD.createLine("2호선", "bg-red-600", 강남역Id, 선릉역Id, 7);
+        Long lineNum = lineResponse.jsonPath().getLong("id");
+
+        //when
+        Map<String, Object> paraMap = new HashMap<>();
+        paraMap.put("upStationId", 선릉역Id);
+        paraMap.put("downStationId", 강남역Id);
+        paraMap.put("distance", 10);
+
+        ExtractableResponse<Response> addResponse = RestAssured
+                .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .pathParam("id", lineNum)
+                    .body(paraMap)
+                .when()
+                    .post("/lines/{id}/sections")
+                .then().log().all()
+                .extract();
+
+        //then
+        assertThat(addResponse.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
     /**
