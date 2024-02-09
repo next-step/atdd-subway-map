4f68fd20fdf998849d86f8a95c65bfebb05deb1e
sunga <eert2260@naver.com>
2024-02-09 16:19:11 +0900

    test : 지하철 노선에서 구간 제거 인수 테스트 작성

--- src/test/java/subway/SectionAcceptanceTest.java
+++ src/test/java/subway/SectionAcceptanceTest.java
@@ -1,11 +1,13 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import subway.common.CommonAcceptanceTest;
 import subway.common.LineRestAssuredCRUD;
 import subway.common.SectionRestAssuredCRUD;
@@ -57,10 +59,10 @@ public class SectionAcceptanceTest extends CommonAcceptanceTest {
     /**
      * Given 지하철 노선을 생성하고
      * When 노선에 등록된 역이 하행역인 구간을 등록하면
-     * Then 예외가 발생한다.
+     * Then 서버오류가 발생한다.
      */
     @Test
-    @DisplayName("새로운 구간의 하행역이 지하철 노선에 등록된 역인 경우 구간 등록 오류 테스트")
+    @DisplayName("하행역이 지하철 노선에 등록된 역인 구간을 추가하면 서버오류가 발생한다.")
     void addSectionExistStationException() {
 
         //given
@@ -79,10 +81,10 @@ public class SectionAcceptanceTest extends CommonAcceptanceTest {
     /**
      * Given 지하철 노선을 생성하고
      * When 지하철 노선의 하행역과 새로운 구간의 상행역이 일치하지 않은 구간을 추가하면
-     * Then 예외가 발생한다.
+     * Then 서버오류가 발생한다.
      */
     @Test
-    @DisplayName("새로운 구간의 상행역이 지하철 노선의 하행역과 일치하지 않는 경우 구간 등록 오류 테스트")
+    @DisplayName("지하철 노선의 하행역과 새로운 구간의 생행역이 일치하지 않는 구간을 등록하면 서버오류가 발생한다.")
     void addSectionNotMatchException() {
 
         //given
@@ -98,7 +100,6 @@ public class SectionAcceptanceTest extends CommonAcceptanceTest {
 
         //then
         assertThat(addResponse.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
-
     }
 
     /**
@@ -106,14 +107,43 @@ public class SectionAcceptanceTest extends CommonAcceptanceTest {
      * When 마지막 구간을 제거하면
      * Then 지하철 노선에서 마지막 구간이 삭제된다.
      */
+    @Test
+    @DisplayName("지하철 노선에서 구간을 제거한다.")
     void deleteSection() {
+        //given
+        Long 강남역Id = extractResponseId(StationRestAssuredCRUD.createStation("강남역"));
+        Long 선릉역Id = extractResponseId(StationRestAssuredCRUD.createStation("선릉역"));
+        ExtractableResponse<Response> lineResponse = LineRestAssuredCRUD.createLine("2호선", "bg-red-600", 강남역Id, 선릉역Id, 7);
+        Long lineId = lineResponse.jsonPath().getLong("id");
 
+        Long 삼성역Id = extractResponseId(StationRestAssuredCRUD.createStation("삼성역"));
+        SectionRestAssuredCRUD.addSection(선릉역Id, 삼성역Id, 10, lineId);
+
+        //when
+        ExtractableResponse<Response> deleteResponse = RestAssured
+                .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .pathParam("lineId", lineId)
+                    .queryParam("stationId", 삼성역Id)
+                .when()
+                    .delete("/lines/{lineId}/sections?stationId={stationId}")
+                .then().log().all()
+                .extract();
+
+        assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        //then
+        lineResponse = LineRestAssuredCRUD.showLine(lineId);
+        List<Station> stations = lineResponse.jsonPath().getList("stations", Station.class);
+        Long maxStationId = stations.stream().mapToLong(v -> v.getId()).max().orElse(0);
+
+        Assertions.assertThat(maxStationId).isEqualTo(선릉역Id);
     }
 
     /**
      * Given 지하철 노선에 새로운 구간 2개를 등록하고
      * When 중간 구간을 제거하면
-     * Then 예외가 발생한다.
+     * Then 서버오류가 발생한다.
      */
     void deleteMiddleSectionException() {
 
@@ -122,7 +152,7 @@ public class SectionAcceptanceTest extends CommonAcceptanceTest {
     /**
      * Given 지하철 노선을 등록하고
      * When 마지막 구간을 제거하면
-     * Then 예외가 발생한다.
+     * Then 서버오류가 발생한다.
      */
     void deleteLastSectionException() {
 
