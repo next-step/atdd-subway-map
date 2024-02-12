c058a2d8e2c9f19fa14ef27d9a28056c82dce97e
woonys <wodns1324@gmail.com>
2024-02-12 17:51:40 +0900

    fix: SectionRequest 생성자 추가

--- src/main/java/subway/dto/SectionRequest.java
+++ src/main/java/subway/dto/SectionRequest.java
@@ -4,6 +4,13 @@ public class SectionRequest {
     private Long upStationId;
     private Long downStationId;
     private int distance;
+
+    public SectionRequest(Long upStationId, Long downStationId, int distance) {
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
     public Long getUpStationId() {
         return upStationId;
     }
