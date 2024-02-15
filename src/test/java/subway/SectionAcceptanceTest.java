43a6b1bff2b57b5e106e0de5083627fb1125018b
Ilyong Hwang <1yongs_@naver.com>
2024-02-16 00:02:08 +0900

    test: 구간 제거 기능 인수테스트 추가

--- src/test/java/subway/SectionAcceptanceTest.java
+++ src/test/java/subway/SectionAcceptanceTest.java
@@ -54,6 +54,42 @@ public class SectionAcceptanceTest {
     assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
   }
 
+  /**
+   * Given 지하철 노선이 생성을 요청 하고
+   * Given 지하철 노선에 새로운 구간 추가를 요청 하고
+   * When 지하철 노선에 구간 제거를 요청 하면
+   * Then 노선에 구간이 제거된다.
+   */
+  @DisplayName("구간을 제거한다.")
+  @Test
+    void deleteSection() {
+        // given
+        Long 강남역 = 지하철역_생성_요청("강남역");
+        Long 역삼역 = 지하철역_생성_요청("역삼역");
+        Long 선릉역 = 지하철역_생성_요청("선릉역");
+
+        Long 이호선 = 지하철노선_생성_요청(강남역, 역삼역);
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("upStationId", 역삼역);
+        params.put("downStationId", 선릉역);
+        params.put("distance", 10);
+
+        RestAssured.given().body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/lines/{lineId}/sections", 이호선)
+            .then().extract();
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+            .given().contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().delete("/lines/{lineId}/sections?stationId={stationId}", 이호선, 역삼역)
+            .then().extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
   private static Long 지하철노선_생성_요청(Long 상행역, Long 하행역) {
     Map<String, Object> params = new HashMap<>();
     params.put("name", "2호선");
