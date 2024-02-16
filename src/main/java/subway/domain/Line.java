4bd375609baa98b41bf77879e95fe08b8adb51d8
Ilyong Hwang <1yongs_@naver.com>
2024-02-16 23:55:06 +0900

    fix: 구간 생성 예외 인수테스트

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -60,7 +60,7 @@ public class Line {
         if (!sections.get(sections.size() - 1).getDownStation().getId().equals(upStation.getId())) {
             throw new BusinessException("상행역은 해당 노선에 등록되어있는 하행 종점역이어야 합니다.");
         }
-        if (sections.stream().anyMatch(section -> section.getUpStationId().getId().equals(upStation.getId()) || section.getDownStation().getId().equals(downStation.getId()))) {
+        if (sections.stream().anyMatch(section -> section.getUpStationId().getId().equals(downStation.getId()) || section.getDownStation().getId().equals(downStation.getId()))) {
             throw new BusinessException("이미 해당 노선에 등록되어있는 역은 새로운 구간의 하행역이 될 수 없습니다.");
         }
         sections.add(new Section(this, upStation, downStation, distance));
