1a71998cbda2bbb6460666045943d0f583c6250f
Hyeon9mak <jinha3507@gmail.com>
2024-02-22 20:25:36 +0900

    refactor: 지하철 노선 삭제 코드 리팩터링

--- src/main/java/subway/line/delete/LineDeleteService.java
+++ src/main/java/subway/line/delete/LineDeleteService.java
@@ -2,30 +2,26 @@ package subway.line.delete;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import subway.line.Line;
 import subway.line.LineRepository;
-import subway.section.SectionRepository;
 
 @Transactional
 @Service
 public class LineDeleteService {
 
     private final LineRepository lineRepository;
-    private final SectionRepository sectionRepository;
 
-    public LineDeleteService(LineRepository lineRepository, SectionRepository sectionRepository) {
+    public LineDeleteService(LineRepository lineRepository) {
         this.lineRepository = lineRepository;
-        this.sectionRepository = sectionRepository;
     }
 
     public void deleteLine(Long lineId) {
-        validateExistLine(lineId);
-        sectionRepository.deleteAllByLineId(lineId);
-        lineRepository.deleteById(lineId);
+        Line line = findLineByLineId(lineId);
+        lineRepository.delete(line);
     }
 
-    private void validateExistLine(Long lineId) {
-        if (!lineRepository.existsById(lineId)) {
-            throw new IllegalArgumentException("존재하지 않는 노선입니다. lineId: " + lineId);
-        }
+    private Line findLineByLineId(Long lineId) {
+        return lineRepository.findById(lineId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 노선입니다. lineId: " + lineId));
     }
 }
