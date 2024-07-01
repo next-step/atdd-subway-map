35d7e9601d7d61aff29d3904371ad5e19dab4c0f
Lee-KyungSeok <leeseok8347@gmail.com>
2024-07-01 23:16:43 +0900

    구글페이지 접근 테스트 추가

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,5 +1,6 @@
 package subway;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -15,8 +16,11 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .when().get("https://www.google.com")
+                        .then().log().all()
+                        .extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
