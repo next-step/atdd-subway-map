3ce4caff091050570fe58e0e7bda54c5f2d97607
mlbase <guddnr2424@gmail.com>
2024-07-01 18:52:28 +0900

    Resolved TODO: 구글 페이지 요청

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -2,6 +2,9 @@ package subway;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,9 +18,15 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
-
+        // testing: 구글 페이지 요청
+        ExtractableResponse<Response> response =
+                given().
+                    param("q", "구글페이지+요청").
+                when().
+                    get("https://www.google.com/search").
+                then().
+                    extract();
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.contentType()).isEqualTo("text/html; charset=ISO-8859-1");
     }
 }
