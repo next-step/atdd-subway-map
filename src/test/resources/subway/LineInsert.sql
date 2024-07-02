07fee198f98e03c78107972b7876aef78834c3e2
mlbase <guddnr2424@gmail.com>
2024-07-02 19:53:11 +0900

    feature: line insert sql scripts

---
+++ src/test/resources/subway/LineInsert.sql
@@ -0,0 +1,4 @@
+INSERT INTO line(name, color, up_station_id, down_station_id, distance)
+values ('신분당선', 'bg-red-600', 1, 2, 10);
+INSERT INTO line(name, color, up_station_id, down_station_id, distance)
+values ('분당선', 'bg-green-60d', 1, 3, 10);
