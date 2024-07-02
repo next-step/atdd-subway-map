69660c29a8238700563061b28bf8afb39ffddc29
mlbase <guddnr2424@gmail.com>
2024-07-02 19:58:16 +0900

    feature: line entity table class

---
+++ src/main/java/subway/Line.java
@@ -0,0 +1,52 @@
+package subway;
+
+import javax.persistence.*;
+
+@Entity
+public class Line {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long upStationId;
+
+    @Column(nullable = false)
+    private Long downStationId;
+
+    @Column(nullable = false)
+    private Long distance;
+
+    public Line() {
+    }
+
+    public Line(String name, Long upStationId, Long downStationId, Long distance) {
+        this.name = name;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Long getDistance() {
+        return distance;
+    }
+}
