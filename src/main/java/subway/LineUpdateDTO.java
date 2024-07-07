aa2298df934ce7d4011b360aa632a34436b39c0d
mlbase <guddnr2424@gmail.com>
2024-07-07 16:53:04 +0900

    feature: LineUpdateDTO

---
+++ src/main/java/subway/LineUpdateDTO.java
@@ -0,0 +1,25 @@
+package subway;
+
+public class LineUpdateDTO {
+    private Long id;
+    private String name;
+    private String color;
+
+    public LineUpdateDTO(Long id, LineUpdateRequest request) {
+        this.id = id;
+        this.name = request.getName();
+        this.color = request.getColor();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
