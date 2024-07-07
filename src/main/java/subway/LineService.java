03931bfb795301086ab27017eb119afb20d0568d
mlbase <guddnr2424@gmail.com>
2024-07-07 19:45:56 +0900

    modified: removing unnecessary interface and implements

--- src/main/java/subway/LineService.java
+++ src/main/java/subway/LineService.java
@@ -1,10 +1,53 @@
 package subway;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
-public interface LineService {
+@Service
+@Transactional(readOnly = true)
+public class LineService {
 
-    void createLine(LineCreateRequest lineCreateRequest);
-    void updateLine(LineUpdateDTO lineUpdateDTO);
-    void deleteLine(Long id);
+    private final LineRepository lineRepository;
+
+    public LineService(LineRepository lineRepository) {
+        this.lineRepository = lineRepository;
+    }
+
+    public Line createLine(LineCreateRequest request) {
+        Line line = Line.builder()
+                .name(request.getName())
+                .color(request.getColor())
+                .upStationId(request.getUpStationId())
+                .downStationId(request.getDownStationId())
+                .distance(request.getDistance())
+                .build();
+        return lineRepository.save(line);
+
+    }
+
+
+    public Line readLine(Long id) throws HttpException {
+        return lineRepository.findById(id).orElseThrow(() -> new HttpException(ErrorCode.BAD_REQUEST));
+    }
+
+
+    public List<Line> readLines() {
+        return lineRepository.findAll();
+    }
+
+    @Transactional
+    public void updateLine(LineUpdateDTO dto) throws HttpException {
+        Line line = lineRepository.findById(dto.getId())
+                .orElseThrow(() -> new HttpException(ErrorCode.BAD_REQUEST));
+        line.changeColor(dto.getColor());
+        line.changeName(dto.getName());
+        lineRepository.save(line);
+    }
+
+    @Transactional
+    public void deleteLine(Long id) throws HttpException {
+        lineRepository.deleteById(id);
+    }
 }
