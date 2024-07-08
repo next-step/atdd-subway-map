c25a58fc54768c231f5d56057c5e9829a0cd4af3
khj4453 <khj4453@soulx.co.kr>
2024-07-08 22:53:24 +0900

    test: 지하철역 삭제 테스트

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -62,7 +62,27 @@ public class StationAcceptanceTest {
 
     }
 
-    private static void addStation(String stationName) {
+    /**
+     * Given 지하철역을 생성하고
+     * When 그 지하철역을 삭제하면
+     * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
+     */
+    @DisplayName("지하철역을 삭제한다.")
+    @Test
+    void deleteStation() {
+        // given
+        ExtractableResponse<Response> response = addStation("강남역");
+
+        // when
+        deleteStation(response.body().jsonPath().getString("id"));
+
+        // then
+        List<String> stationNames = getStationNames();
+        assertThat(stationNames).hasSize(0);
+        assertThat(stationNames).doesNotContain("강남역");
+    }
+
+    private static ExtractableResponse<Response> addStation(String stationName) {
         Map<String, String> params = new HashMap<>();
         params.put("name", stationName);
 
@@ -75,6 +95,8 @@ public class StationAcceptanceTest {
                 .extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        return response;
     }
 
     private static List<String> getStationNames() {
@@ -82,6 +104,13 @@ public class StationAcceptanceTest {
             .when().get("/stations")
             .then().log().all()
             .extract().jsonPath().getList("name", String.class);
+    }
+
+    private void deleteStation(String stationId) {
+        RestAssured.given().log().all()
+            .when().delete("/stations/" + stationId)
+            .then().log().all()
+            .statusCode(HttpStatus.NO_CONTENT.value());
     }
 
     private void deleteAllStations() {
@@ -98,12 +127,5 @@ public class StationAcceptanceTest {
         }
     }
 
-
-    /**
-     * Given 지하철역을 생성하고
-     * When 그 지하철역을 삭제하면
-     * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
-     */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
 
 }
