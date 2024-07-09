2f2930aad12dc84a9d5375df3dbcc1891cad87bb
mlbase <guddnr2424@gmail.com>
2024-07-09 15:39:19 +0900

    modified: adding missing id ErrorCode

--- src/main/java/subway/ErrorCode.java
+++ src/main/java/subway/ErrorCode.java
@@ -1,7 +1,8 @@
 package subway;
 
 public enum ErrorCode {
-    BAD_REQUEST(400, "잘못된 요청입니다.");
+    BAD_REQUEST(400, "잘못된 요청입니다."),
+    MISSING_ID(400, "해당 id 로 data 를 찾을 수 없습니다.");
     private int statusCode;
     private String message;
 
