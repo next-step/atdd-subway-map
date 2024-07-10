ea44ed85620bf50daa51234002cb0d72a7571259
mlbase <guddnr2424@gmail.com>
2024-07-10 17:25:34 +0900

    feature: SectionRepository

--- src/main/java/subway/SectionRepository.java
+++ src/main/java/subway/SectionRepository.java
@@ -1,4 +1,13 @@
 package subway;
 
-public interface SectionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SectionRepository extends JpaRepository<Section, Long> {
+
+    Optional<Section> findByLineIdAndDownStationId(Long lineId, Long downStationId);
+
+    List<Section> findAllByLineIdOrderById(Long lineId);
 }
