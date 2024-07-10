34a3686562ada05acda798a6bdcd01b3b574f5b7
mlbase <guddnr2424@gmail.com>
2024-07-10 17:29:47 +0900

    feature: SectionCreateRequest

--- src/main/java/subway/SectionCreateRequest.java
+++ src/main/java/subway/SectionCreateRequest.java
@@ -1,4 +1,26 @@
 package subway;
 
 public class SectionCreateRequest {
+
+    private Long downStationId;
+    private Long upStationId;
+    private Long distance;
+
+    public SectionCreateRequest(Long downStationId, Long upStationId, Long distance) {
+        this.downStationId = downStationId;
+        this.upStationId = upStationId;
+        this.distance = distance;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDistance() {
+        return distance;
+    }
 }
