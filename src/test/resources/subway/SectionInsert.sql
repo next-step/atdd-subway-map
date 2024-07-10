bfaebcb7434cefcd5c6ca472c5499ffb444237fa
mlbase <guddnr2424@gmail.com>
2024-07-10 14:30:48 +0900

    feature: sql scripts considering SectionAcceptanceTest

---
+++ src/test/resources/subway/SectionInsert.sql
@@ -0,0 +1,8 @@
+INSERT INTO section(up_station_id, down_station_id, distance, line_id)
+VALUES (1, 4, 10 , 1);
+INSERT INTO section(up_station_id, down_station_id, distance, line_id)
+VALUES (1, 5, 10 , 1);
+INSERT INTO section(up_station_id, down_station_id, distance, line_id)
+VALUES (1, 5, 10 , 2);
+INSERT INTO section(up_station_id, down_station_id, distance, line_id)
+VALUES (1, 7, 10 , 2);
--- src/test/resources/subway/StationInsert.sql
+++ src/test/resources/subway/StationInsert.sql
@@ -2,3 +2,7 @@ INSERT INTO station (NAME) VALUES ('수서역');
 INSERT INTO station (NAME) VALUES ('성남역');
 INSERT INTO station (NAME) VALUES ('구성역');
 INSERT INTO station (NAME) VALUES ('동탄역');
+INSERT INTO station (NAME) VALUES ('서울역');
+INSERT INTO station (NAME) VALUES ('강남역');
+INSERT INTO station (NAME) VALUES ('사당역');
+INSERT INTO station (NAME) VALUES ('동작역');
