2da4a3d6d4272b1b2ab1b1bb0846102399a0e7ec
mlbase <guddnr2424@gmail.com>
2024-07-10 17:31:02 +0900

    feature: SectionController

--- src/main/java/subway/SectionController.java
+++ src/main/java/subway/SectionController.java
@@ -1,4 +1,36 @@
 package subway;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
 public class SectionController {
+
+    private final SectionFacade sectionFacade;
+
+    public SectionController(SectionFacade sectionFacade) {
+        this.sectionFacade = sectionFacade;
+    }
+
+    @PostMapping("/lines/{id}/sections")
+    public ResponseEntity<?> createSections(@PathVariable Long id, @RequestBody SectionCreateRequest request) throws HttpException {
+        SectionCreateDTO dto = new SectionCreateDTO(id, request);
+        Long sectionId = sectionFacade.createSection(dto);
+        return ResponseEntity.created(URI.create("/lines/" + id + "/sections/" + sectionId )).build();
+    }
+
+    @GetMapping("/lines/{id}/sections")
+    public ResponseEntity<SectionResponse> readSections(@PathVariable Long id) throws HttpException {
+        SectionResponse sectionResponse = sectionFacade.readSections(id);
+        return ResponseEntity.ok(sectionResponse);
+    }
+
+    @DeleteMapping("/lines/{id}/sections")
+    public ResponseEntity<?> deleteSections(@PathVariable Long id, @RequestParam Long stationId) throws HttpException {
+        SectionDeleteDTO dto = new SectionDeleteDTO(id, stationId);
+        sectionFacade.deleteSection(dto);
+        return ResponseEntity.noContent().build();
+    }
 }
