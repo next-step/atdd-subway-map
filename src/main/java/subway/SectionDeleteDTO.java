591db5a48f454c3e916d08914b1fc7b77e5d6eb1
mlbase <guddnr2424@gmail.com>
2024-07-10 17:30:51 +0900

    feature: SectionDeleteDTO

---
+++ src/main/java/subway/SectionDeleteDTO.java
@@ -0,0 +1,20 @@
+package subway;
+
+public class SectionDeleteDTO {
+
+    private Long lineId;
+    private Long stationId;
+
+    public SectionDeleteDTO(Long lineId, Long stationId) {
+        this.lineId = lineId;
+        this.stationId = stationId;
+    }
+
+    public Long getLineId() {
+        return lineId;
+    }
+
+    public Long getStationId() {
+        return stationId;
+    }
+}
