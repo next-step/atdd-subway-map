ec285fb4b9351b2bbe4f7e26a9f5f57cc71fb9e9
mlbase <guddnr2424@gmail.com>
2024-07-10 17:30:42 +0900

    feature: SectionCreateDTO

--- src/main/java/subway/SectionCreateDTO.java
+++ src/main/java/subway/SectionCreateDTO.java
@@ -1,4 +1,35 @@
 package subway;
 
 public class SectionCreateDTO {
+
+    private Long lineId;
+    private Long upStationId;
+    private Long downStationId;
+    private Long distance;
+
+    public SectionCreateDTO() {
+    }
+
+    public SectionCreateDTO(Long lineId, SectionCreateRequest request) {
+        this.lineId = lineId;
+        this.upStationId = request.getUpStationId();
+        this.downStationId = request.getDownStationId();
+        this.distance = request.getDistance();
+    }
+
+    public Long getLineId() {
+        return lineId;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Long getDistance() {
+        return distance;
+    }
 }
