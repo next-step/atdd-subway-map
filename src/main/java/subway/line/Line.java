30740a96e2b8276e2998b57197316268ea0cdd45
mlbase <guddnr2424@gmail.com>
2024-07-12 13:37:08 +0900

    feature: first-class collection added

--- src/main/java/subway/line/Line.java
+++ src/main/java/subway/line/Line.java
@@ -29,9 +29,8 @@ public class Line {
     @Column(nullable = false)
     private Long distance;
 
-    @OneToMany(mappedBy = "line",fetch = FetchType.LAZY, orphanRemoval = true, cascade = CascadeType.PERSIST)
-    @OrderColumn(name = "section_order")
-    private List<Section>  sections = new ArrayList<>();
+    @Embedded
+    private Sections sections;
 
     public Line() {
     }
@@ -121,43 +120,10 @@ public class Line {
     }
 
     public void addSection(Section section) {
-        validateCreateSection(section);
-        sections.add(section);
+        sections.addSection(section);
     }
 
     public void deleteSection(Long stationId) {
-        validateDeleteSection(stationId);
-        sections.remove(sections.size() - 1);
-    }
-
-    private void validateCreateSection(Section creatingSection) {
-        // 하행 종점이 같은 것이 존재하면 x
-        boolean isExist = sections.stream().anyMatch(section -> section.getDownStationId() == creatingSection.getDownStationId());
-        if(isExist) {
-            throw new HttpException(ErrorCode.DOWN_STATION_NOT_VALID);
-        }
-        // 상행 종점이 같은 것이 존재하면 x
-        isExist = sections.stream().anyMatch(section -> section.getUpStationId() == creatingSection.getUpStationId());
-        if(isExist) {
-            throw new HttpException(ErrorCode.UP_STATION_NOT_VALID);
-        }
-        // 등록하는 상행역이 기존 하행종점과 같지 않으면 x
-        Section lastSection = sections.get(sections.size() - 1);
-        if(lastSection.getDownStationId() != creatingSection.getUpStationId()) {
-            throw new HttpException(ErrorCode.UP_STATION_NOT_VALID);
-        }
-    }
-
-    private void validateDeleteSection(Long stationId) {
-        // 마지막 구간 제거 금지
-        if(sections.size() ==1) {
-            throw new HttpException(ErrorCode.CANNOT_REMOVE_LAST_SECTION);
-        }
-        // 제거하는 역이 하행종점이 아니면 x
-        Section lastSection = sections.get(sections.size() - 1);
-        if(lastSection.getDownStationId() != stationId) {
-            throw new HttpException(ErrorCode.IS_NOT_TERMINAL_STATION);
-        }
-
+        sections.deleteSection(stationId);
     }
 }
