e7c832114ed267e5c78c83cd4070b11c008e5ba0
khj4453 <khj4453@soulx.co.kr>
2024-07-14 13:37:29 +0900

    refactor: 지하철 목록 조회 메소드 분리

--- src/main/java/subway/line/application/LineService.java
+++ src/main/java/subway/line/application/LineService.java
@@ -1,6 +1,8 @@
 package subway.line.application;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,28 +10,43 @@ import subway.line.application.dto.LineRequest;
 import subway.line.application.dto.LineResponse;
 import subway.line.domain.Line;
 import subway.line.domain.LineRepository;
-import subway.station.application.StationService;
+import subway.station.application.dto.StationResponse;
+import subway.station.domain.Station;
+import subway.station.domain.StationRepository;
 
 @Service
 @Transactional(readOnly = true)
 @AllArgsConstructor
 public class LineService {
 
-    private LineRepository lineRepository;
-    private StationService stationService;
+    private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
 
+    @Transactional
     public LineResponse saveLine(LineRequest lineRequest) {
         Line line = lineRepository.save(
             new Line(lineRequest.getName(), lineRequest.getColor(), lineRequest.getUpStationId(), lineRequest.getDownStationId(), lineRequest.getDistance()));
-        return createLineResponse(line);
+
+        List<StationResponse> stations = getStationResponsesByStationIds(List.of(line.getUpStationId(), line.getDownStationId()));
+
+        return createLineResponse(line, stations);
     }
 
-    private LineResponse createLineResponse(Line line) {
+    private List<StationResponse> getStationResponsesByStationIds(Iterable<Long> stationIds) {
+        Map<Long, Station> stationMap = stationRepository.findAllById(stationIds).stream()
+            .collect(Collectors.toMap(Station::getId, station -> station));
+
+        return stationMap.values().stream()
+            .map(station -> new StationResponse(station.getId(), station.getName()))
+            .collect(Collectors.toList());
+    }
+
+    private LineResponse createLineResponse(Line line, List<StationResponse> stations) {
         return new LineResponse(
             line.getId(),
             line.getName(),
             line.getColor(),
-            List.of(stationService.findStation(line.getUpStationId()), stationService.findStation(line.getDownStationId()))
+            stations
         );
     }
 
